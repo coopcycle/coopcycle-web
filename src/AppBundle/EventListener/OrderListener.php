a974d58bbbb6e4e04a1668fe880c7c081b12b0e8
Alexandre Segura <mex.zktk@gmail.com>
2016-11-02 13:14:27 +0100

    Use Doctrine listener to set customer & store in Redis.

--- src/AppBundle/EventListener/OrderListener.php
+++ src/AppBundle/EventListener/OrderListener.php
@@ -5,15 +5,17 @@ namespace AppBundle\EventListener;
 use AppBundle\Entity\Order;
 use AppBundle\Utils\GeoUtils;
 use Doctrine\Common\Persistence\ManagerRegistry;
-use Dunglas\ApiBundle\Event\DataEvent;
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class OrderListener
 {
     private $tokenStorage;
+    private $redis;
 
-    public function __construct(TokenStorageInterface $tokenStorage) {
+    public function __construct(TokenStorageInterface $tokenStorage, \Redis $redis) {
         $this->tokenStorage = $tokenStorage;
+        $this->redis = $redis;
     }
 
     private function getUser()
@@ -31,38 +33,28 @@ class OrderListener
     }
 
     /**
-     * @param DataEvent $event
+     * @param LifecycleEventArgs $args
      */
-    public function onPreCreate(DataEvent $event)
+    public function prePersist(LifecycleEventArgs $args)
     {
-        $data = $event->getData();
+        $entity = $args->getObject();
 
-        if ($data instanceof Order) {
+        if ($entity instanceof Order) {
             $customer = $this->getUser();
-            $data->setCustomer($customer);
+            $entity->setCustomer($customer);
         }
     }
 
     /**
-     * @param DataEvent $event
+     * @param LifecycleEventArgs $args
      */
-    public function onPostCreate(DataEvent $event)
+    public function postPersist(LifecycleEventArgs $args)
     {
-        $data = $event->getData();
+        $entity = $args->getObject();
 
-        $redis = new \Redis();
-        if (!$redis->connect('127.0.0.1', 6379)) {
-            throw new \Exception('Could not connect to Redis');
-        }
-
-        if ($data instanceof Order) {
-
-            $coords = GeoUtils::asGeoCoordinates($data->getRestaurant()->getGeo());
-
-            $redis->geoadd('GeoSet', $coords->getLatitude(), $coords->getLongitude(), 'order:'.$data->getId());
-
-            // $customer = $this->getUser();
-            // $data->setCustomer($customer);
+        if ($entity instanceof Order) {
+            $coords = GeoUtils::asGeoCoordinates($entity->getRestaurant()->getGeo());
+            $this->redis->geoadd('GeoSet', $coords->getLatitude(), $coords->getLongitude(), 'order:'.$entity->getId());
         }
     }
 }
