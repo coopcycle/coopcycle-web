7216cc43a9eca50b9e5e6dd0c388d608cc0f3bde
Alexandre Segura <mex.zktk@gmail.com>
2016-11-18 13:40:39 +0100

    Make spatial index work, limit results.

--- src/AppBundle/Entity/RestaurantRepository.php
+++ src/AppBundle/Entity/RestaurantRepository.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Query\Expr;
 
 class RestaurantRepository extends EntityRepository
 {
-    public function findNearby($latitude, $longitude, $distance = 5000)
+    public function findNearby($latitude, $longitude, $distance = 5000, $limit = 10, $offset = 0)
     {
         $qb = $this->createQueryBuilder('r');
 
@@ -15,6 +15,13 @@ class RestaurantRepository extends EntityRepository
             $qb->expr()->literal("POINT({$latitude} {$longitude})"),
             '4326'
         ));
+
+        $dist = new Expr\Func('ST_Distance', array(
+            $geomFromText,
+            'r.geo'
+        ));
+
+        $qb->addSelect($dist . ' AS HIDDEN distance');
 
         $within = new Expr\Func('ST_DWithin', array(
             $geomFromText,
@@ -27,13 +34,12 @@ class RestaurantRepository extends EntityRepository
             $qb->expr()->literal(true)
         ));
 
+        $qb
+            ->setFirstResult($offset)
+            ->setMaxResults($limit);
+
+        $qb->orderBy('distance');
+
         return $qb->getQuery()->getResult();
     }
-
-    // public function findByRestaurants($restaurants)
-    // {
-    //     $qb = $this->createQueryBuilder('r')
-    //         ->andWhere('r.workingHours IN (:workingHours)')
-    //         ->setParameter('workingHours', $workingHours);
-    // }
 }
