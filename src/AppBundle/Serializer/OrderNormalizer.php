1c1fd9195f6d91e4d5d5b2d964a2fd98e6f1e456
Alexandre Segura <mex.zktk@gmail.com>
2017-03-07 18:25:00 +0100

    Add custom Order Normalizer.

---
+++ src/AppBundle/Serializer/OrderNormalizer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Serializer;
+
+use ApiPlatform\Core\JsonLd\Serializer\ItemNormalizer;
+use AppBundle\Entity\Order;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class OrderNormalizer implements NormalizerInterface, DenormalizerInterface
+{
+    private $normalizer;
+
+    public function __construct(ItemNormalizer $normalizer)
+    {
+        $this->normalizer = $normalizer;
+    }
+
+    public function normalize($object, $format = null, array $context = array())
+    {
+        $data =  $this->normalizer->normalize($object, $format, $context);
+
+        $data['total'] = $object->getTotal();
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $this->normalizer->supportsNormalization($data, $format) && $data instanceof Order;
+    }
+
+    public function denormalize($data, $class, $format = null, array $context = array())
+    {
+        return $this->normalizer->denormalize($data, $class, $format, $context);
+    }
+
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return $this->normalizer->supportsDenormalization($data, $type, $format) && $type instanceof Order;
+    }
+}
