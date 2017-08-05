ec1aea9e41763d5183245baaf4439a03fcf04dc4
Alexandre Segura <mex.zktk@gmail.com>
2017-08-05 02:08:00 +0200

    Introduce Menu entities.

---
+++ src/AppBundle/Entity/Menu.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use AppBundle\Entity\Base\CreativeWork;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A structured representation of food or drink items available from a FoodEstablishment.
+ *
+ * @see http://schema.org/Menu Documentation on Schema.org
+ *
+ * @ORM\Entity
+ * @ApiResource(iri="http://schema.org/Menu",
+ *   attributes={
+ *     "normalization_context"={"groups"={"restaurant"}}
+ *   },
+ *   collectionOperations={},
+ *   itemOperations={
+ *     "get"={"method"="GET"}
+ *   }
+ * )
+ */
+class Menu extends CreativeWork
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="MenuSection", cascade={"persist"})
+     * @ORM\JoinTable(inverseJoinColumns={@ORM\JoinColumn()})
+     * @ORM\OrderBy({"name"="ASC"})
+     * @ApiProperty(iri="https://schema.org/MenuSection")
+     * @Groups({"restaurant"})
+     */
+    private $hasMenuSection;
+
+    public function __construct()
+    {
+        $this->hasMenuSection = new ArrayCollection();
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getHasMenuSection()
+    {
+        return $this->hasMenuSection;
+    }
+}
---
+++ src/AppBundle/Entity/MenuItem.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use AppBundle\Entity\Base\Thing;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A food or drink item listed in a menu or menu section.
+ *
+ * @see http://schema.org/MenuItem Documentation on Schema.org
+ *
+ * @ORM\Entity
+ * @ApiResource(iri="http://schema.org/MenuItem",
+ *   attributes={
+ *     "normalization_context"={"groups"={"restaurant"}}
+ *   },
+ *   collectionOperations={},
+ *   itemOperations={
+ *     "get"={"method"="GET"}
+ *   }
+ * )
+ */
+class MenuItem extends Thing
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var float The offer price of a product, or of a price component when attached to PriceSpecification and its subtypes.
+     *
+     *            Usage guidelines:
+     *
+     *            - Use the [priceCurrency](/priceCurrency) property (with [ISO 4217 codes](http://en.wikipedia.org/wiki/ISO_4217#Active_codes) e.g. "USD") instead of including [ambiguous symbols](http://en.wikipedia.org/wiki/Dollar_sign#Currencies_that_use_the_dollar_or_peso_sign) such as '$' in the value.
+     *            - Use '.' (Unicode 'FULL STOP' (U+002E)) rather than ',' to indicate a decimal point. Avoid using these symbols as a readability separator.
+     *            - Note that both [RDFa](http://www.w3.org/TR/xhtml-rdfa-primer/#using-the-content-attribute) and Microdata syntax allow the use of a "content=" attribute for publishing simple machine-readable values alongside more human-friendly formatting.
+     *            - Use values from 0123456789 (Unicode 'DIGIT ZERO' (U+0030) to 'DIGIT NINE' (U+0039)) rather than superficially similiar Unicode symbols.
+     *
+     * @ORM\Column(type="float", nullable=true)
+     * @Assert\Type(type="float")
+     * @ApiProperty(iri="https://schema.org/price")
+     */
+    protected $price;
+
+    /**
+     * Gets id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets price.
+     *
+     * @param float $price
+     *
+     * @return $this
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Gets price.
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
