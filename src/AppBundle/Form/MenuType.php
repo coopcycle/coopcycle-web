4b2976554539917ada58bec86a7b7d2612044626
Alexandre Segura <mex.zktk@gmail.com>
2017-10-07 19:57:46 +0200

    Simplify MenuSection.
    - Introduce MenuCategory for suggestions.
    - Add name property to MenuSection.
    - Remove association b/w MenuSection & MenuCategory.
    - Use MenuCategory as suggestions.

--- src/AppBundle/Form/MenuType.php
+++ src/AppBundle/Form/MenuType.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\Menu;
-use AppBundle\Entity\MenuSection;
 use AppBundle\Form\MenuType\MenuSectionType;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -13,6 +12,7 @@ use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MenuType extends AbstractType
@@ -20,7 +20,21 @@ class MenuType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('sections', CollectionType::class, [
+            ->add('addSection', TextType::class, array(
+                'mapped' => false,
+                'required' => false,
+            ))
+            ->add('suggestions', EntityType::class, array(
+                'mapped' => false,
+                'class' => Menu\MenuCategory::class,
+                'choice_label' => 'name',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('s')->orderBy('s.name', 'ASC');
+                }
+            ));
+
+        $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) use ($options) {
+            $event->getForm()->add('sections', CollectionType::class, [
                 'entry_type' => MenuSectionType::class,
                 'allow_add' => true,
                 'allow_delete' => true,
@@ -30,29 +44,6 @@ class MenuType extends AbstractType
                     'data-section-added' => $options['section_added'] ? $options['section_added']->getId() : ''
                 ],
             ]);
-
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($options) {
-            $menu = $event->getData();
-            $form = $event->getForm();
-
-            $menuSectionIds = [];
-            foreach ($menu->getSections() as $section) {
-                $menuSectionIds[] = $section->getMenuSection()->getId();
-            }
-
-            $form->add('addSection', EntityType::class, array(
-                'mapped' => false,
-                'class' => 'AppBundle:MenuSection',
-                'choice_label' => 'name',
-                'query_builder' => function (EntityRepository $er) use ($menuSectionIds) {
-                    $qb = $er->createQueryBuilder('s');
-                    if (count($menuSectionIds) > 0) {
-                        $qb->where($qb->expr()->notIn('s.id', $menuSectionIds));
-                    }
-
-                    return $qb->orderBy('s.name', 'ASC');
-                }
-            ));
         });
     }
 
@@ -60,7 +51,7 @@ class MenuType extends AbstractType
     {
         $resolver->setDefaults(array(
             'data_class' => Menu::class,
-            'section_added' => null
+            'section_added' => null,
         ));
     }
 }
