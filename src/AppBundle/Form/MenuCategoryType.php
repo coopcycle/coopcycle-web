d5291935e6868b9198212b7bc5804c13c35f891d
Alexandre Segura <mex.zktk@gmail.com>
2017-10-09 11:02:26 +0200

    Add page to manage menu categories.

---
+++ src/AppBundle/Form/MenuCategoryType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Menu;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MenuCategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class, ['required' => false])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Menu\MenuCategory::class,
+        ));
+    }
+}
