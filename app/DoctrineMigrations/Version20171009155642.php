c4e44723eecb267216cc651653be65e0e9d9f1d2
Alexandre Segura <mex.zktk@gmail.com>
2017-10-24 12:13:49 +0200

    Fix migration.

--- app/DoctrineMigrations/Version20171009155642.php
+++ app/DoctrineMigrations/Version20171009155642.php
@@ -4,6 +4,7 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Ramsey\Uuid\Uuid;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -18,7 +19,19 @@ class Version20171009155642 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE order_ ADD uuid VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE order_ ADD uuid VARCHAR(255) DEFAULT NULL');
+
+        $stmt = $this->connection->prepare("SELECT * FROM order_");
+        $stmt->execute();
+
+        while ($order = $stmt->fetch()) {
+            $this->addSql("UPDATE order_ SET uuid = :uuid WHERE id = :id", [
+                'id' => $order['id'],
+                'uuid' => Uuid::uuid4()->toString(),
+            ]);
+        }
+
+        $this->addSql('ALTER TABLE order_ ALTER COLUMN uuid SET NOT NULL');
     }
 
     /**
