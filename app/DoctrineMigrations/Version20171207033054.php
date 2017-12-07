2054f7d628ee6ac64b3dfc7129ea8ebb64d67338
Alexandre Segura <mex.zktk@gmail.com>
2017-12-07 04:34:22 +0100

    Update tax rates.

---
+++ app/DoctrineMigrations/Version20171207033054.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171207033054 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql("UPDATE sylius_tax_rate SET amount = :amount, calculator = :calculator WHERE code = :code", [
+            'amount' => 0.055,
+            'calculator' => 'float',
+            'code' => 'tva_5_5',
+        ]);
+        $this->addSql("UPDATE sylius_tax_rate SET amount = :amount, calculator = :calculator WHERE code = :code", [
+            'amount' => 0.10,
+            'calculator' => 'float',
+            'code' => 'tva_10',
+        ]);
+        $this->addSql("UPDATE sylius_tax_rate SET amount = :amount, calculator = :calculator WHERE code = :code", [
+            'amount' => 0.20,
+            'calculator' => 'float',
+            'code' => 'tva_20',
+        ]);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
