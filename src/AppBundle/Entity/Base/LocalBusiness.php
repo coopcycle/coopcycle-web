070e1335b609a7f72d63672c9a3b521e343bc63b
Alexandre Segura <mex.zktk@gmail.com>
2017-12-07 03:41:21 +0100

    Add more LocalBusiness properties.

--- src/AppBundle/Entity/Base/LocalBusiness.php
+++ src/AppBundle/Entity/Base/LocalBusiness.php
@@ -18,6 +18,13 @@ use Symfony\Component\Serializer\Annotation\Groups;
 abstract class LocalBusiness
 {
     /**
+     * @var string The official name of the organization, e.g. the registered company name.
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $legalName;
+
+    /**
      * @var string The opening hours for a business. Opening hours can be specified as a weekly time range, starting with days, then times per day. Multiple days can be listed with commas ',' separating each day. Day or time ranges are specified using a hyphen '-'.
      *             - Days are specified using the following two-letter combinations: `Mo`, `Tu`, `We`, `Th`, `Fr`, `Sa`, `Su`.
      *             - Times are specified using 24:00 time. For example, 3pm is specified as `15:00`.
@@ -31,12 +38,36 @@ abstract class LocalBusiness
     protected $openingHours;
 
     /**
-     * @var string The telephone number..
+     * @var string The telephone number.
      *
      * @ORM\Column(nullable=true)
      * @Assert\Type(type="string")
      */
     protected $telephone;
+
+    /**
+     * @var string The Value-added Tax ID of the organization or person.
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $vatID;
+
+    /**
+     * @ORM\Column(type="json_array", nullable=true)
+     */
+    protected $additionalProperties = [];
+
+    public function getLegalName()
+    {
+        return $this->legalName;
+    }
+
+    public function setLegalName($legalName)
+    {
+        $this->legalName = $legalName;
+
+        return $this;
+    }
 
     /**
      * Sets openingHours.
@@ -77,6 +108,67 @@ abstract class LocalBusiness
     public function setTelephone($telephone)
     {
         $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getVatID()
+    {
+        return $this->vatID;
+    }
+
+    public function setVatID($vatID)
+    {
+        $this->vatID = $vatID;
+
+        return $this;
+    }
+
+    public function getAdditionalProperties()
+    {
+        return $this->additionalProperties;
+    }
+
+    public function getAdditionalPropertyValue($name)
+    {
+        foreach ($this->additionalProperties as $additionalProperty) {
+            if ($additionalProperty['name'] === $name) {
+                return $additionalProperty['value'];
+            }
+        }
+    }
+
+    public function setAdditionalProperties(array $additionalProperties)
+    {
+        $this->additionalProperties = $additionalProperties;
+
+        return $this;
+    }
+
+    public function addAdditionalProperty($name, $value)
+    {
+        $this->additionalProperties[] = [
+            'name' => $name,
+            'value' => $value,
+        ];
+
+        return $this;
+    }
+
+    public function setAdditionalProperty($name, $value)
+    {
+        $propertyValue = [
+            'name' => $name,
+            'value' => $value,
+        ];
+
+        $key = array_search($propertyValue, $this->additionalProperties);
+
+        if (false === $key) {
+            $this->addAdditionalProperty($name, $value);
+        } else {
+            $this->additionalProperties[$key]['value'] = $value;
+        }
 
         return $this;
     }
