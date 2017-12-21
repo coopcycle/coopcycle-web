aa465dc30d9acb5652ab65610a058e0846c16b0c
Alexandre Segura <mex.zktk@gmail.com>
2017-12-21 20:12:48 +0100

    Fix database.

---
+++ app/DoctrineMigrations/Version20171221191054.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171221191054 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE tracking_position ADD courier_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE tracking_position ADD CONSTRAINT FK_6E519EEAE3D8151C FOREIGN KEY (courier_id) REFERENCES api_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_6E519EEAE3D8151C ON tracking_position (courier_id)');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE tracking_position DROP CONSTRAINT FK_6E519EEAE3D8151C');
+        $this->addSql('DROP INDEX IDX_6E519EEAE3D8151C');
+        $this->addSql('ALTER TABLE tracking_position DROP courier_id');
+    }
+}
