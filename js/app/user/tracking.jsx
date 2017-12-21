439d348054f7997be8417c53fe07dd2815a05ba2
Alexandre Segura <mex.zktk@gmail.com>
2017-12-21 20:28:11 +0100

    Don't add layers when there are no points.

--- js/app/user/tracking.jsx
+++ js/app/user/tracking.jsx
@@ -18,9 +18,11 @@ const map = MapHelper.init('positions-map')
 
 const latLngs = window.AppData.positions.map(position => [position.coordinates.latitude, position.coordinates.longitude])
 
-var polyline = L.polyline(latLngs, polylineOptions).addTo(map)
-latLngs.forEach(latLng => {
-  L.circleMarker(latLng, circleMarkerOptions).addTo(map)
-})
+if (latLng.length > 0) {
+  var polyline = L.polyline(latLngs, polylineOptions).addTo(map)
+  latLngs.forEach(latLng => {
+    L.circleMarker(latLng, circleMarkerOptions).addTo(map)
+  })
 
-map.fitBounds(polyline.getBounds());
+  map.fitBounds(polyline.getBounds());
+}
