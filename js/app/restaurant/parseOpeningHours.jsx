edb3500f182ea106f6efe445243c8954c7c6d98a
Alexandre Segura <mex.zktk@gmail.com>
2017-12-23 15:56:24 +0100

    Improve human readable time range function.

--- js/app/restaurant/parseOpeningHours.jsx
+++ js/app/restaurant/parseOpeningHours.jsx
@@ -2,6 +2,17 @@ import moment from 'moment'
 import _ from 'lodash'
 import TimeRange from '../utils/TimeRange'
 
+const templates = {
+  en: {
+    single: (day, open, close) => `Open on ${day} ${open} to ${close}`,
+    range: (fromDay, toDay, open, close) => `Open from ${fromDay} to ${toDay} ${open} to ${close}`
+  },
+  fr: {
+    single: (day, open, close) => `Ouvert le ${day} de ${open} à ${close}`,
+    range: (fromDay, toDay, open, close) => `Ouvert du ${fromDay} au ${toDay} de ${open} à ${close}`
+  }
+}
+
 /*
   Takes an opening interval as formatted in the DB and returns it as a human-readable string
  */
@@ -10,29 +21,27 @@ function openingHourIntervalToReadable(openingHourInterval, locale) {
   const { days, start, end } = TimeRange.parse(openingHourInterval)
 
   let startDay = _.first(days),
-      endDay = _.last(days),
-      formattedHours,
-      formattedDays
+      endDay = _.last(days)
 
-  if (locale === 'fr') {
-    formattedHours = ['de', start.replace(':', 'h'), 'à', end.replace(':', 'h')].join(' ')
+  const template = templates.hasOwnProperty(locale) ? templates[locale] : templates['en']
+  const { single, range } = template
 
-    if (startDay === endDay) {
-      formattedDays = 'Ouvert le ' + TimeRange.weekday(startDay, locale)
-    } else {
-      formattedDays = ['Ouvert du', TimeRange.weekday(startDay, locale), 'au', TimeRange.weekday(endDay, locale)].join(' ')
-    }
+  // Format time according to locale
+
+  const localeMoment = moment()
+  localeMoment.locale(locale)
+
+  const [ startHour, startMinute ] = start.split(':')
+  const [ endHour, endMinute ] = end.split(':')
+
+  const startFormatted = localeMoment.hour(startHour).minute(startMinute).format('LT')
+  const endFormatted = localeMoment.hour(endHour).minute(endMinute).format('LT')
+
+  if (startDay === endDay) {
+    return single(TimeRange.weekday(startDay, locale), startFormatted, endFormatted)
   } else {
-    formattedHours = [start.replace(':', 'h'), 'to', start.replace(':', 'h')].join(' ')
-
-    if (startDay === endDay) {
-      formattedDays = 'Open on ' + TimeRange.weekday(startDay, locale)
-    } else {
-      formattedDays = ['Open from', TimeRange.weekday(startDay, locale), 'to', TimeRange.weekday(endDay, locale)].join(' ')
-    }
+    return range(TimeRange.weekday(startDay, locale), TimeRange.weekday(endDay, locale), startFormatted, endFormatted)
   }
-
-  return [formattedDays, formattedHours].join(' ')
 }
 
 export default openingHourIntervalToReadable
