0036d4ceb6e4b09e2bfb899d114f308cc50d19b3
Alexandre Segura <mex.zktk@gmail.com>
2017-12-28 19:43:49 +0100

    Only admin can change pricing.

--- src/AppBundle/Form/StoreType.php
+++ src/AppBundle/Form/StoreType.php
@@ -14,25 +14,42 @@ use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Constraints;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class StoreType extends LocalBusinessType
 {
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
 
-        $builder
-            ->add('pricingRuleSet', EntityType::class, array(
-                'label' => 'form.store_type.pricing_rule_set.label',
-                'class' => PricingRuleSet::class,
-                'choice_label' => 'name',
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('prs')->orderBy('prs.name', 'ASC');
-                }
-            ));
+        $isAdmin = false;
+        if ($token = $this->tokenStorage->getToken()) {
+            if ($user = $token->getUser()) {
+                $isAdmin = $user->hasRole('ROLE_ADMIN');
+            }
+        }
+
+        if ($isAdmin) {
+            $builder
+                ->add('pricingRuleSet', EntityType::class, array(
+                    'label' => 'form.store_type.pricing_rule_set.label',
+                    'class' => PricingRuleSet::class,
+                    'choice_label' => 'name',
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('prs')->orderBy('prs.name', 'ASC');
+                    }
+                ));
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
