5288aaf4eac729dbb79a2977ef7e2507d6ecf0a8
Alexandre Segura <mex.zktk@gmail.com>
2018-01-24 15:37:38 +0100

    Introduce TaskManager.

---
+++ src/AppBundle/Service/TaskManager.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\Delivery;
+use AppBundle\Entity\Task;
+use AppBundle\Entity\TaskAssignment;
+use AppBundle\Event\TaskDoneEvent;
+use AppBundle\Event\TaskAssignEvent;
+use Doctrine\ORM\Query\Expr;
+use FOS\UserBundle\Model\UserInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class TaskManager
+{
+    private $doctrine;
+    private $dispatcher;
+
+    public function __construct(ManagerRegistry $doctrine, EventDispatcherInterface $dispatcher)
+    {
+        $this->doctrine = $doctrine;
+        $this->dispatcher = $dispatcher;
+    }
+
+    public function assign(Task $task, UserInterface $user, $position)
+    {
+        $task->assignTo($user, $position);
+
+        $this->dispatcher->dispatch(TaskAssignEvent::NAME, new TaskAssignEvent($task, $user));
+    }
+
+    public function unassign(Task $task)
+    {
+        $this->doctrine
+            ->getManagerForClass(TaskAssignment::class)
+            ->remove($task->getAssignment());
+
+        $task->unassign();
+
+        $task->getDelivery()->setStatus(Delivery::STATUS_WAITING);
+    }
+
+    public function markAsDone(Task $task)
+    {
+        $task->setStatus(Task::STATUS_DONE);
+
+        $this->dispatcher->dispatch(TaskDoneEvent::NAME, new TaskDoneEvent($task));
+    }
+}
