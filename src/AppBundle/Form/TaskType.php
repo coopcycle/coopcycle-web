895cb23cf0614b0744819e61fcb54801f5e59460
Alexandre Segura <mex.zktk@gmail.com>
2018-02-24 02:13:46 +0100

    Handle tags as string in task form.

--- src/AppBundle/Form/TaskType.php
+++ src/AppBundle/Form/TaskType.php
@@ -4,12 +4,14 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\ApiUser;
 use AppBundle\Entity\Task;
+use AppBundle\Service\TagManager;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
@@ -20,6 +22,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TaskType extends AbstractType
 {
+    private $tagManager;
+
+    public function __construct(TagManager $tagManager)
+    {
+        $this->tagManager = $tagManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -44,6 +53,11 @@ class TaskType extends AbstractType
             ->add('comments', TextareaType::class, [
                 'required' => false,
                 'attr' => ['rows' => '2', 'placeholder' => 'Specify any useful details to complete task']
+            ])
+            ->add('tagsAsString', TextType::class, [
+                'mapped' => false,
+                'required' => false,
+                'label' => 'Tags'
             ])
             ->add('save', SubmitType::class);
 
@@ -76,11 +90,28 @@ class TaskType extends AbstractType
                     },
                     'choice_label' => 'username',
                 ));
+
+                $tags = array_map(function ($tag) {
+                    return $tag->getSlug();
+                }, iterator_to_array($task->getTags()));
+
+                $form->get('tagsAsString')->setData(implode(' ', $tags));
             }
 
             if ($task->isAssigned()) {
                 $form->get('assign')->setData($task->getAssignedCourier());
             }
+        });
+
+        $builder->get('tagsAsString')->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) use ($options) {
+
+            $task = $event->getForm()->getParent()->getData();
+
+            $tagsAsString = $event->getData();
+            $slugs = explode(' ', $tagsAsString);
+            $tags = $this->tagManager->fromSlugs($slugs);
+
+            $task->setTags($tags);
         });
     }
 
