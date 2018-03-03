e3b963457d834622f3fb6b9eed51844e59b8075a
Alexandre Segura <mex.zktk@gmail.com>
2018-03-03 20:49:53 +0100

    Add migration to make sure Stripe has to be configured if empty.

---
+++ app/DoctrineMigrations/Version20180303194651.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180303194651 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+        $this->addSql('DELETE FROM craue_config_setting WHERE name = :name AND value IS NULL', [
+            'name' => 'stripe_publishable_key',
+        ]);
+        $this->addSql('DELETE FROM craue_config_setting WHERE name = :name AND value IS NULL', [
+            'name' => 'stripe_secret_key',
+        ]);
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
