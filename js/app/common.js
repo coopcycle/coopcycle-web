9c18b993848dfecf54d8656fc59174f0322a5aba
Atala <alois.guillope@gmail.com>
2018-03-13 10:29:02 +0100

    fix: add polyfill for startsWith on IE11

--- js/app/common.js
+++ js/app/common.js
@@ -1,3 +1,11 @@
 // @see http://symfony.com/doc/3.4/frontend/encore/legacy-apps.html
 const $ = require('jquery')
 global.$ = global.jQuery = $
+
+// polyfill for `startsWith` not implemented in IE11
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0
+    return this.indexOf(searchString, position) === position
+  }
+}
