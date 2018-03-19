627da6676d9ddabfcec5646b46291cb209c81aaf
Alexandre Segura <mex.zktk@gmail.com>
2018-03-19 11:10:34 +0100

    Introduce Twig extension.

---
+++ src/AppBundle/Twig/CoopCycleExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Twig;
+
+class CoopCycleExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('meters_to_kilometers', array($this, 'metersToKilometers')),
+            new \Twig_SimpleFilter('seconds_to_minutes', array($this, 'secondsToMinutes')),
+            new \Twig_SimpleFilter('price_format', array($this, 'priceFormat')),
+        );
+    }
+
+    public function metersToKilometers($meters)
+    {
+        return sprintf('%s km', number_format($meters / 1000, 2));
+    }
+
+    public function secondsToMinutes($seconds)
+    {
+        return sprintf('%d min', ceil($seconds / 60));
+    }
+
+    public function priceFormat($cents)
+    {
+        return number_format($cents / 100, 2);
+    }
+}
