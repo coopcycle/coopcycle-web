94a18a1ea48f4fa9a4f45a33a23f581c55883ca7
Alexandre Segura <mex.zktk@gmail.com>
2018-04-01 15:56:20 +0200

    Introduce adjustments for delivery & menu item modifiers.

--- src/AppBundle/Sylius/Order/AdjustmentInterface.php
+++ src/AppBundle/Sylius/Order/AdjustmentInterface.php
@@ -7,4 +7,6 @@ use Sylius\Component\Order\Model\AdjustmentInterface as BaseAdjustmentInterface;
 interface AdjustmentInterface extends BaseAdjustmentInterface
 {
     public const TAX_ADJUSTMENT = 'tax';
+    public const DELIVERY_ADJUSTMENT = 'delivery';
+    public const MENU_ITEM_MODIFIER_ADJUSTMENT = 'menu_item_modifier';
 }
