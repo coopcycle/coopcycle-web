ed8910c0d4f2fddd39213a94ad58a50999dedd40
Alexandre Segura <mex.zktk@gmail.com>
2018-04-06 00:02:13 +0200

    Farewell legacy order entities.

---
+++ app/DoctrineMigrations/Version20180405215415.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180405215415 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE order_item_modifier DROP CONSTRAINT fk_6ad159e4e415fb15');
+        $this->addSql('ALTER TABLE order_event DROP CONSTRAINT fk_b8307e5a8d9f6d38');
+        $this->addSql('ALTER TABLE order_item DROP CONSTRAINT fk_52ea1f098d9f6d38');
+
+        $this->addSql('DROP SEQUENCE order__id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE order_event_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE order_item_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE order_item_modifier_id_seq CASCADE');
+
+        $this->addSql('DROP TABLE order_event');
+        $this->addSql('DROP TABLE order_item');
+        $this->addSql('DROP TABLE order_item_modifier');
+        $this->addSql('DROP TABLE order_');
+    }
+
+    public function down(Schema $schema)
+    {
+    }
+}
