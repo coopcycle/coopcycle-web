8dc8339092a5b2d270d8b0804e1c3a08d16588c0
Atala <alois.guillope@gmail.com>
2018-05-07 10:13:48 +0200

    fix: edit migration

--- app/DoctrineMigrations/Version20180507073752.php
+++ app/DoctrineMigrations/Version20180507073752.php
@@ -15,7 +15,6 @@ class Version20180507073752 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE restaurant ADD ordering_delay_minutes INT DEFAULT 0 NOT NULL');
         $this->addSql('ALTER TABLE restaurant RENAME COLUMN max_delivery_expression TO delivery_perimeter_expression');
     }
 
@@ -23,8 +22,7 @@ class Version20180507073752 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
-        $this->addSql('ALTER TABLE restaurant DROP ordering_delay_minutes');
+        
         $this->addSql('ALTER TABLE restaurant DROP delivery_perimeter_expression');
     }
 }
