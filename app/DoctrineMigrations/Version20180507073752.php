fd8de0c316b81d8a240bfcca06332ce55be025a0
Atala <alois.guillope@gmail.com>
2018-05-07 09:44:17 +0200

    fix: add missing migration

---
+++ app/DoctrineMigrations/Version20180507073752.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180507073752 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE restaurant ADD ordering_delay_minutes INT DEFAULT 0 NOT NULL');
+        $this->addSql('ALTER TABLE restaurant RENAME COLUMN max_delivery_expression TO delivery_perimeter_expression');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE restaurant DROP ordering_delay_minutes');
+        $this->addSql('ALTER TABLE restaurant DROP delivery_perimeter_expression');
+    }
+}
