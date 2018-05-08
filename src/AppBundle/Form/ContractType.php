ea3f3b9fb8ba8a8cee9e3142db8f9d33bb6ff39c
Alexandre Segura <mex.zktk@gmail.com>
2018-05-08 18:24:28 +0200

    Add feeRate field to form.

--- src/AppBundle/Form/ContractType.php
+++ src/AppBundle/Form/ContractType.php
@@ -1,22 +1,28 @@
 <?php
 
-
 namespace AppBundle\Form;
-
 
 use AppBundle\Entity\Contract;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\PercentType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 
 class ContractType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('minimumCartAmount', MoneyType::class, ['label' => 'restaurant.contract.minimumCartAmount.label'])
-                ->add('flatDeliveryPrice', MoneyType::class, ['label' => 'restaurant.contract.flatDeliveryPrice.label']);
+        $builder
+            ->add('minimumCartAmount', MoneyType::class, [
+                'label' => 'restaurant.contract.minimumCartAmount.label'
+            ])
+            ->add('flatDeliveryPrice', MoneyType::class, [
+                'label' => 'restaurant.contract.flatDeliveryPrice.label'
+            ])
+            ->add('feeRate', PercentType::class, [
+                'label' => 'restaurant.contract.feeRate.label'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
