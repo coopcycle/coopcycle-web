76c88f586d578b5b1b5168ec02f2d896c1a0e7d0
Alexandre Segura <mex.zktk@gmail.com>
2018-05-12 17:15:37 +0200

    Perform multiple insert.

--- var/sql/init.sql
+++ var/sql/init.sql
@@ -1,3 +1,1 @@
-INSERT INTO sylius_locale (code, created_at, updated_at) VALUES ('fr', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO sylius_locale (code, created_at, updated_at) VALUES ('en', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO sylius_locale (code, created_at, updated_at) VALUES ('es', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO sylius_locale (code, created_at, updated_at) VALUES ('fr', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), ('en', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), ('es', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
