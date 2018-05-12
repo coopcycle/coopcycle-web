3ba75c65afb162d3a7a6085d5e5e00cdd37954d8
Alexandre Segura <mex.zktk@gmail.com>
2018-05-12 16:36:42 +0200

    Add SQL file to init database.

---
+++ var/sql/init.sql
@@ -0,0 +1,3 @@
+INSERT INTO sylius_locale (code, created_at, updated_at) VALUES ('fr', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO sylius_locale (code, created_at, updated_at) VALUES ('en', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO sylius_locale (code, created_at, updated_at) VALUES ('es', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
