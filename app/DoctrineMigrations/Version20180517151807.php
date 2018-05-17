79b5d9049c651426fc9ffac472c63c61f4f19f23
Alexandre Segura <mex.zktk@gmail.com>
2018-05-17 17:19:38 +0200

    Change address.description column to text.

---
+++ app/DoctrineMigrations/Version20180517151807.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180517151807 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE address ALTER description TYPE TEXT');
+        $this->addSql('ALTER TABLE address ALTER description DROP DEFAULT');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE address ALTER description TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE address ALTER description DROP DEFAULT');
+    }
+}
