75744e10ceb20d00592fab4a2068b6eb3463c4db
Alexandre Segura <mex.zktk@gmail.com>
2018-05-21 15:50:08 +0200

    Add adress description in serialized task.

--- src/AppBundle/Entity/Base/BaseAddress.php
+++ src/AppBundle/Entity/Base/BaseAddress.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity\Base;
 
 use ApiPlatform\Core\Annotation\ApiProperty;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class BaseAddress extends Place
@@ -10,6 +11,7 @@ class BaseAddress extends Place
     /**
      * @var string Additional instructions about the place
      *
+     * @Groups({"task"})
      * @Assert\Type(type="string")
      * @ApiProperty(iri="https://schema.org/addressLocality")
      */
