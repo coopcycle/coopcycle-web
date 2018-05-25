4acc84fff5f17090fc5b1b5d7cf2da493a848a89
Alexandre Segura <mex.zktk@gmail.com>
2018-05-25 15:58:51 +0200

    Introduce setup command.

---
+++ src/AppBundle/Command/SetupCommand.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Command;
+
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SetupCommand extends ContainerAwareCommand
+{
+    private $productRepository;
+    private $productManager;
+    private $productFactory;
+
+    private $localeRepository;
+    private $localeFactory;
+
+    private $slugify;
+    private $locales = [
+        'fr',
+        'en',
+        'es'
+    ];
+
+    private $onDemandDeliveryProductNames = [
+        'fr' => 'Livraison à la demande',
+        'en' => 'On demand delivery',
+        'es' => 'Entrega bajo demanda',
+    ];
+
+    protected function configure()
+    {
+        $this
+            ->setName('coopcycle:setup')
+            ->setDescription('Setups some basic stuff.');
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->productRepository = $this->getContainer()->get('sylius.repository.product');
+        $this->productFactory = $this->getContainer()->get('sylius.factory.product');
+        $this->productManager = $this->getContainer()->get('sylius.manager.product');
+
+        $this->localeRepository = $this->getContainer()->get('sylius.repository.locale');
+        $this->localeFactory = $this->getContainer()->get('sylius.factory.locale');
+
+        $this->slugify = $this->getContainer()->get('slugify');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>Setting up CoopCycle…</info>');
+
+        $output->writeln('<info>Checking Sylius locales are present…</info>');
+        foreach ($this->locales as $locale) {
+            $this->createSyliusLocale($locale, $output);
+        }
+
+        $output->writeln('<info>Checking « on demand delivery » product is present…</info>');
+        $this->createOnDemandDeliveryProduct($output);
+    }
+
+    private function createSyliusLocale($code, OutputInterface $output)
+    {
+        $locale = $this->localeRepository->findOneByCode($code);
+
+        if (null !== $locale) {
+            $output->writeln(sprintf('Sylius locale "%s" already exists', $code));
+            return;
+        }
+
+        $locale = $this->localeFactory->createNew();
+        $locale->setCode($code);
+
+        $this->localeRepository->add($locale);
+
+        $output->writeln(sprintf('Sylius locale "%s" created', $code));
+    }
+
+    private function createOnDemandDeliveryProduct(OutputInterface $output)
+    {
+        $product = $this->productRepository->findOneByCode('CPCCL-ODDLVR');
+
+        if (null === $product) {
+
+            $product = $this->productFactory->createNew();
+            $product->setCode('CPCCL-ODDLVR');
+            $product->setEnabled(true);
+
+            $this->productRepository->add($product);
+            $output->writeln('Creating product « on demand delivery »');
+
+        } else {
+            $output->writeln('Product « on demand delivery » already exists');
+        }
+
+        $translations = $product->getTranslations();
+
+        $translated = [];
+        foreach ($translations as $translation) {
+            $translated[] = $translation->getLocale();
+        }
+
+        $missing = array_diff($this->locales, $translated);
+
+        if (count($missing) === 0) {
+            $output->writeln('All translations for product « on demand delivery » already exists');
+            return;
+        }
+
+        foreach ($missing as $locale) {
+
+            $name = $this->onDemandDeliveryProductNames[$locale];
+
+            $translation = $product->getTranslation($locale);
+
+            $translation->setLocale($locale);
+            $translation->setName($name);
+            $translation->setSlug($this->slugify->slugify($name));
+
+            $output->writeln(sprintf('Creating translation for product « on demand delivery » with locale "%s"', $locale));
+        }
+
+        $this->productManager->flush();
+    }
+}
