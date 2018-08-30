8c1d618c25c5ee2ced9801ca9c6d1eab2cc93423
Alexandre Segura <mex.zktk@gmail.com>
2018-08-30 18:09:36 +0200

    Save address when order is created.

---
+++ src/AppBundle/Domain/Order/Reactor/AddCustomerAddress.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Domain\Order\Reactor;
+
+use AppBundle\Domain\Order\Event\OrderCreated;
+
+class AddCustomerAddress
+{
+    public function __invoke(OrderCreated $event)
+    {
+        $order = $event->getOrder();
+
+        $customer = $order->getCustomer();
+        $shippingAddress = $order->getShippingAddress();
+
+        if (!$customer->getAddresses()->contains($shippingAddress)) {
+            $customer->addAddress($shippingAddress);
+        }
+    }
+}
