c604af343a241401514a74c36940ea2e63aeddce
Alexandre Segura <mex.zktk@gmail.com>
2018-08-31 10:43:59 +0200

    Refactor order timeline JavaScript.
    - Forward order events through Redis.
    - Timeline is now event-based.
    - Use icons instead of numbers.
    - Change wordings.

--- js/app/widgets/OrderTimeline.js
+++ js/app/widgets/OrderTimeline.js
@@ -6,11 +6,11 @@ import OrderTimeline from '../order/Timeline'
 
 export default (el, options) => {
 
-  const timeline = render(<OrderTimeline order={ options.order } />, el)
+  const timeline = render(<OrderTimeline order={ options.order } events={ options.events } />, el)
 
   if (!_.includes(['cancelled', 'fulfilled', 'refused'], options.order.state)) {
     const socket = io('//' + window.location.hostname, { path: '/tracking/socket.io' })
-    socket.on(`order:${options.order.id}:state_changed`, order => timeline.updateOrder(order))
+    socket.on(`order:${options.order.id}:events`, event => timeline.addEvent(event))
   }
 
 }
