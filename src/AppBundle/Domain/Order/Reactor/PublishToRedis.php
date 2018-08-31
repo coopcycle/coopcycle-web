6982a2d2af5cc17253058ab6f2e3369cc22abe7b
Alexandre Segura <mex.zktk@gmail.com>
2018-08-31 10:43:26 +0200

    Introduce PublishToRedis reactor.

---
+++ src/AppBundle/Domain/Order/Reactor/PublishToRedis.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Domain\Order\Reactor;
+
+use AppBundle\Domain\Order\Event;
+use Predis\Client as Redis;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class PublishToRedis
+{
+    private $redis;
+    private $serializer;
+
+    public function __construct(Redis $redis, SerializerInterface $serializer)
+    {
+        $this->redis = $redis;
+        $this->serializer = $serializer;
+    }
+
+    public function __invoke(Event $event)
+    {
+        $order = $event->getOrder();
+
+        $channel = sprintf('order:%d:events', $order->getId());
+
+        $message = [
+            'name' => $event::messageName(),
+            'data' => $event->toPayload(),
+            // FIXME We should retrieve the actual date from EventStore
+            'createdAt' => (new \DateTime())->format(\DateTime::ATOM),
+        ];
+
+        $this->redis->publish($channel, json_encode($message));
+    }
+}
