ec9890cf8437129709453fd79763b49f08e63280
Alexandre Segura <mex.zktk@gmail.com>
2018-10-03 00:56:52 +0200

    Refactor address checkout form.
    - Bind order to form to trigger validation.
    - Move all logic to form class.

---
+++ src/AppBundle/Form/Checkout/CheckoutAddressType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\Form\Checkout;
+
+use AppBundle\Form\AddressType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormInterface;
+
+class CheckoutAddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('shippingAddress', AddressType::class, [
+                'label' => false,
+            ]);
+
+        $builder->get('shippingAddress')->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+
+            // Disable streetAddress, postalCode & addressLocality
+            $this->disableChildForm($form, 'streetAddress');
+            $this->disableChildForm($form, 'postalCode');
+            $this->disableChildForm($form, 'addressLocality');
+        });
+
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+
+            $order = $event->getForm()->getData();
+
+            $shippingAddress = $order->getShippingAddress();
+            $customer = $order->getCustomer();
+
+            // Copy customer data into address
+            $shippingAddress->setFirstName($customer->getGivenName());
+            $shippingAddress->setLastName($customer->getFamilyName());
+            $shippingAddress->setTelephone($customer->getTelephone());
+        });
+    }
+
+    private function disableChildForm(FormInterface $form, $name)
+    {
+        $child = $form->get($name);
+
+        $config = $child->getConfig();
+        $options = $config->getOptions();
+        $options['disabled'] = true;
+
+        $form->add($name, get_class($config->getType()->getInnerType()), $options);
+    }
+}
