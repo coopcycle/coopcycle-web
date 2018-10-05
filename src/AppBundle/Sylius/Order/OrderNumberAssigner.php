b49ea41260beec931f844585133178575e3f0cf8
Alexandre Segura <mex.zktk@gmail.com>
2018-10-05 21:56:53 +0200

    Use custom order number assigner.

---
+++ src/AppBundle/Sylius/Order/OrderNumberAssigner.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Sylius\Order;
+
+use Sylius\Component\Order\Model\OrderInterface;
+
+final class OrderNumberAssigner implements OrderNumberAssignerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function assignNumber(OrderInterface $order): void
+    {
+        if (null !== $order->getNumber()) {
+            return;
+        }
+
+        $number = strtoupper(base_convert($order->getId(), 10, 36));
+
+        $order->setNumber($number);
+    }
+}
