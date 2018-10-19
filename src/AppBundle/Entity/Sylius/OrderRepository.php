6fe7437b0de4a6b0d1d451d51f19d1df6464dbf4
Alexandre Segura <mex.zktk@gmail.com>
2018-10-19 11:23:57 +0200

    Cleanup carts when restaurant is soft deleted.

--- src/AppBundle/Entity/Sylius/OrderRepository.php
+++ src/AppBundle/Entity/Sylius/OrderRepository.php
@@ -2,11 +2,24 @@
 
 namespace AppBundle\Entity\Sylius;
 
-use AppBundle\Entity\Delivery;
+use AppBundle\Entity\Restaurant;
+use AppBundle\Sylius\Order\OrderInterface;
 use Sylius\Bundle\OrderBundle\Doctrine\ORM\OrderRepository as BaseOrderRepository;
 
 class OrderRepository extends BaseOrderRepository
 {
+    public function findCartsByRestaurant(Restaurant $restaurant)
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.state = :state')
+            ->andWhere('o.restaurant = :restaurant')
+            ->setParameter('state', OrderInterface::STATE_CART)
+            ->setParameter('restaurant', $restaurant)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
     public function findByShippedAt(\DateTime $date)
     {
         $qb = $this->createQueryBuilder('o');
