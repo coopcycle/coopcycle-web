81a02c1c84a51802da94110b8b562b8b4342e754
Alexandre Segura <mex.zktk@gmail.com>
2018-11-08 01:44:04 +0100

    Stop using ClassLoader component.

--- web/app.php
+++ web/app.php
@@ -17,7 +17,6 @@ $apcLoader->register(true);
 */
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
