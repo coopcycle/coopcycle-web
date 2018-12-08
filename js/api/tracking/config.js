ffab0bf23ca7f7aa170de6e7edf2686ba131dd22
Alexandre Segura <mex.zktk@gmail.com>
2018-12-08 22:11:18 +0100

    Refactor Socket.IO management.
    - Use a room per user for message forwarding.

---
+++ js/api/tracking/config.js
@@ -0,0 +1,46 @@
+const ConfigLoader = require('../ConfigLoader');
+const Sequelize = require('sequelize')
+
+module.exports = function(rootDir) {
+
+  var envMap = {
+    production: 'prod',
+    development: 'dev',
+    test: 'test'
+  };
+
+  try {
+
+    var configFile = 'config.yml';
+    if (envMap[process.env.NODE_ENV]) {
+      configFile = 'config_' + envMap[process.env.NODE_ENV] + '.yml';
+    }
+
+    var configLoader = new ConfigLoader(rootDir + '/app/config/' + configFile);
+    var config = configLoader.load();
+
+  } catch (e) {
+    throw e;
+  }
+
+  var sub = require('../RedisClient')({
+    prefix: config.snc_redis.clients.default.options.prefix,
+    url: config.snc_redis.clients.default.dsn
+  });
+
+  var sequelize = new Sequelize(
+    config.doctrine.dbal.dbname,
+    config.doctrine.dbal.user,
+    config.doctrine.dbal.password,
+    {
+      host: config.doctrine.dbal.host,
+      dialect: 'postgres',
+      logging: false,
+    }
+  );
+
+  return {
+    sub,
+    sequelize
+  }
+}
