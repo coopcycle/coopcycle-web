21ad3a888b71647db63af395a9877f71764ea6ec
Alexandre Segura <mex.zktk@gmail.com>
2018-12-27 23:39:29 +0100

    Deprecate bootstrap file.
    - See https://symfony.com/doc/3.4/performance.html#use-bootstrap-files

--- web/app.php
+++ web/app.php
@@ -4,7 +4,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
 
 // Enable APC for autoloading to improve performance.
 // You should change the ApcClassLoader first argument to a unique prefix
