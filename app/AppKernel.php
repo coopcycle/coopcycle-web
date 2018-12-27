25bf9b1158616d8dd9a8a59e0ddd59ed9c278f92
Alexandre Segura <mex.zktk@gmail.com>
2018-12-27 22:59:01 +0100

    Remove SensioGeneratorBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -63,7 +63,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         } else if (in_array($this->getEnvironment(), ['prod'], true)) {
             $bundles[] = new Sentry\SentryBundle\SentryBundle();
         }
