bc33bb69610d3138fce1bc5de9458724285a2b73
Alexandre Segura <mex.zktk@gmail.com>
2019-01-04 18:56:02 +0100

    Add migration for notification route name.

---
+++ app/DoctrineMigrations/Version20190104174705.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20190104174705 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $stmt = $this->connection->prepare('SELECT * FROM notification WHERE route_name = \'profile_restaurant_dashboard_order\'');
+
+        $stmt->execute();
+        while ($notification = $stmt->fetch()) {
+
+            $routeParameters = json_decode($notification['route_parameters'], true);
+
+            $routeParameters['order'] = $routeParameters['orderId'];
+            unset($routeParameters['orderId']);
+
+            $this->addSql('UPDATE notification SET route_name = :route_name, route_parameters = :route_parameters WHERE id = :id', [
+                'route_name' => 'profile_restaurant_dashboard',
+                'route_parameters' => json_encode($routeParameters),
+                'id' => $notification['id'],
+            ]);
+        }
+    }
+
+    public function down(Schema $schema) : void
+    {
+
+    }
+}
