a01576c1dcdd3a9a61d03cd6903b73a7811e5018
Alexandre Segura <mex.zktk@gmail.com>
2019-01-06 17:13:20 +0100

    Extract price formatting into class.

--- src/AppBundle/Twig/PriceFormatResolver.php
+++ src/AppBundle/Twig/PriceFormatResolver.php
@@ -2,24 +2,20 @@
 
 namespace AppBundle\Twig;
 
-use Sylius\Bundle\CurrencyBundle\Templating\Helper\CurrencyHelperInterface;
-use Sylius\Component\Currency\Context\CurrencyContextInterface;
+use AppBundle\Utils\PriceFormatter;
 use Twig\Extension\RuntimeExtensionInterface;
 
 class PriceFormatResolver implements RuntimeExtensionInterface
 {
-    private $currencyContext;
-    private $currencyHelper;
+    private $priceFormatter;
 
-    public function __construct(CurrencyContextInterface $currencyContext, CurrencyHelperInterface $currencyHelper)
+    public function __construct(PriceFormatter $priceFormatter)
     {
-        $this->currencyContext = $currencyContext;
-        $this->currencyHelper = $currencyHelper;
+        $this->priceFormatter = $priceFormatter;
     }
 
     public function priceFormat($cents)
     {
-        $currencyCode = $this->currencyContext->getCurrencyCode();
-        return number_format($cents / 100, 2) . ' ' . $this->currencyHelper->convertCurrencyCodeToSymbol($currencyCode);
+        return $this->priceFormatter->formatWithSymbol($cents);
     }
 }
