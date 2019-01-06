22fba214a2797bd292061b07858bb5e0ec0da57a
Alexandre Segura <mex.zktk@gmail.com>
2019-01-06 17:17:15 +0100

    Add potentialAction to structured data.

--- src/AppBundle/Serializer/RestaurantNormalizer.php
+++ src/AppBundle/Serializer/RestaurantNormalizer.php
@@ -6,16 +6,36 @@ use ApiPlatform\Core\JsonLd\Serializer\ItemNormalizer;
 use AppBundle\Entity\ClosingRule;
 use AppBundle\Entity\Restaurant;
 use AppBundle\Utils\OpeningHoursSpecification;
+use AppBundle\Utils\PriceFormatter;
+use Cocur\Slugify\SlugifyInterface;
+use Sylius\Component\Currency\Context\CurrencyContextInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class RestaurantNormalizer implements NormalizerInterface, DenormalizerInterface
 {
     private $normalizer;
+    private $urlGenerator;
+    private $currencyContext;
+    private $priceFormatter;
+    private $slugify;
+    private $locale;
 
-    public function __construct(ItemNormalizer $normalizer)
+    public function __construct(
+        ItemNormalizer $normalizer,
+        UrlGeneratorInterface $urlGenerator,
+        CurrencyContextInterface $currencyContext,
+        PriceFormatter $priceFormatter,
+        SlugifyInterface $slugify,
+        $locale)
     {
         $this->normalizer = $normalizer;
+        $this->urlGenerator = $urlGenerator;
+        $this->currencyContext = $currencyContext;
+        $this->priceFormatter = $priceFormatter;
+        $this->slugify = $slugify;
+        $this->locale = $locale;
     }
 
     public function normalize($object, $format = null, array $context = array())
@@ -63,6 +83,41 @@ class RestaurantNormalizer implements NormalizerInterface, DenormalizerInterface
         if (isset($data['openingHours'])) {
             unset($data['openingHours']);
         }
+
+        // @see https://developers.google.com/search/docs/data-types/local-business#order-reservation-scenarios
+
+        $urlTemplate = $this->urlGenerator->generate('restaurant', [
+            'id' => $object->getId(),
+            'slug' => $this->slugify->slugify($object->getName()),
+        ], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $priceCurrency = $this->currencyContext->getCurrencyCode();
+
+        $data['potentialAction'] = [
+            '@type' => 'OrderAction',
+            'target' => [
+                '@type' => 'EntryPoint',
+                'urlTemplate' => $urlTemplate,
+                'inLanguage' => $this->locale,
+                'actionPlatform' => [
+                    'http://schema.org/DesktopWebPlatform',
+                ]
+            ],
+            'deliveryMethod' => [
+                'http://purl.org/goodrelations/v1#DeliveryModeOwnFleet'
+            ],
+            'priceSpecification' => [
+                '@type' => 'DeliveryChargeSpecification',
+                'appliesToDeliveryMethod' => 'http://purl.org/goodrelations/v1#DeliveryModeOwnFleet',
+                'priceCurrency' => $priceCurrency,
+                'price' => $this->priceFormatter->format($object->getFlatDeliveryPrice()),
+                'eligibleTransactionVolume' => [
+                    '@type' => 'PriceSpecification',
+                    'priceCurrency' => $priceCurrency,
+                    'price' => $this->priceFormatter->format($object->getMinimumCartAmount())
+                ]
+            ]
+        ];
 
         return $data;
     }
