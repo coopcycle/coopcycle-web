b585f416ff9683b7de8856cb1e1f575065ee36c5
Alexandre Segura <mex.zktk@gmail.com>
2019-01-18 20:43:53 +0100

    Fix & improve show/hide tasks on map.
    - Introduce Redux selectors.
    - Use selectors in map component.

--- js/app/dashboard/components/LeafletMap.js
+++ js/app/dashboard/components/LeafletMap.js
@@ -3,99 +3,53 @@ import { connect } from 'react-redux'
 import MapHelper from '../../MapHelper'
 import MapProxy from './MapProxy'
 import _ from 'lodash'
+import { selectTasks, selectFilteredTasks } from '../store/selectors'
 
 class LeafletMap extends Component {
+
+  _draw() {
+    const {
+      polylines,
+      tasks,
+      tasksFiltered,
+    } = this.props
+
+    const tasksHidden = _.differenceWith(tasks, tasksFiltered, (a, b) => a['@id'] === b['@id'])
+
+    tasksFiltered.forEach(task => this.proxy.addTask(task))
+    tasksHidden.forEach(task => this.proxy.hideTask(task))
+
+    _.forEach(polylines, (polyline, username) => this.proxy.setPolyline(username, polyline))
+  }
 
   componentDidMount() {
 
     this.map = MapHelper.init('map')
     this.proxy = new MapProxy(this.map)
 
-    let { tasks, showFinishedTasks, showCancelledTasks } = this.props
-
-    if (!showFinishedTasks) {
-      tasks = _.filter(tasks, (task) => { return task.status === 'TODO' })
-    }
-    if (!showCancelledTasks) {
-      tasks = _.filter(tasks, (task) => { return task.status !== 'CANCELLED' })
-    }
-
-    _.forEach(tasks, task => this.proxy.addTask(task))
-    _.forEach(this.props.polylines, (polyline, username) => this.proxy.setPolyline(username, polyline))
+    this._draw()
   }
 
   componentDidUpdate(prevProps) {
+
     const {
       polylineEnabled,
       polylines,
-      tasks,
-      selectedTags,
-      showUntaggedTasks,
-      showFinishedTasks,
-      showCancelledTasks,
       selectedTasks,
       positions,
-      offline
+      offline,
     } = this.props
 
-    if (tasks !== prevProps.tasks) {
-      _.forEach(tasks, task => this.proxy.addTask(task))
-    }
+    this._draw()
 
-    _.forEach(polylines, (polyline, username) => this.proxy.setPolyline(username, polyline))
+    selectedTasks.forEach(task => this.proxy.addTask(task, '#EEB516'))
+
     _.forEach(polylineEnabled, (enabled, username) => {
       if (enabled) {
         this.proxy.showPolyline(username, polylines[username])
       } else {
         this.proxy.hidePolyline(username)
       }
-    })
-
-    if (prevProps.showFinishedTasks !== showFinishedTasks) {
-      const finishedTasks = _.filter(tasks, task => task.status !== 'TODO')
-      if (showFinishedTasks) {
-        _.forEach(finishedTasks, task => this.proxy.addTask(task))
-      } else {
-        _.forEach(finishedTasks, task => this.proxy.hideTask(task))
-      }
-    }
-
-    if ( prevProps.selectedTags !== selectedTags ) {
-      let toHide = _.filter(tasks, (task) => task.tags.length > 0 && _.intersectionBy(task.tags, selectedTags, 'name').length === 0)
-      _.forEach(toHide, task => this.proxy.hideTask(task))
-      let toShow = _.filter(tasks, (task) => task.tags.length > 0 && _.intersectionBy(task.tags, selectedTags, 'name').length > 0)
-      _.forEach(toShow, task => this.proxy.addTask(task))
-    }
-
-    if ( prevProps.showUntaggedTasks !== showUntaggedTasks ) {
-      let untaggedTasks = _.filter(tasks, (task) => task.tags.length === 0)
-      if (showUntaggedTasks) {
-        _.forEach(untaggedTasks, task => this.proxy.addTask(task))
-      } else {
-        _.forEach(untaggedTasks, task => this.proxy.hideTask(task))
-      }
-    }
-
-    if (prevProps.showCancelledTasks !== showCancelledTasks) {
-      const cancelledTasks = _.filter(tasks, task => task.status === 'CANCELLED')
-      if (showCancelledTasks) {
-        _.forEach(cancelledTasks, task => this.proxy.addTask(task))
-      } else {
-        _.forEach(cancelledTasks, task => this.proxy.hideTask(task))
-      }
-    }
-
-    if (prevProps.selectedTasks !== selectedTasks) {
-      // TODO Find a better way to do this instead of looping over & over
-      prevProps.selectedTasks.forEach(task => {
-        this.proxy.removeTask(task)
-        this.proxy.addTask(task)
-      })
-    }
-
-    selectedTasks.forEach(task => {
-      this.proxy.removeTask(task)
-      this.proxy.addTask(task, '#EEB516')
     })
 
     if (prevProps.positions !== positions) {
@@ -123,10 +77,7 @@ class LeafletMap extends Component {
 
 function mapStateToProps(state) {
 
-  const { taskLists, unassignedTasks, polylineEnabled } = state
-
-  const tasks = unassignedTasks.slice(0)
-  _.forEach(taskLists, taskList => taskList.items.forEach(task => tasks.push(task)))
+  const { taskLists, polylineEnabled } = state
 
   let polylines = {}
   _.forEach(taskLists, taskList => {
@@ -134,13 +85,10 @@ function mapStateToProps(state) {
   })
 
   return {
-    tasks,
+    tasks: selectTasks(state),
+    tasksFiltered: selectFilteredTasks(state),
     polylines,
     polylineEnabled,
-    showFinishedTasks: state.taskFinishedFilter,
-    selectedTags: state.tagsFilter.selectedTagsList,
-    showUntaggedTasks: state.tagsFilter.showUntaggedTasks,
-    showCancelledTasks: state.taskCancelledFilter,
     selectedTasks: state.selectedTasks,
     positions: state.positions,
     offline: state.offline
