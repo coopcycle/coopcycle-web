a7bb13760cc11bf8266b9cb4ecb518d03b97a699
Alexandre Segura <mex.zktk@gmail.com>
2019-01-31 08:42:13 +0100

    Fix migration.

--- app/DoctrineMigrations/Version20190128225322.php
+++ app/DoctrineMigrations/Version20190128225322.php
@@ -13,11 +13,14 @@ final class Version20190128225322 extends AbstractMigration
 
         $stmt->execute();
 
-        if ($channel = $stmt->fetch()) {
-            $this->addSql('UPDATE sylius_order SET channel_id = :channel_id', [
-                'channel_id' => $channel['id'],
+        if (!$channel = $stmt->fetch()) {
+            $this->addSql('INSERT INTO sylius_channel (code, name, enabled, created_at, updated_at) VALUES (:code, :name, \'t\', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)', [
+                'code' => 'web',
+                'name' => 'Web'
             ]);
         }
+
+        $this->addSql('UPDATE sylius_order SET channel_id = (SELECT id FROM sylius_channel WHERE code = \'web\')');
     }
 
     public function down(Schema $schema) : void
