fef5a941100c72161f761b04b6aca7c8678f845d
Alexandre Segura <mex.zktk@gmail.com>
2019-02-24 16:40:42 +0100

    Log request & response body for API requests.

---
+++ src/AppBundle/EventSubscriber/ApiLogSubscriber.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\EventSubscriber;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Event\PostResponseEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ApiLogSubscriber implements EventSubscriberInterface
+{
+    private $logger;
+
+    private $secretPaths = [
+        '/api/login_check',
+        '/api/register',
+        '/api/token/refresh',
+    ];
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function onKernelTerminate(PostResponseEvent $event)
+    {
+        $request = $event->getRequest();
+
+        // Avoid logging sensitive information
+        // FIXME Hide sensitive info in formatter
+        foreach ($this->secretPaths as $pathInfo) {
+            if ($pathInfo === $request->getPathInfo()) {
+
+                return;
+            }
+        }
+
+        if (0 !== strpos($request->getPathInfo(), '/api')) {
+
+            return;
+        }
+
+        $response = $event->getResponse();
+
+        $this->logger->info($request->getPathInfo(), [
+            'request' => $request,
+            'response' => $response
+        ]);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::TERMINATE => 'onKernelTerminate',
+        );
+    }
+}
