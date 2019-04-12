525a6f675b4fbf4d29369c7fa4dd53ab3bdacf2e
Alexandre Segura <mex.zktk@gmail.com>
2019-04-12 09:58:58 +0200

    WIP Add comment.

--- app/DoctrineMigrations/Version20190411152858.php
+++ app/DoctrineMigrations/Version20190411152858.php
@@ -14,6 +14,8 @@ final class Version20190411152858 extends AbstractMigration
         $this->addSql('ALTER TABLE sylius_product_option_value ADD tax_category_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE sylius_product_option_value ADD CONSTRAINT FK_F7FF7D4B9DF894ED FOREIGN KEY (tax_category_id) REFERENCES sylius_tax_category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_F7FF7D4B9DF894ED ON sylius_product_option_value (tax_category_id)');
+
+        // TODO Set default tax category?
     }
 
     public function down(Schema $schema) : void
