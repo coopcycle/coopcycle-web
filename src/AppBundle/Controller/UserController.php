04fbe9e531587e357ed618c971dff6685a75c910
Alexandre Segura <mex.zktk@gmail.com>
2019-05-06 12:01:06 +0200

    Fix public profile controller, remove sensitive data.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -37,7 +37,7 @@ class UserController extends AbstractController
      */
     public function indexAction($username, UserManagerInterface $userManager)
     {
-        $user = $this->userManager->findUserByUsername($username);
+        $user = $userManager->findUserByUsername($username);
 
         return [
             'user' => $user,
