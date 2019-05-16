f14539b7de966c309b73543909df10b59a46b263
Alexandre Segura <mex.zktk@gmail.com>
2019-05-16 09:27:19 +0200

    Add image field to Product class.

--- src/AppBundle/Entity/Sylius/Product.php
+++ src/AppBundle/Entity/Sylius/Product.php
@@ -8,6 +8,9 @@ use AppBundle\Entity\Restaurant;
 use AppBundle\Sylius\Product\ProductInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Sylius\Component\Product\Model\Product as BaseProduct;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ApiResource(
@@ -25,10 +28,26 @@ use Sylius\Component\Product\Model\Product as BaseProduct;
  *     "normalization_context"={"groups"={"product"}}
  *   }
  * )
+ * @Vich\Uploadable
  */
 class Product extends BaseProduct implements ProductInterface
 {
     protected $restaurant;
+
+    /**
+     * @Vich\UploadableField(mapping="product_image", fileNameProperty="imageName")
+     * @Assert\File(
+     *   maxSize = "1024k",
+     *   mimeTypes = {"image/jpg", "image/jpeg", "image/png"}
+     * )
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @var string
+     */
+    private $imageName;
 
     protected $deletedAt;
 
@@ -54,6 +73,48 @@ class Product extends BaseProduct implements ProductInterface
     {
         $this->restaurant->clear();
         $this->restaurant->add($restaurant);
+    }
+
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|Symfony\Component\HttpFoundation\File\UploadedFile $image
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
     }
 
     public function getAllergens()
