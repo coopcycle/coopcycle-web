24669972b691ea43eb1ccf0f61d3bf5d299790af
Alexandre Segura <mex.zktk@gmail.com>
2019-06-10 09:33:22 +0200

    Allow modifying maintenance message.

--- src/AppBundle/Form/MaintenanceType.php
+++ src/AppBundle/Form/MaintenanceType.php
@@ -2,13 +2,23 @@
 
 namespace AppBundle\Form;
 
+use Predis\Client as Redis;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class MaintenanceType extends AbstractType
 {
+    private $redis;
+
+    public function __construct(Redis $redis)
+    {
+        $this->redis = $redis;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -22,5 +32,17 @@ class MaintenanceType extends AbstractType
             ->add('disable', SubmitType::class, [
                 'label' => 'form.maintenance.disable.label'
             ]);
+
+        $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) {
+
+            $form = $event->getForm();
+
+            $maintenance = $this->redis->get('maintenance');
+            $maintenanceMessage = $this->redis->get('maintenance_message');
+
+            if (!empty($maintenance) && !empty($maintenanceMessage)) {
+                $form->get('message')->setData($maintenanceMessage);
+            }
+        });
     }
 }
