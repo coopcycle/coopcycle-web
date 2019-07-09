7183fd860377d1e868ec0ae1aed53f3d17cd2f4c
Alexandre Segura <mex.zktk@gmail.com>
2019-07-09 22:21:33 +0200

    Remove useless code.

--- src/AppBundle/Security/FOSUBUserProvider.php
+++ src/AppBundle/Security/FOSUBUserProvider.php
@@ -24,31 +24,6 @@ class FOSUBUserProvider extends BaseProvider
     }
 
     /**
-     * {@inheritDoc}
-     */
-    public function connect(UserInterface $user, UserResponseInterface $response)
-    {
-        $username = $response->getUsername();
-        // Get resource owner prefix
-        $property = $this->getProperty($response);
-        $service = $response->getResourceOwner()->getName();
-        // Access token and the user ID setters
-        $setter = 'set'.ucfirst($service);
-        $setter_id = $setter.'Id';
-        $setter_token = $setter.'AccessToken';
-        // Disconnect previously connected users
-        if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
-            $previousUser->$setter_id(null);
-            $previousUser->$setter_token(null);
-            $this->userManager->updateUser($previousUser);
-        }
-        // Connect user
-        $user->$setter_id($username);
-        $user->$setter_token($response->getAccessToken());
-        $this->userManager->updateUser($user);
-    }
-
-    /**
      * {@inheritdoc}
      */
     public function loadUserByOAuthUserResponse(UserResponseInterface $response)
