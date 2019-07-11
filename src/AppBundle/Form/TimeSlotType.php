942dc415b4bee9fdee794c5d3eb45e9809adf159
Alexandre Segura <mex.zktk@gmail.com>
2019-07-11 23:35:26 +0200

    Add screens to manage time slots.

---
+++ src/AppBundle/Form/TimeSlotType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\TimeSlot;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TimeSlotType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+            	'label' => 'form.time_slot.name.label'
+            ])
+            ->add('choices', CollectionType::class, [
+                'entry_type' => TimeSlotChoiceType::class,
+                'entry_options' => ['label' => false],
+                'label' => 'form.time_slot.choices.label',
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+		    ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => TimeSlot::class,
+        ));
+    }
+}
