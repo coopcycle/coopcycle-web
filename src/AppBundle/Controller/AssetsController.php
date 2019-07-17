089ff2adc04147367e2d207b95f3aad85db9e680
Alexandre Segura <mex.zktk@gmail.com>
2019-07-17 16:46:58 +0200

    Set response content type.

--- src/AppBundle/Controller/AssetsController.php
+++ src/AppBundle/Controller/AssetsController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sylius\Component\Currency\Context\CurrencyContextInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AssetsController extends AbstractController
@@ -14,9 +15,13 @@ class AssetsController extends AbstractController
      */
     public function jsDataAction(CurrencyContextInterface $currencyContext)
     {
+        $response = new Response();
+
+        $response->headers->set('Content-Type', 'text/javascript');
+
         return $this->render('@App/_partials/app.js.twig', [
             'currency_context' => $currencyContext,
             'country_iso' => $this->getParameter('country_iso'),
-        ]);
+        ], $response);
     }
 }
