6de5636f5975b38b2088716aab792e63228ad950
Alexandre Segura <mex.zktk@gmail.com>
2019-07-25 23:51:54 +0200

    Remove workarounds for Webpack Dev Server.

--- docker/webpack/start.sh
+++ docker/webpack/start.sh
@@ -5,4 +5,4 @@ npm install
 
 # Use --keep-public-path to avoid having absolute URLs
 # @see https://github.com/symfony/webpack-encore/blob/b5750b2fb09053687bcaca59b27a79a8396d304b/lib/WebpackConfig.js#L235
-node_modules/.bin/encore dev-server --watch-poll --host 0.0.0.0 --content-base=web/ --disable-host-check --keep-public-path
+node_modules/.bin/encore dev-server --watch-poll --host 0.0.0.0 --port 8080 --content-base=web/ --disable-host-check
