0b98618de8bff569a0077bd3a2f0513fa1a6aeb2
Arnaud Masson <arnaudmasson.dev@gmail.com>
2019-08-01 11:45:58 +0200

    fix routing

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -1243,7 +1243,7 @@ class AdminController extends Controller
 
             $this->get('sylius.manager.promotion_coupon')->flush();
 
-            return $this->redirectToRoute('admin_promotion', ['id' => $id]);
+            return $this->redirectToRoute('admin_promotions');
         }
 
         return $this->render('@App/admin/promotion_coupon.html.twig', [
