e7190ea59d45db9d3edf6c228e02b0624384ecde
Alexandre Segura <mex.zktk@gmail.com>
2019-08-03 23:38:54 +0200

    Use caching in EnabledFilterConfigurator.

--- src/AppBundle/EventListener/EnabledFilterConfigurator.php
+++ src/AppBundle/EventListener/EnabledFilterConfigurator.php
@@ -4,6 +4,8 @@ namespace AppBundle\EventListener;
 
 use AppBundle\Entity\Restaurant;
 use AppBundle\Entity\RestaurantRepository;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationCredentialsNotFoundException;
@@ -17,57 +19,56 @@ class EnabledFilterConfigurator
     protected $tokenStorage;
     protected $restaurantRepository;
     protected $reader;
+    protected $cache;
 
     public function __construct(
         ObjectManager $em,
         TokenStorageInterface $tokenStorage,
         RestaurantRepository $restaurantRepository,
-        Reader $reader)
+        Reader $reader,
+        CacheInterface $enabledFilterConfiguratorCache)
     {
         $this->em = $em;
         $this->tokenStorage = $tokenStorage;
         $this->restaurantRepository = $restaurantRepository;
         $this->reader = $reader;
+        $this->cache = $enabledFilterConfiguratorCache;
     }
 
     public function onKernelRequest()
     {
-        $isAdmin = false;
-        $isRestaurant = false;
         $restaurants = [];
 
         if ($user = $this->getUser()) {
 
             // If this is an admin, we don't enable the filter
-            $isAdmin = $user->hasRole('ROLE_ADMIN');
-            if ($isAdmin) {
+            if ($user->hasRole('ROLE_ADMIN')) {
                 return;
             }
 
-            $isRestaurant = $user->hasRole('ROLE_RESTAURANT');
-            if ($isRestaurant) {
+            $restaurants = $this->cache->get($user->getUsername(), function (ItemInterface $item) use ($user) {
+
+                $item->expiresAfter(600);
+
                 $restaurants = [];
-                foreach ($user->getRestaurants() as $restaurant) {
-                    $restaurants[] = $restaurant;
+                if ($user->hasRole('ROLE_RESTAURANT')) {
+                    $restaurants = $user->getRestaurants()->toArray();
+                } else {
+                    $restaurants = $this->restaurantRepository->findByCustomer($user);
                 }
-            } else {
-                $restaurants = $this->restaurantRepository->findByCustomer($user);
-            }
-        }
 
-        if (!$isAdmin) {
-            $filter = $this->em->getFilters()->enable('enabled_filter');
-            $filter->setAnnotationReader($this->reader);
-            $filter->setParameter('enabled', true, Type::BOOLEAN);
-
-            if (count($restaurants) > 0) {
-
-                $restaurants = array_map(function (Restaurant $restaurant) {
+                return array_map(function (Restaurant $restaurant) {
                     return $restaurant->getId();
                 }, $restaurants);
+            });
+        }
 
-                $filter->setParameter('restaurants', $restaurants, Type::SIMPLE_ARRAY);
-            }
+        $filter = $this->em->getFilters()->enable('enabled_filter');
+        $filter->setAnnotationReader($this->reader);
+        $filter->setParameter('enabled', true, Type::BOOLEAN);
+
+        if (count($restaurants) > 0) {
+            $filter->setParameter('restaurants', $restaurants, Type::SIMPLE_ARRAY);
         }
     }
 
