9b055bb0099b076305cce27d3b7f9c08b2391a16
Alexandre Segura <mex.zktk@gmail.com>
2019-08-04 23:55:52 +0200

    Fix mapping for restaurant/address, use eager fetching.

---
+++ app/DoctrineMigrations/Version20190804215040.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190804215040 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP INDEX uniq_eb95123ff5b7af75');
+        $this->addSql('CREATE INDEX IDX_EB95123FF5B7AF75 ON restaurant (address_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP INDEX IDX_EB95123FF5B7AF75');
+        $this->addSql('CREATE UNIQUE INDEX uniq_eb95123ff5b7af75 ON restaurant (address_id)');
+    }
+}
