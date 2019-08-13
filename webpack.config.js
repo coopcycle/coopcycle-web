c5f46585c68732025b0f83ae4a8bc76410814018
Alexandre Segura <mex.zktk@gmail.com>
2019-08-13 23:20:54 +0200

    Load Catalan locale in Moment.js.

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ Encore
   // @see https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
   .addPlugin(new webpack.ContextReplacementPlugin(
     /moment[/\\]locale$/,
-    /de|es|fr/
+    /ca|de|es|fr/
   ))
 
   .enableSingleRuntimeChunk()
