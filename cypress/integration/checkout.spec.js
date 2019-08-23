25937816f2c62d8c7d9c50c1f3ddfcad70256037
Alexandre Segura <mex.zktk@gmail.com>
2019-08-23 20:19:14 +0200

    Add new test for checkout.

--- cypress/integration/checkout.spec.js
+++ cypress/integration/checkout.spec.js
@@ -9,6 +9,10 @@ context('Checkout', () => {
     }
 
     cy.exec(cmd)
+
+    cy.window().then((win) => {
+      win.sessionStorage.clear()
+    })
   })
 
   it('order something at restaurant', () => {
@@ -28,13 +32,13 @@ context('Checkout', () => {
     cy.contains('Cheeseburger').click()
 
     cy.get('#CHEESEBURGER-options')
-        .should('be.visible')
+      .should('be.visible')
 
     // Make sure to use a precise selector, because 2 products have same options
     cy.get('#CHEESEBURGER-options input[name="options[HAMBURGER_ACCOMPANIMENT]"]')
-        .check('HAMBURGER_ACCOMPANIMENT_FRENCH_FRIES')
+      .check('HAMBURGER_ACCOMPANIMENT_FRENCH_FRIES')
     cy.get('#CHEESEBURGER-options input[name="options[HAMBURGER_DRINK]"]')
-        .check('HAMBURGER_DRINK_COLA')
+      .check('HAMBURGER_DRINK_COLA')
 
     // FIXME We need to use force = true, because the button has disabled=""
     cy.get('#CHEESEBURGER-options button[type="submit"]').click({ timeout: 5000, force: true })
@@ -44,17 +48,20 @@ context('Checkout', () => {
     cy.get('.cart__items').invoke('text').should('match', /Cheeseburger/)
 
     cy.get('.ReactModal__Content--enter-address')
-        .should('be.visible')
+      .should('be.visible')
 
     cy.get('.ReactModal__Content--enter-address input[type="search"]')
-        .type('91 rue de rivoli paris', { timeout: 5000 })
+      .type('91 rue de rivoli paris', { timeout: 5000 })
 
-    cy.contains('91 Rue de Rivoli, Paris, France').click()
+    cy.get('.ReactModal__Content--enter-address')
+      .find('ul[role="listbox"]')
+      .contains('91 Rue de Rivoli, Paris, France')
+      .click()
 
     cy.wait('@postRestaurant')
 
     cy.get('.cart .address-autosuggest__container input[type="search"]')
-        .should('have.value', '91 Rue de Rivoli, Paris, France')
+      .should('have.value', '91 Rue de Rivoli, Paris, France')
 
     cy.contains('Cheese Cake').click()
 
@@ -103,6 +110,53 @@ context('Checkout', () => {
     cy.get('form[name="checkout_payment"]').submit()
 
     cy.location('pathname').should('match', /\/profile\/orders\/[0-9]+/)
+  })
 
+  it('order something at restaurant with existing address', () => {
+
+    cy.visit('/login')
+
+    cy.get('[name="_username"]').type('jane')
+    cy.get('[name="_password"]').type('12345678')
+    cy.get('[name="_submit"]').click()
+
+    cy.location('pathname').should('eq', '/fr/')
+
+    cy.get('#address-search input[type="search"]')
+      .type('1 rue de', { timeout: 5000 })
+
+    cy.get('#address-search')
+      .find('.react-autosuggest__suggestions-container')
+      .find('.react-autosuggest__section-container')
+      // There should be 2 sections
+      .then(($sections) => {
+        cy.wrap($sections).should('have.length', 2)
+      })
+      // The first section should contain saved addresses
+      .then(($sections) => {
+        cy.wrap($sections)
+          .eq(0)
+          .find('.react-autosuggest__section-title')
+          .invoke('text')
+          .should('eq', 'Adresses sauvegardées')
+      })
+
+    // Click on the first suggestion
+    cy.get('#address-search')
+      .find('.react-autosuggest__suggestions-container')
+      .find('.react-autosuggest__section-container')
+      .eq(0)
+      .contains('1, Rue de Rivoli, Paris, France')
+      .click()
+
+    cy.location().then((loc) => {
+      expect(loc.pathname).to.eq('/fr/restaurants')
+      expect(loc.search).to.match(/\?geohash=[a-z0-9]+&address=[a-z0-9]+/)
+    })
+
+    cy.contains('Crazy Hamburger').click()
+
+    cy.get('.cart .address-autosuggest__container input[type="search"]')
+      .should('have.value', '1, Rue de Rivoli, Paris, France')
   })
 })
