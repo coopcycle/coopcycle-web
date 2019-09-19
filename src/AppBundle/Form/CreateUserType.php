3be9688dbbdd92e0ca211d64d70233002c4afa2c
Alexandre Segura <mex.zktk@gmail.com>
2019-09-19 14:36:18 +0200

    Add checkbox to mark user as allowed to make quotes.

--- src/AppBundle/Form/CreateUserType.php
+++ src/AppBundle/Form/CreateUserType.php
@@ -2,22 +2,20 @@
 
 namespace AppBundle\Form;
 
-use libphonenumber\PhoneNumberFormat;
-use Misd\PhoneNumberBundle\Form\Type\PhoneNumberType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CreateUserType extends AbstractType
 {
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('quotesAllowed', CheckboxType::class, [
+                'label' => 'form.user.quotes_allowed.label',
+                'required' => false
+            ])
             ->add('save', SubmitType::class, array('label' => 'basics.save'))
             ->add('sendInvitation', SubmitType::class, array('label' => 'basics.send_invitation'));
     }
