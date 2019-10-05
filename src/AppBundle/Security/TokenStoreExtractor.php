b38a05c42859faba95dd9233e100a28371363949
Alexandre Segura <mex.zktk@gmail.com>
2019-10-05 11:03:42 +0200

    Introduce TokenStoreExtractor.

---
+++ src/AppBundle/Security/TokenStoreExtractor.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Security;
+
+use AppBundle\Entity\ApiApp;
+use Doctrine\Common\Persistence\ManagerRegistry as DoctrineRegistry;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Trikoder\Bundle\OAuth2Bundle\Security\Authentication\Token\OAuth2Token;
+use Trikoder\Bundle\OAuth2Bundle\Manager\AccessTokenManagerInterface;
+
+class TokenStoreExtractor
+{
+    public function __construct(
+        DoctrineRegistry $doctrine,
+        TokenStorageInterface $tokenStorage,
+        AccessTokenManagerInterface $accessTokenManager)
+    {
+        $this->doctrine = $doctrine;
+        $this->tokenStorage = $tokenStorage;
+        $this->accessTokenManager = $accessTokenManager;
+    }
+
+    public function extractStore()
+    {
+        if (null === ($token = $this->tokenStorage->getToken())) {
+            return;
+        }
+
+        if ($token instanceof OAuth2Token) {
+
+            $accessToken = $this->accessTokenManager->find($token->getCredentials());
+            $client = $accessToken->getClient();
+
+            $apiApp = $this->doctrine->getRepository(ApiApp::class)
+                ->findOneByOauth2Client($client);
+
+            return $apiApp->getStore();
+        } else if ($token->hasAttribute('store')) {
+
+            return $token->getAttribute('store');
+        }
+    }
+}
