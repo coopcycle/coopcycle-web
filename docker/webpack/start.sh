5972bf0491ced22440471b55b99c9c5e0131e407
Alexandre Segura <mex.zktk@gmail.com>
2019-11-03 16:20:04 +0100

    Use devServer.public option to avoid using 0.0.0.0.

--- docker/webpack/start.sh
+++ docker/webpack/start.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 cd /srv/coopcycle
+
 npm install
 
-# Use --keep-public-path to avoid having absolute URLs
-# @see https://github.com/symfony/webpack-encore/blob/b5750b2fb09053687bcaca59b27a79a8396d304b/lib/WebpackConfig.js#L235
-node_modules/.bin/encore dev-server --watch-poll --host 0.0.0.0 --port 8080 --content-base=web/ --disable-host-check
+node_modules/.bin/encore dev-server --watch-poll --host 0.0.0.0 --public localhost:8080 --port 8080 --content-base=web/ --disable-host-check
