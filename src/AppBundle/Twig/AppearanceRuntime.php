6d1046e9b74a33fdaf0a7ae9cbcf89ad88fb784e
Alexandre Segura <mex.zktk@gmail.com>
2019-11-06 15:32:31 +0100

    Add coopcycle_company_logo() Twig function, with fallback.

--- src/AppBundle/Twig/AppearanceRuntime.php
+++ src/AppBundle/Twig/AppearanceRuntime.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Twig;
 
 use AppBundle\Service\SettingsManager;
+use Intervention\Image\ImageManagerStatic;
+use League\Flysystem\Filesystem;
 use Symfony\Component\Asset\Packages;
 use Twig\Extension\RuntimeExtensionInterface;
 
@@ -10,11 +12,18 @@ class AppearanceRuntime implements RuntimeExtensionInterface
 {
     private $settingsManager;
     private $packages;
+    private $assetsFilesystem;
 
-    public function __construct(SettingsManager $settingsManager, Packages $packages)
+    public function __construct(
+        SettingsManager $settingsManager,
+        Packages $packages,
+        Filesystem $assetsFilesystem,
+        string $logoFallback)
     {
         $this->settingsManager = $settingsManager;
         $this->packages = $packages;
+        $this->assetsFilesystem = $assetsFilesystem;
+        $this->logoFallback = $logoFallback;
     }
 
     public function logo()
@@ -24,5 +33,17 @@ class AppearanceRuntime implements RuntimeExtensionInterface
         if (!empty($companyLogo)) {
             return $this->packages->getUrl(sprintf('images/assets/%s', $companyLogo));
         }
+    }
+
+    public function companyLogo()
+    {
+        $companyLogo = $this->settingsManager->get('company_logo');
+
+        if (!empty($companyLogo) && $this->assetsFilesystem->has($companyLogo)) {
+
+            return (string) ImageManagerStatic::make($this->assetsFilesystem->read($companyLogo))->encode('data-url');
+        }
+
+        return (string) ImageManagerStatic::make(file_get_contents($this->logoFallback))->encode('data-url');
     }
 }
