fdb70cffe62c8e654832a332be5d1820e56d9350
Alexandre Segura <mex.zktk@gmail.com>
2019-11-15 13:12:38 +0100

    Allow configuring valuesRange in form.

--- src/AppBundle/Form/ProductOptionType.php
+++ src/AppBundle/Form/ProductOptionType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\Sylius\ProductOption;
+use AppBundle\Form\Type\MinMaxType;
 use AppBundle\Sylius\Product\ProductOptionInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -10,8 +11,11 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Translation\TranslatorInterface;
 
@@ -50,6 +54,7 @@ class ProductOptionType extends AbstractType
                 'divisor' => 100,
                 'required' => false
             ])
+            ->add('valuesRange', MinMaxType::class)
             ->add('values', CollectionType::class, [
                 'entry_type' => ProductOptionValueType::class,
                 'allow_add' => true,
@@ -66,6 +71,13 @@ class ProductOptionType extends AbstractType
             ->add('delete', SubmitType::class, [
                 'label' => 'basics.delete',
             ]);
+
+        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) use ($options) {
+            $productOption = $event->getForm()->getData();
+            if (!$productOption->isAdditional()) {
+                $productOption->setValuesRange(null);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
