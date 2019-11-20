3dcab9b3cb6cdcea5aa1f157bad068b66f0986dd
Alexandre Segura <mex.zktk@gmail.com>
2019-11-20 22:12:17 +0100

    Remove useless dependencies.

--- src/AppBundle/Entity/Listener/TaskListener.php
+++ src/AppBundle/Entity/Listener/TaskListener.php
@@ -2,27 +2,11 @@
 
 namespace AppBundle\Entity\Listener;
 
-use AppBundle\Domain\Task\Event\TaskCreated;
-use AppBundle\Entity\ApiUser;
 use AppBundle\Entity\Task;
-use AppBundle\Entity\TaskList;
 use Doctrine\ORM\Event\LifecycleEventArgs;
-use Psr\Log\LoggerInterface;
-use SimpleBus\Message\Bus\MessageBus;
 
 class TaskListener
 {
-    private $eventBus;
-    private $logger;
-
-    public function __construct(
-        MessageBus $eventBus,
-        LoggerInterface $logger)
-    {
-        $this->eventBus = $eventBus;
-        $this->logger = $logger;
-    }
-
     public function prePersist(Task $task, LifecycleEventArgs $args)
     {
         if (null === $task->getDoneAfter()) {
