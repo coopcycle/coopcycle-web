cc734c55ab11d251a45e81c12a9854a72d322c86
Alexandre Segura <mex.zktk@gmail.com>
2019-12-14 14:43:32 +0100

    Do not show dropzone when product is new. Fixes #958.

--- js/app/product/form.js
+++ js/app/product/form.js
@@ -6,19 +6,22 @@ Dropzone.autoDiscover = false
 
 $(function() {
 
-  const formData = document.querySelector('#product-form-data')
+  const el = document.querySelector('#product-image-dropzone')
 
-  new DropzoneWidget($('#product-image-dropzone'), {
-    dropzone: {
-      url: formData.dataset.actionUrl,
-      params: {
-        type: 'product',
-        id: formData.dataset.productId
-      }
-    },
-    image: formData.dataset.productImage,
-    size: [ 256, 256 ]
-  })
+  if (el) {
+    const formData = document.querySelector('#product-form-data')
+    new DropzoneWidget(el, {
+      dropzone: {
+        url: formData.dataset.actionUrl,
+        params: {
+          type: 'product',
+          id: formData.dataset.productId
+        }
+      },
+      image: formData.dataset.productImage,
+      size: [ 256, 256 ]
+    })
+  }
 })
 
 $('#product_reusablePackagingEnabled').click(function() {
