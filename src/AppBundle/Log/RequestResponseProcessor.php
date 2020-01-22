3575aa0af19addedc9e91dc0809d7feb83791955
Alexandre Segura <mex.zktk@gmail.com>
2020-01-22 12:59:13 +0100

    Obfuscate token.

--- src/AppBundle/Log/RequestResponseProcessor.php
+++ src/AppBundle/Log/RequestResponseProcessor.php
@@ -15,10 +15,19 @@ class RequestResponseProcessor
         $request = $record['context']['request'];
         $response = $record['context']['response'];
 
+        $requestHeaders = clone $request->headers;
+        if ($requestHeaders->has('Authorization')) {
+            if (preg_match('/^Bearer (.*)$/', $requestHeaders->get('Authorization'))) {
+                // Obfuscate token
+                $authorization = preg_replace('/^Bearer (.{4})(.*)(.{4})$/', 'Bearer ${1}✱✱✱✱${1}', $requestHeaders->get('Authorization'));
+                $requestHeaders->set('Authorization', $authorization);
+            }
+        }
+
         $record['extra']['method'] = $request->getMethod();
         $record['extra']['request_uri'] = $request->getRequestUri();
         $record['extra']['status_code'] = $response->getStatusCode();
-        $record['extra']['request_headers'] = (string) $request->headers;
+        $record['extra']['request_headers'] = (string) $requestHeaders;
         $record['extra']['request_body'] = (string) $request->getContent();
         $record['extra']['response_headers'] = (string) $response->headers;
         $record['extra']['response_body'] = (string) $response->getContent();
