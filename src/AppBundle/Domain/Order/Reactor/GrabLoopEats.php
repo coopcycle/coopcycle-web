ad34b6713db74d9ead01da41940711673750ce68
Alexandre Segura <mex.zktk@gmail.com>
2020-02-20 18:24:55 +0100

    Grab LoopEats when order is picked.

---
+++ src/AppBundle/Domain/Order/Reactor/GrabLoopEats.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Domain\Order\Reactor;
+
+use AppBundle\LoopEat\Client as LoopEatClient;
+use AppBundle\Domain\Order\Event;
+
+class GrabLoopEats
+{
+    private $client;
+
+    public function __construct(LoopEatClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function __invoke(Event\OrderPicked $event)
+    {
+        $order = $event->getOrder();
+        $restaurant = $order->getRestaurant();
+
+        if (null === $restaurant) {
+            return;
+        }
+
+        if (!$restaurant->isLoopeatEnabled()) {
+            return;
+        }
+
+        if (!$order->isReusablePackagingEnabled() || $order->getReusablePackagingQuantity() < 1) {
+            return;
+        }
+
+        $this->client->grab($order->getCustomer(), $order->getRestaurant(), $order->getReusablePackagingQuantity());
+    }
+}
