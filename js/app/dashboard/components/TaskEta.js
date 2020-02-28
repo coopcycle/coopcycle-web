c3058aced4d29fad1a538fb9d373ec56ee334563
Alexandre Segura <mex.zktk@gmail.com>
2020-02-28 11:44:27 +0100

    Use percentages.

--- js/app/dashboard/components/TaskEta.js
+++ js/app/dashboard/components/TaskEta.js
@@ -52,38 +52,23 @@ class TaskEta extends React.Component {
   constructor(props) {
     super(props)
 
-    this.ref = React.createRef()
-
     this.state = {
-      width: 0,
       timeframeLeft: 0,
       timeframeWidth: 0,
     }
   }
 
-  componentDidMount () {
-    setTimeout(() => {
-      const rect = this.ref.current.getBoundingClientRect()
-      const { width } = rect
-      this.setState({ width })
-    }, 0)
-  }
-
   render() {
 
     const { after, before } = this.props.task
-    const { width } = this.state
-
     const [ percentAfter, percentBefore ] = timeframeToPercentage([ after, before ], moment(this.props.date))
-
-    const timeframeLeft = width * percentAfter
-    const timeframeWidth = ((width * percentBefore) - timeframeLeft)
-
     const isSameDay = moment(this.props.date).format('YYYY-MM-DD') === moment().format('YYYY-MM-DD')
+    const timeframeLeft = (percentAfter * 100)
+    const timeframeWidth = ((percentBefore - percentAfter) * 100)
 
     return (
-      <span className="task__eta" ref={ this.ref }>
-        <span className="task__eta__timeframe" style={{ left: timeframeLeft, width: timeframeWidth }}></span>
+      <span className="task__eta">
+        <span className="task__eta__timeframe" style={{ left: `${timeframeLeft.toFixed(4)}%`, width: `${timeframeWidth.toFixed(4)}%` }}></span>
         { isSameDay && <NowCursor /> }
       </span>
     )
