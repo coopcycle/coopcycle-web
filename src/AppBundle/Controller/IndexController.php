22057834e5c81490c38fcbaf190e80d6984df3c4
Alexandre Segura <mex.zktk@gmail.com>
2020-03-16 18:24:19 +0100

    Show shops on homepage.

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Annotation\HideSoftDeleted;
 use AppBundle\Controller\Utils\UserTrait;
 use AppBundle\Entity\RestaurantRepository;
+use AppBundle\Entity\ShopRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,12 +20,16 @@ class IndexController extends AbstractController
      * @Template
      * @HideSoftDeleted
      */
-    public function indexAction(RestaurantRepository $repository)
+    public function indexAction(
+        RestaurantRepository $restautantRepository,
+        ShopRepository $shopRepository)
     {
-        $restaurants = $repository->findAllSorted();
+        $restaurants = $restautantRepository->findAllSorted();
+        $shops = $shopRepository->findAllSorted();
 
         return array(
             'restaurants' => array_slice($restaurants, 0, self::MAX_RESULTS),
+            'shops' => array_slice($shops, 0, self::MAX_RESULTS),
             'max_results' => self::MAX_RESULTS,
             'show_more' => count($restaurants) > self::MAX_RESULTS,
             'addresses_normalized' => $this->getUserAddresses(),
