822ce2a06cddc8210316585c1a1154b4b3c5d882
Alexandre Segura <mex.zktk@gmail.com>
2020-04-15 06:53:10 +0200

    Retrieve logo in settings.

--- src/AppBundle/Action/Settings.php
+++ src/AppBundle/Action/Settings.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Action;
 
 use AppBundle\Service\SettingsManager;
+use League\Flysystem\Filesystem;
+use Liip\ImagineBundle\Service\FilterService;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 class Settings
 {
     private $settingsManager;
+    private $assetsFilesystem;
     private $country;
     private $locale;
 
@@ -22,9 +25,16 @@ class Settings
         'currency_code',
     ];
 
-    public function __construct(SettingsManager $settingsManager, $country, $locale)
+    public function __construct(
+        SettingsManager $settingsManager,
+        Filesystem $assetsFilesystem,
+        FilterService $imagineFilter,
+        $country,
+        $locale)
     {
         $this->settingsManager = $settingsManager;
+        $this->assetsFilesystem = $assetsFilesystem;
+        $this->imagineFilter = $imagineFilter;
         $this->country = $country;
         $this->locale = $locale;
     }
@@ -45,6 +55,11 @@ class Settings
 
         foreach ($this->keys as $key) {
             $data[$key] = $this->settingsManager->get($key);
+        }
+
+        $companyLogo = $this->settingsManager->get('company_logo');
+        if (!empty($companyLogo) && $this->assetsFilesystem->has($companyLogo)) {
+            $data['logo'] = $this->imagineFilter->getUrlOfFilteredImage($companyLogo, 'logo_thumbnail');
         }
 
         if ($request->query->has('format') && 'hash' === $request->query->get('format')) {
