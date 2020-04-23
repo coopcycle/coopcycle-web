0aa690cb965c9ea10e921854f7d070be4a522f74
Alexandre Segura <mex.zktk@gmail.com>
2020-04-23 11:42:11 +0200

    Introduce web notifications on orders dashboard.

--- js/app/foodtech/dashboard/redux/middlewares.js
+++ js/app/foodtech/dashboard/redux/middlewares.js
@@ -1,12 +1,17 @@
 import _ from 'lodash'
+import { Howl } from 'howler'
 
 import {
   orderCreated,
   orderAccepted,
   orderRefused,
   orderCancelled,
-  orderFulfilled
+  orderFulfilled,
+  ORDER_CREATED,
 } from './actions'
+
+import { asText } from '../../../components/ShippingTimeRange'
+import i18n from '../../../i18n'
 
 let socket
 
@@ -70,6 +75,50 @@ export const title = ({ getState }) => {
 
     const result = next(action)
     document.title = pageTitle(getState(), initialTitle)
+
+    return result
+  }
+}
+
+const sound = new Howl({
+  src: ['/sounds/383624__newagesoup__bicycle-bell-13.wav']
+})
+
+function notify(title, payload) {
+  var n = new Notification(title, payload)
+  n.onshow = function () {
+    sound.play()
+  }
+  n.onclick = function () {
+    window.focus()
+  }
+}
+
+export const notification = ({ getState }) => {
+
+  if (window.Notification && Notification.permission !== 'granted') {
+    Notification.requestPermission((status) => {
+      if (status === 'granted') {
+        notify(i18n.t('NOTIFICATIONS_ENABLED'), { body: '🔔🔔🔔' })
+      }
+    })
+  }
+
+  return next => action => {
+
+    const prevState = getState()
+    const result = next(action)
+    const state = getState()
+
+    if (action.type === ORDER_CREATED && (state.orders.length > prevState.orders.length)) {
+      const { restaurant, shippingTimeRange } = action.payload
+      if (window.Notification && Notification.permission === 'granted') {
+        notify(restaurant.name, {
+          body: asText(shippingTimeRange),
+          tag: 'order:created',
+        })
+      }
+    }
 
     return result
   }
--- js/app/foodtech/dashboard/redux/store.js
+++ js/app/foodtech/dashboard/redux/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducers, { initialState } from './reducers'
-import { socketIO, title } from './middlewares'
+import { socketIO, title, notification } from './middlewares'
 
-const middlewares = [ thunk, socketIO, title ]
+const middlewares = [ thunk, socketIO, title, notification ]
 
 export const createStoreFromPreloadedState = preloadedState => {
   return createStore(
