6e32973887f44773790febb563b8eddbaf194dc6
Alexandre Segura <mex.zktk@gmail.com>
2020-05-07 13:57:34 +0200

    Lint.

--- js/app/foodtech/dashboard/components/ModalContent.js
+++ js/app/foodtech/dashboard/components/ModalContent.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withTranslation } from 'react-i18next'
-import moment from 'moment'
 import { parsePhoneNumberFromString } from 'libphonenumber-js'
 
 import {
