cc2afa36121c7ea5a7dda0efa141616174e6507e
Alexandre Segura <mex.zktk@gmail.com>
2020-05-11 06:37:49 +0200

    Improve order total, translate.

--- js/app/foodtech/dashboard/components/OrderTotal.js
+++ js/app/foodtech/dashboard/components/OrderTotal.js
@@ -1,26 +1,20 @@
 import React from 'react'
 import { withTranslation } from 'react-i18next'
 
-class OrderTotal extends React.Component {
-  render() {
+export default withTranslation()(({ order, t }) => {
 
-    const { order } = this.props
-
-    return (
-      <table className="table table-condensed">
-        <tbody>
-          <tr>
-            <td><strong>Total TTC</strong></td>
-            <td className="text-right"><strong>{ (order.total / 100).formatMoney(2, window.AppData.currencySymbol) }</strong></td>
-          </tr>
-          <tr>
-            <td><strong>Dont TVA</strong></td>
-            <td className="text-right"><strong>{ (order.taxTotal / 100).formatMoney(2, window.AppData.currencySymbol) }</strong></td>
-          </tr>
-        </tbody>
-      </table>
-    )
-  }
-}
-
-export default withTranslation()(OrderTotal)
+  return (
+    <table className="table table-condensed">
+      <tbody>
+        <tr>
+          <td><strong>{ t('CART_TOTAL_PRODUCTS') }</strong></td>
+          <td className="text-right"><strong>{ (order.itemsTotal / 100).formatMoney(2, window.AppData.currencySymbol) }</strong></td>
+        </tr>
+        <tr>
+          <td><strong>{ t('CART_TOTAL') }</strong></td>
+          <td className="text-right"><strong>{ (order.total / 100).formatMoney(2, window.AppData.currencySymbol) }</strong></td>
+        </tr>
+      </tbody>
+    </table>
+  )
+})
