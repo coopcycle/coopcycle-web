795901474d592ac8c46fe930cec822118e124746
Alexandre Segura <mex.zktk@gmail.com>
2020-05-15 08:51:58 +0200

    Add function to resolve ant.design locale data.

--- js/app/i18n/index.js
+++ js/app/i18n/index.js
@@ -13,9 +13,15 @@ import 'moment-timezone'
 
 import de from './locales/de.json'
 import en from './locales/en.json'
-import fr from './locales/fr.json'
 import es from './locales/es.json'
+import fr from './locales/fr.json'
 import pl from './locales/pl.json'
+
+import de_DE from 'antd/es/locale/de_DE'
+import en_US from 'antd/es/locale/en_US'
+import es_ES from 'antd/es/locale/es_ES'
+import fr_FR from 'antd/es/locale/fr_FR'
+import pl_PL from 'antd/es/locale/pl_PL'
 
 export const localeDetector = () => $('html').attr('lang')
 
@@ -41,3 +47,14 @@ i18next
 export default i18next
 
 export const setTimezone = timezone => moment.tz.setDefault(timezone)
+
+const antdLocaleMap = {
+  'de': de_DE,
+  'en': en_US,
+  'es': es_ES,
+  'fr': fr_FR,
+  'pl': pl_PL,
+}
+
+export const antdLocale =
+  Object.prototype.hasOwnProperty.call(antdLocaleMap, localeDetector()) ? antdLocaleMap[localeDetector()] : en_US
