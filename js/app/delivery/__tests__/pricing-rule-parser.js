8facdfb8d0af4d1f4aa53456eb7ab567f4154115
Alexandre Segura <mex.zktk@gmail.com>
2020-05-17 14:33:11 +0200

    Test with Jest.

--- js/app/delivery/__tests__/pricing-rule-parser.js
+++ js/app/delivery/__tests__/pricing-rule-parser.js
@@ -1,14 +1,11 @@
-import parsePricingRule from '../app/delivery/pricing-rule-parser'
-import chai from 'chai'
-
-const expect = chai.expect
+import parsePricingRule from '../pricing-rule-parser'
 
 describe('Pricing rule parser', function() {
 
   it('should parse in_zone', function() {
     const expression = 'in_zone(pickup.address, "bordeaux_centre_ville")'
     const result = parsePricingRule(expression)
-    expect(result).to.deep.equal([{
+    expect(result).toEqual([{
       left: 'pickup.address',
       operator: 'in_zone',
       right: 'bordeaux_centre_ville'
@@ -18,7 +15,7 @@ describe('Pricing rule parser', function() {
   it('should parse out_zone', function() {
     const expression = 'out_zone(pickup.address, "bordeaux_centre_ville")'
     const result = parsePricingRule(expression)
-    expect(result).to.deep.equal([{
+    expect(result).toEqual([{
       left: 'pickup.address',
       operator: 'out_zone',
       right: 'bordeaux_centre_ville'
@@ -28,7 +25,7 @@ describe('Pricing rule parser', function() {
   it('should parse in_zone with zone containing "and"', function() {
     const expression = 'in_zone(pickup.address, "commander")'
     const result = parsePricingRule(expression)
-    expect(result).to.deep.equal([{
+    expect(result).toEqual([{
       left: 'pickup.address',
       operator: 'in_zone',
       right: 'commander'
@@ -38,7 +35,7 @@ describe('Pricing rule parser', function() {
   it('should parse vehicle', function() {
     const expression = 'vehicle == "cargo_bike"'
     const result = parsePricingRule(expression)
-    expect(result).to.deep.equal([{
+    expect(result).toEqual([{
       left: 'vehicle',
       operator: '==',
       right: 'cargo_bike'
@@ -48,7 +45,7 @@ describe('Pricing rule parser', function() {
   it('should parse vehicle with extra space', function() {
     const expression = 'vehicle  == "cargo_bike"'
     const result = parsePricingRule(expression)
-    expect(result).to.deep.equal([{
+    expect(result).toEqual([{
       left: 'vehicle',
       operator: '==',
       right: 'cargo_bike'
@@ -58,7 +55,7 @@ describe('Pricing rule parser', function() {
   it('should parse distance with bounds', function() {
     const expression = 'distance in 0..3000'
     const result = parsePricingRule(expression)
-    expect(result).to.deep.equal([{
+    expect(result).toEqual([{
       left: 'distance',
       operator: 'in',
       right: [ 0, 3000 ]
@@ -68,7 +65,7 @@ describe('Pricing rule parser', function() {
   it('should parse distance with comparison', function() {
     const expression = 'distance < 3000'
     const result = parsePricingRule(expression)
-    expect(result).to.deep.equal([{
+    expect(result).toEqual([{
       left: 'distance',
       operator: '<',
       right: 3000
@@ -78,7 +75,7 @@ describe('Pricing rule parser', function() {
   it('should parse weight with bounds', function() {
     const expression = 'weight in 4000..6000'
     const result = parsePricingRule(expression)
-    expect(result).to.deep.equal([{
+    expect(result).toEqual([{
       left: 'weight',
       operator: 'in',
       right: [ 4000, 6000 ]
@@ -88,7 +85,7 @@ describe('Pricing rule parser', function() {
   it('should parse weight with comparison', function() {
     const expression = 'weight > 4000'
     const result = parsePricingRule(expression)
-    expect(result).to.deep.equal([{
+    expect(result).toEqual([{
       left: 'weight',
       operator: '>',
       right: 4000
@@ -97,7 +94,7 @@ describe('Pricing rule parser', function() {
 
   it('should return empty array', function() {
     const result = parsePricingRule('')
-    expect(result).to.deep.equal([])
+    expect(result).toEqual([])
   })
 
 })
