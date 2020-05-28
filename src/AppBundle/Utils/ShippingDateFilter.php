80fd47b98bcc9866daf7fd6217e6ca81766640f9
Alexandre Segura <mex.zktk@gmail.com>
2020-05-28 12:01:47 +0200

    More debug.

--- src/AppBundle/Utils/ShippingDateFilter.php
+++ src/AppBundle/Utils/ShippingDateFilter.php
@@ -5,15 +5,18 @@ namespace AppBundle\Utils;
 use AppBundle\Sylius\Order\OrderInterface;
 use AppBundle\Utils\TimeRange;
 use Carbon\Carbon;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 class ShippingDateFilter
 {
     private $preparationTimeResolver;
     private $openingHoursCache = [];
 
-    public function __construct(PreparationTimeResolver $preparationTimeResolver)
+    public function __construct(PreparationTimeResolver $preparationTimeResolver, LoggerInterface $logger = null)
     {
         $this->preparationTimeResolver = $preparationTimeResolver;
+        $this->logger = $logger ?? new NullLogger();
     }
 
     /**
@@ -30,12 +33,22 @@ class ShippingDateFilter
 
         // Obviously, we can't ship in the past
         if ($dropoff <= $now) {
+
+            $this->logger->info(sprintf('ShippingDateFilter::accept() - date "%s" is in the past',
+                $dropoff->format(\DateTime::ATOM))
+            );
+
             return false;
         }
 
         $preparation = $this->preparationTimeResolver->resolve($order, $dropoff);
 
         if ($preparation <= $now) {
+
+            $this->logger->info(sprintf('ShippingDateFilter::accept() - preparation time "%s" is in the past',
+                $preparation->format(\DateTime::ATOM))
+            );
+
             return false;
         }
 
@@ -45,10 +58,20 @@ class ShippingDateFilter
         $openingHours = $restaurant->getOpeningHours($fulfillmentMethod);
 
         if ($restaurant->hasClosingRuleForNow($preparation)) {
+
+            $this->logger->info(sprintf('ShippingDateFilter::accept() - there is a closing rule for "%s"',
+                $preparation->format(\DateTime::ATOM))
+            );
+
             return false;
         }
 
         if (!$this->isOpen($openingHours, $preparation)) {
+
+            $this->logger->info(sprintf('ShippingDateFilter::accept() - closed at "%s"',
+                $preparation->format(\DateTime::ATOM))
+            );
+
             return false;
         }
 
