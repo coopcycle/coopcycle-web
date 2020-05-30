c77a03a4fd0eafabc09f39a63653f47bcc3a3d2f
Alexandre Segura <mex.zktk@gmail.com>
2020-05-30 13:19:44 +0200

    Add billing address prop.

--- src/AppBundle/Entity/Sylius/OrderReceipt.php
+++ src/AppBundle/Entity/Sylius/OrderReceipt.php
@@ -4,5 +4,25 @@ namespace AppBundle\Entity\Sylius;
 
 class OrderReceipt extends FrozenOrder
 {
+    protected $billingAddress;
 
+    /**
+     * @return mixed
+     */
+    public function getBillingAddress()
+    {
+        return $this->billingAddress;
+    }
+
+    /**
+     * @param mixed $billingAddress
+     *
+     * @return self
+     */
+    public function setBillingAddress($billingAddress)
+    {
+        $this->billingAddress = $billingAddress;
+
+        return $this;
+    }
 }
