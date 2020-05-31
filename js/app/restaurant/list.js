9dc8ebcd4d70b566157adaf54b3bffdb65dda6e3
Alexandre Segura <mex.zktk@gmail.com>
2020-05-31 09:17:22 +0200

    Fix badge when there is only one method.

--- js/app/restaurant/list.js
+++ js/app/restaurant/list.js
@@ -3,12 +3,12 @@ import { render } from 'react-dom'
 
 import { asText } from '../components/ShippingTimeRange'
 
-const FulfillmentBadge = ({ delivery }) => {
+const FulfillmentBadge = ({ range }) => {
 
   return (
     <span className="restaurant-item__time-range">
       <i className="fa fa-clock-o mr-2"></i>
-      <span>{ asText(delivery.range) }</span>
+      <span>{ asText(range) }</span>
     </span>
   )
 }
@@ -16,14 +16,17 @@ const FulfillmentBadge = ({ delivery }) => {
 document.querySelectorAll('[data-fulfillment]').forEach(el => {
   $.getJSON(el.dataset.fulfillment).then(data => {
 
-    if (!data.delivery) {
+    if (!data.delivery && !data.collection) {
       return
     }
 
-    if (!data.delivery.range) {
+    if (data.delivery && data.delivery.range) {
+      render(<FulfillmentBadge range={ data.delivery.range } />, el)
       return
     }
 
-    render(<FulfillmentBadge { ...data } />, el)
+    if (data.collection && data.collection.range) {
+      render(<FulfillmentBadge range={ data.collection.range } />, el)
+    }
   })
 })
