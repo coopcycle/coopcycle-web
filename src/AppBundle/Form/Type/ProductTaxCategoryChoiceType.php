66ad60d7d19c20852f630b9041e694f4ebeac53b
Alexandre Segura <mex.zktk@gmail.com>
2020-06-11 10:54:14 +0200

    Translate categories.

--- src/AppBundle/Form/Type/ProductTaxCategoryChoiceType.php
+++ src/AppBundle/Form/Type/ProductTaxCategoryChoiceType.php
@@ -2,21 +2,25 @@
 
 namespace AppBundle\Form\Type;
 
+use AppBundle\Entity\Sylius\TaxCategory;
 use Sylius\Bundle\TaxationBundle\Form\Type\TaxCategoryChoiceType as BaseTaxCategoryChoiceType;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ProductTaxCategoryChoiceType extends AbstractType
 {
     /** @var RepositoryInterface */
     private $taxCategoryRepository;
+    private $translator;
     private $country;
 
-    public function __construct(RepositoryInterface $taxCategoryRepository, string $country)
+    public function __construct(RepositoryInterface $taxCategoryRepository, TranslatorInterface $translator, string $country)
     {
         $this->taxCategoryRepository = $taxCategoryRepository;
+        $this->translator = $translator;
         $this->country = $country;
     }
 
@@ -25,13 +29,20 @@ class ProductTaxCategoryChoiceType extends AbstractType
         $resolver->setDefault('choices', function (Options $options) {
 
             $qb = $this->taxCategoryRepository->createQueryBuilder('c');
-            $qb->add('where', $qb->expr()->notIn('c.code', ['SERVICE', 'SERVICE_TAX_EXEMPT']));
+            $qb->andWhere($qb->expr()->notIn('c.code', ['SERVICE', 'SERVICE_TAX_EXEMPT']));
 
             if ('ca' === $this->country) {
-                $qb->add('where', $qb->expr()->in('c.code', ['FOOD', 'DRINK_ALCOHOL', 'JEWELRY']));
+                $qb->andWhere($qb->expr()->in('c.code', ['FOOD', 'DRINK_ALCOHOL', 'JEWELRY']));
+            } else {
+                $qb->andWhere($qb->expr()->notIn('c.code', ['FOOD', 'DRINK_ALCOHOL', 'JEWELRY']));
             }
 
             return $qb->getQuery()->getResult();
+        });
+
+        $resolver->setDefault('choice_label', function (?TaxCategory $taxCategory) {
+
+            return $this->translator->trans($taxCategory->getName(), [], 'taxation');
         });
     }
 
