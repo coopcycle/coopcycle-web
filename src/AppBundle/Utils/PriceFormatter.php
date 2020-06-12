0c546f29ff59120791258d320663751800b6d98c
Alexandre Segura <mex.zktk@gmail.com>
2020-06-13 01:03:11 +0200

    Format price using Twig Intl.

--- src/AppBundle/Utils/PriceFormatter.php
+++ src/AppBundle/Utils/PriceFormatter.php
@@ -4,18 +4,22 @@ namespace AppBundle\Utils;
 
 use Sylius\Bundle\CurrencyBundle\Templating\Helper\CurrencyHelperInterface;
 use Sylius\Component\Currency\Context\CurrencyContextInterface;
+use Twig\Extra\Intl\IntlExtension;
 
 class PriceFormatter
 {
     private $currencyContext;
     private $currencyHelper;
+    private $intl;
 
     public function __construct(
         CurrencyContextInterface $currencyContext,
-        CurrencyHelperInterface $currencyHelper)
+        CurrencyHelperInterface $currencyHelper,
+        IntlExtension $intl)
     {
         $this->currencyContext = $currencyContext;
         $this->currencyHelper = $currencyHelper;
+        $this->intl = $intl;
     }
 
     public function format(int $price)
@@ -25,10 +29,9 @@ class PriceFormatter
 
     public function formatWithSymbol(int $price)
     {
-        $currencySymbol = $this->currencyHelper->convertCurrencyCodeToSymbol(
-            $this->currencyContext->getCurrencyCode()
+        return $this->intl->formatCurrency(
+            ($price / 100),
+            strtoupper($this->currencyContext->getCurrencyCode())
         );
-
-        return $this->format($price).' '.$currencySymbol;
     }
 }
