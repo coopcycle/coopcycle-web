bf77823e21b117046bb36fb3a27dfd4c11360ad7
Alexandre Segura <mex.zktk@gmail.com>
2020-06-21 07:31:41 +0200

    Use Kernel::getProjectDir().

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -78,23 +78,13 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
-    public function getRootDir()
+    public function getProjectDir(): string
     {
-        return __DIR__;
-    }
-
-    public function getCacheDir()
-    {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
-    }
-
-    public function getLogDir()
-    {
-        return dirname(__DIR__).'/var/logs';
+        return dirname(__DIR__);
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getProjectDir().'/app/config/config_'.$this->getEnvironment().'.yml');
     }
 }
