2f9283b53eb8bf8f5f347217c900b4f8eaa51758
Alexandre Segura <mex.zktk@gmail.com>
2020-06-21 11:13:48 +0200

    Remove WebServerBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -72,7 +72,6 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
         }
 
         return $bundles;
