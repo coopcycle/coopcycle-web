16c5215d3d6ee8cf702b1d29ddfc879e6e51f954
Alexandre Segura <mex.zktk@gmail.com>
2020-07-28 17:37:58 +0200

    Use the "Email" message.

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace AppBundle\Controller;
 
 use AppBundle\Form\Company\RegistrationType;
+use AppBundle\Message\Email;
+use AppBundle\Service\EmailManager;
+use AppBundle\Service\SettingsManager;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,6 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 
 /**
@@ -37,11 +41,17 @@ class CompanyController
         MessageBusInterface $bus,
         FormFactoryInterface $formFactory,
         Environment $twig,
+        TranslatorInterface $translator,
+        SettingsManager $settingsManager,
+        EmailManager $emailManager,
         RouterInterface $router
     ) {
         $this->bus = $bus;
         $this->formFactory = $formFactory;
         $this->twig = $twig;
+        $this->translator = $translator;
+        $this->settingsManager = $settingsManager;
+        $this->emailManager = $emailManager;
         $this->router = $router;
     }
 
@@ -55,7 +65,19 @@ class CompanyController
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
 
             $companyRegistration = $form->getData();
-            $this->bus->dispatch($companyRegistration);
+            // $this->bus->dispatch($companyRegistration);
+
+            $message = $this->emailManager->createHtmlMessage(
+                $this->translator->trans('registration.company', [], 'emails'),
+                $this->twig->render('emails/company/request_registration.mjml.twig', [
+                    'registration' => $companyRegistration
+                ])
+            );
+
+            $this->bus->dispatch(new Email(
+                $message,
+                $this->settingsManager->get('administrator_email')
+            ));
 
             return new RedirectResponse($this->router->generate('company_registration'));
         }
