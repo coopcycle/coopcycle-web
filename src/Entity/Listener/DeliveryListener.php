b6cd6825abf055c49d0aa2e50c2d9e1778aac4f7
Alexandre Segura <mex.zktk@gmail.com>
2020-08-10 15:40:02 +0200

    Inherit tags from Store. Fixes #1097.

---
+++ src/Entity/Listener/DeliveryListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Entity\Listener;
+
+use AppBundle\Entity\Delivery;
+use AppBundle\Service\TagManager;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+class DeliveryListener
+{
+    public function prePersist(Delivery $delivery, LifecycleEventArgs $args)
+    {
+        $store = $delivery->getStore();
+
+        if (null === $store) {
+            return;
+        }
+
+        $tags = $store->getTags();
+
+        foreach ($delivery->getTasks() as $task) {
+            $task->setTags($tags);
+        }
+    }
+}
