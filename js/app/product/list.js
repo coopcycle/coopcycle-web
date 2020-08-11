0eefd220917555b68d6a0553691e8d02aaaa8338
Alexandre Segura <mex.zktk@gmail.com>
2020-08-11 14:27:06 +0200

    Allow toggling products from list. Fixes #1507.

--- js/app/product/list.js
+++ js/app/product/list.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import axios from 'axios'
 import { render, unmountComponentAtNode } from 'react-dom'
+import { Switch } from 'antd'
 
 import SpreadsheetDropzone from '../components/SpreadsheetDropzone'
+
+const baseURL = location.protocol + '//' + location.hostname
 
 $('#products-import-modal').on('show.bs.modal', function () {
 
@@ -21,4 +25,38 @@ $('#products-import-modal').on('show.bs.modal', function () {
 $('#products-import-modal').on('hidden.bs.modal', function () {
   const el = this.querySelector('[data-spreadsheet-dropzone]')
   unmountComponentAtNode(el)
+})
+
+document.querySelectorAll('table[data-entity="product"] tbody tr').forEach(row => {
+
+  const cell = row.querySelector('[data-cell="toggle"]')
+  cell.innerHTML = ''
+
+  const enabled = JSON.parse(row.dataset.enabled)
+  const iri = row.dataset.iri
+
+  const container = document.createElement('div')
+  cell.appendChild(container)
+
+  render(
+    <Switch
+      size="small"
+      onChange={ checked => {
+        $.getJSON(window.Routing.generate('profile_jwt')).then(token => {
+          axios({
+            method: 'put',
+            url: baseURL + iri,
+            data: {
+              enabled: checked
+            },
+            headers: {
+              'Accept': 'application/ld+json',
+              'Content-Type': 'application/ld+json',
+              Authorization: `Bearer ${token}`
+            }
+          })
+        })
+      }}
+      defaultChecked={ enabled }
+    />, container)
 })
