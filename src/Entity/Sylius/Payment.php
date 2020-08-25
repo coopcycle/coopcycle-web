b5b2b5177fc1e8422d6a634734ff3ce37fc2cd25
Alexandre Segura <mex.zktk@gmail.com>
2020-08-25 12:42:07 +0200

    Introduce Refund entity.

--- src/Entity/Sylius/Payment.php
+++ src/Entity/Sylius/Payment.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity\Sylius;
 
 use AppBundle\Sylius\Payment\MercadopagoTrait;
 use AppBundle\Sylius\Payment\StripeTrait;
+use Doctrine\Common\Collections\ArrayCollection;
 use Sylius\Component\Order\Model\OrderInterface;
 use Sylius\Component\Order\Model\OrderAwareInterface;
 use Sylius\Component\Payment\Model\Payment as BasePayment;
@@ -14,6 +15,14 @@ class Payment extends BasePayment implements OrderAwareInterface
     use MercadopagoTrait;
 
     protected $order;
+    protected $refunds;
+
+    public function __construct()
+    {
+        $this->refunds = new ArrayCollection();
+
+        parent::__construct();
+    }
 
     public function getOrder(): ?OrderInterface
     {
