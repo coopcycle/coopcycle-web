1042bbcaec00155601cbad02d79263acd7b0a2e2
Alexandre Segura <mex.zktk@gmail.com>
2020-09-08 12:32:02 +0200

    Cleanup session when user is disconnected. Fixes #1715.

---
+++ src/EventListener/DeauthenticatedListener.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Http\Event\DeauthenticatedEvent;
+
+class DeauthenticatedListener
+{
+    private $session;
+    private $sessionKeyName;
+
+    public function __construct(SessionInterface $session, string $sessionKeyName)
+    {
+        $this->session = $session;
+        $this->sessionKeyName = $sessionKeyName;
+    }
+
+    public function logoutOnChange(DeauthenticatedEvent $event)
+    {
+        $this->session->remove('restaurantId');
+        $this->session->remove($this->sessionKeyName);
+    }
+}
