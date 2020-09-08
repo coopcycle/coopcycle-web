42d9a25c0bce317154a6327c03d88a0562afa538
Alexandre Segura <mex.zktk@gmail.com>
2020-09-08 15:13:32 +0200

    Add Cypress test for checkout.

--- cypress/integration/checkout.spec.js
+++ cypress/integration/checkout.spec.js
@@ -210,4 +210,84 @@ context('Checkout', () => {
     cy.get('.cart .address-autosuggest__container input[type="search"]')
       .should('have.value', '91 Rue de Rivoli, Paris, France')
   })
+
+  it('order something at restaurant with existing address (via modal)', () => {
+
+    cy.server()
+    cy.route('POST', '/fr/restaurant/*-crazy-hamburger').as('postRestaurant')
+    cy.route('POST', '/fr/restaurant/*/cart/product/*').as('postProduct')
+
+    cy.visit('/login')
+
+    cy.get('[name="_username"]').type('jane')
+    cy.get('[name="_password"]').type('12345678')
+    cy.get('[name="_submit"]').click()
+
+    cy.contains('Crazy Hamburger').click()
+
+    cy.location('pathname').should('match', /\/fr\/restaurant\/[0-9]+-crazy-hamburger/)
+
+    cy.wait('@postRestaurant')
+
+    cy.contains('Cheeseburger').click()
+
+    cy.get('#CHEESEBURGER-options')
+      .should('be.visible')
+
+    // Make sure to use a precise selector, because 2 products have same options
+    cy.get('#CHEESEBURGER-options input[value="HAMBURGER_ACCOMPANIMENT_FRENCH_FRIES"]')
+      .check()
+    cy.get('#CHEESEBURGER-options input[value="HAMBURGER_DRINK_COLA"]')
+      .check()
+
+    cy.get('#CHEESEBURGER-options input[value="HAMBURGER_ACCOMPANIMENT_FRENCH_FRIES"]').should('be.checked')
+    cy.get('#CHEESEBURGER-options input[value="HAMBURGER_DRINK_COLA"]').should('be.checked')
+
+    cy.get('#CHEESEBURGER-options form [data-direction="up"]').click()
+    cy.get('#CHEESEBURGER-options form [data-direction="up"]').click()
+
+    cy.get('#CHEESEBURGER-options button[type="submit"]')
+      .should('not.be.disabled')
+      .click()
+
+    cy.wait('@postProduct')
+
+    cy.get('.cart__items').invoke('text').should('match', /Cheeseburger/)
+
+    cy.get('.ReactModal__Content--enter-address')
+      .should('be.visible')
+
+    cy.get('.ReactModal__Content--enter-address input[type="search"]')
+      .type('1 rue de', { timeout: 5000, delay: 30 })
+
+    cy.get('.ReactModal__Content--enter-address')
+      .find('.react-autosuggest__suggestions-container')
+      .find('.react-autosuggest__section-container')
+      // There should be 2 sections
+      .then(($sections) => {
+        cy.wrap($sections).should('have.length', 2)
+      })
+      // The first section should contain saved addresses
+      .then(($sections) => {
+        cy.wrap($sections)
+          .eq(0)
+          .find('.react-autosuggest__section-title')
+          .invoke('text')
+          .should('eq', 'Adresses sauvegardées')
+      })
+
+    // Click on the first suggestion
+    cy.get('.ReactModal__Content--enter-address')
+      .find('.react-autosuggest__suggestions-container')
+      .find('.react-autosuggest__section-container')
+      .eq(0)
+      .contains('1, Rue de Rivoli, Paris, France')
+      .click()
+
+    cy.wait('@postRestaurant')
+
+    cy.get('form[name="cart"]').submit()
+
+    cy.location('pathname').should('eq', '/order/')
+  })
 })
