81776d4a26920367977f4ef1cd040bb70ddb004e
Alexandre Segura <mex.zktk@gmail.com>
2020-09-25 19:39:20 +0200

    Rebuild address object from HTML elements.

--- js/app/common.js
+++ js/app/common.js
@@ -91,11 +91,36 @@ document.addEventListener('DOMContentLoaded', function() {
       }
 
       inputs.forEach(el => {
+
+        // Try to build an address object
+        let address = {
+          streetAddress: el.value
+        }
+        for (const addressProp in addressElements) {
+          const addressEl = document.getElementById(
+            el.getAttribute('id').replace(/([aA])ddress_streetAddress/, addressElements[addressProp])
+          )
+          if (addressEl) {
+            address = {
+              ...address,
+              [addressProp]: addressEl.value
+            }
+          }
+        }
+
+        address = {
+          ...address,
+          geo: {
+            latitude: address.latitude,
+            longitude: address.longitude,
+          }
+        }
+
         new AddressAutosuggest(
           el.closest('.form-group'),
           {
             required: true,
-            address: el.value,
+            address,
             inputId: el.getAttribute('id'),
             inputName: el.getAttribute('name'),
             onAddressSelected: (text, address) => {
