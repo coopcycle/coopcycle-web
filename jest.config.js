cc1454327113c446b0db5d98669cbe8e784ffec7
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2020-10-01 12:44:04 +0200

    fix jest tests

--- jest.config.js
+++ jest.config.js
@@ -163,9 +163,10 @@ module.exports = {
   // transform: null,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  transformIgnorePatterns: [
+    // "node_modules/(?!(react-native|my-project|react-native-button)/)"
+    "node_modules/(?!(coopcycle-frontend-js)/)"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
