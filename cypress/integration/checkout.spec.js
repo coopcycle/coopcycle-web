f9cf87c6251b3e1782853c6490952c9b5bda66f4
Alexandre Segura <mex.zktk@gmail.com>
2020-10-06 13:04:13 +0200

    Add Cypress test for tips.

--- cypress/integration/checkout.spec.js
+++ cypress/integration/checkout.spec.js
@@ -367,4 +367,88 @@ context('Checkout', () => {
       .invoke('trim')
       .should('match', /^21,00/)
   })
+
+  it('order something at restaurant with a tip', () => {
+
+    cy.server()
+    cy.route('POST', '/fr/restaurant/*-crazy-hamburger').as('postRestaurant')
+    cy.route('POST', '/fr/restaurant/*/cart/product/*').as('postProduct')
+    cy.route('POST', '/order/').as('postOrder')
+
+    cy.visit('/fr/')
+
+    cy.contains('Crazy Hamburger').click()
+
+    cy.location('pathname').should('match', /\/fr\/restaurant\/[0-9]+-crazy-hamburger/)
+
+    cy.wait('@postRestaurant')
+
+    cy.contains('Cheeseburger').click()
+
+    cy.get('#CHEESEBURGER-options')
+      .should('be.visible')
+
+    // Make sure to use a precise selector, because 2 products have same options
+    cy.get('#CHEESEBURGER-options input[value="HAMBURGER_ACCOMPANIMENT_FRENCH_FRIES"]')
+      .check()
+    cy.get('#CHEESEBURGER-options input[value="HAMBURGER_DRINK_COLA"]')
+      .check()
+
+    cy.get('#CHEESEBURGER-options input[value="HAMBURGER_ACCOMPANIMENT_FRENCH_FRIES"]').should('be.checked')
+    cy.get('#CHEESEBURGER-options input[value="HAMBURGER_DRINK_COLA"]').should('be.checked')
+
+    cy.get('#CHEESEBURGER-options button[type="submit"]')
+      .should('not.be.disabled')
+      .click()
+
+    cy.wait('@postProduct')
+
+    cy.get('.cart__items').invoke('text').should('match', /Cheeseburger/)
+
+    cy.get('.ReactModal__Content--enter-address')
+      .should('be.visible')
+
+    cy.get('.ReactModal__Content--enter-address input[type="search"]')
+      .type('91 rue de rivoli paris', { timeout: 5000, delay: 30 })
+
+    cy.get('.ReactModal__Content--enter-address')
+      .find('ul[role="listbox"] li', { timeout: 5000 })
+      .contains('91 Rue de Rivoli, Paris, France')
+      .click()
+
+    cy.wait('@postRestaurant')
+
+    cy.get('.cart .address-autosuggest__container input[type="search"]')
+      .should('have.value', '91 Rue de Rivoli, Paris, France')
+
+    cy.contains('Cheese Cake').click()
+
+    cy.wait('@postProduct')
+
+    cy.get('.cart__items').invoke('text').should('match', /Cheese Cake/)
+
+    // FIXME Use click instead of submit
+    cy.get('form[name="cart"]').submit()
+
+    cy.location('pathname').should('eq', '/login')
+
+    cy.get('[name="_username"]').type('bob')
+    cy.get('[name="_password"]').type('12345678')
+    cy.get('[name="_submit"]').click()
+
+    cy.location('pathname').should('eq', '/order/')
+
+    cy.get('.table-order-items tfoot tr:last-child td ')
+      .invoke('text')
+      .invoke('trim')
+      .should('match', /^20,00/)
+
+    cy.get('#tip-incr').click()
+    cy.wait('@postOrder')
+
+    cy.get('.table-order-items tfoot tr:last-child td ')
+      .invoke('text')
+      .invoke('trim')
+      .should('match', /^21,00/)
+  })
 })
