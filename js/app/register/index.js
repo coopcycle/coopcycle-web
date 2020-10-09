d8385d922b8d4e79a8a53007cdd6f70c5e772877
Alexandre Segura <mex.zktk@gmail.com>
2020-10-09 18:31:44 +0200

    Show suggestions.

--- js/app/register/index.js
+++ js/app/register/index.js
@@ -19,11 +19,20 @@ const checkUsername = _.debounce(function() {
   let feedbackEl =
     formGroup.querySelector('.form-control-feedback')
 
+  let suggestionsEl =
+    formGroup.querySelector('.username-suggestions')
+
   if (!feedbackEl) {
     feedbackEl = document.createElement('span')
     feedbackEl.classList.add('fa', 'form-control-feedback')
     feedbackEl.setAttribute('aria-hidden', 'true')
     usernameInput.parentNode.insertBefore(feedbackEl, usernameInput.nextSibling)
+  }
+
+  if (!suggestionsEl) {
+    suggestionsEl = document.createElement('div')
+    suggestionsEl.classList.add('help-block', 'username-suggestions')
+    formGroup.insertBefore(suggestionsEl, formGroup.querySelector('.help-block'))
   }
 
   feedbackEl.classList.remove('fa-check', 'fa-warning')
@@ -38,11 +47,37 @@ const checkUsername = _.debounce(function() {
     usernameInput.setAttribute('disabled', false)
     usernameInput.removeAttribute('disabled')
 
-    formGroup
-      .classList
+    formGroup.classList
       .add(result.exists ? 'has-error' : 'has-success')
 
     feedbackEl.classList.add(result.exists ? 'fa-warning' : 'fa-check')
+
+    suggestionsEl.textContent = '';
+
+    const titleEl = document.createElement('strong')
+    titleEl.innerHTML = 'Suggestions'
+    titleEl.classList.add('mr-2')
+    suggestionsEl.appendChild(titleEl)
+
+    result.suggestions.forEach(suggestion => {
+
+      const suggestionEl = document.createElement('a')
+
+      suggestionEl.setAttribute('href', '#')
+      suggestionEl.classList.add('text-monospace')
+      suggestionEl.innerHTML = suggestion
+      suggestionEl.addEventListener('click', (e) => {
+        e.preventDefault()
+        usernameInput.value = e.currentTarget.textContent
+        feedbackEl.classList.remove('fa-warning')
+        feedbackEl.classList.add('fa-check')
+        formGroup.classList.remove('has-error')
+        formGroup.classList.add('has-success')
+        suggestionsEl.textContent = '';
+      }, false)
+
+      suggestionsEl.appendChild(suggestionEl)
+    })
 
   })
 
