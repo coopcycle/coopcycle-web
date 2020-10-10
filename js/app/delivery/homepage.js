128ba1d4500f847354b5c32e37e7b3f3faea063c
Alexandre Segura <mex.zktk@gmail.com>
2020-10-10 14:55:16 +0200

    Fix search box on homepage.

--- js/app/delivery/homepage.js
+++ js/app/delivery/homepage.js
@@ -1,19 +1,23 @@
 import AddressBook from './AddressBook'
 
-let prevInitMap
-if (window.initMap && typeof window.initMap === 'function') {
-  prevInitMap = window.initMap
-}
+const el = document.querySelector(`#delivery_dropoff_address`)
 
-window.initMap = function() {
+if (el) {
+  let prevInitMap
+  if (window.initMap && typeof window.initMap === 'function') {
+    prevInitMap = window.initMap
+  }
 
-  new AddressBook(document.querySelector(`#delivery_dropoff_address`), {
-    existingAddressControl: document.querySelector(`#delivery_dropoff_address_existingAddress`),
-    newAddressControl: document.querySelector(`#delivery_dropoff_address_newAddress_streetAddress`),
-    isNewAddressControl: document.querySelector(`#delivery_dropoff_address_isNewAddress`),
-  })
+  window.initMap = function() {
 
-  if (prevInitMap) {
-    prevInitMap()
+    new AddressBook(el, {
+      existingAddressControl: document.querySelector(`#delivery_dropoff_address_existingAddress`),
+      newAddressControl: document.querySelector(`#delivery_dropoff_address_newAddress_streetAddress`),
+      isNewAddressControl: document.querySelector(`#delivery_dropoff_address_isNewAddress`),
+    })
+
+    if (prevInitMap) {
+      prevInitMap()
+    }
   }
 }
