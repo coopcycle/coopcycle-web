b7b008700f25387ed7a4a78e33ca9d9e519bb065
Alexandre Segura <mex.zktk@gmail.com>
2020-10-14 11:17:39 +0200

    Add prop to toggle sms.

---
+++ app/DoctrineMigrations/Version20200225180922.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200225180922 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE store ADD sms_enabled BOOLEAN DEFAULT NULL');
+        $this->addSql('UPDATE store SET sms_enabled = \'f\'');
+        $this->addSql('ALTER TABLE store ALTER COLUMN sms_enabled SET NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE store DROP sms_enabled');
+    }
+}
