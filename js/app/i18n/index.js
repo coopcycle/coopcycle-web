54cdc0cf77199aa63f43e8da64f8a640d8d54110
Alexandre Segura <mex.zktk@gmail.com>
2020-10-29 11:12:58 +0100

    Enable Catala in JS.

--- js/app/i18n/index.js
+++ js/app/i18n/index.js
@@ -11,6 +11,7 @@ import { initReactI18next } from 'react-i18next'
 import moment from 'moment'
 import 'moment-timezone'
 
+import ca from './locales/ca.json'
 import de from './locales/de.json'
 import en from './locales/en.json'
 import es from './locales/es.json'
@@ -50,7 +51,7 @@ i18next
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
-    resources: { de, en, fr, es, pl, "pt-BR": pt_BR },
+    resources: { ca, de, en, fr, es, pl, "pt-BR": pt_BR },
     ns: ['common'],
     defaultNS: 'common',
     debug: process.env.DEBUG
