c7a8d74d7846a42a038e255016bc056051b0e73d
andrewcunnin <acunningham@etsy.com>
2020-10-30 18:34:05 -0400

    add some documentation to Vehicle

--- src/Entity/Vehicle.php
+++ src/Entity/Vehicle.php
@@ -3,16 +3,20 @@
 namespace AppBundle\Entity;
 
 /**
- * a Vehicle represents a method of transportation with certain capacities for accomplishing tasks
- **/
+ * a Vehicle represents a method of transportation with certain capacities for accomplishing tasks.
+ * vehicles will have an address that they start from and end at. for instance, if a vehicle must start from homebase 1
+ * and needs to go to point A, point B, point C, and point D in some arbitrary order, and then it must return to
+ * homebase 2, start would be homebase 1 and end would be homebase 2.
+ */
 
 class Vehicle
 {
-    private $id;
 
-    private $start;
+    private $id; // an int representing a unique id for a vehicle
 
-    private $end;
+    private $start; // an address that this vehicle starts its route from
+
+    private $end; // an address that this vehicle needs to end at
 
     public function __construct(int $id, Address $start, Address $end)
     {
