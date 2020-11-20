e94bc21e2323931dec89c400bd3b7fe052545f63
Alexandre Segura <mex.zktk@gmail.com>
2020-11-20 10:00:59 +0100

    Add command to deduplicate addresses.

---
+++ src/Command/DeduplicateCustomerAddressesCommand.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Utils\GeoUtils;
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+Class DeduplicateCustomerAddressesCommand extends ContainerAwareCommand
+{
+    private $connection;
+    private $entityManager;
+
+    public function __construct(
+        Connection $connection,
+        EntityManagerInterface $entityManager)
+    {
+        $this->connection = $connection;
+        $this->entityManager = $entityManager;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('coopcycle:data:deduplicate-addresses')
+            ->setDescription('Deduplicates customer addresses.')
+            ->addOption(
+                'dry-run',
+                null,
+                InputOption::VALUE_NONE,
+                'Dry run'
+            )
+            ;
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new SymfonyStyle($input, $output);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $getCustomerEmail = $this->connection
+            ->prepare("SELECT email_canonical FROM sylius_customer WHERE id = :id");
+
+        $getCustomerAddresses = $this->connection
+            ->prepare("SELECT a.id, a.street_address, ST_AsText(a.geo) AS coords, a.name, a.description, a.telephone, a.contact_name, a.postal_code, a.address_locality FROM address a JOIN sylius_customer_address ca ON a.id = ca.address_id WHERE ca.customer_id = :customer_id");
+
+        $stmt = $this->connection
+            ->prepare("SELECT customer_id, COUNT(*) AS address_count FROM sylius_customer_address GROUP BY customer_id HAVING COUNT(*) > 1 ORDER BY COUNT(*) DESC");
+        $stmt->execute();
+
+        while ($customer = $stmt->fetch()) {
+
+            $getCustomerEmail->bindParam('id', $customer['customer_id']);
+            $getCustomerEmail->execute();
+
+            $email = $getCustomerEmail->fetchColumn();
+
+            $this->io->text(sprintf('Customer "%s" has %d addresses', $email, $customer['address_count']));
+
+            $getCustomerAddresses->bindParam('customer_id', $customer['customer_id']);
+            $getCustomerAddresses->execute();
+
+            $customerAddresses = [];
+            while ($address = $getCustomerAddresses->fetch()) {
+                $customerAddresses[$address['coords']][] = $address;
+                // $coords = GeoUtils::asGeoCoordinates($address['coords']);
+            }
+
+            $duplicates = [];
+            foreach ($customerAddresses as $coords => $addresses) {
+                if (count($addresses) > 1) {
+                    $duplicates[$coords] = $addresses;
+                }
+            }
+
+            if (count($duplicates) === 0) {
+                $this->io->text(sprintf('No duplicates found for customer "%s"', $email));
+                continue;
+            }
+
+            $tableData = [];
+            foreach ($duplicates as $coords => $addresses) {
+                foreach ($addresses as $addr) {
+                    $tableData[] = [
+                        $email,
+                        $addr['id'],
+                        $addr['street_address'],
+                        $addr['coords']
+                    ];
+                }
+            }
+
+            $this->io->table(
+                ['Email', 'ID', 'Street address', 'Coords'],
+                $tableData
+            );
+        }
+
+        return 0;
+    }
+}
