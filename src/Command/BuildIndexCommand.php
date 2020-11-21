15b6bf54f2d1dbe55649b5c040125ba373391a58
Alexandre Segura <mex.zktk@gmail.com>
2020-11-21 15:27:49 +0100

    Introduce TNTSearch, build index of restaurants. See #149.

---
+++ src/Command/BuildIndexCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Doctrine\DBAL\Connection;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TeamTNT\TNTSearch\TNTSearch;
+use TeamTNT\TNTSearch\Stemmer\PorterStemmer;
+
+Class BuildIndexCommand extends ContainerAwareCommand
+{
+    private $connection;
+    private $projectDir;
+
+    public function __construct(Connection $connection, string $projectDir)
+    {
+        $this->connection = $connection;
+        $this->projectDir = $projectDir;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('coopcycle:index:build')
+            ->setDescription('Build TNTSearch index.');
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new SymfonyStyle($input, $output);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $tnt = new TNTSearch;
+
+        $tntSearchDir = $this->projectDir . '/var/tntsearch';
+
+        if (!file_exists($tntSearchDir)) {
+            mkdir($tntSearchDir, 0755);
+        }
+
+        $tnt->loadConfig([
+            'driver'    => 'pgsql',
+            'host'      => $this->connection->getHost(),
+            'database'  => $this->connection->getDatabase(),
+            'username'  => $this->connection->getUsername(),
+            'password'  => $this->connection->getPassword(),
+            'storage'   => $tntSearchDir,
+            'stemmer'   => PorterStemmer::class // optional
+        ]);
+
+        $indexer = $tnt->createIndex('restaurants.index');
+        $indexer->query('SELECT id, name FROM restaurant;');
+        $indexer->run();
+
+        return 0;
+    }
+}
