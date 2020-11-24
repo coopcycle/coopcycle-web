53fac3d11c5211db2d99f51de096f05d554d8802
Alexandre Segura <mex.zktk@gmail.com>
2020-11-24 15:30:53 +0100

    Hotfix SessionSubscriber clearing carts of disabled restaurants.

--- src/EventListener/AjaxSessionCloseListener.php
+++ src/EventListener/AjaxSessionCloseListener.php
@@ -4,11 +4,16 @@ namespace AppBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
+/**
+ * @see https://tideways.com/profiler/blog/slow-ajax-requests-in-your-symfony-application-apply-this-simple-fix
+ */
 class AjaxSessionCloseListener
 {
     protected static $whitelist = [
         'profile_notifications',
         'profile_jwt',
+        'restaurant_fulfillment_timing',
+        'fos_js_routing_js',
     ];
 
     public function onKernelRequest(RequestEvent $event)
