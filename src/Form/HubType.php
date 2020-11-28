529753b1acad0dd5a7a8e7d6d4f60d351ba691fb
Alexandre Segura <mex.zktk@gmail.com>
2020-11-28 07:24:11 +0100

    Do not show advanced options for hub.

--- src/Form/HubType.php
+++ src/Form/HubType.php
@@ -39,7 +39,9 @@ class HubType extends AbstractType
 
         $builder
             ->add('name', TextType::class, ['label' => 'basics.name'])
-            ->add('contract', ContractType::class)
+            ->add('contract', ContractType::class, [
+                'with_advanced_options' => false,
+            ])
             ->add('restaurants', CollectionType::class, [
                 'entry_type' => EntityType::class,
                 'entry_options' => [
