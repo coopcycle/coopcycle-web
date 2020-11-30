08d4b0d4314242ebe84bdc95fbbb7f812d25ebc1
Alexandre Segura <mex.zktk@gmail.com>
2020-11-30 16:40:30 +0100

    Start PM2 in env mode based on APP_ENV var.

--- docker/nodejs/start.sh
+++ docker/nodejs/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 cd /srv/coopcycle
+
 npm install
-pm2-runtime pm2.config.js
+
+if [ "$APP_ENV" = "test" ]; then
+    pm2-runtime pm2.config.js --env=test
+else
+    pm2-runtime pm2.config.js
+fi
