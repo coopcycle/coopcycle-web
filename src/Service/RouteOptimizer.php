2ff65060c5affa877b5af7289fa35fa3e95ddb78
Alexandre Segura <mex.zktk@gmail.com>
2020-12-06 17:01:20 +0100

    Cleanup code.

--- src/Service/RouteOptimizer.php
+++ src/Service/RouteOptimizer.php
@@ -20,40 +20,40 @@ class RouteOptimizer
         $this->client = $client;
     }
 
-    public function optimize(RoutingProblem $routingProblem)
     /**
-    * return a list of tasks sorted into an optimal route as obtained from the vroom api
-    *
-    * @param RoutingProblem $routingProblem a set of jobs and vehicles to optimally dispatch
-    */
+     * return a list of tasks sorted into an optimal route as obtained from the vroom api
+     *
+     * @param RoutingProblem $routingProblem a set of jobs and vehicles to optimally dispatch
+     */
+    public function optimize(RoutingProblem $routingProblem)
     {
-         $response = $this->client->request('POST', '', [
+        // TODO Catch Exception
+        $response = $this->client->request('POST', '', [
             'headers' => ['Content-Type'=> 'application/json'],
             'body' => json_encode($this->normalizer->normalize($routingProblem)),
         ]);
 
         $tasks = $routingProblem->getTasks();
         $data = json_decode((string) $response->getBody(), true);
+
         $firstRoute = $data['routes'][0];
         array_shift($firstRoute['steps']);
         $jobIds = [];
         // extract task ids from steps
-        foreach($firstRoute['steps'] as $step){
-            if(array_key_exists('id', $step)){
+        foreach ($firstRoute['steps'] as $step) {
+            if (array_key_exists('id', $step)) {
                 $jobIds[] = $step['id'];
-                }
+            }
         }
 
         // sort tasks by ids in steps
         usort($tasks, function($a, $b) use($jobIds){
             $ka = array_search($a->getId(), $jobIds);
             $kb = array_search($b->getId(), $jobIds);
-            return ($ka-$kb);
+            return ($ka - $kb);
         });
 
         return $tasks;
     }
 
 }
-
-?>
