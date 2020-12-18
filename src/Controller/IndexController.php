e24d8467218a15cb6eb3811101ee6d624838f930
Alexandre Segura <mex.zktk@gmail.com>
2020-12-18 17:56:06 +0100

    Fix missing import.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -19,6 +19,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Cache\ItemInterface;
 
