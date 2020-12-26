fc5a56c0779bc2a27de2115aeeb40aebd37cc7d6
Alexandre Segura <mex.zktk@gmail.com>
2020-12-26 20:31:04 +0100

    Remove dead code.

--- src/Controller/Utils/AccessControlTrait.php
+++ src/Controller/Utils/AccessControlTrait.php
@@ -5,22 +5,18 @@ namespace AppBundle\Controller\Utils;
 use AppBundle\Entity\Delivery;
 use AppBundle\Entity\LocalBusiness;
 use AppBundle\Entity\Store;
-use AppBundle\Sylius\Order\OrderInterface;
 use AppBundle\Utils\AccessControl;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 trait AccessControlTrait
 {
+    /**
+     * @var Delivery|LocalBusiness|Store $object
+     */
     protected function accessControl($object)
     {
         if ($object instanceof Delivery) {
             if (!AccessControl::delivery($this->getUser(), $object)) {
-                throw new AccessDeniedHttpException();
-            }
-        }
-
-        if ($object instanceof OrderInterface) {
-            if (!AccessControl::order($this->getUser(), $object)) {
                 throw new AccessDeniedHttpException();
             }
         }
--- src/Utils/AccessControl.php
+++ src/Utils/AccessControl.php
@@ -5,10 +5,7 @@ namespace AppBundle\Utils;
 use AppBundle\Entity\Delivery;
 use AppBundle\Entity\LocalBusiness;
 use AppBundle\Entity\Store;
-use AppBundle\Sylius\Customer\CustomerInterface;
-use AppBundle\Sylius\Order\OrderInterface;
 use FOS\UserBundle\Model\UserInterface;
-use Webmozart\Assert\Assert;
 
 class AccessControl
 {
@@ -18,20 +15,6 @@ class AccessControl
         $ownsStore = null !== $delivery->getStore() && $user->ownsStore($delivery->getStore());
 
         return $isAdmin || $ownsStore;
-    }
-
-    public static function order(UserInterface $user, OrderInterface $order)
-    {
-        $isAdmin = $user->hasRole('ROLE_ADMIN');
-        $ownsRestaurant = $user->ownsRestaurant($order->getRestaurant());
-
-        $customer = $order->getCustomer();
-
-        Assert::isInstanceOf($customer, CustomerInterface::class);
-
-        $isCustomer = $customer->hasUser() && $user === $customer->getUser();
-
-        return $isAdmin || $ownsRestaurant || $isCustomer;
     }
 
     public static function store(UserInterface $user, Store $store)
