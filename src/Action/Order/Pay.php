9c44aeba3e24b19721a0bc80a2d62bbbb69442ae
Alexandre Segura <mex.zktk@gmail.com>
2020-12-28 12:30:18 +0100

    Handle Stripe Payment Intents on API.

--- src/Action/Order/Pay.php
+++ src/Action/Order/Pay.php
@@ -2,9 +2,15 @@
 
 namespace AppBundle\Action\Order;
 
+use AppBundle\Api\Dto\StripeOutput;
 use AppBundle\Entity\Sylius\Order;
 use AppBundle\Service\OrderManager;
-use Doctrine\Persistence\ManagerRegistry;
+use AppBundle\Service\StripeManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Stripe\Exception\ApiErrorException;
+use Sylius\Bundle\OrderBundle\NumberAssigner\OrderNumberAssignerInterface;
 use Sylius\Component\Payment\Model\PaymentInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -12,12 +18,22 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 class Pay
 {
     private $dataManager;
-    private $doctrine;
+    private $entityManager;
+    private $stripeManager;
+    private $orderNumberAssigner;
 
-    public function __construct(OrderManager $dataManager, ManagerRegistry $doctrine)
+    public function __construct(
+        OrderManager $dataManager,
+        EntityManagerInterface $entityManager,
+        StripeManager $stripeManager,
+        OrderNumberAssignerInterface $orderNumberAssigner,
+        LoggerInterface $logger = null)
     {
         $this->orderManager = $dataManager;
-        $this->doctrine = $doctrine;
+        $this->entityManager = $entityManager;
+        $this->stripeManager = $stripeManager;
+        $this->orderNumberAssigner = $orderNumberAssigner;
+        $this->logger = $logger ?? new NullLogger();
     }
 
     public function __invoke($data, Request $request)
@@ -28,19 +44,84 @@ class Pay
             $body = json_decode($content, true);
         }
 
-        if (!isset($body['stripeToken'])) {
+        if (!isset($body['stripeToken']) && !isset($body['paymentMethodId']) && !isset($body['paymentIntentId'])) {
             throw new BadRequestHttpException('Stripe token is missing');
         }
 
         $payment = $data->getLastPayment(PaymentInterface::STATE_CART);
 
-        $this->orderManager->checkout($data, $body['stripeToken']);
-        $this->doctrine->getManagerForClass(Order::class)->flush();
+        // Legacy
+        if (isset($body['stripeToken'])) {
 
-        if (PaymentInterface::STATE_FAILED === $payment->getState()) {
-            throw new BadRequestHttpException($payment->getLastError());
+            $this->orderManager->checkout($data, $body['stripeToken']);
+            $this->entityManager->flush();
+
+            if (PaymentInterface::STATE_FAILED === $payment->getState()) {
+                throw new BadRequestHttpException($payment->getLastError());
+            }
+
+            return $data;
         }
 
-        return $data;
+        if (isset($body['paymentIntentId'])) {
+
+            $this->orderManager->checkout($data, $body['paymentIntentId']);
+            $this->entityManager->flush();
+
+            if (PaymentInterface::STATE_FAILED === $payment->getState()) {
+                throw new BadRequestHttpException($payment->getLastError());
+            }
+
+            return $data;
+        }
+
+        // Assign order number now because it is needed for Stripe
+        $this->orderNumberAssigner->assignNumber($data);
+
+        $this->stripeManager->configure();
+
+        try {
+
+            $payment->setPaymentMethod($body['paymentMethodId']);
+
+            $intent = $this->stripeManager->createIntent($payment);
+            $payment->setPaymentIntent($intent);
+
+            $this->entityManager->flush();
+
+        } catch (ApiErrorException $e) {
+
+            throw new BadRequestHttpException($e->getMessage());
+        }
+
+        $response = new StripeOutput();
+
+        if ($payment->requiresUseStripeSDK()) {
+
+            $this->logger->info(
+                sprintf('Order #%d | Payment Intent requires action "%s"', $data->getId(), $payment->getPaymentIntentNextAction())
+            );
+
+            $response->requiresAction = true;
+            $response->paymentIntentClientSecret = $payment->getPaymentIntentClientSecret();
+
+        // When the status is "succeeded", it means we captured automatically
+        // When the status is "requires_capture", it means we separated authorization and capture
+        } elseif ('succeeded' === $payment->getPaymentIntentStatus() || $payment->requiresCapture()) {
+
+            $this->logger->info(
+                sprintf('Order #%d | Payment Intent status is "%s"', $data->getId(), $payment->getPaymentIntentStatus())
+            );
+
+            // The payment didn’t need any additional actions and completed!
+            // Handle post-payment fulfillment
+            $response->requiresAction = false;
+            $response->paymentIntentId = $payment->getPaymentIntent();
+
+        } else {
+            throw new BadRequestHttpException('Invalid PaymentIntent status');
+        }
+
+        return $response;
     }
 }
--- tests/AppBundle/Action/Order/PayTest.php
+++ tests/AppBundle/Action/Order/PayTest.php
@@ -5,9 +5,11 @@ namespace Tests\AppBundle\Action\Order;
 use AppBundle\Action\Order\Pay;
 use AppBundle\Entity\Sylius\Order;
 use AppBundle\Service\OrderManager;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use AppBundle\Service\StripeManager;
+use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
+use Sylius\Bundle\OrderBundle\NumberAssigner\OrderNumberAssignerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
@@ -19,14 +21,21 @@ class PayTest extends TestCase
     {
         $this->expectException(BadRequestHttpException::class);
 
-        $doctrine = $this->prophesize(ManagerRegistry::class);
+        $doctrine = $this->prophesize(EntityManagerInterface::class);
         $orderManager = $this->prophesize(OrderManager::class);
+        $stripeManager = $this->prophesize(StripeManager::class);
+        $ona = $this->prophesize(OrderNumberAssignerInterface::class);
 
         $order = new Order();
 
         $request = Request::create('/foo');
 
-        $pay = new Pay($orderManager->reveal(), $doctrine->reveal());
+        $pay = new Pay(
+            $orderManager->reveal(),
+            $doctrine->reveal(),
+            $stripeManager->reveal(),
+            $ona->reveal()
+        );
 
         $response = call_user_func_array($pay, [$order, $request]);
     }
