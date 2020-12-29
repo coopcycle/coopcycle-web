eeeeb00f1b6b4685d76715c7241ca59e5faf1ac7
Alexandre Segura <mex.zktk@gmail.com>
2020-12-29 23:30:00 +0100

    Set SameSite=None, Secure when embedded.

--- src/EventSubscriber/EmbedSubscriber.php
+++ src/EventSubscriber/EmbedSubscriber.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\EventSubscriber;
 
+use Symfony\Component\HttpFoundation\Session\Storage\SessionStorageInterface;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\EventListener\AbstractSessionListener;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -9,6 +12,38 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class EmbedSubscriber implements EventSubscriberInterface
 {
+    private $storage;
+    private $debug;
+
+    public function __construct(SessionStorageInterface $storage, bool $debug)
+    {
+        $this->storage = $storage;
+        $this->debug = $debug;
+    }
+
+    /**
+     * @see https://web.dev/samesite-cookies-explained/
+     * @see https://web.dev/samesite-cookie-recipes/
+     */
+    public function setCookieSameSiteNoneSecure(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+
+        if ($this->debug) {
+            return;
+        }
+
+        if ($request->query->has('embed')) {
+            // @see Symfony\Component\HttpKernel\EventListener\SessionListener
+            if ($this->storage instanceof NativeSessionStorage) {
+                $this->storage->setOptions([
+                    'cookie_samesite' => Cookie::SAMESITE_NONE,
+                    'cookie_secure' => true,
+                ]);
+            }
+        }
+    }
+
     public function onKernelRequest(RequestEvent $event)
     {
         $request = $event->getRequest();
@@ -25,7 +60,12 @@ class EmbedSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::REQUEST => 'onKernelRequest',
+            KernelEvents::REQUEST => [
+                // Run before Symfony\Component\HttpKernel\EventListener\SessionListener
+                ['setCookieSameSiteNoneSecure', 256],
+                // Run after Symfony\Component\HttpKernel\EventListener\SessionListener
+                ['onKernelRequest', 64],
+            ],
         ];
     }
 }
