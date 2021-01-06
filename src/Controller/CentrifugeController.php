790e6c2df8a0289c33f07084766afd722bbff83f
Alexandre Segura <mex.zktk@gmail.com>
2021-01-06 13:43:06 +0100

    Add endpoint for Centrifugo private channels.

--- src/Controller/CentrifugeController.php
+++ src/Controller/CentrifugeController.php
@@ -28,4 +28,54 @@ class CentrifugeController extends AbstractController
             'token' => $centrifugoClient->generateConnectionToken($user->getUsername(), (time() + 3600)),
         ]);
     }
+
+    /**
+     * @see https://centrifugal.github.io/centrifugo/server/private_channels/
+     * @see https://github.com/centrifugal/centrifuge-js#private-channels-subscription
+     *
+     * @Route("/centrifuge/subscribe", name="centrifuge_refresh", methods={"POST"})
+     */
+    public function subscribeAction(Request $request, CentrifugoClient $centrifugoClient)
+    {
+        $data = [];
+        $content = $request->getContent();
+        if (!empty($content)) {
+            $data = json_decode($content, true);
+        }
+
+        // {
+        //     "client": "<CLIENT ID>",
+        //     "channels": ["$chan1", "$chan2"]
+        // }
+
+        $response = [
+            'channels' => []
+        ];
+
+        $trackingChannel = sprintf('$%s_tracking', $this->getParameter('centrifugo_namespace'));
+
+        foreach ($data['channels'] as $channel) {
+            if ($channel === $trackingChannel && $this->isGranted('ROLE_ADMIN')) {
+                $response['channels'][] = [
+                    'channel' => $channel,
+                    'token' => $centrifugoClient->generatePrivateChannelToken($data['client'], $channel, (time() + 3600)),
+                ];
+            }
+        }
+
+        // {
+        //     "channels": [
+        //         {
+        //             "channel": "$chan1",
+        //             "token": "<SUBSCRIPTION JWT TOKEN>"
+        //         },
+        //         {
+        //             "channel": "$chan2",
+        //             "token": <SUBSCRIPTION JWT TOKEN>
+        //         }
+        //     ]
+        // }
+
+        return new JsonResponse($response);
+    }
 }
