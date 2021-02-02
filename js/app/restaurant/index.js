237f810ee7157db5f107c5d3be20e82e8362993b
Alexandre Segura <mex.zktk@gmail.com>
2021-02-02 14:09:15 +0100

    Revert "Rewrite product options modal using React Context API."
    This reverts commit 684e7709457514099546ff863b48ad6404705b3b.

--- js/app/restaurant/index.js
+++ js/app/restaurant/index.js
@@ -16,7 +16,6 @@ import Cart from '../cart/components/Cart'
 import { validateForm } from '../utils/address'
 import ProductOptionsModal from './components/ProductOptionsModal'
 import ProductImagesCarousel from './components/ProductImagesCarousel'
-import { ProductOptionsModalProvider } from './components/ProductOptionsModalContext'
 
 require('gasparesganga-jquery-loading-overlay')
 
@@ -55,32 +54,29 @@ const init = function() {
       JSON.parse(event.relatedTarget.dataset.productImages)
 
     render(
-      <ProductOptionsModalProvider
+      <ProductOptionsModal
+        price={ JSON.parse(event.relatedTarget.dataset.productPrice) }
+        code={ event.relatedTarget.dataset.productCode }
         options={ productOptions }
-        price={ JSON.parse(event.relatedTarget.dataset.productPrice) }>
-        <ProductOptionsModal
-          code={ event.relatedTarget.dataset.productCode }
-          options={ productOptions }
-          formAction={ event.relatedTarget.dataset.formAction }
-          images={ productImages }
-          onSubmit={ (e) => {
-            e.preventDefault()
+        formAction={ event.relatedTarget.dataset.formAction }
+        images={ productImages }
+        onSubmit={ (e) => {
+          e.preventDefault()
 
-            const $form = $modal.find('form')
+          const $form = $modal.find('form')
 
-            const data = $form.serializeArray()
-            const quantity = $form.find('[data-product-quantity]').val() || 1
+          const data = $form.serializeArray()
+          const quantity = $form.find('[data-product-quantity]').val() || 1
 
-            if (data.length > 0) {
-              store.dispatch(addItemWithOptions($form.attr('action'), data, quantity))
-            } else {
-              store.dispatch(addItem($form.attr('action'), quantity))
-            }
+          if (data.length > 0) {
+            store.dispatch(addItemWithOptions($form.attr('action'), data, quantity))
+          } else {
+            store.dispatch(addItem($form.attr('action'), quantity))
+          }
 
-            $modal.modal('hide')
+          $modal.modal('hide')
 
-          } } />
-      </ProductOptionsModalProvider>,
+        } } />,
       this.querySelector('.modal-body [data-options-container]')
     )
   })
