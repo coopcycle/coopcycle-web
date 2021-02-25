bb854bd136e276da715a4036f7b68fad88d12466
Alexandre Segura <mex.zktk@gmail.com>
2021-02-25 10:14:44 +0100

    Remove useless ConfigProvider.

--- js/app/dashboard/components/FiltersModalContent.js
+++ js/app/dashboard/components/FiltersModalContent.js
@@ -4,10 +4,9 @@ import { connect } from 'react-redux'
 import { withTranslation } from 'react-i18next'
 import { Form } from '@ant-design/compatible';
 import '@ant-design/compatible/assets/index.css';
-import { ConfigProvider, Slider, Switch } from 'antd';
+import { Slider, Switch } from 'antd';
 import { Formik } from 'formik'
 
-import { antdLocale } from '../../i18n'
 import TagsSelect from '../../components/TagsSelect'
 import Avatar from '../../components/Avatar'
 import {
@@ -66,106 +65,104 @@ class FiltersModalContent extends React.Component {
           handleSubmit,
           setFieldValue,
         }) => (
-          <ConfigProvider locale={ antdLocale }>
-            <form onSubmit={ handleSubmit } autoComplete="off" className="form-horizontal">
-              <ul className="nav nav-tabs" role="tablist">
-                <li role="presentation" className="active">
-                  <a href="#filters_general" aria-controls="filters_general" role="tab" data-toggle="tab">
-                    { this.props.t('ADMIN_DASHBOARD_FILTERS_TAB_GENERAL') }
-                  </a>
-                </li>
-                <li role="presentation">
-                  <a href="#filters_tags" aria-controls="filters_tags" role="tab" data-toggle="tab">
-                    { this.props.t('ADMIN_DASHBOARD_FILTERS_TAB_TAGS') }
-                  </a>
-                </li>
-                <li role="presentation">
-                  <a href="#filters_couriers" aria-controls="filters_couriers" role="tab" data-toggle="tab">
-                    { this.props.t('ADMIN_DASHBOARD_FILTERS_TAB_COURIERS') }
-                  </a>
-                </li>
-                <li role="presentation">
-                  <a href="#filters_timerange" aria-controls="filters_timerange" role="tab" data-toggle="tab">
-                    { this.props.t('ADMIN_DASHBOARD_FILTERS_TAB_TIMERANGE') }
-                  </a>
-                </li>
-              </ul>
-              <div className="tab-content">
-                <div role="tabpanel" className="tab-pane active" id="filters_general">
-                  <div className="dashboard__modal-filters__tabpane">
-                    <Form.Item label={ this.props.t('ADMIN_DASHBOARD_FILTERS_COMPLETED_TASKS') }
-                      labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} colon={ false }>
-                      <Switch
-                        checkedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_SHOW') }
-                        unCheckedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_HIDE') }
-                        defaultChecked={ values.showFinishedTasks }
-                        onChange={ (checked) => setFieldValue('showFinishedTasks', checked) } />
-                    </Form.Item>
-                    <Form.Item label={ this.props.t('ADMIN_DASHBOARD_FILTERS_CANCELLED_TASKS') }
-                      labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} colon={ false }>
-                      <Switch
-                        checkedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_SHOW') }
-                        unCheckedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_HIDE') }
-                        defaultChecked={ values.showCancelledTasks }
-                        onChange={ (checked) => setFieldValue('showCancelledTasks', checked) } />
-                    </Form.Item>
-                    <Form.Item label={ this.props.t('ADMIN_DASHBOARD_FILTERS_ALWAYS_SHOW_UNASSIGNED') }
-                      labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} colon={ false }>
-                      <Switch
-                        checkedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_SHOW') }
-                        unCheckedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_HIDE') }
-                        defaultChecked={ values.alwayShowUnassignedTasks }
-                        onChange={ (checked) => setFieldValue('alwayShowUnassignedTasks', checked) } />
-                    </Form.Item>
-                  </div>
-                </div>
-                <div role="tabpanel" className="tab-pane" id="filters_tags">
-                  <div className="dashboard__modal-filters__tabpane">
-                    <TagsSelect tags={ this.props.tags }
-                      defaultValue={ this.props.selectedTags }
-                      onChange={ tags => setFieldValue('tags', _.map(tags, tag => tag.slug)) } />
-                  </div>
-                </div>
-                <div role="tabpanel" className="tab-pane" id="filters_couriers">
-                  <div className="dashboard__modal-filters__tabpane my-4">
-                    { this.props.couriers.map(username => (
-                      <div className="dashboard__modal-filters__courier" key={ username }>
-                        <span>
-                          <Avatar username={ username } /> <span>{ username }</span>
-                        </span>
-                        <div>
-                          <Switch
-                            checkedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_SHOW') }
-                            unCheckedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_HIDE') }
-                            defaultChecked={ !isHidden(values.hiddenCouriers, username) }
-                            onChange={ checked => {
-                              if (checked) {
-                                setFieldValue('hiddenCouriers', _.filter(values.hiddenCouriers, u => u !== username))
-                              } else {
-                                setFieldValue('hiddenCouriers', values.hiddenCouriers.concat([ username ]))
-                              }
-                            }} />
-                        </div>
-                      </div>
-                    )) }
-                  </div>
-                </div>
-                <div role="tabpanel" className="tab-pane" id="filters_timerange">
-                  <div className="dashboard__modal-filters__tabpane mx-4">
-                    <Slider range
-                      marks={ timeSteps }
-                      defaultValue={ values.timeRange }
-                      max={ 24 }
-                      step={ null }
-                      onAfterChange={ value => setFieldValue('timeRange', value) } />
-                  </div>
+          <form onSubmit={ handleSubmit } autoComplete="off" className="form-horizontal">
+            <ul className="nav nav-tabs" role="tablist">
+              <li role="presentation" className="active">
+                <a href="#filters_general" aria-controls="filters_general" role="tab" data-toggle="tab">
+                  { this.props.t('ADMIN_DASHBOARD_FILTERS_TAB_GENERAL') }
+                </a>
+              </li>
+              <li role="presentation">
+                <a href="#filters_tags" aria-controls="filters_tags" role="tab" data-toggle="tab">
+                  { this.props.t('ADMIN_DASHBOARD_FILTERS_TAB_TAGS') }
+                </a>
+              </li>
+              <li role="presentation">
+                <a href="#filters_couriers" aria-controls="filters_couriers" role="tab" data-toggle="tab">
+                  { this.props.t('ADMIN_DASHBOARD_FILTERS_TAB_COURIERS') }
+                </a>
+              </li>
+              <li role="presentation">
+                <a href="#filters_timerange" aria-controls="filters_timerange" role="tab" data-toggle="tab">
+                  { this.props.t('ADMIN_DASHBOARD_FILTERS_TAB_TIMERANGE') }
+                </a>
+              </li>
+            </ul>
+            <div className="tab-content">
+              <div role="tabpanel" className="tab-pane active" id="filters_general">
+                <div className="dashboard__modal-filters__tabpane">
+                  <Form.Item label={ this.props.t('ADMIN_DASHBOARD_FILTERS_COMPLETED_TASKS') }
+                    labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} colon={ false }>
+                    <Switch
+                      checkedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_SHOW') }
+                      unCheckedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_HIDE') }
+                      defaultChecked={ values.showFinishedTasks }
+                      onChange={ (checked) => setFieldValue('showFinishedTasks', checked) } />
+                  </Form.Item>
+                  <Form.Item label={ this.props.t('ADMIN_DASHBOARD_FILTERS_CANCELLED_TASKS') }
+                    labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} colon={ false }>
+                    <Switch
+                      checkedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_SHOW') }
+                      unCheckedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_HIDE') }
+                      defaultChecked={ values.showCancelledTasks }
+                      onChange={ (checked) => setFieldValue('showCancelledTasks', checked) } />
+                  </Form.Item>
+                  <Form.Item label={ this.props.t('ADMIN_DASHBOARD_FILTERS_ALWAYS_SHOW_UNASSIGNED') }
+                    labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} colon={ false }>
+                    <Switch
+                      checkedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_SHOW') }
+                      unCheckedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_HIDE') }
+                      defaultChecked={ values.alwayShowUnassignedTasks }
+                      onChange={ (checked) => setFieldValue('alwayShowUnassignedTasks', checked) } />
+                  </Form.Item>
                 </div>
               </div>
-              <button type="submit" className="btn btn-block btn-primary">
-                { this.props.t('ADMIN_DASHBOARD_FILTERS_APPLY') }
-              </button>
-            </form>
-          </ConfigProvider>
+              <div role="tabpanel" className="tab-pane" id="filters_tags">
+                <div className="dashboard__modal-filters__tabpane">
+                  <TagsSelect tags={ this.props.tags }
+                    defaultValue={ this.props.selectedTags }
+                    onChange={ tags => setFieldValue('tags', _.map(tags, tag => tag.slug)) } />
+                </div>
+              </div>
+              <div role="tabpanel" className="tab-pane" id="filters_couriers">
+                <div className="dashboard__modal-filters__tabpane my-4">
+                  { this.props.couriers.map(username => (
+                    <div className="dashboard__modal-filters__courier" key={ username }>
+                      <span>
+                        <Avatar username={ username } /> <span>{ username }</span>
+                      </span>
+                      <div>
+                        <Switch
+                          checkedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_SHOW') }
+                          unCheckedChildren={ this.props.t('ADMIN_DASHBOARD_FILTERS_HIDE') }
+                          defaultChecked={ !isHidden(values.hiddenCouriers, username) }
+                          onChange={ checked => {
+                            if (checked) {
+                              setFieldValue('hiddenCouriers', _.filter(values.hiddenCouriers, u => u !== username))
+                            } else {
+                              setFieldValue('hiddenCouriers', values.hiddenCouriers.concat([ username ]))
+                            }
+                          }} />
+                      </div>
+                    </div>
+                  )) }
+                </div>
+              </div>
+              <div role="tabpanel" className="tab-pane" id="filters_timerange">
+                <div className="dashboard__modal-filters__tabpane mx-4">
+                  <Slider range
+                    marks={ timeSteps }
+                    defaultValue={ values.timeRange }
+                    max={ 24 }
+                    step={ null }
+                    onAfterChange={ value => setFieldValue('timeRange', value) } />
+                </div>
+              </div>
+            </div>
+            <button type="submit" className="btn btn-block btn-primary">
+              { this.props.t('ADMIN_DASHBOARD_FILTERS_APPLY') }
+            </button>
+          </form>
         )}
       </Formik>
     )
