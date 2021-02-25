03fd5872ab8b15fc39d6afb2904f89d8aef75084
Alexandre Segura <mex.zktk@gmail.com>
2021-02-25 08:46:27 +0100

    Stop exporting unused widget.

--- js/app/widgets/index.js
+++ js/app/widgets/index.js
@@ -1,5 +1,4 @@
 import DatePicker from './DatePicker'
-import DateTimePicker from './DateTimePicker'
 import OpeningHoursInput from './OpeningHoursInput'
 import OrderTimeline from './OrderTimeline'
 import StripePaymentForm from './StripePaymentForm'
@@ -8,7 +7,6 @@ import Search from './Search'
 window.CoopCycle = window.CoopCycle || {}
 
 window.CoopCycle.DatePicker = DatePicker
-window.CoopCycle.DateTimePicker = DateTimePicker
 window.CoopCycle.OpeningHoursInput = OpeningHoursInput
 window.CoopCycle.OrderTimeline = OrderTimeline
 window.CoopCycle.StripePaymentForm = StripePaymentForm
