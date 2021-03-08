5ea5b0d0e8a4bbe610678e20ff4cf99d2b98b162
Alexandre Segura <mex.zktk@gmail.com>
2021-03-08 12:03:27 +0100

    Lint.

--- js/app/delivery/list.js
+++ js/app/delivery/list.js
@@ -1,7 +1,7 @@
 /* */
 
 new CoopCycle.DatePicker(document.querySelector('#data_export_start_widget'), {
-  onChange: function(date, dateString) {
+  onChange: function(date) {
     if (date) {
       document.querySelector('#data_export_start').value = date.format('YYYY-MM-DD');
     }
@@ -9,7 +9,7 @@ new CoopCycle.DatePicker(document.querySelector('#data_export_start_widget'), {
 });
 
 new CoopCycle.DatePicker(document.querySelector('#data_export_end_widget'), {
-  onChange: function(date, dateString) {
+  onChange: function(date) {
     if (date) {
       document.querySelector('#data_export_end').value = date.format('YYYY-MM-DD');
     }
