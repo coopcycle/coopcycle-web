1ba8de98170d9e66ff95b66af2b7d55e59957a92
Alexandre Segura <mex.zktk@gmail.com>
2021-03-12 18:25:01 +0100

    Use default theme from package.

--- js/app/components/Autocomplete.js
+++ js/app/components/Autocomplete.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react'
 import Autosuggest from 'react-autosuggest'
+import { defaultTheme } from 'react-autosuggest/dist/theme'
 import { debounce } from 'lodash'
 
 // ------------------ Autosuggest ------------------
 
 
-const getSuggestionValue = suggestion => suggestion.name,
-  renderSuggestion = suggestion => (<span>{suggestion.name}</span>),
-  themeDefaults = {
-    container:                'react-autosuggest__container',
-    containerOpen:            'react-autosuggest__container--open',
-    input:                    'react-autosuggest__input',
-    inputOpen:                'react-autosuggest__input--open',
-    inputFocused:             'react-autosuggest__input--focused',
-    suggestionsContainer:     'react-autosuggest__suggestions-container',
-    suggestionsContainerOpen: 'react-autosuggest__suggestions-container--open',
-    suggestionsList:          'react-autosuggest__suggestions-list',
-    suggestion:               'react-autosuggest__suggestion',
-    suggestionFirst:          'react-autosuggest__suggestion--first',
-    suggestionHighlighted:    'react-autosuggest__suggestion--highlighted',
-    sectionContainer:         'react-autosuggest__section-container',
-    sectionContainerFirst:    'react-autosuggest__section-container--first',
-    sectionTitle:             'react-autosuggest__section-title'
-  }
+const getSuggestionValue = suggestion => suggestion.name
+
+const renderSuggestion = suggestion => (<span>{suggestion.name}</span>)
+
+const loadingTheme = {
+  ...defaultTheme,
+  container: `${defaultTheme.container} react-autosuggest__container--loading`,
+}
 
 export default class extends Component {
 
@@ -89,17 +80,9 @@ export default class extends Component {
       className: 'form-control'
     }
 
-    let theme = themeDefaults
-    if (isFetching) {
-      theme = {
-        ...themeDefaults,
-        container: 'react-autosuggest__container react-autosuggest__container--loading',
-      }
-    }
-
     return (
       <Autosuggest
-        theme={theme}
+        theme={ isFetching ? loadingTheme : defaultTheme }
         suggestions={suggestions}
         onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
         onSuggestionsClearRequested={this.onSuggestionsClearRequested.bind(this)}
