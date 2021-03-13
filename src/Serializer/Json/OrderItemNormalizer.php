2f2ce4ec7f30f3984df286bfa85cac6f254ae6e2
Alexandre Segura <mex.zktk@gmail.com>
2021-03-13 10:22:06 +0100

    Fix/simplify adjustment normalization.

--- src/Serializer/Json/OrderItemNormalizer.php
+++ src/Serializer/Json/OrderItemNormalizer.php
@@ -6,7 +6,6 @@ use AppBundle\Entity\LocalBusinessRepository;
 use AppBundle\Sylius\Order\AdjustmentInterface;
 use ApiPlatform\Core\Api\IriConverterInterface;
 use ApiPlatform\Core\JsonLd\Serializer\ItemNormalizer;
-use Sylius\Component\Order\Model\AdjustmentInterface as BaseAdjustmentInterface;
 use Sylius\Component\Order\Model\OrderInterface;
 use Sylius\Component\Order\Model\OrderItemInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
@@ -29,38 +28,44 @@ class OrderItemNormalizer implements NormalizerInterface, DenormalizerInterface
         $this->iriConverter = $iriConverter;
     }
 
-    private function normalizeAdjustments(array $adjustments)
-    {
-        $data = array_map(function (BaseAdjustmentInterface $adjustment) {
-
-            return [
-                'id' => $adjustment->getId(),
-                'label' => $adjustment->getLabel(),
-                'amount' => $adjustment->getAmount(),
-            ];
-        }, $adjustments);
-
-        return array_values($data);
-    }
-
     public function normalize($object, $format = null, array $context = array())
     {
         $data = $this->normalizer->normalize($object, $format, $context);
 
-        $optionsAdjustments = $object->getAdjustments(AdjustmentInterface::MENU_ITEM_MODIFIER_ADJUSTMENT)->toArray();
-        $packagingAdjustments = $object->getAdjustments(AdjustmentInterface::REUSABLE_PACKAGING_ADJUSTMENT)->toArray();
-        $taxAdjustments = $object->getAdjustments(AdjustmentInterface::TAX_ADJUSTMENT)->toArray();
+        // Change the shape of the "adjustments" prop
 
-        $data['name'] = $object->getVariant()->getProduct()->getName();
-        $data['adjustments'] = [
-            AdjustmentInterface::MENU_ITEM_MODIFIER_ADJUSTMENT => $this->normalizeAdjustments($optionsAdjustments),
-            AdjustmentInterface::TAX_ADJUSTMENT => $this->normalizeAdjustments($taxAdjustments),
+        $adjustments = array_values($data['adjustments']);
+
+        $adjustmentsByType = [];
+
+        $adjustmentTypes = [
+            AdjustmentInterface::MENU_ITEM_MODIFIER_ADJUSTMENT,
+            AdjustmentInterface::REUSABLE_PACKAGING_ADJUSTMENT,
+            AdjustmentInterface::TAX_ADJUSTMENT,
         ];
 
-        if (count($packagingAdjustments) > 0) {
-            $data['adjustments'][AdjustmentInterface::REUSABLE_PACKAGING_ADJUSTMENT]
-                = $this->normalizeAdjustments($packagingAdjustments);
+        foreach ($adjustments as $adjustment) {
+            if (in_array($adjustment['type'], $adjustmentTypes)) {
+                $adjustmentsByType[$adjustment['type']][] = $adjustment;
+            }
         }
+
+        foreach ($adjustmentsByType as $adjustmentType => $adjustmentsArray) {
+            $adjustmentsByType[$adjustmentType] = array_map(function ($adj) {
+
+                return [
+                    // FIXME
+                    // Actually, we don't need the id to be serialized
+                    'id' => $adj['@id'],
+                    'label' => $adj['label'],
+                    'amount' => $adj['amount'],
+                ];
+            }, $adjustmentsArray);
+        }
+
+        $data['adjustments'] = $adjustmentsByType;
+
+        $data['name'] = $object->getVariant()->getProduct()->getName();
 
         $restaurant = $this->localBusinessRepository->findOneByProduct(
             $object->getVariant()->getProduct()
