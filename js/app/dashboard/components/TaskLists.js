d217e56fe4b6f20342476d01358ea943b3427d49
Alexandre Segura <mex.zktk@gmail.com>
2021-03-20 20:45:12 +0100

    Fix loading.

--- js/app/dashboard/components/TaskLists.js
+++ js/app/dashboard/components/TaskLists.js
@@ -67,7 +67,7 @@ function mapStateToProps (state) {
 
   return {
     taskLists: selectTaskLists(state),
-    taskListsLoading: state.logistics.taskListsLoading,
+    taskListsLoading: state.logistics.ui.taskListsLoading,
     hiddenCouriers: state.filters.hiddenCouriers,
   }
 }
