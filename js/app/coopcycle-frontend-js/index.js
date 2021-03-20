c584d0c6f8566dfbc1290c3644acceade6cfdb0d
Alexandre Segura <mex.zktk@gmail.com>
2021-03-20 21:42:35 +0100

    Use subrepo.

--- js/app/coopcycle-frontend-js/index.js
+++ js/app/coopcycle-frontend-js/index.js
@@ -1,5 +1,1 @@
-// published version
-export * from 'coopcycle-frontend-js'
-
-// local version (to be used only during development)
-// export * from '../shared-js-lib-local'
+export * from '../../shared'
