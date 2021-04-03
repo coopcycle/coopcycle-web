8f668a52ab6f84dc7c367243899241a32e8ce810
Alexandre Segura <mex.zktk@gmail.com>
2021-04-03 19:38:40 +0200

    Add plugin-proposal-optional-chaining for Safari.

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
   ],
   "plugins": [
     "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-proposal-optional-chaining",
     ["import", { "libraryName": "antd", "style": (path, file) => {
       if (path === 'antd/lib/col' || path === 'antd/lib/row') {
         return 'antd/lib/grid/style/index.css'
