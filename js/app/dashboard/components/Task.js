4452f73a9b5afdafd5e25b12e79557929ab60a22
AniaCuapio <ania.cuapio@gmail.com>
2021-07-02 22:03:33 +0200

    Show order number, if any.

--- js/app/dashboard/components/Task.js
+++ js/app/dashboard/components/Task.js
@@ -18,9 +18,11 @@ const TaskCaption = ({ task }) => {
 
   const { t } = useTranslation()
 
+  const name = t('ADMIN_DASHBOARD_ORDERS_ORDER')
+
   return (
     <span>
-      <span className="mr-1">#{ task.id }</span>
+      <span className="mr-1">#{ task.id } { !_.isEmpty(task.metadata.order_number) && (' | ' + name + ' ' + task.metadata.order_number) }</span>
       { (task.orgName && !_.isEmpty(task.orgName)) && (
         <span>
           <span className="font-weight-bold">{ task.orgName }</span>
