c2d054f142d69e8bd85380a31704acb8365d4957
Alexandre Segura <mex.zktk@gmail.com>
2021-07-24 13:19:42 +0200

    Change only one column.

--- app/DoctrineMigrations/Version20210723161106.php
+++ app/DoctrineMigrations/Version20210723161106.php
@@ -20,16 +20,12 @@ final class Version20210723161106 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('DROP INDEX uniq_527edb25bc2d6b55');
-        $this->addSql('DROP INDEX uniq_527edb254f382b24');
         $this->addSql('CREATE INDEX IDX_527EDB25BC2D6B55 ON task (previous_task_id)');
-        $this->addSql('CREATE INDEX IDX_527EDB254F382B24 ON task (next_task_id)');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('DROP INDEX IDX_527EDB25BC2D6B55');
-        $this->addSql('DROP INDEX IDX_527EDB254F382B24');
         $this->addSql('CREATE UNIQUE INDEX uniq_527edb25bc2d6b55 ON task (previous_task_id)');
-        $this->addSql('CREATE UNIQUE INDEX uniq_527edb254f382b24 ON task (next_task_id)');
     }
 }
