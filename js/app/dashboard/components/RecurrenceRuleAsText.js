4485d3d147bb851aeac9863d894a3aa80d587bd8
Alexandre Segura <mex.zktk@gmail.com>
2021-08-02 13:54:28 +0200

    Use translations, fix missing tokens.

--- js/app/dashboard/components/RecurrenceRuleAsText.js
+++ js/app/dashboard/components/RecurrenceRuleAsText.js
@@ -1,12 +1,43 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
-import { toTextArgs } from '../utils/rrule'
+import { localeDetector } from '../../i18n'
+import moment from 'moment'
+
+const tokenForLocale = (locale, t) => {
+
+  return (token) => {
+
+    const transKey = `RRULE.${token}`
+    const translated = t(transKey)
+
+    if (translated === transKey) {
+      return token
+    }
+
+    return translated
+  }
+}
+
+export const toTextArgs = (t) => {
+  const locale = localeDetector()
+
+  return [
+    tokenForLocale(locale, t),
+    {
+      dayNames: moment.localeData(locale).weekdays()
+    }
+  ]
+}
+
 
 const AsText = ({ rrule }) => {
 
+  const { t } = useTranslation()
+
   return (
     <span>
-      { rrule.toText(...toTextArgs()) }
+      { rrule.toText(...toTextArgs(t)) }
     </span>
   )
 }
