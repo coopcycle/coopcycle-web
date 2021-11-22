1e2f91bb77e126cea9672910b23b9c51e44ee047
Alexandre Segura <mex.zktk@gmail.com>
2021-11-22 17:08:57 +0100

    Add endpoint to verify Google id token.

---
+++ src/Action/GoogleSignIn.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Action;
+
+use Nucleos\UserBundle\Model\UserManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Events;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationSuccessResponse;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class GoogleSignIn
+{
+    private $appId;
+
+    public function __construct(string $appId)
+    {
+        $this->appId = $appId;
+    }
+
+    /**
+     * @Route(
+     *     path="/google_sign_in/login",
+     *     name="api_google_sign_in_login",
+     *     methods={"POST"}
+     * )
+     */
+    public function googleSignInAction(Request $request)
+    {
+        $data = [];
+        $content = $request->getContent();
+        if (!empty($content)) {
+            $data = json_decode($content, true);
+        }
+
+        $client = new \Google_Client(['client_id' => $this->appId]);
+        $payload = $client->verifyIdToken($data['idToken']);
+
+        if (!$payload) {
+          throw new AccessDeniedHttpException();
+        }
+
+        $email = $payload['email'];
+
+        $user = $this->userManager->findUserByEmail($email);
+
+        if (null === $user) {
+            throw new AccessDeniedHttpException();
+        }
+
+        $jwt = $this->jwtManager->create($user);
+
+        // @see Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Authentication\AuthenticationSuccessHandler
+        $response = new JWTAuthenticationSuccessResponse($jwt);
+        $event    = new AuthenticationSuccessEvent(['token' => $jwt], $user, $response);
+
+        $this->dispatcher->dispatch($event, Events::AUTHENTICATION_SUCCESS);
+        $response->setData($event->getData());
+
+        return $response;
+    }
+}
