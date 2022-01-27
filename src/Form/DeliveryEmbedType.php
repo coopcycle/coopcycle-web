33b5d2848767623ae2b160353ae3bff67d002b0d
Alexandre Segura <mex.zktk@gmail.com>
2022-01-27 11:29:09 +0100

    Revert "Re-disable CSRF protection."
    This reverts commit 9c22b8785cc5ae40f8113bc4acb6f43cc1d3676b.

--- src/Form/DeliveryEmbedType.php
+++ src/Form/DeliveryEmbedType.php
@@ -77,9 +77,5 @@ class DeliveryEmbedType extends DeliveryType
         parent::configureOptions($resolver);
 
         $resolver->setDefault('with_tags', false);
-
-        // Disable CSRF protection to allow being used in iframes
-        // @see https://github.com/coopcycle/coopcycle-web/issues/735
-        $resolver->setDefault('csrf_protection', false);
     }
 }
