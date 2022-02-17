50bbd1b4445b0b3f12679fed4cd01ce0a01f8a0a
Alexandre Segura <mex.zktk@gmail.com>
2022-02-17 14:46:17 +0100

    Improve rendering of time slots human readable.

--- js/app/time-slot/list.js
+++ js/app/time-slot/list.js
@@ -3,13 +3,5 @@ import { localeDetector } from '../i18n'
 
 document.querySelectorAll('[data-opening-hours]').forEach(el => {
   const openingHours = JSON.parse(el.dataset.openingHours)
-  const lines = openingHours.map(oh => openingHourIntervalToReadable(oh, localeDetector(), 'time_slot'))
-  const ul = document.createElement('ul')
-  ul.classList.add('list-unstyled')
-  lines.forEach(line => {
-    const li = document.createElement('li')
-    li.innerHTML = line
-    ul.appendChild(li)
-  })
-  el.appendChild(ul)
+  el.innerHTML = openingHourIntervalToReadable(openingHours, localeDetector(), 'time_slot')
 })
