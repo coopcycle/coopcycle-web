03e689ca610096dd5f23010ead6821463e53c108
Alexandre Segura <mex.zktk@gmail.com>
2022-04-06 17:00:49 +0200

    Use simple arrays instead of PHP objects to export deliveries data.

--- src/Spreadsheet/DeliveryDataExporter.php
+++ src/Spreadsheet/DeliveryDataExporter.php
@@ -2,8 +2,13 @@
 
 namespace AppBundle\Spreadsheet;
 
+use AppBundle\Entity\Address;
 use AppBundle\Entity\Delivery;
+use AppBundle\Entity\Package;
+use AppBundle\Entity\Sylius\Order;
+use AppBundle\Entity\Organization;
 use AppBundle\Entity\Task;
+use AppBundle\Entity\Task\Package as TaskPackage;
 use AppBundle\Entity\TaskCollectionItem;
 use AppBundle\Entity\TaskRepository;
 use Doctrine\ORM\Query\Expr;
@@ -12,16 +17,133 @@ final class DeliveryDataExporter extends AbstractDataExporter
 {
     protected function getData(\DateTime $start, \DateTime $end): array
     {
-        $qb = $this->entityManager->getRepository(Delivery::class)
-            ->createQueryBuilder('d');
-
-        $qb
-            ->join(TaskCollectionItem::class, 'i', Expr\Join::WITH, 'i.parent = d.id')
-            ->join(Task::class, 't', Expr\Join::WITH, 'i.task = t.id')
-            ;
+        $qb = $this->entityManager->getRepository(Task::class)
+            ->createQueryBuilder('t');
 
         $qb = TaskRepository::addRangeClause($qb, $start, $end);
+        $qb = $qb->join(Address::class, 'a', Expr\Join::WITH, 't.address = a.id');
+        $qb = $qb->leftJoin(Organization::class, 'o', Expr\Join::WITH, 't.organization = o.id');
+        // Add join with delivery, to exclude standalone tasks
+        $qb = $qb->join(Delivery::class, 'd', Expr\Join::WITH, 't.delivery = d.id');
 
-        return $qb->getQuery()->getResult();
+        $qb
+            ->select('t.id')
+            ->addSelect('t.type')
+            ->addSelect('IDENTITY(t.delivery) AS delivery')
+            ->addSelect('t.doneAfter')
+            ->addSelect('t.doneBefore')
+            ->addSelect('t.weight')
+            ->addSelect('a.streetAddress')
+            ->addSelect('d.distance')
+            ->addSelect('o.name AS orgName')
+            ;
+
+        $tasks = $qb->getQuery()->getArrayResult();
+
+        $taskIds = array_map(fn ($task) => $task['id'], $tasks);
+
+        $packagesByTask = $this->getPackagesByTask($taskIds);
+
+        $deliveryIds = array_map(fn ($t) => $t['delivery'], $tasks);
+        $deliveryIds = array_values(array_unique($deliveryIds));
+
+        $ordersByDelivery = $this->getOrdersByDelivery($deliveryIds);
+
+        $deliveries = array();
+        foreach ($tasks as $task) {
+            $deliveries[$task['delivery']][$task['type']] = $task;
+        }
+
+        $deliveries = array_values($deliveries);
+
+        $deliveries = array_map(function ($delivery) use ($ordersByDelivery, $packagesByTask) {
+
+            $weight = 0;
+            foreach ($delivery as $task) {
+                $weight += $task['weight'];
+            }
+
+            $orderNumber = '';
+            $orderTotal = '';
+            if (isset($ordersByDelivery[$delivery['PICKUP']['delivery']])) {
+                $order = $ordersByDelivery[$delivery['PICKUP']['delivery']];
+                $orderNumber = $order['number'];
+                $orderTotal = $order['total'];
+            }
+
+            $packages = '';
+            if (isset($packagesByTask[$delivery['DROPOFF']['id']])) {
+                $packages = implode(', ', $packagesByTask[$delivery['DROPOFF']['id']]);
+            }
+
+            return [
+                'organization'    => $delivery['PICKUP']['orgName'],
+                'pickup.address'  => $delivery['PICKUP']['streetAddress'],
+                'pickup.after'    => $delivery['PICKUP']['doneAfter']->format(\DateTime::ATOM),
+                'dropoff.address' => $delivery['DROPOFF']['streetAddress'],
+                'dropoff.before'  => $delivery['DROPOFF']['doneBefore']->format(\DateTime::ATOM),
+                'weight'          => $weight,
+                'distance'        => $delivery['PICKUP']['distance'],
+                'order.number'    => $orderNumber,
+                'order.total'     => $orderTotal,
+                'packages'        => $packages,
+            ];
+
+        }, $deliveries);
+
+        return $deliveries;
+    }
+
+    private function getPackagesByTask(array $taskIds)
+    {
+        $packagesQb = $this->entityManager
+            ->getRepository(Package::class)
+            ->createQueryBuilder('p');
+
+        $packagesQb = $packagesQb->join(TaskPackage::class, 'tp', Expr\Join::WITH, 'tp.package = p.id');
+        $packagesQb = $packagesQb->join(Task::class, 't', Expr\Join::WITH, 'tp.task = t.id');
+
+        $packagesQb
+            ->select('p.name')
+            ->addSelect('tp.quantity')
+            ->addSelect('t.id AS task')
+            ->andWhere(
+                $packagesQb->expr()->in('t.id', $taskIds)
+            );
+
+        $packages = $packagesQb->getQuery()->getArrayResult();
+
+        $packagesByTask = [];
+        foreach ($packages as $package) {
+            $packagesByTask[$package['task']][] = sprintf('%d × %s', $package['quantity'], $package['name']);
+        }
+
+        return $packagesByTask;
+    }
+
+    private function getOrdersByDelivery(array $deliveryIds)
+    {
+        $ordersQb = $this->entityManager
+            ->getRepository(Order::class)
+            ->createQueryBuilder('o');
+
+        $ordersQb
+            ->select('o.id')
+            ->addSelect('o.number')
+            ->addSelect('d.id AS delivery')
+            ->addSelect('o.total')
+            ->innerJoin(Delivery::class, 'd', Expr\Join::WITH, 'd.order = o.id')
+            ->andWhere(
+                $ordersQb->expr()->in('d.id', $deliveryIds)
+            );
+
+        $orders = $ordersQb->getQuery()->getArrayResult();
+
+        $ordersByDelivery = [];
+        foreach ($orders as $order) {
+            $ordersByDelivery[$order['delivery']] = $order;
+        }
+
+        return $ordersByDelivery;
     }
 }
