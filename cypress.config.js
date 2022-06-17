ba1e4c336c07d67745dbe25d0280849bb78e23a9
Alexandre Segura <mex.zktk@gmail.com>
2022-06-17 16:17:01 +0200

    WIP

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    "COMMAND_PREFIX": "docker-compose exec -T php",
+    "coverage": false,
+  },
+  chromeWebSecurity: false,
+  e2e: {
+    baseUrl: 'http://localhost:9080'
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+      // optionally pass in webpack config
+      webpackConfig: require('./webpack.config'),
+    },
+  },
+}
