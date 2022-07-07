122da37cd0ea6a31608a7f53eb777d35e4b2c23f
Alexandre Segura <mex.zktk@gmail.com>
2022-07-07 19:52:11 +0200

    Introduce quote system for deliveries.

---
+++ src/Action/Delivery/ConfirmQuote.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Action\Delivery;
+
+use AppBundle\Entity\Delivery;
+use AppBundle\Entity\DeliveryQuote;
+use AppBundle\Sylius\Order\OrderFactory;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ConfirmQuote
+{
+    private $serializer;
+    private $orderFactory;
+    private $entityManager;
+
+    public function __construct(
+        SerializerInterface $serializer,
+        OrderFactory $orderFactory,
+        EntityManagerInterface $entityManager)
+    {
+        $this->serializer = $serializer;
+        $this->orderFactory = $orderFactory;
+        $this->entityManager = $entityManager;
+    }
+
+    public function __invoke(DeliveryQuote $data)
+    {
+        $delivery = $this->serializer->deserialize($data->getPayload(), Delivery::class, 'jsonld');
+        $order = $this->orderFactory->createForDelivery($delivery, $data->getAmount());
+
+        $this->entityManager->persist($order);
+
+        $data->setDelivery($delivery);
+        $data->setState(DeliveryQuote::STATE_CONFIRMED);
+
+        return $data;
+    }
+}
