a78f24d42f24a71eee5751bb9087544300b85b93
Lucas Ferraro <lferraro@fiqus.com>
2022-07-07 17:11:40 -0300

    woopit upload documents

--- src/Entity/Listener/TaskListener.php
+++ src/Entity/Listener/TaskListener.php
@@ -2,11 +2,32 @@
 
 namespace AppBundle\Entity\Listener;
 
+use ApiPlatform\Core\Api\IriConverterInterface;
 use AppBundle\Entity\Task;
+use AppBundle\Entity\Woopit\Delivery as WoopitDelivery;
+use AppBundle\Message\WoopitDocumentWebhook;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class TaskListener
 {
+    private $messageBus;
+
+    public function __construct(
+        MessageBusInterface $messageBus,
+        IriConverterInterface $iriConverter,
+        EntityManagerInterface $entityManager,
+        LoggerInterface $logger)
+    {
+        $this->messageBus = $messageBus;
+        $this->iriConverter = $iriConverter;
+        $this->entityManager = $entityManager;
+        $this->logger = $logger;
+    }
+
     public function prePersist(Task $task, LifecycleEventArgs $args)
     {
         if (null === $task->getDoneAfter()) {
@@ -15,4 +36,25 @@ class TaskListener
             $task->setDoneAfter($doneAfter);
         }
     }
+
+    public function preUpdate(Task $task, PreUpdateEventArgs $event)
+    {
+        if ($event->hasChangedField('images')) {
+            $woopitDelivery = $this->entityManager
+                ->getRepository(WoopitDelivery::class)
+                ->findOneBy(['delivery' => $task->getDelivery()]);
+
+            if ($woopitDelivery) {
+                foreach($task->getImages() as $taskImage) {
+                    $this->messageBus->dispatch(
+                        new WoopitDocumentWebhook(
+                            $this->iriConverter->getIriFromItem($taskImage),
+                            'EVIDENCE'
+                        )
+                    );
+                }
+            }
+        }
+    }
+
 }
