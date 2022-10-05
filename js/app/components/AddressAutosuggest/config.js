5c81a1aa5127207a68edd0167aec73c6e620f282
Alexandre Segura <mex.zktk@gmail.com>
2022-10-05 23:44:10 +0200

    Farewell Algolia Places. Fixes #2007.

--- js/app/components/AddressAutosuggest/config.js
+++ js/app/components/AddressAutosuggest/config.js
@@ -1,9 +1,5 @@
 export const getAdapter = (props, document) => {
 
-  if (Object.prototype.hasOwnProperty.call(props, 'algolia')) {
-    return 'algolia'
-  }
-
   if (Object.prototype.hasOwnProperty.call(props, 'geocodeEarth')) {
     return 'geocode-earth'
   }
@@ -18,31 +14,26 @@ export const getAdapter = (props, document) => {
 
   const el = document.getElementById('autocomplete-adapter')
 
-  return (el && el.dataset.value) || 'algolia'
+  return (el && el.dataset.value) || 'geocodeEarth'
 }
 
 export const getAdapterOptions = (props, document) => {
 
   let adapterOptions = {
-    algolia: {},
     'geocode-earth': {},
     locationiq: {},
     google: {},
   }
-  if (Object.prototype.hasOwnProperty.call(props, 'algolia')) {
-    adapterOptions.algolia = props.algolia
-  } else if (Object.prototype.hasOwnProperty.call(props, 'geocodeEarth')) {
+  if (Object.prototype.hasOwnProperty.call(props, 'geocodeEarth')) {
     adapterOptions['geocode-earth'] = props.geocodeEarth
   } else if (Object.prototype.hasOwnProperty.call(props, 'locationIQ')) {
     adapterOptions.locationiq = props.locationIQ
   } else {
 
-    const algoliaEl      = document.getElementById('algolia-places')
     const geocodeEarthEl = document.getElementById('geocode-earth')
     const locationIQEl   = document.getElementById('locationiq')
     const googleEl       = document.getElementById('google')
 
-    adapterOptions.algolia = (algoliaEl && { ...algoliaEl.dataset }) || {}
     adapterOptions['geocode-earth'] = (geocodeEarthEl && { ...geocodeEarthEl.dataset }) || {}
     adapterOptions.locationiq = (locationIQEl && { ...locationIQEl.dataset }) || {}
     adapterOptions.google = (googleEl && { ...googleEl.dataset }) || {}
