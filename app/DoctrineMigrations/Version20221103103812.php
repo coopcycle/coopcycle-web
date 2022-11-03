64b03f2d98f36ac59f75fe3ff20c076382fa2df8
Alexandre Segura <mex.zktk@gmail.com>
2022-11-03 15:03:24 +0100

    Simplify migration.

--- app/DoctrineMigrations/Version20221103103812.php
+++ app/DoctrineMigrations/Version20221103103812.php
@@ -19,61 +19,26 @@ final class Version20221103103812 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $taskStmt = $this->connection->prepare('SELECT id, type, weight FROM task WHERE delivery_id = :delivery_id');
+        $pickupSql =
+            'select id, delivery_id, weight from task where type = \'PICKUP\' and delivery_id is not null and weight > 0';
+        $dropoffSql =
+            'select delivery_id, sum(weight) as total_weight from task where type = \'DROPOFF\' and delivery_id is not null and weight > 0 group by delivery_id';
 
         $stmt = $this->connection->prepare(
-            'SELECT delivery_id FROM task WHERE delivery_id IS NOT NULL AND type = \'PICKUP\' AND weight > 0'
+            sprintf('select p.delivery_id, p.id as pickup_id, p.weight as pickup_weight, d.total_weight as dropoffs_weight from (%s) p join (%s) d on p.delivery_id = d.delivery_id where p.weight = d.total_weight', $pickupSql, $dropoffSql)
         );
         $stmt->execute();
 
-        while ($t = $stmt->fetch()) {
-
-            $taskStmt->bindParam('delivery_id', $t['delivery_id']);
-            $taskStmt->execute();
-
-            $delivery = [];
-            while ($task = $taskStmt->fetch()) {
-                $delivery[] = $task;
-            }
-
-            if (!$this->shouldBeMigrated($delivery)) {
-                continue;
-            }
-
-            $pickups = array_filter($delivery, fn($t) => $t['type'] === 'PICKUP');
-            $pickup = current($pickups);
+        while ($row = $stmt->fetch()) {
 
             $this->addSql('UPDATE task SET weight = NULL WHERE id = :id', [
-                'id' => $pickup['id']
+                'id' => $row['pickup_id']
             ]);
 
             $this->addSql('DELETE FROM task_package WHERE task_id = :id', [
-                'id' => $pickup['id']
+                'id' => $row['pickup_id']
             ]);
         }
-    }
-
-    private function shouldBeMigrated(array $delivery)
-    {
-        $pickupWeight = 0;
-        $dropoffWeightSum = 0;
-        $pickupCount = 0;
-
-        foreach ($delivery as $task) {
-            if ($task['type'] === 'PICKUP') {
-                $pickupCount += 1;
-                $pickupWeight = $task['weight'];
-            }
-            if ($task['type'] === 'DROPOFF') {
-                $dropoffWeightSum += $task['weight'];
-            }
-        }
-
-        if ($pickupCount !== 1) {
-            return false;
-        }
-
-        return $pickupWeight === $dropoffWeightSum;
     }
 
     public function down(Schema $schema): void
