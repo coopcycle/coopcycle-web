d6ab99d4d63c01845c7bc2d477b492f65693336c
Alexandre Segura <mex.zktk@gmail.com>
2023-01-09 10:06:15 +0100

    Use client from TypesenseBundle.

--- src/Entity/LocalBusiness.php
+++ src/Entity/LocalBusiness.php
@@ -1056,4 +1056,9 @@ class LocalBusiness extends BaseLocalBusiness implements
 
         return $categories;
     }
+
+    public function getShopType()
+    {
+        return self::getKeyForType($this->getType());
+    }
 }
