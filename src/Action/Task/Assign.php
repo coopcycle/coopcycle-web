07f8da8d745aacdd51a16d8cae285f445c624303
Lucas Ferraro <lferraro@fiqus.com>
2023-02-28 15:17:03 -0300

    improve trait and fix lint

--- src/Action/Task/Assign.php
+++ src/Action/Task/Assign.php
@@ -2,19 +2,14 @@
 
 namespace AppBundle\Action\Task;
 
-use ApiPlatform\Core\Exception\ItemNotFoundException;
-use AppBundle\Api\Exception\BadRequestHttpException;
 use AppBundle\Service\TaskManager;
 use Nucleos\UserBundle\Model\UserManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class Assign extends Base
 {
     use AssignTrait;
-
-    private $userManager;
 
     public function __construct(
         TokenStorageInterface $tokenStorage,
@@ -37,18 +32,6 @@ class Assign extends Base
             $payload = json_decode($content, true);
         }
 
-        if (isset($payload['username'])) {
-            $user = $this->userManager->findUserByUsername($payload['username']);
-
-            if (!$user) {
-
-                throw new ItemNotFoundException(sprintf('User "%s" does not exist',
-                    $this->getUser()->getUsername()));
-            }
-        } else {
-            $user = $this->getUser();
-        }
-
-        return $this->assign($task, $user);
+        return $this->assign($task, $payload);
     }
 }
