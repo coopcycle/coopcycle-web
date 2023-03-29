69b2328f0270538948576e04721363ed88eab8f7
Antoine Bagnaud <contact@antoinebagnaud.me>
2023-03-29 13:43:32 +0200

    test: Implement functional tests to ensure Orders Rate Limiting feature is working correctly.

--- tests/AppBundle/Utils/OrdersRateLimitFunctionalTest.php
+++ tests/AppBundle/Utils/OrdersRateLimitFunctionalTest.php
@@ -2,7 +2,11 @@
 
 namespace AppBundle\Utils;
 
+use AppBundle\Domain\Order\Event\OrderCancelled;
 use AppBundle\Domain\Order\Event\OrderCreated;
+use AppBundle\Domain\Order\Event\OrderDelayed;
+use AppBundle\Domain\Order\Event\OrderPicked;
+use AppBundle\Domain\Order\Event\OrderRefused;
 use AppBundle\Entity\LocalBusiness;
 use AppBundle\Entity\Sylius\Order;
 use AppBundle\Entity\Sylius\OrderTimeline;
@@ -25,6 +29,8 @@ class OrdersRateLimitFunctionalTest extends KernelTestCase
         parent::setUp();
 
         self::bootKernel();
+
+        Carbon::setTestNow(Carbon::parse('2023-03-28 12:30:00'));
 
         // @see https://symfony.com/blog/new-in-symfony-4-1-simpler-service-testing
         $this->redis = self::$container->get(Redis::class);
@@ -59,7 +65,6 @@ class OrdersRateLimitFunctionalTest extends KernelTestCase
 
     public function testHandleOrderCreatedEvent()
     {
-        Carbon::setTestNow(Carbon::parse('2023-03-28 12:30:00'));
 
     	$orders = [
     		new \DateTime('2023-03-28 12:05:00'),
@@ -100,5 +105,211 @@ class OrdersRateLimitFunctionalTest extends KernelTestCase
         $anotherOrder = $this->createOrder(($i + 1), $restaurant->reveal(), $anotherOrderPickup);
 
         $this->assertEquals(false, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup));
+    }
+
+    public function testHandleOrderDelayedEvent()
+    {
+
+        $orders = [
+            new \DateTime('2023-03-28 12:05:00'),
+            new \DateTime('2023-03-28 12:05:00'),
+            new \DateTime('2023-03-28 12:38:00'),
+        ];
+
+        $restaurant = $this->prophesize(LocalBusiness::class);
+        $restaurant->getId()->willReturn(1);
+
+        // We want a max 10 orders per 30 minutes
+        $restaurant->getMaxOrdersAmount()->willReturn(2);
+        $restaurant->getMaxOrdersRangeDuration()->willReturn(30);
+
+        foreach ($orders as $i => $pickupExpectedAt) {
+            $order = $this->createOrder(($i + 1), $restaurant->reveal(), $pickupExpectedAt);
+            $orderCreated = new OrderCreated($order);
+
+            $this->rateLimiter->handleEvent($orderCreated);
+        }
+
+        $this->assertEquals(1, $this->redis->exists('rate_limit:1'));
+
+        // Create an order for 12:05
+        $anotherOrderPickup5 = new \DateTime('2023-03-28 12:05:00');
+        $anotherOrder = $this->createOrder(($i + 1), $restaurant->reveal(), $anotherOrderPickup5);
+
+        // Check if the shift 12:05 - 12h35 is full (it should)
+        $this->assertEquals(true, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup5));
+
+        // Delay an order from 12:05 - 12h35 shift to the next shift
+        $anotherOrderPickup36 = new \DateTime('2023-03-28 12:36:00');
+        $order = $this->createOrder(1, $restaurant->reveal(), $anotherOrderPickup36);
+        $this->rateLimiter->handleEvent(new OrderDelayed($order, 1860));
+
+        // Check if the shift 12:05 - 12h35 is full (should not)
+        $this->assertEquals(false, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup5));
+
+        // Check if the shift 12:35 - 13h05 if full (it should)
+        $this->assertEquals(true, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup36));
+    }
+
+    public function testHandleOrderDelayedEventShouldExceedLimit()
+    {
+
+        $orders = [
+            new \DateTime('2023-03-28 12:05:00'),
+            new \DateTime('2023-03-28 12:05:00'),
+            new \DateTime('2023-03-28 12:37:00'),
+            new \DateTime('2023-03-28 12:38:00'),
+        ];
+
+        $restaurant = $this->prophesize(LocalBusiness::class);
+        $restaurant->getId()->willReturn(1);
+
+        // We want a max 10 orders per 30 minutes
+        $restaurant->getMaxOrdersAmount()->willReturn(2);
+        $restaurant->getMaxOrdersRangeDuration()->willReturn(30);
+
+        foreach ($orders as $i => $pickupExpectedAt) {
+            $order = $this->createOrder(($i + 1), $restaurant->reveal(), $pickupExpectedAt);
+            $orderCreated = new OrderCreated($order);
+
+            $this->rateLimiter->handleEvent($orderCreated);
+        }
+
+        $this->assertEquals(1, $this->redis->exists('rate_limit:1'));
+
+        // Create an order for 12:05
+        $anotherOrderPickup5 = new \DateTime('2023-03-28 12:05:00');
+        $anotherOrder = $this->createOrder(($i + 1), $restaurant->reveal(), $anotherOrderPickup5);
+
+        // Check if the shift 12:05 - 12h35 is full (it should)
+        $this->assertEquals(true, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup5));
+
+        // Delay an order from 12:05 - 12h35 shift to the next shift
+        $anotherOrderPickup36 = new \DateTime('2023-03-28 12:36:00');
+        $order = $this->createOrder(1, $restaurant->reveal(), $anotherOrderPickup36);
+
+        // Check if the shift 12:35 - 13h05 if full (it should)
+        $this->assertEquals(true, $this->rateLimiter->isRangeFull($order, $anotherOrderPickup36));
+
+        // Event if the shift 12:35 - 13h05 if full, it should be accepted
+        $this->rateLimiter->handleEvent(new OrderDelayed($order, 1860));
+
+        // Check if the shift 12:05 - 12h35 is full (should not)
+        $this->assertEquals(false, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup5));
+
+        // Check if the shift 12:35 - 13h05 if full (it should)
+        $this->assertEquals(true, $this->rateLimiter->isRangeFull($order, $anotherOrderPickup36));
+    }
+    public function testHandleOrderCancelledEvent()
+    {
+
+        $orders = [
+            new \DateTime('2023-03-28 12:05:00'),
+            new \DateTime('2023-03-28 12:05:00'),
+        ];
+
+        $restaurant = $this->prophesize(LocalBusiness::class);
+        $restaurant->getId()->willReturn(1);
+
+        // We want a max 10 orders per 30 minutes
+        $restaurant->getMaxOrdersAmount()->willReturn(2);
+        $restaurant->getMaxOrdersRangeDuration()->willReturn(30);
+
+        foreach ($orders as $i => $pickupExpectedAt) {
+            $order = $this->createOrder(($i + 1), $restaurant->reveal(), $pickupExpectedAt);
+            $orderCreated = new OrderCreated($order);
+
+            $this->rateLimiter->handleEvent($orderCreated);
+        }
+
+        $this->assertEquals(1, $this->redis->exists('rate_limit:1'));
+
+        // Create an order for 12:05
+        $anotherOrderPickup5 = new \DateTime('2023-03-28 12:05:00');
+        $anotherOrder = $this->createOrder(($i + 1), $restaurant->reveal(), $anotherOrderPickup5);
+
+        // Check if the shift 12:05 - 12h35 is full (it should)
+        $this->assertEquals(true, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup5));
+
+        $order = $this->createOrder(1, $restaurant->reveal(), $anotherOrderPickup5);
+        $this->rateLimiter->handleEvent(new OrderCancelled($order));
+
+        // Check if the shift 12:05 - 12h35 is full (it should)
+        $this->assertEquals(false, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup5));
+    }
+
+    public function testHandleOrderPickedEvent()
+    {
+
+        $orders = [
+            new \DateTime('2023-03-28 12:05:00'),
+            new \DateTime('2023-03-28 12:05:00'),
+        ];
+
+        $restaurant = $this->prophesize(LocalBusiness::class);
+        $restaurant->getId()->willReturn(1);
+
+        // We want a max 10 orders per 30 minutes
+        $restaurant->getMaxOrdersAmount()->willReturn(2);
+        $restaurant->getMaxOrdersRangeDuration()->willReturn(30);
+
+        foreach ($orders as $i => $pickupExpectedAt) {
+            $order = $this->createOrder(($i + 1), $restaurant->reveal(), $pickupExpectedAt);
+            $orderCreated = new OrderCreated($order);
+
+            $this->rateLimiter->handleEvent($orderCreated);
+        }
+
+        $this->assertEquals(1, $this->redis->exists('rate_limit:1'));
+
+        // Create an order for 12:05
+        $anotherOrderPickup5 = new \DateTime('2023-03-28 12:05:00');
+        $anotherOrder = $this->createOrder(($i + 1), $restaurant->reveal(), $anotherOrderPickup5);
+
+        // Check if the shift 12:05 - 12h35 is full (it should)
+        $this->assertEquals(true, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup5));
+
+        $order = $this->createOrder(1, $restaurant->reveal(), $anotherOrderPickup5);
+        $this->rateLimiter->handleEvent(new OrderPicked($order));
+
+        // Check if the shift 12:05 - 12h35 is full (it should)
+        $this->assertEquals(false, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup5));
+    }
+    public function testHandleOrderRefusedEvent()
+    {
+
+        $orders = [
+            new \DateTime('2023-03-28 12:05:00'),
+            new \DateTime('2023-03-28 12:05:00'),
+        ];
+
+        $restaurant = $this->prophesize(LocalBusiness::class);
+        $restaurant->getId()->willReturn(1);
+
+        // We want a max 10 orders per 30 minutes
+        $restaurant->getMaxOrdersAmount()->willReturn(2);
+        $restaurant->getMaxOrdersRangeDuration()->willReturn(30);
+
+        foreach ($orders as $i => $pickupExpectedAt) {
+            $order = $this->createOrder(($i + 1), $restaurant->reveal(), $pickupExpectedAt);
+            $orderCreated = new OrderCreated($order);
+
+            $this->rateLimiter->handleEvent($orderCreated);
+        }
+
+        $this->assertEquals(1, $this->redis->exists('rate_limit:1'));
+
+        // Create an order for 12:05
+        $anotherOrderPickup5 = new \DateTime('2023-03-28 12:05:00');
+        $anotherOrder = $this->createOrder(($i + 1), $restaurant->reveal(), $anotherOrderPickup5);
+
+        // Check if the shift 12:05 - 12h35 is full (it should)
+        $this->assertEquals(true, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup5));
+
+        $order = $this->createOrder(1, $restaurant->reveal(), $anotherOrderPickup5);
+        $this->rateLimiter->handleEvent(new OrderRefused($order));
+
+        // Check if the shift 12:05 - 12h35 is full (it should)
+        $this->assertEquals(false, $this->rateLimiter->isRangeFull($anotherOrder, $anotherOrderPickup5));
     }
 }
