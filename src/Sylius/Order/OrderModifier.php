338866160c21988634cb0c51b1e71c21d1636564
Antoine Bagnaud <contact@antoinebagnaud.me>
2023-04-18 19:37:11 +0200

    fix: set customer to avoid unassigned items

--- src/Sylius/Order/OrderModifier.php
+++ src/Sylius/Order/OrderModifier.php
@@ -2,30 +2,39 @@
 
 namespace AppBundle\Sylius\Order;
 
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Psr\Log\LoggerInterface;
-use Sylius\Component\Order\Modifier\OrderModifierInterface;
 use Sylius\Component\Order\Model\OrderInterface;
 use Sylius\Component\Order\Model\OrderItemInterface;
-use Sylius\Component\Order\Processor\OrderProcessorInterface;
+use Sylius\Component\Order\Modifier\OrderModifierInterface;
 
 final class OrderModifier implements OrderModifierInterface
 {
 	public function __construct(
-        OrderModifierInterface $orderModifier,
-        OrderInvitationContext $context,
-    	LoggerInterface $logger)
-    {
-        $this->orderModifier = $orderModifier;
-        $this->context = $context;
-        $this->logger = $logger;
-    }
+        private OrderModifierInterface $orderModifier,
+        private OrderInvitationContext $context,
+        private ContainerInterface $container,
+    	private LoggerInterface $logger)
+    { }
 
+    /**
+     * @throws \Exception
+     */
     public function addToOrder(OrderInterface $cart, OrderItemInterface $cartItem): void
     {
         if ($this->context->isPlayerOf($cart)) {
             $customer = $this->context->getCustomer();
             $cartItem->setCustomer($customer);
             $this->logger->debug("OrderModifier | adding item by {$customer->getEmail()}");
+        } else {
+                if (
+                    !is_null($token = $this->container->get('security.token_storage')->getToken()) &&
+                    !is_null($user = $token->getUser())
+                ) {
+                    $cartItem->setCustomer($user->getCustomer());
+                }
         }
 
         $this->orderModifier->addToOrder($cart, $cartItem);
