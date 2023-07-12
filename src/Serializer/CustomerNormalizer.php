75f1357f75224a7d4c4c1ed43c160d9fc8711ff3
Alexandre Segura <mex.zktk@gmail.com>
2023-07-13 01:43:50 +0200

    Only admins can see customer tags in API responses.

--- src/Serializer/CustomerNormalizer.php
+++ src/Serializer/CustomerNormalizer.php
@@ -4,6 +4,7 @@ namespace AppBundle\Serializer;
 
 use AppBundle\Sylius\Customer\CustomerInterface;
 use AppBundle\Service\TagManager;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class CustomerNormalizer implements NormalizerInterface
@@ -11,10 +12,11 @@ class CustomerNormalizer implements NormalizerInterface
     private $normalizer;
     private $tagManager;
 
-    public function __construct(NormalizerInterface $normalizer, TagManager $tagManager)
+    public function __construct(NormalizerInterface $normalizer, TagManager $tagManager, AuthorizationCheckerInterface $authorizationChecker)
     {
         $this->normalizer = $normalizer;
         $this->tagManager = $tagManager;
+        $this->authorizationChecker = $authorizationChecker;
     }
 
     public function normalize($object, $format = null, array $context = array())
@@ -22,7 +24,12 @@ class CustomerNormalizer implements NormalizerInterface
         $data = $this->normalizer->normalize($object, $format, $context);
 
         if (isset($data['tags']) && is_array($data['tags']) && count($data['tags']) > 0) {
-            $data['tags'] = $this->tagManager->expand($data['tags']);
+            // Only admins can see customer tags
+            if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+                $data['tags'] = $this->tagManager->expand($data['tags']);
+            } else {
+                $data['tags'] = [];
+            }
         }
 
         return $data;
