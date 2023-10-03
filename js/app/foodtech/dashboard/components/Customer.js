2df063df75b98c1bc7420aec0ff02a33044025d9
Alexandre Segura <mex.zktk@gmail.com>
2023-10-03 09:38:17 +0200

    Do not break if customer is null.

--- js/app/foodtech/dashboard/components/Customer.js
+++ js/app/foodtech/dashboard/components/Customer.js
@@ -5,7 +5,7 @@ export default ({ customer }) => {
   return (
     <>
       <i className="fa fa-user mr-1"></i>
-      <span>{ customer.username }</span>
+      <span>{ customer?.username }</span>
       { (Array.isArray(customer.tags) && customer.tags.length > 0) &&
         <span className="ml-1">
           { customer.tags.map((tag) => (
