60e8272360cae9bf9bdf0decc8ff41d08447e4da
Alexandre Segura <mex.zktk@gmail.com>
2023-11-16 07:08:00 +0100

    Do not install deps in storybook container.

--- docker/storybook/start.sh
+++ docker/storybook/start.sh
@@ -2,7 +2,4 @@
 
 cd /srv/coopcycle
 
-# https://docs.cypress.io/guides/getting-started/installing-cypress.html#Skipping-installation
-CYPRESS_INSTALL_BINARY=0 npm install
-
 node_modules/.bin/storybook dev -p 6006
