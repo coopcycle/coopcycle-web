acf2069a261b255a588b2ea18675715e8a15b092
Alexandre Segura <mex.zktk@gmail.com>
2023-11-21 15:42:44 +0100

    WIP

--- cube.js
+++ cube.js
@@ -1,6 +1,7 @@
 // https://cube.dev/docs/multitenancy-setup#multiple-db-instances-with-same-schema
 
 const PostgresDriver = require('@cubejs-backend/postgres-driver');
+const axios = require('axios')
 
 module.exports = {
   contextToAppId: ({ securityContext }) =>
@@ -9,11 +10,38 @@ module.exports = {
     new PostgresDriver({
       database: `${securityContext && securityContext.database ? securityContext.database : 'coopcycle'}`,
     }),
-  // https://cube.dev/docs/config#options-reference-scheduled-refresh-contexts
+  // https://cube.dev/docs/reference/configuration/config#repository_factory
+  repositoryFactory: ({ securityContext }) => {
+
+    return {
+      dataSchemaFiles: async () => {
+
+        const schemaFiles = []
+
+        const response = await axios.get(`${securityContext.base_url}/api/cube_data_schema_files`)
+
+        await Promise.all(response.data['hydra:member'].map(async (item) => {
+          const itemResponse = await axios.get(`${securityContext.base_url}${item['@id']}`)
+          schemaFiles.push(itemResponse.data)
+        }));
+
+        await Promise.resolve(schemaFiles.map((schemaFile) => {
+          return {
+            filename: schemaFile.id + '.js',
+            content: schemaFile.contents
+          }
+        }))
+      }
+    };
+  },
+  // Leaving scheduled_refresh_contexts unconfigured will lead to issues where the security context will be undefined.
+  // This is because there is no way for Cube to know how to generate a context without the required input.
+  // https://cube.dev/docs/reference/configuration/config#scheduled_refresh_contexts
   scheduledRefreshContexts: async () => [
     {
       securityContext: {
         database: 'coopcycle',
+        base_uri: 'http://nginx'
       },
     },
   ],
