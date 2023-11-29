27359293c7f54378c5152c9b01b08b4e2efdf413
Alexandre Segura <mex.zktk@gmail.com>
2023-11-29 10:38:43 +0100

    Fix static analysis.

--- src/Geography/CityZoneImporter.php
+++ src/Geography/CityZoneImporter.php
@@ -7,22 +7,22 @@ use AppBundle\Geography\CityZoneImporter;
 
 class CityZoneImporter
 {
-	/**
-	 * @param CityZoneImporterInterface[]
-	 */
-	public function __construct(private array $importers)
-	{}
+    /**
+     * @param CityZoneImporterInterface[] $importers
+     */
+    public function __construct(private array $importers)
+    {}
 
-	/**
-	 * @return CityZone[]
-	 */
-	public function import(string $url, string $type): array
-	{
-		if (isset($this->importers[$type])) {
+    /**
+     * @return CityZone[]
+     */
+    public function import(string $url, string $type): array
+    {
+        if (isset($this->importers[$type])) {
 
-			return $this->importers[$type]->import($url);
-		}
+            return $this->importers[$type]->import($url);
+        }
 
-		throw new \InvalidArgumentException(sprintf('Unknow importer "%s"', $type));
-	}
+        throw new \InvalidArgumentException(sprintf('Unknow importer "%s"', $type));
+    }
 }
