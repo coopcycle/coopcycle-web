2a9a6b2f16152a71f2b9108c4889cba3a81267c1
Alexandre Segura <mex.zktk@gmail.com>
2023-11-30 16:08:02 +0100

    Change fulfillment style, add skeleton loader.

--- js/app/restaurant/list.js
+++ js/app/restaurant/list.js
@@ -32,8 +32,17 @@ const FulfillmentBadge = ({ range }) => {
 
   return (
     <span className="restaurant-item__time-range rendered-badge">
-      <i className="fa fa-clock-o mr-2"></i>
-      <span>{ asText(range, false, true) }</span>
+      <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-clock"
+        width="44" height="44"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="#2c3e50" fill="none"
+        strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"/>
+        <path d="M12 7v5l3 3"/>
+      </svg>
+      { asText(range, false, true) }
     </span>
   )
 }
