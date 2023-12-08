252b2384bd56ae4c82fe7e9abb2a60c28da6f980
Alexandre Segura <mex.zktk@gmail.com>
2023-12-08 16:11:07 +0100

    Add lang parameter to Pixabay searches.

--- src/Pixabay/Client.php
+++ src/Pixabay/Client.php
@@ -11,10 +11,15 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class Client
 {
+    private static $acceptedLangs = [
+        'cs', 'da, de, en, es, fr, id, it, hu, nl, no, pl, pt, ro, sk, fi, sv, tr, vi, th, bg, ru, el, ja, ko, zh'
+    ];
+
     public function __construct(
         private HttpClientInterface $pixabayClient,
         private CacheInterface $appCache,
-        private SluggerInterface $slugger)
+        private SluggerInterface $slugger,
+        private string $lang = 'en')
     {}
 
     /**
@@ -22,11 +27,13 @@ class Client
      */
     public function search(string $query, int $page = 1)
     {
-        $cacheKey = sprintf('pixabay-search-%s-p%d', $this->slugger->slug($query), $page);
+        $lang = in_array($this->lang, self::$acceptedLangs) ? $this->lang : 'en';
+
+        $cacheKey = sprintf('pixabay-search-%s-%s-p%d', $lang, $this->slugger->slug($query), $page);
 
         // $this->appCache->delete($cacheKey);
 
-        return $this->appCache->get($cacheKey, function (ItemInterface $item) use ($query, $page) {
+        return $this->appCache->get($cacheKey, function (ItemInterface $item) use ($query, $page, $lang) {
 
             // To keep the Pixabay API fast for everyone, requests must be cached for 24 hours.
             // https://pixabay.com/api/docs/#api_rate_limit
@@ -39,7 +46,8 @@ class Client
                     'orientation' => 'horizontal',
                     'image_type' => 'photo',
                     'safesearch' => 'true',
-                    'category' => 'food'
+                    'category' => 'food',
+                    'lang' => $lang,
                 ]
             ]);
 
