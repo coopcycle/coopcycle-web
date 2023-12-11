f6ee99504650eef640bbd0c720cc76244c9e53be
Atala <alois.guillope@gmail.com>
2023-12-11 08:50:29 +0100

    clean: add index and not null on tour.date

--- app/DoctrineMigrations/Version20230807105004.php
+++ app/DoctrineMigrations/Version20230807105004.php
@@ -20,8 +20,7 @@ final class Version20230807105004 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE tour ADD date DATE');
-        $this->addSql('COMMENT ON COLUMN task_list.date IS NULL');
-        
+        $this->addSql('CREATE INDEX IDX_6AD1F969AA9E377A ON tour (date)');      
 
         // Get a Task from each Tour to set Tour's date to Task's `done_after` date. 
         $stmt = $this->connection->prepare('select distinct on (tour_id) tour_id, id, done_after from task where tour_id is not null;');
@@ -36,6 +35,9 @@ final class Version20230807105004 extends AbstractMigration
                 'id' => $row['tour_id'],
             ]);
         }
+
+        $this->addSql('ALTER TABLE tour ALTER COLUMN date SET NOT NULL');
+
     }
 
     public function down(Schema $schema): void
