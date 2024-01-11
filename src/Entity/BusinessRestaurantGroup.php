49bd03a00a3e2e2440aee97a470b01eb14c165c5
Lucas Ferraro <lferraro@fiqus.com>
2024-01-11 15:57:23 -0300

    new intermediate with metada entity

--- src/Entity/BusinessRestaurantGroup.php
+++ src/Entity/BusinessRestaurantGroup.php
@@ -2,10 +2,20 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 class BusinessRestaurantGroup extends LocalBusinessGroup
 {
     private $cutoffTime;
     private $businessAccount;
+    private $restaurantsWithMenu;
+
+    public function __construct()
+    {
+        $this->restaurantsWithMenu = new ArrayCollection();
+
+        parent::__construct();
+    }
 
     public function getCutoffTime()
     {
@@ -58,5 +68,33 @@ class BusinessRestaurantGroup extends LocalBusinessGroup
             return $this->restaurants->first->getAddress();
         }
         return null;
+    }
+
+    public function getRestaurantsWithMenu()
+    {
+        return $this->restaurantsWithMenu;
+    }
+
+    public function setRestaurantsWithMenu($restaurantsWithMenu)
+    {
+        $this->restaurantsWithMenu = $restaurantsWithMenu;
+
+        return $this;
+    }
+
+    /**
+     * @param BusinessRestaurantGroupRestaurantMenu $restaurantMenu
+     */
+    public function addRestaurantWithMenu(BusinessRestaurantGroupRestaurantMenu $restaurantMenu)
+    {
+        $this->restaurantsWithMenu->add($restaurantMenu);
+    }
+
+    /**
+     * @param BusinessRestaurantGroupRestaurantMenu $restaurantMenu
+     */
+    public function removeRestaurantWithMenu(BusinessRestaurantGroupRestaurantMenu $restaurantMenu): void
+    {
+        $this->restaurantsWithMenu->removeElement($restaurantMenu);
     }
 }
