4c4835ce6061a94604131c16fc2d60f7d076bc44
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-01-11 13:12:41 -0800

    generalise getCaller function

--- src/Service/LoggingUtils.php
+++ src/Service/LoggingUtils.php
@@ -4,19 +4,22 @@ namespace AppBundle\Service;
 
 class LoggingUtils
 {
-    public function getCaller(): string {
-        $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS,3);
+    public function getCallerAtFrame($frameNumber): string {
+        $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS,$frameNumber + 1);
 
+        $callerFunction = $backtrace[$frameNumber]['function'];
+        $callerPoint = $backtrace[$frameNumber - 1];
+        return sprintf('function: %s | file: %s | line: %s', $callerFunction, $callerPoint['file'], $callerPoint['line']);
+    }
+
+    public function getCaller(): string {
         /**
          * Example:
          * (0) | function: getCaller | file: /var/www/html/src/Controller/RestaurantController.php | line: 907
          * (1) | function: persistAndFlushCart | file: /var/www/html/src/Controller/RestaurantController.php | line: 639
          * (2) | function: addProductToCartAction | file: /var/www/html/vendor/symfony/symfony/src/Symfony/Component/HttpKernel/HttpKernel.php | line: 163
          */
-
-        $callerFunction = $backtrace[2]['function'];
-        $callerPoint = $backtrace[1];
-        return sprintf('function: %s | file: %s | line: %s', $callerFunction, $callerPoint['file'], $callerPoint['line']);
+        return $this->getCallerAtFrame(2);
     }
 
     public function getOrderId($order): string {
