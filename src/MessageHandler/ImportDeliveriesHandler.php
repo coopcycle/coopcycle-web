ad275c2d984608a88b4a020eaf5be39a0e60e792
Alexandre Segura <mex.zktk@gmail.com>
2024-01-16 15:27:54 +0100

    Flush deliveries by batches of 10.

--- src/MessageHandler/ImportDeliveriesHandler.php
+++ src/MessageHandler/ImportDeliveriesHandler.php
@@ -21,6 +21,8 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ImportDeliveriesHandler implements MessageHandlerInterface
 {
+    private static $batchSize = 10;
+
     public function __construct(
         private EntityManagerInterface $entityManager,
         private Filesystem $deliveryImportsFilesystem,
@@ -62,6 +64,7 @@ class ImportDeliveriesHandler implements MessageHandlerInterface
 
         $result = $this->spreadsheetParser->parse($tempnam);
 
+        $i = 0;
         foreach ($result->getData() as $rowNumber => $delivery) {
 
             // Validate data
@@ -92,6 +95,11 @@ class ImportDeliveriesHandler implements MessageHandlerInterface
             }
 
             $this->entityManager->persist($delivery);
+
+            if (($i++ % self::$batchSize) === 0) {
+                $this->entityManager->flush();
+                $this->entityManager->clear();
+            }
         }
 
         $this->entityManager->flush();
