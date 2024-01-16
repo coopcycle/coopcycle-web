b91b97a2ca86037b99bd9b775cd1a1d3da9a0f2e
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-01-16 15:58:58 -0800

    fixed: mock

--- src/Service/NullLoggingUtils.php
+++ src/Service/NullLoggingUtils.php
@@ -2,8 +2,15 @@
 
 namespace AppBundle\Service;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+
 class NullLoggingUtils extends LoggingUtils
 {
+    public function __construct()
+    {
+        parent::__construct(new RequestStack());
+    }
+
     public function getBacktrace(int $firstFrame = 1, int $lastFrame = 3): string
     {
         return 'function: ? | file: ? | line: ?';
