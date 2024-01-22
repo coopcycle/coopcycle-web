2a23f86b502952c77c17f83b28fcf8e8dc349c40
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-01-22 15:25:26 -0800

    updated chart.js and react-chartjs-2

--- js/app/metrics/components/DistributionOfTasksByPercentage.js
+++ js/app/metrics/components/DistributionOfTasksByPercentage.js
@@ -1,6 +1,7 @@
 import { QueryRenderer } from '@cubejs-client/react';
 import { Spin } from 'antd';
 import React from 'react';
+import 'chart.js/auto'; // ideally we should only import the component that we need: https://react-chartjs-2.js.org/docs/migration-to-v4/#tree-shaking
 import { Bar } from 'react-chartjs-2';
 import {getCubeDateRange} from "../utils";
 import { useDeepCompareMemo } from 'use-deep-compare'
