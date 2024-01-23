c299cf771959b366e95071d11fdb1139c6398418
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-01-23 14:56:08 -0800

    switched to a new (react 18) Client Rendering APIs: optins

--- js/app/optins/index.js
+++ js/app/optins/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next'
 import Modal from 'react-modal'
 
@@ -39,12 +39,11 @@ const init = async () => {
 
     const brandName = JSON.parse(container.dataset.brandName)
 
-    render(
+    const root = createRoot(container);
+    root.render(
         <I18nextProvider i18n={ i18n }>
           <AskForOptinsModal httpClient={httpClient} brandName={brandName} />
-        </I18nextProvider>,
-        container
-      )
+        </I18nextProvider>)
 }
 
 init()
