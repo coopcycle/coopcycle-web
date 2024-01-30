4e5129ffbb09111fa1e03ada24cc274ab41edc8b
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-01-29 16:42:55 -0800

    updated: menu layout

--- src/Controller/StorybookController.php
+++ src/Controller/StorybookController.php
@@ -14,6 +14,7 @@ class StorybookController extends AbstractController
         'restaurant_badge' => 'components/restaurant/restaurant_badge/restaurant_badge.html.twig',
         'restaurant_tag' => 'components/restaurant/restaurant_tag/restaurant_tag.html.twig',
         'product' => 'components/restaurant/product/menu_item.html.twig',
+        'product_badge' => 'components/restaurant/product_badge/product_badge.html.twig',
     ];
 
 	/**
