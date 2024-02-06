920aaff2848465a478786986dd0ad801d047824f
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-02-06 12:04:51 -0800

    product details modal: updated layout

--- js/app/checkout/components/ProductDetails/ProductModalHeader.js
+++ js/app/checkout/components/ProductDetails/ProductModalHeader.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProductModalHeader = ({ name, onClickClose }) => {
 
   return (
-    <header className="d-flex justify-content-between align-items-center pb-3">
+    <header className="d-flex justify-content-between align-items-center">
       <h4 className="m-0 font-weight-bold">{name}</h4>
       <a className="pl-3 modal-close" href="#" onClick={e => {
         e.preventDefault()
