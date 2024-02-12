1dd9094c71b412fb15039e8632fdd3a67aed10a4
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-02-12 11:37:30 -0800

    render React in StrictMode

--- js/app/restaurant/item.js
+++ js/app/restaurant/item.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux'
 import { I18nextProvider } from 'react-i18next'
@@ -110,12 +110,14 @@ const init = function() {
 
   const root = createRoot(container);
   root.render(
-    <Provider store={ store }>
-      <I18nextProvider i18n={ i18n }>
-        <Cart />
-        <ProductOptionsModal />
-      </I18nextProvider>
-    </Provider>
+    <StrictMode>
+      <Provider store={ store }>
+        <I18nextProvider i18n={ i18n }>
+          <Cart />
+          <ProductOptionsModal />
+        </I18nextProvider>
+      </Provider>
+    </StrictMode>
   )
 
 }
