db9a3c1490a0dda9f4e9002f89cdffb3cadeae12
Alexandre Segura <mex.zktk@gmail.com>
2024-02-13 17:33:41 +0100

    Add show_deposit param when needed.

--- src/Twig/LoopeatRuntime.php
+++ src/Twig/LoopeatRuntime.php
@@ -11,15 +11,18 @@ class LoopeatRuntime implements RuntimeExtensionInterface
     public function __construct(private Client $client)
     {}
 
-    public function getAuthorizationUrl(OrderInterface $order, int $requiredCredits = 0)
+    public function getAuthorizationUrl(OrderInterface $order, int $requiredCredits = 0, bool $showDeposit = false)
     {
         $params = [
             'state' => $this->client->createStateParamForOrder($order),
-            'show_deposit' => 'true',
         ];
 
         if ($requiredCredits > 0) {
             $params['required_credits_cents'] = $requiredCredits;
+        }
+
+        if ($showDeposit) {
+            $params['show_deposit'] = 'true';
         }
 
         return $this->client->getOAuthAuthorizeUrl($params);
