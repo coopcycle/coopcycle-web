64c105b2063d1b21bb5ca052e6bed0c36e600b6a
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-02-19 17:55:13 -0800

    added: cart items stories

---
+++ js/app/restaurant/components/Order/CartItems.stories.js
@@ -0,0 +1,161 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStoreFromPreloadedState } from '../../redux/store'
+import CartItems from './CartItems'
+
+export default {
+  title: 'Foodtech/4. Order/1. Cart Items',
+  tags: [ 'autodocs' ],
+  component: CartItems,
+
+  decorators: [
+    (Story, context) => {
+      const store = createStoreFromPreloadedState(context.args._store)
+
+      return (
+        <Provider store={ store }>
+          {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */ }
+          <Story />
+        </Provider>
+      )
+    },
+  ],
+}
+
+export const Basic = {
+  args: {
+    _store: {
+      cart: {
+        items: [
+          {
+            id: 1,
+            name: 'Pizza Margherita',
+            total: 1000,
+            quantity: 1,
+            adjustments: {},
+            vendor: {
+              name: 'Pizza Hut',
+            },
+            player: null,
+          },
+          {
+            id: 2,
+            name: 'Pizza Pepperoni',
+            total: 1000,
+            quantity: 1,
+            adjustments: {},
+            vendor: {
+              name: 'Pizza Hut',
+            },
+            player: null,
+          },
+        ],
+      },
+      isPlayer: false,
+      player: {
+        player: null,
+      },
+    },
+  },
+}
+
+export const EmptyState = {
+  args: {
+    _store: {
+      cart: {
+        items: [],
+      },
+      isPlayer: false,
+      player: {
+        player: null,
+      },
+    },
+  },
+}
+
+export const HubOrder = {
+  args: {
+    _store: {
+      cart: {
+        items: [
+          {
+            id: 1,
+            name: 'Pizza Margherita',
+            total: 1000,
+            quantity: 1,
+            adjustments: {},
+            vendor: {
+              name: 'Pizza Hut',
+            },
+            player: null,
+          },
+          {
+            id: 2,
+            name: 'Pizza Pepperoni',
+            total: 1000,
+            quantity: 1,
+            adjustments: {},
+            vendor: {
+              name: 'Pizza Hut',
+            },
+            player: null,
+          },
+          {
+            id: 3,
+            name: 'Sushi Box',
+            total: 1000,
+            quantity: 1,
+            adjustments: {},
+            vendor: {
+              name: 'Sushi Restaurant',
+            },
+            player: null,
+          },
+        ],
+      },
+      isPlayer: false,
+      player: {
+        player: null,
+      },
+    },
+  },
+}
+
+export const GroupOrder = {
+  args: {
+    _store: {
+      cart: {
+        items: [
+          {
+            id: 1,
+            name: 'Pizza Margherita',
+            total: 1000,
+            quantity: 1,
+            adjustments: {},
+            vendor: {
+              name: 'Pizza Hut',
+            },
+            player: null,
+          },
+          {
+            id: 2,
+            name: 'Pizza Pepperoni',
+            total: 1000,
+            quantity: 1,
+            adjustments: {},
+            vendor: {
+              name: 'Pizza Hut',
+            },
+            player: {
+              username: 'John Doe',
+            },
+          },
+        ],
+      },
+      isPlayer: false,
+      player: {
+        player: null,
+      },
+    },
+  },
+}
