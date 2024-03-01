6a575f7cb98d417c2f72f8b1d23dcfb942002d63
Antoine Bagnaud <contact@antoinebagnaud.me>
2024-03-01 15:32:06 +0100

    feat: allow restaurant to set custom failure reasons
    Fixes: #3837

--- src/Entity/OrganizationRepository.php
+++ src/Entity/OrganizationRepository.php
@@ -7,4 +7,18 @@ use Doctrine\ORM\EntityRepository;
 
 class OrganizationRepository extends EntityRepository
 {
+
+    public function reverseFindByOrganizarionID($org)
+    {
+        $query = $this->createQueryBuilder('o')
+        ->select('s.id as store_id', 'r.id as restaurant_id')
+        ->leftJoin(Store::class, 's', 'WITH', 's.organization = o.id')
+        ->leftJoin(LocalBusiness::class, 'r', 'WITH', 'r.organization = o.id')
+        ->setMaxResults(1)
+        ->where('o = :org')
+        ->setParameter('org', $org);
+
+        return $query->getQuery()->getResult();
+    }
+
 }
