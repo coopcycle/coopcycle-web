b5220c2553e8136c620c28e8d2e23c876e716c84
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-03-12 12:11:33 -0700

    updated webpack config

--- docker/webpack/start.sh
+++ docker/webpack/start.sh
@@ -5,4 +5,4 @@ cd /srv/coopcycle
 # https://docs.cypress.io/guides/getting-started/installing-cypress.html#Skipping-installation
 CYPRESS_INSTALL_BINARY=0 npm install
 
-node_modules/.bin/encore dev-server --public http://localhost:8080
+node_modules/.bin/encore dev-server --public http://206.189.30.248:8080 --no-static-watch --no-live-reload --no-hot
