ac41e62fcaf50d859eeb1e64a940b2a5accc1c9c
Lucas Ferraro <lferraro@fiqus.com>
2024-03-19 16:40:24 -0300

    fix migration

--- app/DoctrineMigrations/Version20240223142423.php
+++ app/DoctrineMigrations/Version20240223142423.php
@@ -16,9 +16,11 @@ final class Version20240223142423 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE restaurant ADD edenred_enabled BOOLEAN DEFAULT \'false\' NOT NULL');
-        $this->addSql('ALTER TABLE restaurant ADD edenred_sync_sent BOOLEAN DEFAULT \'false\' NOT NULL');
-        $this->addSql('ALTER TABLE restaurant ADD edenred_trcard_enabled BOOLEAN DEFAULT \'false\' NOT NULL');
+        $this->addSql('ALTER TABLE restaurant ADD edenred_enabled BOOLEAN DEFAULT \'false\'');
+        $this->addSql('ALTER TABLE restaurant ADD edenred_sync_sent BOOLEAN DEFAULT \'false\'');
+        $this->addSql('ALTER TABLE restaurant ADD edenred_trcard_enabled BOOLEAN DEFAULT \'false\'');
+
+        $this->addSql('UPDATE restaurant SET edenred_enabled = \'f\', edenred_sync_sent = \'f\', edenred_trcard_enabled = \'f\'');
     }
 
     public function down(Schema $schema): void
