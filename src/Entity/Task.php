8953e9e7c9533efbdbe44874d38faf6318e53afa
Atala <alois.guillope@gmail.com>
2024-03-28 17:03:16 +0100

    clean: use custom query to get package
    issue #4086

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -468,6 +468,11 @@ class Task implements TaggableInterface, OrganizationAwareInterface, PackagesAwa
         return $this->id;
     }
 
+    public function getDeliveryId()
+    {
+        return $this->delivery_id;
+    }
+
     public function getDelivery()
     {
         return $this->delivery;
