4193ea3e33b7167d9c7a9b0d0a2f5dc514f8d7ec
Antoine Bagnaud <contact@antoinebagnaud.me>
2024-04-03 15:58:51 +0200

    fix small issue with sidebar

--- assets/react/controllers/OrderDetails.jsx
+++ assets/react/controllers/OrderDetails.jsx
@@ -1,87 +1,139 @@
-import React from 'react'
-import moment from 'moment'
-import './OrderDetails.scss'
+import React from "react";
+import moment from "moment";
+import { Button } from "antd";
+import "./OrderDetails.scss";
 
-//TODO: Make money format dynamic
-function money(amount) {
-  return new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR'}).format(amount/100)
+function money(amount, { coutry, currencyCode }) {
+  return new Intl.NumberFormat(coutry, {
+    style: "currency",
+    currency: currencyCode,
+  }).format(amount / 100);
 }
 
-function formatTime (task) {
-  return moment(task.after).format('LL');
+function formatTime(task) {
+  return moment(task.after).format("LL");
 }
 
 function heading(task, delivery, order) {
+  const button = link => (
+    <Button
+      onClick={() => window.open(link, "_blank")}
+      type="dashed"
+      style={{ float: "right" }}
+      shape="circle"
+      size="small"
+      icon={<i className="fa fa-external-link" style={{ fontSize: "12px" }} />}
+    />
+  );
+  const header = (title, btn = null) => (
+    <h4 style={{ lineHeight: "24px" }}>
+      {title}
+      {btn}
+    </h4>
+  );
   if (order?.number) {
-    return `Order N° ${order.number}`
+    const link = window.Routing.generate("admin_order", { id: order.id });
+    return header(`Order N° ${order.number}`, button(link));
   }
 
   if (order?.id) {
-    return `Order #${order.id}`
+    const link = window.Routing.generate("admin_order", { id: order.id });
+    return header(`Order #${order.id}`, button(link));
   }
 
   if (delivery?.id) {
-    return `Delivery #${delivery.id}`
+    const link = window.Routing.generate("admin_delivery", { id: order.id });
+    return header(`Delivery #${delivery.id}`, button(link));
   }
 
-  return `Task: #${task.id}`
+  return header(`Task #${task.id}`);
 }
 
 function customerShowName(customer) {
-  let customerName = customer?.username
+  let customerName = customer?.username;
   if (customer?.fullName != null) {
-    customerName = customer.fullName
+    customerName = customer.fullName;
   }
-  return <p title={customer?.username}>Customer<span>{customerName}</span></p>
+  return (
+    <p title={customer?.username}>
+      Customer<span>{customerName}</span>
+    </p>
+  );
 }
 
-function showAdjustement(adjustments, adjustmentType) {
-  const total = adjustments[adjustmentType].reduce((total, adjustment) => total + adjustment.amount, 0)
+function showAdjustement(adjustments, adjustmentType, config) {
+  const total = adjustments[adjustmentType].reduce(
+    (total, adjustment) => total + adjustment.amount,
+    0,
+  );
   if (total === 0) {
     return;
   }
-  return adjustments[adjustmentType].map(adjustment => (
-    <p key={adjustment.id}>{adjustment.label}<span>{money(adjustment.amount)}</span></p>
-  ))
+  return adjustments[adjustmentType].map((adjustment) => (
+    <p key={adjustment.id}>
+      {adjustment.label}
+      <span>{money(adjustment.amount, config)}</span>
+    </p>
+  ));
 }
 
-function showOrderDetails(order) {
-      return <><h5>Order Details</h5>
-      <p>Subtotal<span>{money(order.itemsTotal)}</span></p>
-      {showAdjustement(order.adjustments, 'delivery')}
-      {showAdjustement(order.adjustments, 'tax')}
-      <p>Total<span>{money(order.total)}</span></p>
-      <hr/></>
+function showOrderDetails(order, config) {
+  return (
+    <>
+      <h5>Order Details</h5>
+      <p>
+        Subtotal<span>{money(order.itemsTotal, config)}</span>
+      </p>
+      {showAdjustement(order.adjustments, "delivery", config)}
+      {showAdjustement(order.adjustments, "tax", config)}
+      <p>
+        Total<span>{money(order.total, config)}</span>
+      </p>
+      <hr />
+    </>
+  );
 }
 
 function showCustomerDetails(customer) {
-    return <><h5>Customer information</h5>
+  return (
+    <>
+      <h5>Customer information</h5>
       {customerShowName(customer)}
-      {customer?.email && <p>Email<span>{customer?.email}</span></p>}
-      {customer?.telephone && <p>Phone<span>{customer?.telephone}</span></p>}</>
-
+      {customer?.email && (
+        <p>
+          Email<span>{customer?.email}</span>
+        </p>
+      )}
+      {customer?.telephone && (
+        <p>
+          Phone<span>{customer?.telephone}</span>
+        </p>
+      )}
+    </>
+  );
 }
 
-export default function ({task, delivery, order}) {
-  task = JSON.parse(task)
-  delivery = JSON.parse(delivery)
-  order = JSON.parse(order)
+export default function ({ task, delivery, order }) {
+  task = JSON.parse(task);
+  delivery = JSON.parse(delivery);
+  order = JSON.parse(order);
 
-  console.log(task, delivery, order)
+  const { coutry, currencyCode } = document.body.dataset;
+  console.log(task, delivery, order);
 
   return (
     <div className="order-details-card">
       <h4>{heading(task, delivery, order)}</h4>
       <p className="text-muted">Date: {formatTime(task)}</p>
-      <hr/>
-      {order && showOrderDetails(order)}
+      <hr />
+      {order && showOrderDetails(order, { coutry, currencyCode })}
       <h5>Shipping information</h5>
       <p>{task.address.name}</p>
       <p>{task.address.streetAddress}</p>
       <p>{task.address.telephone}</p>
       {task.weight && <p>{task.weight} kg</p>}
-      <hr/>
+      <hr />
       {order?.customer && showCustomerDetails(order.customer)}
-      </div>
-  )
+    </div>
+  );
 }
