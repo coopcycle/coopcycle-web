96f07b5ad5c80bc69f3435f9a8f752fc4676838d
Antoine Bagnaud <contact@antoinebagnaud.me>
2024-04-05 11:08:54 +0200

    redesign badge

--- js/app/dashboard/components/TaskStatusBadge.js
+++ js/app/dashboard/components/TaskStatusBadge.js
@@ -5,24 +5,24 @@ export default function ({ task }) {
   const { status } = task;
   return (
     <span
-      className={classNames("label", {
-        "label-default": status == "TODO",
-        "label-info": status == "DOING",
-        "label-warning": status == "FAILED",
-        "label-success": status == "DONE",
-        "label-danger": status == "CANCELLED",
-      })}
+      style={{
+        borderRadius: "18px",
+        padding: "2px 8px",
+        border: "1px dashed #d9d9d9",
+        textTransform: "capitalize",
+        fontSize: "0.9em",
+      }}
     >
       <i
         className={classNames("fa", {
-          "fa-clock-o": status == "TODO",
-          "fa-bicycle": status == "DOING",
-          "fa-exclamation-triangle": status == "FAILED",
-          "fa-check": status == "DONE",
-          "fa-times": status == "CANCELLED",
+          "fa-clock-o text-default": status == "TODO",
+          "fa-bicycle text-info": status == "DOING",
+          "fa-exclamation-triangle text-warning": status == "FAILED",
+          "fa-check text-success": status == "DONE",
+          "fa-times text-danger": status == "CANCELLED",
         })}
       ></i>{" "}
-      {status}
+      {status.toLowerCase()}
     </span>
   );
 }
