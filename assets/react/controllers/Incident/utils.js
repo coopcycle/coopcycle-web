a2523eb0ee68a7337fb2f7786c7d476dfb0900f5
Antoine Bagnaud <contact@antoinebagnaud.me>
2024-04-12 17:19:56 +0200

    fix OrderDetails

--- assets/react/controllers/Incident/utils.js
+++ assets/react/controllers/Incident/utils.js
@@ -6,6 +6,14 @@ export function money(amount) {
   }).format(amount / 100);
 }
 
+export function weight(amount) {
+  const { coutry } = document.body.dataset;
+  return new Intl.NumberFormat(coutry, {
+    style: "unit",
+    unit: "kilogram",
+  }).format(amount / 1000);
+}
+
 export async function incidentAction(id, payload) {
   return fetch(window.Routing.generate("api_incidents_action_item", { id }), {
     method: "PUT",
