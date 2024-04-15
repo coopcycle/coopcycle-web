caf8d096d754a7196603695ca7301ae488d858ec
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-04-15 12:04:44 -0700

    send a different notification when a restaurant auto-accepts orders

--- src/Domain/Order/Event/OrderAccepted.php
+++ src/Domain/Order/Event/OrderAccepted.php
@@ -6,6 +6,12 @@ use AppBundle\Domain\DomainEvent;
 use AppBundle\Domain\HasIconInterface;
 use AppBundle\Domain\Order\Event;
 
+/**
+ * An event that occurs when a user accepts an order.
+ * IMPORTANT: This event is dispatched before the order is moved to the "accepted" state.
+ * If you want to be notified when the order is in the "accepted" state, listen to OrderStateChanged event instead.
+ * @see OrderStateChanged
+ */
 class OrderAccepted extends Event implements DomainEvent, HasIconInterface
 {
     public static function messageName(): string
