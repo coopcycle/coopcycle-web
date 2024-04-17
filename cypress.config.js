f0304b4f91fa038d36be54b11d2c139268a5db85
Atala <alois.guillope@gmail.com>
2024-04-17 09:43:40 +0200

    fix: fix component testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,37 @@
+// override means that we won't override options from the command line
+require('dotenv').config({override: false})
+
+let env = process.env
+
 module.exports = {
   chromeWebSecurity: false,
+
   env: {
-    COMMAND_PREFIX: 'docker compose exec -T php',
+    ...env,
+    COMMAND_PREFIX: "docker compose exec -T php",
     coverage: false,
-    STRIPE_CONNECT_CLIENT_ID: '',
-    STRIPE_SECRET_KEY: '',
-    STRIPE_PUBLISHABLE_KEY: ''
   },
-  nodeVersion: 'system',
+
+  nodeVersion: "system",
   viewportWidth: 1600,
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://localhost:9080',
+    baseUrl: "http://localhost:9080",
   },
+
   component: {
     setupNodeEvents(on, config) {},
   },
-}
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+};
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,21 +11,11 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-require('dotenv').config()
-
 const wp = require('@cypress/webpack-preprocessor')
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-
-  config.env.GEOCODE_EARTH_API_KEY = process.env.GEOCODE_EARTH_API_KEY
-  config.env.LOCATIONIQ_ACCESS_TOKEN = process.env.LOCATIONIQ_ACCESS_TOKEN
-
-  config.env.STRIPE_PUBLISHABLE_KEY = process.env.STRIPE_PUBLISHABLE_KEY
-  config.env.STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
-  config.env.STRIPE_CONNECT_CLIENT_ID = process.env.STRIPE_CONNECT_CLIENT_ID
-
 
   config.env.webpackFilename = 'webpack.cypress.js'
 
@@ -36,8 +26,7 @@ module.exports = (on, config) => {
 
   on('file:preprocessor', wp(options))
 
-
   // IMPORTANT to return the config object
-  // with the any changed environment variables
+  // with the changed environment variables
   return config
 }
