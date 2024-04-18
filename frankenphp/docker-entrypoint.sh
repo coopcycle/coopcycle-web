6a0e3f80419a1b20bfdaaa49c64ab41982345081
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-04-17 17:24:50 -0700

    fixed (some) missing configs

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -2,25 +2,23 @@
 set -e
 
 if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	# Install the project the first time PHP is started
-	# After the installation, the following block can be deleted
-#	if [ ! -f composer.json ]; then
-#		rm -Rf tmp/
-#		composer create-project "symfony/skeleton $SYMFONY_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction --no-install
-#
-#		cd tmp
-#		cp -Rp . ..
-#		cd -
-#		rm -Rf tmp/
-#
-#		composer require "php:>=$PHP_VERSION" runtime/frankenphp-symfony
-#		composer config --json extra.symfony.docker 'true'
-#
-#		if grep -q ^DATABASE_URL= .env; then
-#			echo "To finish the installation please press Ctrl+C to stop Docker Compose and run: docker compose up --build -d --wait"
-#			sleep infinity
-#		fi
-#	fi
+
+  if [ ! -d var/jwt ]; then
+      mkdir -p var/jwt
+  fi
+
+  if [ ! -f var/jwt/private.pem ]; then
+      printf "\e[0;32mGenerating RSA key to encrypt webtokens..\e[0m\n"
+      openssl genrsa -out var/jwt/private.pem -passout pass:coursiers -aes256 4096;
+      chgrp www-data var/jwt/private.pem
+      chmod 644 var/jwt/private.pem
+  fi
+
+  if [ ! -f var/jwt/public.pem ]; then
+      openssl rsa -pubout -passin pass:coursiers -in var/jwt/private.pem -out var/jwt/public.pem
+      chgrp www-data var/jwt/public.pem
+      chmod 644 var/jwt/public.pem
+  fi
 
 	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
 		composer install --prefer-dist --no-progress --no-interaction
