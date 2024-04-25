db641319b1b28ef1bf01b8650ddf4e303055578e
Atala <alois.guillope@gmail.com>
2024-04-25 09:32:15 +0200

    fix: cypress, actually wait for the input to be visible

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -48,7 +48,10 @@ Cypress.Commands.add('searchAddress', (selector, search, match) => {
   cy.get(selector)
     .should('be.visible')
 
-  cy.wait(1000)
+  cy.wait(500)
+
+  cy.get(`${selector} input[type="search"]`)
+  .should('be.visible')
 
   cy.get(`${selector} input[type="search"]`)
     .eq(1)  // take the 2nd input on the restaurant page. to be changed when fix for https://github.com/coopcycle/coopcycle-web/issues/4149
