e3fa2b5cc993e8cd11c068e654fbd158accc9ea3
Atala <alois.guillope@gmail.com>
2024-05-15 15:29:22 +0200

    clean: factorize display of Applications in one component

--- assets/react/controllers/PricingRuleSetApplications.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Impl from '../../../js/app/components/Applications'
-
-export default function PricingRuleSetApplications(props) {
-  const { pricingRuleSetId } = props,
-    url = window.Routing.generate('api_pricing_rule_sets_applications_item', {id: pricingRuleSetId})
-  props = {...props, url}
-  return (<Impl {...props} />)
-}
