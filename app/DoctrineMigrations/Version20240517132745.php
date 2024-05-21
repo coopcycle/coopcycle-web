37a9fb28093942a8746f69b6960a7bac4f1a5241
Antoine Bagnaud <contact@antoinebagnaud.me>
2024-05-21 14:01:39 +0200

    feat(multitrans): add unique index on `transporter`

--- app/DoctrineMigrations/Version20240517132745.php
+++ app/DoctrineMigrations/Version20240517132745.php
@@ -19,14 +19,16 @@ final class Version20240517132745 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE store ADD transporter VARCHAR(255) DEFAULT \'NULL\'');
+        $this->addSql('ALTER TABLE store ADD transporter VARCHAR(255)');
         $this->addSql('UPDATE store SET transporter = \'DBSCHENKER\' WHERE db_schenker = true');
         $this->addSql('ALTER TABLE store DROP db_schenker');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_FF575877A036E2D4 ON store (transporter)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_FF575877A036E2D4');
         $this->addSql('ALTER TABLE store ADD db_schenker BOOLEAN DEFAULT \'false\' NOT NULL');
         $this->addSql('UPDATE store SET db_schenker = true WHERE transporter = \'DBSCHENKER\'');
         $this->addSql('ALTER TABLE store DROP transporter');
