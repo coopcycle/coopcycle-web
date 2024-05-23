91ca2ac36fbbfb30d5a09c104c0c279a63dce96f
Antoine Bagnaud <contact@antoinebagnaud.me>
2024-05-23 16:50:56 +0200

    test: add more cases

--- tests/AppBundle/Transporter/SyncTransportersCommandTest.php
+++ tests/AppBundle/Transporter/SyncTransportersCommandTest.php
@@ -5,6 +5,7 @@ namespace Tests\AppBundle\Transporter;
 use AppBundle\Command\SyncTransportersCommand;
 use AppBundle\Entity\Base\GeoCoordinates;
 use AppBundle\Entity\Delivery;
+use AppBundle\Entity\Edifact\EDIFACTMessage;
 use AppBundle\Entity\Task;
 use AppBundle\Service\SettingsManager;
 use AppBundle\Transporter\ImportFromPoint;
@@ -30,7 +31,12 @@ class SyncTransportersCommandTest extends KernelTestCase {
     UNA:+,? ' UNB+UNOC:1+123456789:22+987654321:22+240325:1951+2206' UNH+1+SCONTR:3:2:GT:GTF210+ACG' BGM++240325' NAD+FW+12345678900935:05++DBSCHENKER TESTING INC' DTM+DEP+240325' NAD+DP+98765432100010:05++COOPCYCLE TESTING INC' TSR+++3' CAG+P+V' TDT++++3' DOC+730+++ACG+2278663' UNS+D' RFF+CN+JOY0123456789' GID++1:23+1:21' MSE+CGW+15:KG' NAD+CN+++JOHN DOE:ZIMP COMPANY+64 RUE ALEXANDRE DUMAS+PARIS++75+FR' CTA+IC+:JOHN DOE+06 01 02 03 04:AL' NAD+CO+++HOME DEPOT+54 ROUTE DE TREGUIER:BP 8+LOUANNEC++22+FR' DTM+DES+240322' NAD+FW+12345678900935:05++DBSCHENKER TESTING INC+LE BREHAT:ALLEE DES CHATELETS+PLOUFRAGAN++22440+FR' CAG+P+V+++++++++227004' TSR++D:E+3' TXT+DEL+TEL ?: 06 01 02 03 04 POUR PRENDRE UN RENDEZ VOUS DE LIVRAISON' GDS+G+DIVERS' PCI+23' GIN+BN+*2222121907222700470100691001300' DOC+WBL::JOY0123456789+++ACG+70100691+219072' DOC+824+++PRI+FRSBK830689437' UNS+S' UNT+26+1' UNZ+1+2206'
     EDI;
 
-    const FS_MASK = 'testing';
+    const INVALID_ADDRESS_EDI_SAMPLE = <<<EDI
+    UNA:+,? ' UNB+UNOC:1+123456789:22+987654321:22+240325:1951+2206' UNH+1+SCONTR:3:2:GT:GTF210+ACG' BGM++240325' NAD+FW+12345678900935:05++DBSCHENKER TESTING INC' DTM+DEP+240325' NAD+DP+98765432100010:05++COOPCYCLE TESTING INC' TSR+++3' CAG+P+V' TDT++++3' DOC+730+++ACG+2278663' UNS+D' RFF+CN+JOY0123456789' GID++1:23+1:21' MSE+CGW+15:KG' NAD+CN+++JOHN DOE:ZIMP COMPANY+INVALID ADDRESS+VOID CITY++00+FR' NAD+CO+++HOME DEPOT+54 ROUTE DE TREGUIER:BP 8+LOUANNEC++22+FR' DTM+DES+240322' NAD+FW+12345678900935:05++DBSCHENKER TESTING INC+LE BREHAT:ALLEE DES CHATELETS+PLOUFRAGAN++22440+FR' CAG+P+V+++++++++227004' TSR++D:E+3' GDS+G+DIVERS' PCI+23' GIN+BN+*2222121907222700470100691001300' DOC+WBL::JOY0123456789+++ACG+70100691+219072' DOC+824+++PRI+FRSBK830689437' UNS+S' UNT+26+1' UNZ+1+2206'
+    EDI;
+
+    const FS_MASK_DBS = 'testingdbs';
+    const FS_MASK_BMV = 'testingbmv';
 
 
     protected EntityManagerInterface $entityManager;
@@ -86,13 +92,24 @@ class SyncTransportersCommandTest extends KernelTestCase {
                     'name' => 'DBSchenker test',
                     'legal_name' => 'DBSchenker Testing Inc.',
                     'legal_id' => '0000011',
-                    'fs_mask' => self::FS_MASK,
+                    'fs_mask' => self::FS_MASK_DBS,
+                    'sync_uri' => $this->syncFs,
+                ],
+                'BMV' => [
+                    'enabled' => true,
+                    'name' => 'BMV test',
+                    'legal_name' => 'BMV Testing Inc.',
+                    'legal_id' => '0000022',
+                    'fs_mask' => self::FS_MASK_BMV,
                     'sync_uri' => $this->syncFs,
                 ]
             ]);
 
-        $this->syncFs->createDirectory(sprintf('to_%s', self::FS_MASK));
-        $this->syncFs->createDirectory(sprintf('from_%s', self::FS_MASK));
+        $this->syncFs->createDirectory(sprintf('to_%s', self::FS_MASK_DBS));
+        $this->syncFs->createDirectory(sprintf('from_%s', self::FS_MASK_DBS));
+
+        $this->syncFs->createDirectory(sprintf('to_%s', self::FS_MASK_BMV));
+        $this->syncFs->createDirectory(sprintf('from_%s', self::FS_MASK_BMV));
 
     }
 
@@ -108,6 +125,210 @@ class SyncTransportersCommandTest extends KernelTestCase {
         );
     }
 
+    //
+    // TESTING CONFIGURATIONS
+    //
+
+    public function testMissconfiguredInstanceLatLng(): void
+    {
+        $settingManager = $this->prophesize(SettingsManager::class);
+    $settingManager->get('latlng')
+            ->willReturn(null);
+    $settingManager->get('company_legal_name')
+            ->willReturn('Coopcycle Testing Inc.');
+    $settingManager->get('company_legal_id')
+            ->willReturn('4447190000');
+        $params = $this->prophesize(ParameterBagInterface::class);
+    $params->get('transporters_config')
+            ->willReturn([
+                'DBSCHENKER' => [
+                    'enabled' => true,
+                    'name' => 'DBSchenker test',
+                    'legal_name' => 'DBSchenker Testing Inc.',
+                    'legal_id' => '0000011',
+                    'fs_mask' => self::FS_MASK_DBS,
+                    'sync_uri' => $this->syncFs,
+                ]
+            ]);
+
+        $command = new SyncTransportersCommand(
+            $this->entityManager,
+            $params->reveal(),
+            $settingManager->reveal(),
+            self::$container->get(ImportFromPoint::class),
+            self::$container->get(ReportFromCC::class),
+            $this->edifactFs
+        );
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Invalid lat-lng setting');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'transporter' => 'DBSCHENKER'
+        ]);
+    }
+
+    public function testMissconfiguredInstanceLegalName(): void
+    {
+        $settingManager = $this->prophesize(SettingsManager::class);
+        $settingManager->get('latlng')
+            ->willReturn('48.8534,2.3488');
+        $settingManager->get('company_legal_name')
+            ->willReturn(null);
+        $settingManager->get('company_legal_id')
+            ->willReturn('4447190000');
+        $params = $this->prophesize(ParameterBagInterface::class);
+        $params->get('transporters_config')
+            ->willReturn([
+                'DBSCHENKER' => [
+                    'enabled' => true,
+                    'name' => 'DBSchenker test',
+                    'legal_name' => 'DBSchenker Testing Inc.',
+                    'legal_id' => '0000011',
+                    'fs_mask' => self::FS_MASK_DBS,
+                    'sync_uri' => $this->syncFs,
+                ]
+            ]);
+
+        $command = new SyncTransportersCommand(
+            $this->entityManager,
+            $params->reveal(),
+            $settingManager->reveal(),
+            self::$container->get(ImportFromPoint::class),
+            self::$container->get(ReportFromCC::class),
+            $this->edifactFs
+        );
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Company name not set');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'transporter' => 'DBSCHENKER'
+        ]);
+    }
+
+    public function testMissconfiguredInstanceLegalId(): void
+    {
+        $settingManager = $this->prophesize(SettingsManager::class);
+        $settingManager->get('latlng')
+            ->willReturn('48.8534,2.3488');
+        $settingManager->get('company_legal_name')
+            ->willReturn('Coopcycle Testing Inc.');
+        $settingManager->get('company_legal_id')
+            ->willReturn(null);
+        $params = $this->prophesize(ParameterBagInterface::class);
+        $params->get('transporters_config')
+            ->willReturn([
+                'DBSCHENKER' => [
+                    'enabled' => true,
+                    'name' => 'DBSchenker test',
+                    'legal_name' => 'DBSchenker Testing Inc.',
+                    'legal_id' => '0000011',
+                    'fs_mask' => self::FS_MASK_DBS,
+                    'sync_uri' => $this->syncFs,
+                ]
+            ]);
+
+        $command = new SyncTransportersCommand(
+            $this->entityManager,
+            $params->reveal(),
+            $settingManager->reveal(),
+            self::$container->get(ImportFromPoint::class),
+            self::$container->get(ReportFromCC::class),
+            $this->edifactFs
+        );
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Company ID not set');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'transporter' => 'DBSCHENKER'
+        ]);
+    }
+
+    public function testMissconfiguredTransporter(): void
+    {
+        $settingManager = $this->prophesize(SettingsManager::class);
+        $settingManager->get('latlng')
+            ->willReturn('48.8534,2.3488');
+        $settingManager->get('company_legal_name')
+            ->willReturn('Coopcycle Testing Inc.');
+        $settingManager->get('company_legal_id')
+            ->willReturn('4447190000');
+        $params = $this->prophesize(ParameterBagInterface::class);
+        $params->get('transporters_config')
+            ->willReturn([
+                'FOOBAR' => [
+                    'enabled' => true,
+                    'name' => 'DBSchenker test',
+                    'legal_name' => 'DBSchenker Testing Inc.',
+                    'legal_id' => '0000011',
+                    'fs_mask' => self::FS_MASK_DBS,
+                    'sync_uri' => $this->syncFs,
+                ]
+            ]);
+
+        $command = new SyncTransportersCommand(
+            $this->entityManager,
+            $params->reveal(),
+            $settingManager->reveal(),
+            self::$container->get(ImportFromPoint::class),
+            self::$container->get(ReportFromCC::class),
+            $this->edifactFs
+        );
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('DBSCHENKER is not configured or enabled');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'transporter' => 'DBSCHENKER'
+        ]);
+    }
+
+    public function testNoStoreConfigured(): void
+    {
+        $settingManager = $this->prophesize(SettingsManager::class);
+        $settingManager->get('latlng')
+            ->willReturn('48.8534,2.3488');
+        $settingManager->get('company_legal_name')
+            ->willReturn('Coopcycle Testing Inc.');
+        $settingManager->get('company_legal_id')
+            ->willReturn('4447190000');
+        $params = $this->prophesize(ParameterBagInterface::class);
+        $params->get('transporters_config')
+            ->willReturn([
+                'BMV' => [
+                    'enabled' => true,
+                    'name' => 'BMV test',
+                    'legal_name' => 'BMV Testing Inc.',
+                    'legal_id' => '0000022',
+                    'fs_mask' => self::FS_MASK_DBS,
+                    'sync_uri' => $this->syncFs,
+                ]
+            ]);
+
+        $command = new SyncTransportersCommand(
+            $this->entityManager,
+            $params->reveal(),
+            $settingManager->reveal(),
+            self::$container->get(ImportFromPoint::class),
+            self::$container->get(ReportFromCC::class),
+            $this->edifactFs
+        );
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('No store with transporter "BMV" connected');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'transporter' => 'BMV'
+        ]);
+    }
+
+
+    //
+    // TESTING IMPORT
+    //
+
     public function testValidEmptySync(): void
     {
         $command = $this->initCommand();
@@ -122,16 +343,92 @@ class SyncTransportersCommandTest extends KernelTestCase {
         $this->assertStringContainsString('No messages to send', $output);
     }
 
+    public function testInvalidAddressSync(): void
+    {
+        // Insert edi to sync
+        $this->syncFs->write(
+            sprintf('to_%s/test.edi', self::FS_MASK_DBS),
+            self::INVALID_ADDRESS_EDI_SAMPLE
+        );
+
+        // Valid the file is there
+        $dir_list = $this->syncFs->listContents(sprintf('to_%s', self::FS_MASK_DBS))->toArray();
+        $this->assertCount(1, $dir_list);
+
+        $command = $this->initCommand();
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'transporter' => 'DBSCHENKER'
+        ]);
+
+        $commandTester->assertCommandIsSuccessful();
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('imported 1 tasks', $output);
+        $this->assertStringContainsString('No messages to send', $output);
+
+        // Check if command removed the file to sync
+        $dir_list = $this->syncFs->listContents(sprintf('to_%s', self::FS_MASK_DBS))->toArray();
+        $this->assertCount(0, $dir_list);
+
+        $delivery = $this->entityManager->getRepository(Delivery::class)->findAll();
+        $this->assertCount(1, $delivery);
+
+        /** @var Delivery $delivery */
+        $delivery = array_shift($delivery);
+        $this->assertCount(2, $delivery->getTasks());
+
+
+        /** @var Task $pickup */
+        $pickup = $delivery->getPickup();
+        /** @var Task $dropoff */
+        $dropoff = $delivery->getDropoff();
+
+        $this->assertEquals(
+            '18, avenue Ledru-Rollin 75012 Paris 12ème',
+            $pickup->getAddress()->getStreetaddress()
+        );
+        $this->assertEquals(new GeoCoordinates(48.864577, 2.333338), $pickup->getAddress()->getGeo());
+
+        $this->assertEquals(
+            "INVALID ADDRESS",
+            $dropoff->getAddress()->getStreetaddress()
+        );
+        $this->assertEquals(
+            'JOHN DOE ZIMP COMPANY',
+            $dropoff->getAddress()->getCompany()
+        );
+        $this->assertContains('review-needed', $dropoff->getTags());
+
+        $this->assertEquals(15000, $delivery->getWeight());
+
+        $ediMessage = $dropoff->getImportMessage();
+        $this->assertNotNull($ediMessage);
+        $this->assertEquals('JOY0123456789', $ediMessage->getReference());
+        $this->assertEquals('DBSCHENKER', $ediMessage->getTransporter());
+        $this->assertEquals(
+            EDIFACTMessage::DIRECTION_INBOUND,
+            $ediMessage->getDirection()
+        );
+
+        $this->assertEquals(
+            EDIFACTMessage::MESSAGE_TYPE_SCONTR,
+            $ediMessage->getMessageType()
+        );
+
+
+    }
+
+
     public function testValidSyncOneTask(): void
     {
         // Insert edi to sync
         $this->syncFs->write(
-            sprintf('to_%s/test.edi', self::FS_MASK),
+            sprintf('to_%s/test.edi', self::FS_MASK_DBS),
             self::EDI_SAMPLE
         );
 
         // Valid the file is there
-        $dir_list = $this->syncFs->listContents(sprintf('to_%s', self::FS_MASK))->toArray();
+        $dir_list = $this->syncFs->listContents(sprintf('to_%s', self::FS_MASK_DBS))->toArray();
         $this->assertCount(1, $dir_list);
 
         $command = $this->initCommand();
@@ -147,7 +444,7 @@ class SyncTransportersCommandTest extends KernelTestCase {
         $this->assertStringContainsString('No messages to send', $output);
 
         // Check if command removed the file to sync
-        $dir_list = $this->syncFs->listContents(sprintf('to_%s', self::FS_MASK))->toArray();
+        $dir_list = $this->syncFs->listContents(sprintf('to_%s', self::FS_MASK_DBS))->toArray();
         $this->assertCount(0, $dir_list);
 
         $delivery = $this->entityManager->getRepository(Delivery::class)->findAll();
@@ -198,7 +495,19 @@ class SyncTransportersCommandTest extends KernelTestCase {
 
         $this->assertEquals(15000, $delivery->getWeight());
 
+        $ediMessage = $dropoff->getImportMessage();
+        $this->assertNotNull($ediMessage);
+        $this->assertEquals('JOY0123456789', $ediMessage->getReference());
+        $this->assertEquals('DBSCHENKER', $ediMessage->getTransporter());
+        $this->assertEquals(
+            EDIFACTMessage::DIRECTION_INBOUND,
+            $ediMessage->getDirection()
+        );
 
+        $this->assertEquals(
+            EDIFACTMessage::MESSAGE_TYPE_SCONTR,
+            $ediMessage->getMessageType()
+        );
 
     }
 
