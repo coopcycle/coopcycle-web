021b758436080409b166c4447c36e2a0e03c1fa7
Atala <alois.guillope@gmail.com>
2024-05-27 14:31:58 +0200

    fix: PHPdoc

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -488,7 +488,7 @@ class Task implements TaggableInterface, OrganizationAwareInterface, PackagesAwa
 
     /**
     * Non-DB-mapped property to store packages and weight aggregates (see on $weight and $packages property for aggregates definitions)
-    * @return ['weight' => int|null, 'packages' => Array['name' => string, 'type' => string, 'quantity' => int]|null]
+    * @return ['weight' => int|null, 'packages' => ['name' => string, 'type' => string, 'quantity' => int]|null]
     */
     private $prefetchedPackagesAndWeight;
 
