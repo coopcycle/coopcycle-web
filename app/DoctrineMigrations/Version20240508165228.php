deb98948ef042b49f69bb3e628f227b58af486c1
Atala <alois.guillope@gmail.com>
2024-05-30 11:39:19 +0200

    fix: fix migrations in case of corrupted tour data

--- app/DoctrineMigrations/Version20240508165228.php
+++ app/DoctrineMigrations/Version20240508165228.php
@@ -36,18 +36,28 @@ final class Version20240508165228 extends AbstractMigration
         // select on task_collection_item tci the tour ID - it is linked to the task_collection with type tour
         // select on task_collection_item tci_tl the tasklist ID - it is linked to the task_collection with type task_list
         // take the MIN of matching task_collection_item tci_tl to set tour position at the "beginning" of tour
+        // take the MIN of matching task_lists tci_tl because of various crashes around tour feature some DBs have tours that are linked to several tasklist
+        // due to several bugs in the early tour feature we may have tasks that are in a tour assigned to a different rider (tour is assigned in majority to one rider but one task of the touris assigned to someone else) -> we use RANK on task count to get the tasklist which has most of the tour tasks
         $this->addSql(
             "INSERT INTO task_list_item (parent_id, tour_id, position) (
-                select
-                    tci_tl.parent_id as parent_id,
-                    tc.id  as tour_id,
-                    min(tci_tl.position) as position
-                from task_collection_item tci
-                inner join task_collection tc on (tc.id = tci.parent_id and tc.type ='tour')
-                inner join task_collection_item tci_tl on tci_tl.id in (
-                    select task_collection_item.id from task_collection_item inner join task_collection on task_collection.id = task_collection_item.parent_id  where task_collection.type = 'task_list'
-                ) and tci_tl.task_id = tci.task_id
-                group by tci_tl.parent_id, tc.id
+                select parent_id, tour_id, position
+                from
+                    (select
+                            tci_tl.parent_id as parent_id,
+                            tc.id  as tour_id,
+                            min(tci_tl.position) as position,
+                            RANK() OVER (PARTITION BY tc.id ORDER BY COUNT(tci.task_id) DESC) AS rnk
+                        from task_collection_item tci
+                        inner join task_collection tc on (tc.id = tci.parent_id and tc.type ='tour')
+                        inner join task_collection_item tci_tl on tci_tl.id in (
+                            select task_collection_item.id
+                            from task_collection_item
+                            inner join task_collection on task_collection.id = task_collection_item.parent_id
+                            where task_collection.type = 'task_list'
+                        ) and tci_tl.task_id = tci.task_id
+                        group by tci_tl.parent_id, tc.id
+                    ) as tour_collection_item_with_tasks_count
+                where tour_collection_item_with_tasks_count.rnk = 1
             );"
         );
 
