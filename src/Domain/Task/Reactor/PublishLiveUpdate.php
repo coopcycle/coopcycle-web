64c36679b2df041f382876d1f44f131a0abf9724
Atala <alois.guillope@gmail.com>
2024-06-04 16:00:20 +0200

    fix: tasklist_updated should only be send to riders

--- src/Domain/Task/Reactor/PublishLiveUpdate.php
+++ src/Domain/Task/Reactor/PublishLiveUpdate.php
@@ -19,13 +19,16 @@ class PublishLiveUpdate
     public function __invoke(Event $event)
     {
         // this event is used to update the rider TaskList in the app
-        // then this is the only task event we want to send to user/riders, not only to admin/dispatchers
+        // then this is the only task event we want to send to user/riders
+        // we don't send it to admin/dispatchers because the app is listening to live update on this without any filter/condition on username so a rider who is an admin will receives updates for all riders
+        // ref: https://github.com/coopcycle/coopcycle-app/blob/58d84b3519ccb16d53f8ae2948a211378f4929f7/src/redux/Courier/taskEntityReducer.js#L302
         // legacy event and new version of event
         // see https://github.com/coopcycle/coopcycle-app/issues/1803
         if ($event instanceof TaskListUpdated || $event instanceof TaskListUpdatedv2) {
             $user = $event->getTaskList()->getCourier();
             $this->liveUpdates->toUsers([ $user ], $event);
+        } else {
+            $this->liveUpdates->toAdmins($event);
         }
-        $this->liveUpdates->toAdmins($event);
     }
 }
