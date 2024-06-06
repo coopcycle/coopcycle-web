ef448c290d2f9ce91f0f0d3c2341434785b54984
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-06-06 12:25:32 -0700

    fix: Binding entities to query parameters only allowed for entities that have an identifier. Class "AppBundle\Entity\Task" does not have an identifier in behat tests
    fix: Binding entities to query parameters only allowed for entities that have an identifier. Class "AppBundle\Entity\Task" does not have an identifier in behat tests

--- src/Entity/TourRepository.php
+++ src/Entity/TourRepository.php
@@ -11,6 +11,11 @@ class TourRepository extends EntityRepository
 {
     public function findOneByTask(Task $task)
     {
+        // the task has not been persisted yet
+        if (null === $task->getId()) {
+            return null;
+        }
+
         return $this->createQueryBuilder('to')
             ->join(TaskCollectionItem::class, 'tci', Expr\Join::WITH, 'tci.parent = to.id')
             ->where('tci.task = :task')
