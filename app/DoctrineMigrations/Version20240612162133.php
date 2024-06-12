839f296382da1d829b7a55c7781d51648e2bd36a
Antoine Bagnaud <contact@antoinebagnaud.me>
2024-06-12 18:30:52 +0200

    feat(store): update timeslot data model

---
+++ app/DoctrineMigrations/Version20240612162133.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240612162133 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add position fieds to pivot table between Store and Timeslot';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE store_time_slot ADD COLUMN position INTEGER NOT NULL DEFAULT 0');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE store_time_slot DROP COLUMN position');
+    }
+}
