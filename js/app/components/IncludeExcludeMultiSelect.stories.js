712f6ef6241d602ba103b3654209c3bd48cc3fd5
Atala <alois.guillope@gmail.com>
2024-06-18 13:09:13 +0200

    clean: don't override TagSelect component, it is used elsewhere

--- js/app/components/IncludeExcludeMultiSelect.stories.js
+++ js/app/components/IncludeExcludeMultiSelect.stories.js
@@ -23,5 +23,6 @@ export const Basic = {
         console.debug(selected)
     },
     selectOptions: [{value: 'value', label: 'Displayed label', prop1: 'prop11', prop2: 'prop12'}, {value: 'value2', label: 'Displayed label 2', prop1: 'prop11', prop2: 'prop12'}],
+    isLoading: false
   }
 }
