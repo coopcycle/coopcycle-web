ad50acbbaa0a9acbfbdf731105946e6ab1a4b008
Atala <alois.guillope@gmail.com>
2024-07-11 12:29:49 +0200

    clean: soft delete existing vehicles

--- app/DoctrineMigrations/Version20240703124328.php
+++ app/DoctrineMigrations/Version20240703124328.php
@@ -14,21 +14,32 @@ final class Version20240703124328 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Fill new vehicle fields';
+        return 'Fill legacy vehicle fields';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("UPDATE vehicle SET color = '#0000ff', is_electric = false;");
+        $this->addSql('ALTER TABLE trailer ADD deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+        $this->addSql('ALTER TABLE vehicle ADD deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+
+        // at the moment we write the feature we assume legacy vehicles are not useful so let's soft delete them
+        // also they misses the "warehouse" field so they would need user-update for sure
+        $this->addSql("UPDATE vehicle SET color = '#0000ff', is_electric = false, deleted_at = NOW();");
+
         $this->addSql("ALTER TABLE vehicle RENAME COLUMN volume_units TO max_volume_units;");
         $this->addSql("ALTER TABLE vehicle ALTER COLUMN is_electric SET NOT NULL;");
         $this->addSql("ALTER TABLE vehicle ALTER COLUMN color SET NOT NULL;");
+        $this->addSql("ALTER TABLE vehicle ALTER COLUMN warehouse_id SET NOT NULL;");
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE trailer DROP deleted_at');
+        $this->addSql('ALTER TABLE vehicle DROP deleted_at');
+        $this->addSql("ALTER TABLE vehicle ALTER COLUMN is_electric DROP NOT NULL;");
+        $this->addSql("ALTER TABLE vehicle ALTER COLUMN color DROP NOT NULL;");
 
     }
 }
