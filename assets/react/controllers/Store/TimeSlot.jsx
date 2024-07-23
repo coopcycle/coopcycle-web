fccb7b02cdc9b457260419ceac59f34fce80d255
Alexandre Segura <mex.zktk@gmail.com>
2024-07-23 18:43:54 +0200

    Fix API call.

--- assets/react/controllers/Store/TimeSlot.jsx
+++ assets/react/controllers/Store/TimeSlot.jsx
@@ -15,10 +15,12 @@ async function _fetchTimeSlots() {
 
 async function _putTimeSlots(id, data) {
   const httpClient = new window._auth.httpClient();
-  return await httpClient.patch(
-    window.Routing.generate("api_stores_get_item", { id }),
-    data,
-  );
+  const uri = window.Routing.generate('api_stores_get_item', { id })
+
+  return await httpClient.patch(uri, {
+    '@id': uri,
+    ...data
+  });
 }
 
 async function _updateTimeSlots(id, timeSlots, setFetching) {
