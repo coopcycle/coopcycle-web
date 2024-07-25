a22062b5490833f3dccd0805ee63adce0b2bf1e2
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-07-24 17:24:52 -0700

    WIP; osrm image

--- docker/osrm/start.sh
+++ docker/osrm/start.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+if [ ! -f /data/marker ]; then
+  echo "Executing script because this is the first launch...";
+
+  wget --no-check-certificate https://coopcycle-assets.sfo2.digitaloceanspaces.com/osm/paris-france.osm.pbf -O /data/data.osm.pbf
+  osrm-extract -p /opt/bicycle.lua /data/data.osm.pbf
+  osrm-partition /data/data.osrm
+  osrm-customize /data/data.osrm
+
+  touch /data/marker;
+else
+  echo "Script has already been executed in a previous launch.";
+fi
+
 if [ -f "/data/$OSRM_FILENAME" ]; then
     osrm-routed --algorithm mld /data/$OSRM_FILENAME
 fi
