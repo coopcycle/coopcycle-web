b7e9b54f9dd4c12dfe0b632419f12c53ccde42b0
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-07-25 14:35:44 -0700

    added: test create delivery flow with createOrders enabled

--- cypress/e2e/local-commerce/create_delivery_as_admin.cy.js
+++ cypress/e2e/local-commerce/create_delivery_as_admin.cy.js
@@ -13,7 +13,153 @@ context('Delivery (role: admin)', () => {
     cy.visit('/login')
     cy.login('admin', '12345678')
   })
-  
+
+  it('create delivery order for store', function () {
+    cy.visit('/admin/stores')
+
+    cy.get('[data-testid=store_Acme__list_item]')
+      .find('.dropdown-toggle')
+      .click()
+
+    cy.get('[data-testid=store_Acme__list_item]')
+      .contains('Créer une livraison')
+      .click()
+
+    // Pickup
+
+    cy.searchAddress(
+      '[data-form="task"]:nth-of-type(1)',
+      '23 Avenue Claude Vellefaux, 75010 Paris, France',
+      /^23,? Avenue Claude Vellefaux,? 75010,? Paris,? France/i,
+    )
+
+    cy.get('[data-form="task"]:nth-of-type(1)')
+      .find('[data-testid=address_name__popover_pill]')
+      .click()
+    cy.get('form[id=delivery_tasks_0_address_name__popover]')
+      .find('input')
+      .clear()
+    cy.get('form[id=delivery_tasks_0_address_name__popover]')
+      .find('input')
+      .type('Office')
+    cy.get('form[id=delivery_tasks_0_address_name__popover]')
+      .find('button')
+      .click()
+
+    cy.get('[data-form="task"]:nth-of-type(1)')
+      .find('[data-testid=address_telephone__popover_pill]')
+      .click()
+    cy.get('form[id=delivery_tasks_0_address_telephone__popover]')
+      .find('input')
+      .clear()
+    cy.get('form[id=delivery_tasks_0_address_telephone__popover]')
+      .find('input')
+      .type('+33112121212')
+    cy.get('form[id=delivery_tasks_0_address_telephone__popover]')
+      .find('button')
+      .click()
+
+    cy.get('[data-form="task"]:nth-of-type(1)')
+      .find('[data-testid=address_contactName__popover_pill]')
+      .click()
+    cy.get('form[id=delivery_tasks_0_address_contactName__popover]')
+      .find('input')
+      .clear()
+    cy.get('form[id=delivery_tasks_0_address_contactName__popover]')
+      .find('input')
+      .type('John Doe')
+    cy.get('form[id=delivery_tasks_0_address_contactName__popover]')
+      .find('button')
+      .click()
+
+    cy.get('#delivery_tasks_0_comments').type('Pickup comments')
+
+    // Dropoff
+
+    cy.searchAddress(
+      '[data-form="task"]:nth-of-type(2)',
+      '72 Rue Saint-Maur, 75011 Paris, France',
+      /^72,? Rue Saint-Maur,? 75011,? Paris,? France/i,
+    )
+
+    cy.get('[data-form="task"]:nth-of-type(2)')
+      .find('[data-testid=address_name__popover_pill]')
+      .click()
+    cy.get('form[id=delivery_tasks_1_address_name__popover]')
+      .find('input')
+      .clear()
+    cy.get('form[id=delivery_tasks_1_address_name__popover]')
+      .find('input')
+      .type('Warehouse')
+    cy.get('form[id=delivery_tasks_1_address_name__popover]')
+      .find('button')
+      .click()
+
+    cy.get('[data-form="task"]:nth-of-type(2)')
+      .find('[data-testid=address_telephone__popover_pill]')
+      .click()
+    cy.get('form[id=delivery_tasks_1_address_telephone__popover]')
+      .find('input')
+      .clear()
+    cy.get('form[id=delivery_tasks_1_address_telephone__popover]')
+      .find('input')
+      .type('+33114141414')
+    cy.get('form[id=delivery_tasks_1_address_telephone__popover]')
+      .find('button')
+      .click()
+
+    cy.get('[data-form="task"]:nth-of-type(2)')
+      .find('[data-testid=address_contactName__popover_pill]')
+      .click()
+    cy.get('form[id=delivery_tasks_1_address_contactName__popover]')
+      .find('input')
+      .clear()
+    cy.get('form[id=delivery_tasks_1_address_contactName__popover]')
+      .find('input')
+      .type('Jane Smith')
+    cy.get('form[id=delivery_tasks_1_address_contactName__popover]')
+      .find('button')
+      .click()
+
+    cy.get('#delivery_tasks_1_weight').clear()
+    cy.get('#delivery_tasks_1_weight').type(2.5)
+
+    cy.get('#delivery_tasks_1_comments').type('Dropoff comments')
+
+    cy.get('#delivery-submit').click()
+
+    // list of deliveries page
+    cy.location('pathname', { timeout: 3000 }).should(
+      'match',
+      /\/admin\/stores\/[0-9]+\/deliveries$/,
+    )
+    cy.get('[data-testid=delivery__list_item]')
+      .contains(/23,? Avenue Claude Vellefaux,? 75010,? Paris,? France/)
+      .should('exist')
+    cy.get('[data-testid=delivery__list_item]')
+      .contains(/72,? Rue Saint-Maur,? 75011,? Paris,? France/)
+      .should('exist')
+
+    cy.get('[data-testid="delivery__list_item"]')
+      .find('[data-testid="delivery_id"]')
+      .click()
+
+    // Delivery page
+    cy.get('[data-testid="breadcrumb"]')
+      .find('[data-testid="order_id"]')
+      .should('exist')
+
+    cy.get('[data-testid="breadcrumb"]')
+      .find('[data-testid="order_id"]')
+      .click()
+
+    // Order page
+    cy.location('pathname', { timeout: 3000 }).should(
+      'match',
+      /\/admin\/orders\/[0-9]+$/,
+    )
+  })
+
   it('create delivery for store with createOrders disabled', function () {
     cy.visit('/admin/stores')
 
