a086d503faa253e55fd55f5f02828e8c527277f6
Alexandre Segura <mex.zktk@gmail.com>
2024-09-17 11:27:55 +0200

    Fix static analysis.

--- src/Action/Order/PaymentDetails.php
+++ src/Action/Order/PaymentDetails.php
@@ -3,22 +3,12 @@
 namespace AppBundle\Action\Order;
 
 use AppBundle\Api\Dto\PaymentDetailsOutput;
-use AppBundle\Service\StripeManager;
-use Doctrine\ORM\EntityManagerInterface;
 use Sylius\Component\Payment\Model\PaymentInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class PaymentDetails
 {
-    private $stripeManager;
-
-    public function __construct(
-        StripeManager $stripeManager)
-    {
-        $this->stripeManager = $stripeManager;
-    }
-
     public function __invoke($data, Request $request)
     {
         $output = new PaymentDetailsOutput();
