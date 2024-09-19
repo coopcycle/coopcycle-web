0ea24ab6d90826ec37cfe7e5dffe87ef4a8605a1
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-09-19 14:53:11 -0700

    added: test 'invite business account manager' flow

---
+++ cypress/e2e/foodtech/platform-catering/invite_manager.cy.js
@@ -0,0 +1,35 @@
+describe('Platform catering; admin', () => {
+  beforeEach(() => {
+    cy.symfonyConsole(
+      'coopcycle:fixtures:load -f cypress/fixtures/foodtech.yml',
+    )
+
+    cy.window().then(win => {
+      win.sessionStorage.clear()
+    })
+  })
+
+  it("should send an invitation to a business account's manager", () => {
+    cy.visit('/login')
+    cy.login('admin', '12345678')
+
+    cy.visit('/admin/restaurants/business-accounts')
+
+    cy.get('[href="/admin/restaurants/business-account/new"]').click()
+    cy.get('#company_name').clear('')
+    cy.get('#company_name').type('Business01')
+    cy.get('#company_managerEmail').clear()
+    cy.get('#company_managerEmail').type('business01@demo.coopcycle.org')
+    cy.searchAddress(
+      '#company_address',
+      '91 rue de rivoli paris',
+      /^91,? Rue de Rivoli,? 75001,? Paris,? France/i,
+    )
+    cy.get('#company_businessRestaurantGroup').select('0')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('include', '/admin/restaurants/business-account')
+    cy.get('.alert-success').should('exist')
+    cy.get('table').contains('Business01').should('exist')
+  })
+})
