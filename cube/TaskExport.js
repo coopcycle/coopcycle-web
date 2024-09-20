1e4bcfff158831cbbf8f5b8388ed42f5316c6f31
Alexandre Segura <mex.zktk@gmail.com>
2024-09-20 16:22:02 +0200

    Configure TaskExport cube.

--- cube/TaskExport.js
+++ cube/TaskExport.js
@@ -1,41 +1,40 @@
-  const {securityContext} = COMPILE_CONTEXT
+const { securityContext } = COMPILE_CONTEXT
 
-  const partitionPath = ({instance, year, month}) => {
-    const _ = (name, value) => value ? `${name}=${value}` : "*"
-    return `${_("instance", instance)}/${_("year", year)}/${_("month", month)}`
-  }
+const partitionPath = ({instance, year, month}) => {
+  const _ = (name, value) => value ? `${name}=${value}` : "*"
+  return `${_("instance", instance)}/${_("year", year)}/${_("month", month)}`
+}
 
-  cube(`TaskExport`, {
-    sql: `SELECT * FROM read_parquet('s3://billing/tasks/${partitionPath(securityContext)}/*.parquet', hive_partitioning = true)`,
+const resolvePath = ({ s3_path }) => s3_path && s3_path.replace(/^\/|\/$/g, '').replace('%type%', 'orders')
 
-    refresh_key: {
-      every: "1 second"
+cube(`TaskExport`, {
+  sql: `SELECT * FROM read_parquet('s3://${resolvePath(securityContext)}/*/*/*.parquet', hive_partitioning = true)`,
+
+  refresh_key: {
+    every: "1 second"
+  },
+
+  dimensions: {
+    order_code: {
+      sql: `order_code`,
+      type: `string`,
+      primary_key: true
     },
-
-    // Group By / Index
-    dimensions: {
-      order_code: {
-        sql: `order_code`,
-        type: `string`,
-        primary_key: true
-      },
-      type: {
-        sql: `type`,
-        type: `string`
-      },
-      instance: {
-        sql: `instance`,
-        type: `string`
-      },
+    type: {
+      sql: `type`,
+      type: `string`
     },
-
-
-    // Data aggregations
-    measures: {
-      count: {
-        type: 'count'
-      }
+    instance: {
+      sql: `instance`,
+      type: `string`
     },
+  },
 
-    dataSource: "duckdb"
-  });
+  measures: {
+    count: {
+      type: 'count'
+    }
+  },
+
+  dataSource: "duckdb"
+});
