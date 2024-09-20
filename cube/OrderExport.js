ecc091989defd0395cbd8713200f0d6a8e950751
Alexandre Segura <mex.zktk@gmail.com>
2024-09-20 08:16:00 +0200

    Update OrderExport cube.

--- cube/OrderExport.js
+++ cube/OrderExport.js
@@ -1,61 +1,79 @@
-  const {securityContext} = COMPILE_CONTEXT
+const { securityContext } = COMPILE_CONTEXT
 
-  const PER_TASK = 0.125
-  const TOTAL_PERCENTAGE = 0.03
+const partitionPath = ({instance, year, month}) => {
+  const _ = (name, value) => value ? `${name}=${value}` : "*"
+  return `${_("instance", instance)}/${_("year", year)}/${_("month", month)}`
+}
 
-  const partitionPath = ({instance, year, month}) => {
-    const _ = (name, value) => value ? `${name}=${value}` : "*"
-    return `${_("instance", instance)}/${_("year", year)}/${_("month", month)}`
-  }
+cube(`OrderExport`, {
+  sql: `SELECT * FROM read_parquet('s3://coopcycle-data/exports/orders/instance=naofood/*/*/*.parquet', hive_partitioning = true)`,
 
-  cube(`OrderExport`, {
-    sql: `SELECT * FROM read_parquet('s3://billing/orders/${partitionPath(securityContext)}/*.parquet', hive_partitioning = true)`,
+  // TODO: Check join per instances to avoid order code collision
+  // joins: {
+  //   TaskExport: {
+  //     relationship: `one_to_many`,
+  //     sql: `${CUBE}.order_code = ${TaskExport.order_code}`
+  //   }
+  // },
 
-    refresh_key: {
-      every: "1 second"
+  dimensions: {
+    order_code: {
+      sql: `order_code`,
+      type: `string`,
+      primary_key: true,
+      public: true
     },
-
-    //TODO: Check join per instances to avoid order code collision
-    joins: {
-      TaskExport: {
-        relationship: `one_to_many`,
-        sql: `${CUBE}.order_code = ${TaskExport.order_code}`
-      }
+    instance: {
+      sql: `instance`,
+      type: `string`
     },
-
-    // Group By / Index
-    dimensions: {
-      order_code: {
-        sql: `order_code`,
-        type: `string`,
-        primary_key: true,
-        public: true
-      },
-      instance: {
-        sql: `instance`,
-        type: `string`
-      },
+    restaurant: {
+      sql: `restaurant`,
+      type: `string`
     },
-
-
-    // Data aggregations
-    measures: {
-
-      total_vat: {
-        sql: `total_vat`,
-        type: `sum`,
-        format: `currency`
-      },
-      order_coopcycle_fee_percentage: {
-        sql: `ROUND(${CUBE.total_vat} * ${TOTAL_PERCENTAGE}, 2)`,
-        type: `number`,
-        format: `currency`
-      },
-      order_coopcycle_fee_per_task: {
-        sql: `ROUND(GREATEST((${TaskExport.count} - 1) * ${PER_TASK}, 0), 2)`,
-        type: `number`,
-      },
+    completed_at: {
+      sql: `completed_at`,
+      type: `time`
     },
+    day_of_week: {
+      sql: `DATE_PART('isodow', ${CUBE.completed_at})`,
+      type: `number`
+    },
+    week: {
+      sql: `DATE_PART('week', ${CUBE.completed_at})`,
+      type: `number`
+    },
+    hour: {
+      sql: `DATE_PART('hour', ${CUBE.completed_at})`,
+      type: `number`
+    },
+  },
 
-    dataSource: "duckdb"
-  });
+  measures: {
+    count: {
+      type: `count`
+    },
+    total_incl_tax: {
+      sql: `total_incl_tax / 100`,
+      type: `sum`,
+      format: `currency`
+    },
+    platform_fee: {
+      sql: `platform_fee / 100`,
+      type: `sum`,
+      format: `currency`
+    },
+    income: {
+      sql: `IF (platform_fee != 0, platform_fee / 100, total_products_incl_vat / 100)`,
+      type: `sum`,
+      format: `currency`,
+    },
+    platform_fee_avg: {
+      sql: `ROUND((platform_fee / 100), 2)`,
+      type: `avg`,
+      format: `currency`
+    }
+  },
+
+  dataSource: "duckdb"
+});
