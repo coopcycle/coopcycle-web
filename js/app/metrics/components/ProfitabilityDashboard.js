a051e2c43506a70a11b7f132db6c1202cbb2f898
Alexandre Segura <mex.zktk@gmail.com>
2024-09-23 14:24:54 +0200

    Add translations.

--- js/app/metrics/components/ProfitabilityDashboard.js
+++ js/app/metrics/components/ProfitabilityDashboard.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { InputNumber, Form } from 'antd';
 import _ from 'lodash'
+import { useTranslation } from 'react-i18next'
 
 import ProfitabilityHeatmap from './ProfitabilityHeatmap'
 import ProfitabilityBars from './ProfitabilityBars'
@@ -10,12 +11,13 @@ import ChartPanel from './ChartPanel'
 const Dashboard = ({ cubejsApi, dateRange }) => {
 
   const [ fixedCosts, setFixedCosts ] = useState(0)
+  const { t } = useTranslation()
 
   const setFixedCostsDebounced = _.debounce(setFixedCosts, 300)
 
   return (
     <div>
-      <Form.Item label="Monthly fixed costs">
+      <Form.Item label={ t('MONTHLY_FIXED_COSTS') }>
         <InputNumber defaultValue={ fixedCosts } onChange={ setFixedCostsDebounced } />
       </Form.Item>
       <div className="metrics-grid">
