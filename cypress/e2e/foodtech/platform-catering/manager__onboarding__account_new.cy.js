946ff10a571dc70fccae6479974fb0d3dd9d7b15
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-10-04 15:49:36 -0700

    split manager onboarding tests

--- cypress/e2e/foodtech/platform-catering/manager__onboarding__account_new.cy.js
+++ cypress/e2e/foodtech/platform-catering/manager__onboarding__account_new.cy.js
@@ -1,11 +1,11 @@
-describe('Platform catering; manager; onboarding', () => {
+describe('Platform catering; manager; onboarding with a new user account', () => {
   beforeEach(() => {
     cy.window().then(win => {
       win.sessionStorage.clear()
     })
   })
 
-  it('should activate a business account with a new user account', () => {
+  it('should activate a business account', () => {
     cy.symfonyConsole(
       'coopcycle:fixtures:load -f cypress/fixtures/business_account_manager_invitation_new_user.yml',
     )
@@ -24,34 +24,6 @@ describe('Platform catering; manager; onboarding', () => {
       '12345678',
     )
     cy.get('#businessAccountRegistration_user_legal').check()
-    cy.get('button[type="submit"]').click()
-
-    // Company info step
-    cy.get('#businessAccountRegistration_businessAccount_legalName').clear('')
-    cy.get('#businessAccountRegistration_businessAccount_legalName').type(
-      'Business Name Ltd',
-    )
-    cy.get('#businessAccountRegistration_businessAccount_vatNumber').clear('')
-    cy.get('#businessAccountRegistration_businessAccount_vatNumber').type(
-      'FR12345678901',
-    )
-    cy.get('.btn-primary').click()
-
-    // Confirmation page
-    cy.url().should('include', '/register/confirmed')
-    cy.get('.content').should('contain', 'Félicitations')
-  })
-
-  it('should activate a business account with an existing user account', () => {
-    cy.symfonyConsole(
-      'coopcycle:fixtures:load -f cypress/fixtures/business_account_manager_invitation_existing_user.yml',
-    )
-
-    cy.visit('/invitation/define-password/INVITATION_MANAGER')
-
-    // Personal info step
-    cy.get('#guest-checkout-password').clear('')
-    cy.get('#guest-checkout-password').type('12345678')
     cy.get('button[type="submit"]').click()
 
     // Company info step
