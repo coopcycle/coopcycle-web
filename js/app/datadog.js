72acbb8be76d1ccbe578aa1a171ac94cf0e13989
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-11-28 17:22:52 -0800

    exclude bot sessions

--- js/app/datadog.js
+++ js/app/datadog.js
@@ -4,6 +4,13 @@ import { datadogRum } from '@datadog/browser-rum'
 const el = document.getElementById('datadog')
 const datadogEnabled = Boolean(el)
 
+// regex patterns to identify known bot instances:
+// see https://docs.datadoghq.com/real_user_monitoring/guide/identify-bots-in-the-ui/#filter-out-bot-sessions-on-intake
+const botPattern = '(googlebot|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)'
+
+const regex = new RegExp(botPattern, 'i')
+const isBot = regex.test(navigator.userAgent)
+
 if (datadogEnabled) {
   datadogLogs.init({
     clientToken: el.dataset.clientToken,
@@ -11,7 +18,7 @@ if (datadogEnabled) {
     service: el.dataset.service,
     forwardErrorsToLogs: true,
     // Only tracked sessions send logs; from 0 to 100
-    sessionSampleRate: 100,
+    sessionSampleRate: isBot ? 0 : 100,
     telemetrySampleRate: 0,
   })
 
@@ -23,9 +30,9 @@ if (datadogEnabled) {
     // Specify a version number to identify the deployed version of your application in Datadog
     // version: '1.0.0',
     // 'Browser RUM' session sample rate; from 0 to 100
-    sessionSampleRate: 1,
+    sessionSampleRate: isBot ? 0 : 5,
     // 'Browser RUM & Session Replay' sample rate (% from sessions tracked by RUM/sessionSampleRate); from 0 to 100
-    sessionReplaySampleRate: 10,
+    sessionReplaySampleRate: isBot ? 0 : 10,
     telemetrySampleRate: 0,
     trackUserInteractions: true,
     trackResources: true,
