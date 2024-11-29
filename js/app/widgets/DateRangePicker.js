cd8ad79e95a008afc78ddcd251bdd5f912ca375e
Camille Nerriere <camille.nerriere@yahoo.fr>
2024-11-29 09:48:09 +0000

    updated disabled options and second default value

--- js/app/widgets/DateRangePicker.js
+++ js/app/widgets/DateRangePicker.js
@@ -9,103 +9,34 @@ import 'antd/es/input/style/index.css'
 
 import { antdLocale } from '../i18n'
 
-// class DateRangePicker extends React.Component {
-
-//   constructor(props) {
-//     super(props)
-
-//     let value = []
-//     if (this.props.defaultValue) {
-//       value = [moment(this.props.defaultValue.after), moment(this.props.defaultValue.before)]
-//     }
-
-//     this.state = {
-//       value
-//     }
-//     // si on passe par la classe, on doit bind le onChange à l'instance
-//     this.onChange = this.onChange.bind(this)
-//   }
-
-//   onChange(value) {
-//     // When the input has been cleared
-//     if (!value) {
-//       return
-//     }
-
-//     const values = {
-//       after: value[0],
-//       before: value[1],
-//     }
-
-//     this.setState(value)
-
-//     this.props.onChange(values)
-//   }
-
-//   render() {
-
-//     let props = {}
-//     if (this.props.showTime) {
-//       props = {showTime: {
-//       ...timePickerProps,
-//           hideDisabledOptions: true,
-//       }}
-//     }
-
-//     return (
-//       <DatePicker.RangePicker
-//         style={{ width: '100%' }}
-//         format={this.props.format}
-//         defaultValue={this.state.value}
-//         onChange={(value) => this.onChange(value)}
-//         {...props}
-//     />
-//     )
-//   }
-// }
-
-// export default function(el, options) {
-
-//   const defaultProps = {
-//     getDatePickerContainer: null,
-//     getTimePickerContainer: null,
-//     onChange: () => {},
-//     format: 'LLL',
-//   }
-
-//   const props = { ...defaultProps, ...options }
-
-//   render(
-//     <ConfigProvider locale={ antdLocale }>
-//       <DateRangePicker { ...props } />
-//     </ConfigProvider>, el)
-// }
-
 const DateRangePicker = ({ defaultValue, onChange, format, showTime }) => {
+  console.error('valeurs', defaultValue)
 
-  console.error("valeurs", defaultValue);
-  
+  const [value, setValue] = useState(() =>
+    defaultValue
+      ? [moment(defaultValue.after), moment(defaultValue.before)]
+      : [],
+  )
 
-  
-  const [value, setValue] = useState(() => defaultValue ? [moment(defaultValue.after), moment(defaultValue.before)] : [])
-
-  const handleDateChange = (newValue) => {
-    if (!newValue) return; 
+  const handleDateChange = newValue => {
+    if (!newValue) return
 
     onChange({
       after: newValue[0],
-      before: newValue[1]
+      before: newValue[1],
     })
 
-    setValue(newValue); 
+    setValue(newValue)
   }
 
   let props = {}
   if (showTime) {
-    props = {showTime: {
-    ...timePickerProps,
+    props = {
+      showTime: {
+        ...timePickerProps,
         hideDisabledOptions: true,
-    }}
+      },
+    }
   }
   return (
     <DatePicker.RangePicker
@@ -115,8 +46,8 @@ const DateRangePicker = ({ defaultValue, onChange, format, showTime }) => {
       onChange={handleDateChange}
       {...props}
     />
-    )
-  }
+  )
+}
 
 export default function(el, options) {
 
