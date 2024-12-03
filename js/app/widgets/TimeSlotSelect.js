83b94748f5425d5bd037995e4279ffeefc3623f5
Aloïs Guillopé <alois.guillope@gmail.com>
2024-12-03 15:52:17 +0100

    feat: add base select

--- js/app/widgets/TimeSlotSelect.js
+++ js/app/widgets/TimeSlotSelect.js
@@ -1,5 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 
-export default ({ initialChoices }) => {
-    return <span>{initialChoices.map(choice => <span key={choice.value}>{choice.value}</span>) }</span>
+export default ({ initialChoices, onChange }) => {
+
+    const [value, setValue] = useState(initialChoices[0].value)
+
+    return <select
+        onChange={e => {setValue(e.target.value); onChange(e.target.value)}}
+        value={value}
+        >
+        {initialChoices.map(choice => <option key={choice.value} value={choice.value}>{choice.value}</option>) }
+    </select>
 }
