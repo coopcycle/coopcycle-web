93570c5d769935e80fa7273472c00588dd6c1a79
Antoine Bagnaud <contact@antoinebagnaud.me>
2024-12-09 13:15:25 +0100

    add `should_throw` bool on DoneTrait

--- src/Action/Task/DoneTrait.php
+++ src/Action/Task/DoneTrait.php
@@ -13,11 +13,14 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 trait DoneTrait
 {
-    protected function done(Task $task, Request $request)
+    protected function done(Task $task, Request $request, bool $should_throw = false)
     {
         try {
             $this->taskManager->markAsDone($task, $this->getNotes($request), $this->getContactName($request));
         } catch (PreviousTaskNotCompletedException $e) {
+            if ($should_throw) {
+                throw new BadRequestHttpException($e->getMessage());
+            }
             return new JsonResponse([
                 'error' => $e->getMessage(),
                 'required_action' => 'validate_previous_task',
