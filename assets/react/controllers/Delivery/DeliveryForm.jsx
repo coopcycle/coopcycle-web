ef9b64d2a00cdbd7914232184178e551fe5946dc
Camille Nerriere <camille.nerriere@yahoo.fr>
2024-12-17 09:19:38 +0000

    get addresses from API and manage select and autocomplete name, telephone and contact

--- assets/react/controllers/Delivery/DeliveryForm.jsx
+++ assets/react/controllers/Delivery/DeliveryForm.jsx
@@ -1,6 +1,88 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Formik, Form } from 'formik'
+import PickUp from './PickUp'
+import { antdLocale } from '../../../../js/app/i18n'
+import { ConfigProvider } from 'antd'
+import axios from 'axios'
+import AddressBookNew from '../../../../js/app/delivery/AddressBookNew'
 
-export default function({ isNew }) {
+
+
+export default function ({ isNew, storeId }) {
+
+  const [addresses, setAddresses] = useState([])
+
+  const [deliveryAddress, setDeliveryAddress] = useState({
+    streetAddress: "",
+    name: "", 
+    contactName: "", 
+    telephone: ""
+  })
+
+  console.log("deliveryAddress", deliveryAddress)
+  
+  const initialValues = {
+    name: '',
+    contactName: '',
+    streetAddress: '',
+    telephone: '',
+    timeSlot: '',
+    commentary: '',
+  }
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    console.log(values)
+    setSubmitting(false)
+  }
+
+  const baseURL = location.protocol + '//' + location.host
+
+  useEffect(() => {
+    
+    const getAddresses = async () => {
+    const jwtResp = await $.getJSON(window.Routing.generate('profile_jwt'))
+    const jwt = jwtResp.jwt
+    const url = `${baseURL}/api/stores/${storeId}/addresses`
+    const response = await axios.get(
+      url,
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`
+        }
+      }
+    )
+    const addresses = await response.data["hydra:member"]   
+    setAddresses(addresses)
+    }
+
+    if (storeId) {
+      getAddresses()
+    }
+  }, [storeId])
+
+    
+
   console.log(isNew)
-  return <h1>Delivery Form</h1>
+  console.log(storeId)
+  return (
+    <ConfigProvider locale={antdLocale}>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      {({ isSubmitting }) => (
+          <Form>
+            
+            <AddressBookNew
+              addresses={addresses}
+              deliveryAddress={deliveryAddress}
+              setDeliveryAddress={setDeliveryAddress}
+            />
+            <PickUp initialValues={initialValues} />
+            
+          <button type="submit" disabled={isSubmitting}>
+            Soumettre
+          </button>
+        </Form>
+      )}
+      </Formik>
+      </ConfigProvider>
+  )
 }
---
+++ assets/react/controllers/Delivery/TimeslotPicker.jsx
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react'
+import { DatePicker, Select, Radio } from 'antd'
+import moment from 'moment'
+
+import 'antd/es/input/style/index.css'
+
+export default ({ choices, initialChoices, onChange }) => {
+  const timeSlotsLabel = []
+  for (const timeSlot in choices) {
+    timeSlotsLabel.push(timeSlot)
+  }
+
+  const [timeSlotChoices, setTimeSlotChoices] = useState(initialChoices)
+
+  const datesWithTimeslots = {}
+
+  timeSlotChoices.forEach(choice => {
+    const [date, hour] = choice.value.split(' ')
+    if (Object.prototype.hasOwnProperty.call(datesWithTimeslots, date)) {
+      datesWithTimeslots[date].push(hour)
+    } else {
+      datesWithTimeslots[date] = [hour]
+    }
+  })
+
+  const dates = []
+
+  for (const date in datesWithTimeslots) {
+    dates.push(moment(date))
+  }
+
+  function disabledDate(current) {
+    return !dates.some(date => date.isSame(current, 'day'))
+  }
+
+  const [values, setValues] = useState({})
+
+  const [options, setOptions] = useState([])
+
+  useEffect(() => {
+    setOptions(datesWithTimeslots[dates[0].format('YYYY-MM-DD')])
+    setValues({
+      date: dates[0],
+      option: datesWithTimeslots[dates[0].format('YYYY-MM-DD')][0],
+    })
+  }, [timeSlotChoices])
+
+  const handleInitialChoicesChange = timeSlot => {
+    setTimeSlotChoices(choices[timeSlot.target.value])
+  }
+
+  const handleDateChange = newDate => {
+    setValues({
+      date: newDate,
+      option: datesWithTimeslots[newDate.format('YYYY-MM-DD')][0],
+    })
+    setOptions(datesWithTimeslots[newDate.format('YYYY-MM-DD')])
+    const formatedDate = newDate.format('YYYY-MM-DD')
+    onChange(`${formatedDate} ${values.option}`)
+  }
+
+  const handleTimeSlotChange = newTimeslot => {
+    setValues(prevState => ({ ...prevState, option: newTimeslot }))
+    const formatedDate = values.date.format('YYYY-MM-DD')
+    onChange(`${formatedDate} ${newTimeslot}`)
+  }
+
+  return (
+    <>
+      {Object.keys(choices).length > 1 ? (
+        <Radio.Group
+          defaultValue={timeSlotsLabel[0]}
+          buttonStyle="solid"
+          style={{ display: 'flex' }}>
+          {timeSlotsLabel.map(label => (
+            <Radio.Button
+              key={label}
+              value={label}
+              onChange={timeSlot => {
+                handleInitialChoicesChange(timeSlot)
+              }}>
+              {label}
+            </Radio.Button>
+          ))}
+        </Radio.Group>
+      ) : null}
+
+      <div style={{ display: 'flex', marginTop: '0.5em' }}>
+        <DatePicker
+          format="LL"
+          style={{ width: '60%' }}
+          className="mr-2"
+          disabledDate={disabledDate}
+          disabled={dates.length > 1 ? false : true}
+          value={values.date}
+          onChange={date => {
+            handleDateChange(date)
+          }}
+        />
+
+        <Select
+          style={{ width: '35%' }}
+          onChange={option => {
+            handleTimeSlotChange(option)
+          }}
+          value={values.option}>
+          {options.length >= 1 &&
+            options.map(option => (
+              <Select.Option key={option} value={option}>
+                {option}
+              </Select.Option>
+            ))}
+        </Select>
+      </div>
+      </>
+  )
+}
