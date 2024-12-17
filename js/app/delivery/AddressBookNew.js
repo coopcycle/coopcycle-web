5fc34417d03aeadf4b2cebd2c8213466ec705423
Camille Nerriere <camille.nerriere@yahoo.fr>
2024-12-17 15:58:33 +0000

    add update fields when completed, checked if address is to be modified or remembered

--- js/app/delivery/AddressBookNew.js
+++ js/app/delivery/AddressBookNew.js
@@ -1,26 +1,42 @@
 import './AddressBook.scss'
-import React from 'react'
+import React, { useState } from 'react'
 import { Field } from 'formik'
-import { Input, Select } from 'antd'
+import { Input, Select, Checkbox, Button } from 'antd'
+import AddressAutosuggest from '../components/AddressAutosuggest'
+import Modal from 'react-modal'
+import { useTranslation } from 'react-i18next'
 
 export default function ({ addresses, setDeliveryAddress, deliveryAddress }) {
+  const { t } = useTranslation()
+
+  const [isModalOpen, setModalOpen] = useState(false)
+  const [alreadyAskedForModification, setAlreadyAskedForModification] =
+    useState(false)
+
+  const handleModifyAddress = () => {
+    if (
+      deliveryAddress.address['@id'] &&
+      !deliveryAddress.toBeModified &&
+      !alreadyAskedForModification
+    ) {
+      setModalOpen(true)
+    }
+  }
+
   const onAddressSelected = value => {
     const selectedAddress = addresses.find(
       address => address.streetAddress === value,
     )
 
-    setDeliveryAddress({
-      streetAddress: selectedAddress.streetAddress,
-      name: selectedAddress.name,
-      contactName: selectedAddress.contactName,
-      telephone: selectedAddress.telephone,
-    })
+    setDeliveryAddress(prevState => ({
+      ...prevState,
+      address: selectedAddress,
+    }))
   }
 
   /**
    * TODO :
    * Format telephone number
-   * Add AddressAutoSuggest
    */
 
   return (
@@ -34,6 +50,7 @@ export default function ({ addresses, setDeliveryAddress, deliveryAddress }) {
                 {...field}
                 showSearch
                 placeholder="Rechercher une adresse enregistrée"
+                value={deliveryAddress.address.streetAddress}
                 optionFilterProp="label"
                 onChange={value => {
                   form.setFieldValue(field.name, value)
@@ -59,9 +76,25 @@ export default function ({ addresses, setDeliveryAddress, deliveryAddress }) {
             {({ field, form }) => (
               <Input
                 {...field}
-                onChange={value => form.setFieldValue(field.name, value)}
+                onChange={e => {
+                  const newValue = e.target.value
+
+                  form.setFieldValue(field.name, newValue)
+                  handleModifyAddress()
+
+                  setDeliveryAddress(prevState => {
+                    const newState = {
+                      ...prevState,
+                      address: {
+                        ...prevState.address,
+                        name: newValue,
+                      },
+                    }
+                    return newState
+                  })
+                }}
                 placeholder="Nom"
-                value={deliveryAddress.name}
+                value={deliveryAddress.address.name}
               />
             )}
           </Field>
@@ -71,9 +104,25 @@ export default function ({ addresses, setDeliveryAddress, deliveryAddress }) {
             {({ field, form }) => (
               <Input
                 {...field}
-                onChange={value => form.setFieldValue(field.name, value)}
+                onChange={e => {
+                  const newValue = e.target.value
+
+                  form.setFieldValue(field.name, newValue)
+                  handleModifyAddress()
+
+                  setDeliveryAddress(prevState => {
+                    const newState = {
+                      ...prevState,
+                      address: {
+                        ...prevState.address,
+                        telephone: newValue,
+                      },
+                    }
+                    return newState
+                  })
+                }}
                 placeholder="Téléphone"
-                value={deliveryAddress.telephone}
+                value={deliveryAddress.address.telephone}
               />
             )}
           </Field>
@@ -83,26 +132,120 @@ export default function ({ addresses, setDeliveryAddress, deliveryAddress }) {
             {({ field, form }) => (
               <Input
                 {...field}
-                onChange={value => form.setFieldValue(field.name, value)}
+                onChange={e => {
+                  const newValue = e.target.value
+
+                  form.setFieldValue(field.name, newValue)
+                  handleModifyAddress()
+
+                  setDeliveryAddress(prevState => {
+                    const newState = {
+                      ...prevState,
+                      address: {
+                        ...prevState.address,
+                        contactName: newValue,
+                      },
+                    }
+                    return newState
+                  })
+                }}
                 placeholder="Contact"
-                value={deliveryAddress.contactName}
+                value={deliveryAddress.address.contactName}
               />
             )}
           </Field>
         </div>
         <div className="col-md-12">
           <Field name="streetAddress">
-            {/* il faut que ça soit remplacé par AddresseAutoSuggest */}
             {({ field, form }) => (
-              <Input
+              <AddressAutosuggest
                 {...field}
-                onChange={value => form.setFieldValue(field.name, value)}
-                placeholder="Adresse"
+                address={deliveryAddress.address}
+                addresses={addresses}
+                required={true}
+                reportValidity={true}
+                preciseOnly={true}
+                onAddressSelected={(value, address) => {
+                  console.log(value, address)
+                  setDeliveryAddress(prevState => ({
+                    ...prevState,
+                    address,
+                  }))
+
+                  form.setFieldValue(field.name, value)
+                  console.log('value', value)
+                }}
+                onClear={() => {
+                  setDeliveryAddress(prevState => ({
+                    ...prevState,
+                    selectedAddress: {
+                      streetAddress: '',
+                      name: '',
+                      contactName: '',
+                      telephone: '',
+                    },
+                  }))
+                }}
               />
             )}
           </Field>
+          <Checkbox
+            onChange={e => {
+              if (e.target.checked) {
+                setDeliveryAddress(prevState => ({
+                  ...prevState,
+                  toBeRemembered: true,
+                }))
+              } else {
+                setDeliveryAddress(prevState => ({
+                  ...prevState,
+                  toBeRemembered: false,
+                }))
+              }
+            }}>
+            Se souvenir de cette adresse
+          </Checkbox>
         </div>
       </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => {
+          setModalOpen(false)
+          setAlreadyAskedForModification(true)
+        }}
+        shouldCloseOnOverlayClick={false}
+        contentLabel={t('ADDRESS_BOOK_PROP_CHANGED_DISCLAIMER')}
+        overlayClassName="ReactModal__Overlay--addressProp"
+        className="ReactModal__Content--addressProp"
+        htmlOpenClassName="ReactModal__Html--open"
+        bodyOpenClassName="ReactModal__Body--open">
+        <h4 className="text-center">
+          {deliveryAddress.address.name} -{' '}
+          {deliveryAddress.address.streetAddress}
+        </h4>
+        <p>{t('ADDRESS_BOOK_PROP_CHANGED_DISCLAIMER')}</p>
+        <div className="d-flex justify-content-center">
+          <Button
+            className="mr-4"
+            onClick={() => {
+              setDeliveryAddress(prevState => ({
+                ...prevState,
+                toBeModified: true,
+              }))
+              setModalOpen(false)
+            }}>
+            {t('ADDRESS_BOOK_PROP_CHANGED_UPDATE')}
+          </Button>
+          <Button
+            type="primary"
+            onClick={() => {
+              setModalOpen(false)
+            }}>
+            {t('ADDRESS_BOOK_PROP_CHANGED_ONLY_ONCE')}
+          </Button>
+        </div>
+      </Modal>
     </>
   )
 }
