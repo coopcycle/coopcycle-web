6248b31e3589c7b634bd8c301820374c71e59a23
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2024-12-17 17:38:51 -0800

    re-format code

--- js/app/components/OrdersToInvoice/index.js
+++ js/app/components/OrdersToInvoice/index.js
@@ -7,18 +7,17 @@ import { useLazyGetOrdersQuery } from '../../api/slice'
 import Button from '../core/Button'
 
 export default () => {
+  const [storeId, setStoreId] = useState(null)
+  const [dateRange, setDateRange] = useState(null)
 
-  const [ storeId, setStoreId ] = useState(null)
-  const [ dateRange, setDateRange ] = useState(null)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [pageSize, setPageSize] = useState(10)
 
-  const [ currentPage, setCurrentPage ] = useState(1)
-  const [ pageSize, setPageSize ] = useState(10)
-
-  const [ trigger, { isLoading, data } ] = useLazyGetOrdersQuery()
+  const [trigger, { isLoading, data }] = useLazyGetOrdersQuery()
 
   const { t } = useTranslation()
 
-  const [ isModalOpen, setModalOpen ] = useState(false)
+  const [isModalOpen, setModalOpen] = useState(false)
 
   const { dataSource, total } = useMemo(() => {
     if (!data) {
@@ -26,7 +25,7 @@ export default () => {
     }
 
     return {
-      dataSource: data['hydra:member'].map((order) => ({
+      dataSource: data['hydra:member'].map(order => ({
         ...order,
         key: order['@id'],
         number: order.number,
@@ -35,114 +34,130 @@ export default () => {
         subTotal: order.subTotal,
         taxTotal: order.taxTotal,
         total: order.total,
-      })), total: data['hydra:totalItems'],
+      })),
+      total: data['hydra:totalItems'],
     }
-  }, [ data ])
+  }, [data])
 
   const columns = [
     {
       title: t('ADMIN_ORDERS_TO_INVOICE_ORDER_NUMBER_LABEL'),
       dataIndex: 'number',
       key: 'number',
-    }, {
+    },
+    {
       title: t('ADMIN_ORDERS_TO_INVOICE_DATE_LABEL'),
       dataIndex: 'shippedAt',
       key: 'shippedAt',
-    }, {
+    },
+    {
       title: t('ADMIN_ORDERS_TO_INVOICE_DESCRIPTION_LABEL'),
       dataIndex: 'description',
       key: 'description',
-    }, {
+    },
+    {
       title: t('ADMIN_ORDERS_TO_INVOICE_SUB_TOTAL_LABEL'),
       dataIndex: 'subTotal',
       key: 'subTotal',
-    }, {
+    },
+    {
       title: t('ADMIN_ORDERS_TO_INVOICE_TAXES_LABEL'),
       dataIndex: 'taxTotal',
       key: 'taxTotal',
-    }, {
+    },
+    {
       title: t('ADMIN_ORDERS_TO_INVOICE_TOTAL_LABEL'),
       dataIndex: 'total',
       key: 'total',
-    }, {
+    },
+    {
       key: 'action',
       dataIndex: 'id',
       align: 'right',
-      render: (id) => <a
-        href={ window.Routing.generate('admin_order_edit', { id }) } target="_blank"
-        rel="noopener noreferrer">
-        { t('EDIT') }
-      </a>,
-    } ]
+      render: id => (
+        <a
+          href={window.Routing.generate('admin_order_edit', { id })}
+          target="_blank"
+          rel="noopener noreferrer">
+          {t('EDIT')}
+        </a>
+      ),
+    },
+  ]
 
   const reloadData = (page, pageSize) => {
     trigger({
       storeId,
       dateRange: [
         dateRange[0].format('YYYY-MM-DD'),
-        dateRange[1].format('YYYY-MM-DD') ],
+        dateRange[1].format('YYYY-MM-DD'),
+      ],
       page: page,
       pageSize: pageSize,
     })
   }
 
-  return (<div>
-      <h3>{ t('ADMIN_ORDERS_TO_INVOICE_TITLE') }</h3>
+  return (
+    <div>
+      <h3>{t('ADMIN_ORDERS_TO_INVOICE_TITLE')}</h3>
       <div>
-        { t('ADMIN_ORDERS_TO_INVOICE_FILTER_STORE') }
-        <InputNumber min={ 0 } onChange={ setStoreId } />
-        { t('ADMIN_ORDERS_TO_INVOICE_FILTER_RANGE') }
-        <DatePicker.RangePicker onChange={ setDateRange } />
-        <Button primary onClick={ () => {
-          if (!storeId) {
-            return
-          }
+        {t('ADMIN_ORDERS_TO_INVOICE_FILTER_STORE')}
+        <InputNumber min={0} onChange={setStoreId} />
+        {t('ADMIN_ORDERS_TO_INVOICE_FILTER_RANGE')}
+        <DatePicker.RangePicker onChange={setDateRange} />
+        <Button
+          primary
+          onClick={() => {
+            if (!storeId) {
+              return
+            }
 
-          if (!dateRange) {
-            return
-          }
+            if (!dateRange) {
+              return
+            }
 
-          reloadData(currentPage, pageSize)
-
-        } }>{ t('ADMIN_ORDERS_TO_INVOICE_REFRESH') }</Button>
+            reloadData(currentPage, pageSize)
+          }}>
+          {t('ADMIN_ORDERS_TO_INVOICE_REFRESH')}
+        </Button>
       </div>
       <div className="row">
         <div className="col-md-12">
           <Table
-            columns={ columns }
-            loading={ isLoading }
-            dataSource={ dataSource }
+            columns={columns}
+            loading={isLoading}
+            dataSource={dataSource}
             rowKey="@id"
-            pagination={ {
-              pageSize, total,
-            } }
-            onChange={ (pagination) => {
+            pagination={{
+              pageSize,
+              total,
+            }}
+            onChange={pagination => {
               reloadData(pagination.current, pagination.pageSize)
 
               setCurrentPage(pagination.current)
               setPageSize(pagination.pageSize)
-            } }
+            }}
           />
         </div>
       </div>
       <Modal
-        isOpen={ isModalOpen }
-        appElement={ document.getElementById('warehouse') }
+        isOpen={isModalOpen}
+        appElement={document.getElementById('warehouse')}
         className="ReactModal__Content--no-default" // disable additional inline style from react-modal
-        shouldCloseOnOverlayClick={ true }
-        shouldCloseOnEsc={ true }
-        style={ { content: { overflow: 'unset' } } }
-      >
+        shouldCloseOnOverlayClick={true}
+        shouldCloseOnEsc={true}
+        style={{ content: { overflow: 'unset' } }}>
         <div className="modal-header">
           <h4 className="modal-title">
-            { t('ADMIN_WAREHOUSE_FORM_TITLE') }
-            <a className="pull-right" onClick={ () => setModalOpen(false) }><i
-              className="fa fa-close"></i></a>
+            {t('ADMIN_WAREHOUSE_FORM_TITLE')}
+            <a className="pull-right" onClick={() => setModalOpen(false)}>
+              <i className="fa fa-close"></i>
+            </a>
           </h4>
         </div>
-        {/*<WarehouseForm initialValues={initialValues} onSubmit={onSubmit}/>*/ }
+        {/*<WarehouseForm initialValues={initialValues} onSubmit={onSubmit}/>*/}
       </Modal>
     </div>
-
   )
 }
