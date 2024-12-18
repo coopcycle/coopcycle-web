01586b5e861fede2f616bb60e2ae38d845ddead2
Camille Nerriere <camille.nerriere@yahoo.fr>
2024-12-18 16:29:39 +0000

    start add time slot picker

--- assets/react/controllers/Delivery/SwitchTimeSlotFreePicker.jsx
+++ assets/react/controllers/Delivery/SwitchTimeSlotFreePicker.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from 'react'
-// import TimeSlotPicker from '../../../../js/app/components/delivery/TimeSlotPicker'
+import TimeSlotPicker from '../../../../js/app/components/delivery/TimeSlotPicker'
 
 export default ({ timeSlotsOptions, defaultTimeSlotName }) => {
   // const [values, setValues] = useState([])
   const [initialTimeSlotChoices, setInitialChoices] = useState({})
   useEffect(() => {
-    const initialChoices = {}
-    initialChoices[defaultTimeSlotName] = timeSlotsOptions[defaultTimeSlotName]
+    const initialChoices = timeSlotsOptions[defaultTimeSlotName]
     setInitialChoices(initialChoices)
   }, [timeSlotsOptions, defaultTimeSlotName])
 
-  console.log("ici", timeSlotsOptions, defaultTimeSlotName, initialChoices)
+  console.log("ici", timeSlotsOptions, defaultTimeSlotName, initialTimeSlotChoices)
   return (
     <>
     <div>Switch</div>
     <TimeSlotPicker 
-        choices={timeSlotsOptions} 
-        initialChoices={initialTimeSlotChoices}/>
+        choices={timeSlotsOptions}
+        initialChoices={initialTimeSlotChoices}
+        defaultTimeSlotName={defaultTimeSlotName} />
+      
       </>
   )
 }
