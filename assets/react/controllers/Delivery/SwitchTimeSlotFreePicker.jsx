695f0a8dfca0c307004af22d1015bc0bb5303e56
Camille Nerriere <camille.nerriere@yahoo.fr>
2024-12-19 13:54:02 +0000

    functionnal timeslotpicker

--- assets/react/controllers/Delivery/SwitchTimeSlotFreePicker.jsx
+++ assets/react/controllers/Delivery/SwitchTimeSlotFreePicker.jsx
@@ -1,23 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import TimeSlotPicker from '../../../../js/app/components/delivery/TimeSlotPicker'
 
-export default ({ timeSlotsOptions, defaultTimeSlotName, storeId }) => {
+export default ({ storeId }) => {
   // const [values, setValues] = useState([])
-  const [initialTimeSlotChoices, setInitialChoices] = useState({})
-  useEffect(() => {
-    const initialChoices = timeSlotsOptions[defaultTimeSlotName]
-    setInitialChoices(initialChoices)
-  }, [timeSlotsOptions, defaultTimeSlotName])
+
 
   return (
     <>
     <div>Switch</div>
     <TimeSlotPicker 
-        choices={timeSlotsOptions}
-        initialChoices={initialTimeSlotChoices}
-        defaultTimeSlotName={defaultTimeSlotName}
         storeId={storeId} />
-      
       </>
   )
 }
