fb2a452e86e36639895356b18c8c9493362b4d35
Camille Nerriere <camille.nerriere@yahoo.fr>
2024-12-20 15:16:56 +0000

    generate multiple tasks in delivery form

--- assets/react/controllers/Delivery/DeliveryForm.jsx
+++ assets/react/controllers/Delivery/DeliveryForm.jsx
@@ -3,19 +3,84 @@ import Task from './Task'
 import { antdLocale } from '../../../../js/app/i18n'
 import { ConfigProvider } from 'antd'
 import axios from 'axios'
+import moment from 'moment'
 
+function getNextRoundedTime() {
+  const now = moment();
+  now.add(15, 'minutes');
+  const roundedMinutes = Math.ceil(now.minutes() / 5) * 5;
+  if (roundedMinutes >= 60) {
+    now.add(1, 'hour');
+    now.minutes(roundedMinutes - 60);
+  } else {
+    now.minutes(roundedMinutes);
+  }
+  now.seconds(0);
+
+  return now;
+}
 
 const baseURL = location.protocol + '//' + location.host
 
 export default function ({ isNew, storeId }) {
-  
+
+  const initialTasks = [
+  {
+    type: 'pickup',
+    afterValue: getNextRoundedTime(),
+    beforeValue: getNextRoundedTime().add(15, 'minutes'),
+    timeSlot: null,
+    commentary: '',
+    deliveryAddress: {
+      address: {
+        streetAddress: '',
+        name: '',
+        contactName: '',
+        telephone: '',
+      },
+      toBeRemembered: false,
+      toBeModified: false,
+    },
+  },
+  {
+    type: 'dropoff',
+    afterValue: getNextRoundedTime(),
+    beforeValue: getNextRoundedTime().add(30, 'minutes'),
+    timeSlot: null,
+    commentary: '',
+    deliveryAddress: {
+      address: {
+        streetAddress: '',
+        name: '',
+        contactName: '',
+        telephone: '',
+      },
+      toBeRemembered: false,
+      toBeModified: false,
+    },
+  },
+];
+
+  const [tasks, setTasks] = useState(initialTasks)
   const [addresses, setAddresses] = useState([])
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [storeDeliveryInfos, setStoreDeliveryInfos] = useState({})
 
-  const handleSubmitStatus = (status) => {
-    setIsSubmitting(status);
-  }
+  console.log(tasks)
+  
+
+//   const addTask = (task) => {
+//   setTasks((prevTasks) => [...prevTasks, task]);
+// };
+
+  const updateTask = (index, updatedTask) => {
+  setTasks((prevTasks) =>
+    prevTasks.map((task, i) => (i === index ? { ...task, ...updatedTask } : task))
+  );
+  };
+
+//   const deleteTask = (index) => {
+//   setTasks((prevTasks) => prevTasks.filter((_, i) => i !== index));
+// };
 
   useEffect(() => {
     
@@ -62,16 +127,22 @@ export default function ({ isNew, storeId }) {
   console.log(isNew)
   
   return (
-    <ConfigProvider locale={antdLocale}>
+  <ConfigProvider locale={antdLocale}>
+    {tasks.map((task, index) => (
       <Task
+        key={index}
+        task={task}
         addresses={addresses}
-        onSubmitStatus={handleSubmitStatus}
         storeId={storeId}
         storeDeliveryInfos={storeDeliveryInfos}
+        onUpdate={(updatedTask) => updateTask(index, updatedTask)}
+        // onDelete={() => deleteTask(index)}
       />
-        <button type="submit" disabled={isSubmitting}>
-          Soumettre
-        </button>
-      </ConfigProvider>
-  )
+    ))}
+    <button type="submit">
+      Soumettre
+    </button>
+  </ConfigProvider>
+);
+
 }
