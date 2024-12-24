60d657d22c4cb00c788ea2a6280d8e791fca0cf5
Aloïs Guillopé <alois.guillope@gmail.com>
2024-12-24 10:15:26 +0100

    fix: setStore on delivery when extracting from token

--- src/Service/DeliveryManager.php
+++ src/Service/DeliveryManager.php
@@ -112,6 +112,7 @@ class DeliveryManager
 
         if (null === $store = $delivery->getStore()) {
             $store = $this->storeExtractor->extractStore();
+            $delivery->setStore($store);
         }
 
         // If no pickup address is specified, use the store address
