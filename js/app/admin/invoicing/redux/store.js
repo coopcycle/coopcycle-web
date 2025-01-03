3ba4eeceeb4c735e3e17cf34c2e7c3b7ddb28b4e
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-01-03 14:46:11 -0800

    remove unused slices

--- js/app/admin/invoicing/redux/store.js
+++ js/app/admin/invoicing/redux/store.js
@@ -1,17 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { accountSlice } from '../../../entities/account/reduxSlice'
-import { orderSlice } from '../../../entities/order/reduxSlice'
 import { apiSlice } from '../../../api/slice'
 
 export function createStoreFromPreloadedState(preloadedState) {
   return configureStore({
     reducer: {
       [accountSlice.name]: accountSlice.reducer,
-      [orderSlice.name]: orderSlice.reducer,
       [apiSlice.reducerPath]: apiSlice.reducer,
     },
     preloadedState,
-    middleware: getDefaultMiddleware => getDefaultMiddleware()
-      .concat(apiSlice.middleware),
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware().concat(apiSlice.middleware),
   })
 }
