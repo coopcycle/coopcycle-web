d67325b0ef160e04e04a8077e817beba38e9a98d
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-01-03 14:40:55 -0800

    remove unused slices

--- js/app/admin/invoicing/index.js
+++ js/app/admin/invoicing/index.js
@@ -2,22 +2,13 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { createStoreFromPreloadedState } from '../../order/redux/store'
 import { accountSlice } from '../../entities/account/reduxSlice'
-import { orderSlice } from '../../entities/order/reduxSlice'
 import OrdersToInvoice from './components/OrdersToInvoice'
 import { TopNav } from '../../components/TopNav'
 import { useTranslation } from 'react-i18next'
 
 const buildInitialState = () => {
-  // const shippingTimeRange = JSON.parse(orderDataElement.dataset.orderShippingTimeRange || null)
-  // const persistedTimeRange = JSON.parse(window.sessionStorage.getItem(getTimingPathForStorage(orderNodeId)))
-
   return {
     [accountSlice.name]: accountSlice.getInitialState(),
-    [orderSlice.name]: {
-      ...orderSlice.getInitialState(),
-      // '@id': orderNodeId,
-      // shippingTimeRange: shippingTimeRange,
-    },
   }
 }
 
