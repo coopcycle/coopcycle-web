3d8fa66fec56fe91424ca364d424f2df2569c8b7
Camille Nerriere <camille.nerriere@yahoo.fr>
2025-01-16 14:05:46 +0000

    edit packages to enter the value in input

--- js/app/components/delivery-form/Packages.js
+++ js/app/components/delivery-form/Packages.js
@@ -29,6 +29,8 @@ export default ({ index, packages, deliveryId }) => {
 
   const [packagesPicked, setPackagesPicked] = useState(picked)
 
+  console.log('packagesPicked', packagesPicked)
+
   const { t } = useTranslation()
 
   /** Insert the data in edit mode */
@@ -86,7 +88,7 @@ export default ({ index, packages, deliveryId }) => {
   }
 
   /**Used to make the input a controlated field */
-  const gatPackageQuantity = item => {
+  const getPackagesItems = item => {
     const sameTypePackage = packagesPicked.find(p => p.type === item.name)
     return sameTypePackage.quantity
   }
@@ -105,11 +107,23 @@ export default ({ index, packages, deliveryId }) => {
 
             <Input
               className="packages-item__quantity__input text-center"
-              value={gatPackageQuantity(item)}
+              value={getPackagesItems(item)}
               style={
-                gatPackageQuantity(item) !== 0 ? { fontWeight: '700' } : null
+                getPackagesItems(item) !== 0 ? { fontWeight: '700' } : null
               }
               ref={inputRef}
+              onChange={e => {
+                const packageIndex = packagesPicked.findIndex(
+                  p => p.type === item.name,
+                )
+                const newPackagesPicked = [...packagesPicked]
+                newPackagesPicked[packageIndex] = {
+                  type: item.name,
+                  quantity: e.target.value,
+                }
+                console.log('new', newPackagesPicked)
+                setPackagesPicked(newPackagesPicked)
+              }}
             />
 
             <Button
