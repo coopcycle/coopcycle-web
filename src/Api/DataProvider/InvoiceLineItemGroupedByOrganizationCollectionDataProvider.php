eb454a7479d99d2644dca5f9764589ebab90472f
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-01-16 15:37:18 -0800

    changed: optimized /api/invoice_line_items/grouped_by_organization endpoint

--- src/Api/DataProvider/InvoiceLineItemGroupedByOrganizationCollectionDataProvider.php
+++ src/Api/DataProvider/InvoiceLineItemGroupedByOrganizationCollectionDataProvider.php
@@ -8,10 +8,10 @@ use ApiPlatform\Core\DataProvider\ArrayPaginator;
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use AppBundle\Api\Dto\InvoiceLineItemGroupedByOrganization;
-use AppBundle\Entity\Delivery;
 use AppBundle\Entity\Sylius\Order;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
+use ShipMonk\DoctrineEntityPreloader\EntityPreloader;
 
 final class InvoiceLineItemGroupedByOrganizationCollectionDataProvider implements ContextAwareCollectionDataProviderInterface, RestrictedDataProviderInterface
 {
@@ -31,7 +31,21 @@ final class InvoiceLineItemGroupedByOrganizationCollectionDataProvider implement
     public function getCollection(string $resourceClass, string $operationName = null, array $context = []): iterable
     {
         $qb = $this->entityManager->getRepository(Order::class)->createQueryBuilder('o');
-        $qb->join(Delivery::class, 'd', Join::WITH, 'd.order = o');
+        //Optimization: to avoid extra queries during Hydration preload bi-directional one-to-one relations
+        // https://stackoverflow.com/questions/12362901/doctrine2-one-to-one-relation-auto-loads-on-query/34353840#34353840
+        // https://github.com/doctrine/orm/issues/4389
+        $qb->addSelect([
+            'order_delivery',
+            'order_timeline',
+            'order_invitation',
+            'order_loopeat_credentials',
+            'order_loopeat_details'
+        ])
+            ->leftJoin('o.delivery', 'order_delivery')
+            ->leftJoin('o.timeline', 'order_timeline')
+            ->leftJoin('o.invitation', 'order_invitation')
+            ->leftJoin('o.loopeatCredentials', 'order_loopeat_credentials')
+            ->leftJoin('o.loopeatDetails', 'order_loopeat_details');
 
         $queryNameGenerator = new QueryNameGenerator();
         foreach ($this->collectionExtensions as $extension) {
@@ -50,7 +64,7 @@ final class InvoiceLineItemGroupedByOrganizationCollectionDataProvider implement
                 );
             } else {
                 // Fetch all orders first, and then apply the pagination extension
-                $orders = $qb->getQuery()->getResult();
+                $orders = $this->getResultWithPreloadedEntities($qb);
                 $ordersGrouppedByStore = $this->groupByStore($orders);
 
                 // Relying on API Platform's pagination extension to get the pagination parameters (offset and page size)
@@ -71,8 +85,26 @@ final class InvoiceLineItemGroupedByOrganizationCollectionDataProvider implement
             }
         }
 
-        $orders = $qb->getQuery()->getResult();
+        $orders = $this->getResultWithPreloadedEntities($qb);
         return $this->groupByStore($orders);
+    }
+
+    private function getResultWithPreloadedEntities(QueryBuilder $qb): array
+    {
+        $orders = $qb->getQuery()->getResult();
+
+        //Optimization: to avoid extra queries preload one-to-many relations that will be used later
+        $preloader = new EntityPreloader($this->entityManager);
+
+        $orderItems = $preloader->preload($orders, 'items');
+        $preloader->preload($orders, 'adjustments');
+
+        $preloader->preload($orderItems, 'adjustments');
+
+        $delivery = $preloader->preload($orders, 'delivery');
+        $preloader->preload($delivery, 'store');
+
+        return $orders;
     }
 
     private function groupByStore($orders)
