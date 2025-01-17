ba815b30a8ed0c17ce2d23e92f5c9e0ae1ac6dec
Alexandre Segura <mex.zktk@gmail.com>
2025-01-17 16:37:16 +0100

    Increase length of product variant translation.

---
+++ src/Entity/Sylius/ProductVariantTranslation.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AppBundle\Entity\Sylius;
+
+use Sylius\Component\Product\Model\ProductVariantTranslation as BaseProductVariantTranslation;
+
+class ProductVariantTranslation extends BaseProductVariantTranslation
+{
+}
