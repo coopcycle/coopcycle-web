b55a79d77f4489434df9b206f464f713e5baf79a
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-01-20 16:16:39 -0800

    Revert "Increase length of product variant translation."
    This reverts commit b80f2a7cacdb193f364481bae01ea606d3a204ea.

--- src/Entity/Sylius/ProductVariantTranslation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AppBundle\Entity\Sylius;
-
-use Sylius\Component\Product\Model\ProductVariantTranslation as BaseProductVariantTranslation;
-
-class ProductVariantTranslation extends BaseProductVariantTranslation
-{
-}
