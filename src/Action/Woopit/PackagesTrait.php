6329a29a1f4ea9ad1bb414e906543d5b1923b24c
Alexandre Segura <mex.zktk@gmail.com>
2025-01-29 14:47:17 +0100

    Create packages on the fly for Woopit.

--- src/Action/Woopit/PackagesTrait.php
+++ src/Action/Woopit/PackagesTrait.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Action\Woopit;
 
+use AppBundle\Entity\Package;
+use AppBundle\Entity\PackageSet;
 use AppBundle\Entity\Task;
 use AppBundle\Entity\Woopit\QuoteRequest as WoopitQuoteRequest;
 
@@ -9,20 +11,62 @@ trait PackagesTrait
 {
     protected function parseAndApplyPackages(WoopitQuoteRequest $data, Task $task)
     {
+        $packageSet = $this->entityManager->getRepository(PackageSet::class)->findOneBy(['name' => 'Woopit']);
+        if (null === $packageSet) {
+            $packageSet = new PackageSet();
+            $packageSet->setName('Woopit');
+
+            $this->entityManager->persist($packageSet);
+            $this->entityManager->flush();
+        }
+
         if ($data->packages) {
+
             $packagesString = '';
 
-            foreach($data->packages as $package) {
-                if (!empty($packagesString)) {
-                    $packagesString .= ', ';
-                }
-                $packagesString .= $package['quantity'];
-                if (isset($package['weight'])) {
-                    $packagesString .= ' x ' . $package['weight']['value'] . ' ' . $package['weight']['unit'];
-                }
-            }
+            $qb = $this->entityManager->getRepository(Package::class)
+                ->createQueryBuilder('p')
+                ->andWhere('p.packageSet = :package_set')
+                ->andWhere('p.name = :name')
+                ->setParameter('package_set', $packageSet);
 
-            $task->setComments(sprintf('Packages: %s', $packagesString));
+            foreach ($data->packages as $package) {
+
+                $packageName = $this->generatePackageName($package);
+
+                $qb->setParameter('name', $packageName);
+                $p = $qb->getQuery()->getOneOrNullResult();
+
+                if (null === $p) {
+
+                    $p = new Package();
+                    $p->setPackageSet($packageSet);
+
+                    $p->setName($packageName);
+                    $p->setDescription($packageName);
+                    $p->setAverageVolumeUnits(1);
+                    $p->setMaxVolumeUnits(1);
+                    $p->setColor('#FFFFFF');
+                    $p->setAverageWeight(1000);
+                    $p->setMaxWeight(1000);
+                    $p->setShortCode('AA');
+
+                    $this->entityManager->persist($p);
+                    $this->entityManager->flush();
+                }
+
+                $task->addPackageWithQuantity($p, $package['quantity']);
+            }
         }
+    }
+
+    private function generatePackageName(array $data): string
+    {
+        $params = [];
+        foreach (['width', 'height', 'length', 'weight'] as $key) {
+            $params[] = $data[$key]['value'] . $data[$key]['unit'];
+        }
+
+        return sprintf('%s x %s x %s, %s', ...$params);
     }
 }
