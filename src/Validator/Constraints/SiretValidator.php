fc5ab1e9e81db2bf5b089b0b545d62a040ab04a0
Alexandre Segura <mex.zktk@gmail.com>
2025-02-06 18:55:48 +0100

    Use new INSEE API.

--- src/Validator/Constraints/SiretValidator.php
+++ src/Validator/Constraints/SiretValidator.php
@@ -12,10 +12,7 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class SiretValidator extends ConstraintValidator
 {
-    public function __construct(
-        private HttpClientInterface $inseeClient,
-        private string $apiKey,
-        private string $apiSecret)
+    public function __construct(private HttpClientInterface $inseeClient)
     {}
 
     public function validate($value, Constraint $constraint)
@@ -26,20 +23,12 @@ class SiretValidator extends ConstraintValidator
             return;
         }
 
-        if (empty($this->apiKey) && empty($this->apiSecret)) {
-            return;
-        }
-
         // Remove spaces
         $value = preg_replace('/\s+/', '', $value);
 
         try {
 
-            $this->inseeClient->request('GET', sprintf('entreprises/sirene/V3.11/siret/%s', $value), [
-                'headers' => [
-                    'Authorization' => sprintf('Bearer %s', $this->getToken()),
-                ]
-            ]);
+            $this->inseeClient->request('GET', sprintf('siret/%s', $value));
 
         } catch (ClientException $e) {
 
@@ -51,28 +40,6 @@ class SiretValidator extends ConstraintValidator
         } catch (ServerException $e) {
             // Die in silence
         }
-    }
-
-    /**
-     * @see https://api.insee.fr/catalogue/site/themes/wso2/subthemes/insee/pages/help.jag
-     */
-    private function getToken(): string
-    {
-        $base64 = base64_encode(sprintf('%s:%s', $this->apiKey, $this->apiSecret));
-
-        $response = $this->inseeClient->request('POST', 'token', [
-            'body' => [
-                'grant_type' => 'client_credentials',
-                'validity_period' => '604800',
-            ],
-            'headers' => [
-                'Authorization' => sprintf('Basic %s', $base64),
-            ]
-        ]);
-
-        $data = $response->toArray();
-
-        return $data['access_token'];
     }
 }
 
