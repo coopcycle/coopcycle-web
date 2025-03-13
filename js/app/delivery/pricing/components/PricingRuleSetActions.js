b16a72730dc71b8a9f2d97d9eff3989d288e0292
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-03-13 16:47:11 -0700

    added help text for Add rule buttons

--- js/app/delivery/pricing/components/PricingRuleSetActions.js
+++ js/app/delivery/pricing/components/PricingRuleSetActions.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import HelpIcon from '../../../components/HelpIcon'
+import Button from '../../../components/core/Button'
 
 export default function PricingRuleSetActions({ onAddRule }) {
   const { t } = useTranslation()
 
   return (
-    <div className="d-flex justify-content-end gap-2">
-      <button
-        type="button"
-        className="btn btn-success"
-        onClick={() => onAddRule('DELIVERY')}>
-        <i className="fa fa-plus"></i>
-        {t('PRICING_ADD_RULE')}
-      </button>
-      <button
-        type="button"
-        className="btn btn-success"
-        onClick={() => onAddRule('TASK')}>
-        <i className="fa fa-plus"></i>
-        {t('PRICING_ADD_RULE_PER_TASK')}
-      </button>
+    <div className="d-flex justify-content-end gap-4">
+      <div>
+        <Button success icon="plus" onClick={() => onAddRule('DELIVERY')}>
+          {t('PRICING_ADD_RULE')}
+        </Button>
+        <HelpIcon className="ml-1" tooltipText={t('PRICING_ADD_RULE_HELP')} />
+      </div>
+
+      <div>
+        <Button success icon="plus" onClick={() => onAddRule('TASK')}>
+          {t('PRICING_ADD_RULE_PER_TASK')}
+        </Button>
+        <HelpIcon
+          className="ml-1"
+          tooltipText={t('PRICING_ADD_RULE_PER_TASK_HELP')}
+        />
+      </div>
     </div>
   )
 }
