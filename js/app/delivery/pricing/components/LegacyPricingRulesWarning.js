64c6404ec8b9ce3e77f439be20dec896d76bc733
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-03-13 15:27:01 -0700

    updated legacy warning

--- js/app/delivery/pricing/components/LegacyPricingRulesWarning.js
+++ js/app/delivery/pricing/components/LegacyPricingRulesWarning.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Button } from 'antd'
+import { Button, Card } from 'antd'
 import Alert from '../../../components/core/Alert'
 
 export default function LegacyPricingRulesWarning({ migrateToTarget }) {
@@ -11,18 +11,22 @@ export default function LegacyPricingRulesWarning({ migrateToTarget }) {
       <div className="ml-2 d-flex flex-column">
         <div>{t('RULE_LEGACY_TARGET_DYNAMIC_MIGRATE_RATIONALE')}</div>
         <div className="mt-4 d-flex flex-column gap-4">
-          <div className="d-flex flex-column gap-2">
-            <Button type="primary" onClick={() => migrateToTarget('DELIVERY')}>
+          <Card>
+            <Button onClick={() => migrateToTarget('DELIVERY')}>
               {t('RULE_LEGACY_TARGET_DYNAMIC_MIGRATE_TARGET_DELIVERY')}
             </Button>
-            {t('RULE_LEGACY_TARGET_DYNAMIC_MIGRATE_TARGET_DELIVERY_HELP')}
-          </div>
-          <div className="d-flex flex-column gap-2">
+            <span className="ml-2 text-muted">
+              {t('RULE_LEGACY_TARGET_DYNAMIC_MIGRATE_TARGET_DELIVERY_HELP')}
+            </span>
+          </Card>
+          <Card>
             <Button onClick={() => migrateToTarget('TASK')}>
               {t('RULE_LEGACY_TARGET_DYNAMIC_MIGRATE_TARGET_TASK')}
             </Button>
-            {t('RULE_LEGACY_TARGET_DYNAMIC_MIGRATE_TARGET_TASK_HELP')}
-          </div>
+            <span className="ml-2 text-muted">
+              {t('RULE_LEGACY_TARGET_DYNAMIC_MIGRATE_TARGET_TASK_HELP')}
+            </span>
+          </Card>
         </div>
       </div>
     </Alert>
