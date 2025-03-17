4adef4a4cd7e203bcd43259f6ea69583cc911883
Aloïs Guillopé <alois.guillope@gmail.com>
2025-03-17 15:50:10 +0100

    feat: update rector to latest version

--- rector.php
+++ rector.php
@@ -2,54 +2,47 @@
 
 declare(strict_types=1);
 
+use Invenso\Rector\Set\ApiPlatformSetList;
+use Rector\Config\RectorConfig;
 use Rector\Core\Configuration\Option;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
-use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Php74\Rector\Assign\NullCoalescingOperatorRector;
-use Rector\Set\ValueObject\SetList;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\Symfony\Set\SymfonySetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
 
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
+use Rector\Set\ValueObject\SetList;
 
-    // Define what rule sets will be applied
-    $parameters->set(Option::SETS, [
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPreparedSets(deadCode: true)
+    ->withSets([
         SetList::DEAD_CODE,
-        SetList::PHP_74,
-        SetList::SYMFONY_44,
-    ]);
-
-    // set paths to directories with your code
-    $parameters->set(Option::PATHS, [
+        SetList::PHP_83,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+        SymfonySetList::SYMFONY_54
+    ])
+    ->withPaths([
         __DIR__ . '/app',
         __DIR__ . '/src',
-        // __DIR__ . '/tests',
-    ]);
-
-    // is your PHP version different from the one your refactor to? [default: your PHP version], uses PHP_VERSION_ID format
-    // $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
-
-    $parameters->set(Option::SKIP, [
+    ])
+    ->withSkip([
         __DIR__ . '/app/DoctrineMigrations',
         __DIR__ . '/src/Command',
 
         TypedPropertyFromStrictConstructorRector::class,
-        TypedPropertyRector::class,
+        // TypedPropertyRector::class,
         ClosureToArrowFunctionRector::class,
-        NullCoalescingOperatorRector::class,
-    ]);
+        NullCoalescingOperatorRector::class
+        ])
+    ->withSymfonyContainerXml(__DIR__ . '/var/cache/dev/AppKernelDevDebugContainer.xml')
+    // ->withSets([
+    //     ApiPlatformSetList::ANNOTATIONS_TO_ATTRIBUTES,
+    // ])
+;
 
-    $parameters->set(
-        Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER,
-        __DIR__ . '/var/cache/dev/AppKernelDevDebugContainer.xml'
-    );
-
-    // get services (needed for register a single rule)
-    // $services = $containerConfigurator->services();
-
-    // register a single rule
-    // $services->set(TypedPropertyRector::class);
-};
