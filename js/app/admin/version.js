af207d3441d009d544325955eb7b8886957d080d
Alexandre Segura <mex.zktk@gmail.com>
2025-03-17 14:58:20 +0100

    Change version number viz.
    - Remove changelog widget.
    - Show current version number.
    - Seek attention when new version has been deployed.

---
+++ js/app/admin/version.js
@@ -0,0 +1,24 @@
+import Cookies from 'js-cookie';
+import { compare } from 'compare-versions'
+
+import 'animate.css';
+
+const versionEl = document.getElementById('coopcycle-version');
+const version = versionEl.innerHTML;
+
+const lastViewedVersion = Cookies.get('__changelog_latest')
+
+if (version !== 'dev-master') {
+
+    const shouldHighlight = lastViewedVersion ? compare(version, lastViewedVersion, '>') : true;
+
+    versionEl.addEventListener('click', () => Cookies.set('__changelog_latest', version));
+
+    if (shouldHighlight) {
+        setTimeout(() => {
+            versionEl.classList.add('font-weight-bold');
+            versionEl.classList.add('text-warning');
+            versionEl.classList.add('animate__animated', 'animate__shakeX');
+        }, 400);
+    }
+}
