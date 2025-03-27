ffd4fbba3a402ef61b4be613405e804df5289fd4
Alexandre Segura <mex.zktk@gmail.com>
2025-03-27 17:00:28 +0100

    Remove useless imports.

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,6 @@ namespace AppBundle;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use AppBundle\DependencyInjection\Security\Factory\TokenBearerFactory;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class Kernel extends BaseKernel
 {
