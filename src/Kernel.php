2ed55d374ed79642f090a2c78110a4e6cc1ce2be
Alexandre Segura <mex.zktk@gmail.com>
2025-03-27 11:16:30 +0100

    Move code to Kernel.php.

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,18 @@ namespace AppBundle;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use AppBundle\DependencyInjection\Security\Factory\TokenBearerFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new TokenBearerFactory());
+    }
 }
