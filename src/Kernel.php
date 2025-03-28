512b98b91b5a73aebb36b8e2dd9064fb3d2865f8
Alexandre Segura <mex.zktk@gmail.com>
2025-03-28 09:36:31 +0100

    Override kernel methods.

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,27 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getProjectDir(): string
+    {
+        if (isset($_ENV['APP_PROJECT_DIR'])) {
+            return $_ENV['APP_PROJECT_DIR'];
+        } elseif (isset($_SERVER['APP_PROJECT_DIR'])) {
+            return $_SERVER['APP_PROJECT_DIR'];
+        }
+
+        return parent::getProjectDir();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLogDir(): string
+    {
+        // Just to add the "s"
+        return $_SERVER['APP_LOG_DIR'] ?? ($this->getProjectDir().'/var/logs');
+    }
 }
