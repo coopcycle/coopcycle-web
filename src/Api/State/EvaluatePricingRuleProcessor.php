f00e49083a63d2c6a9b18b7dccb1692f00bd5095
Alexandre Segura <mex.zktk@gmail.com>
2025-04-18 10:50:31 +0200

    Add EvaluatePricingRuleProcessor.

---
+++ src/Api/State/EvaluatePricingRuleProcessor.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Api\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Doctrine\Orm\State\ItemProvider;
+use ApiPlatform\State\ProcessorInterface;
+use AppBundle\Api\Dto\YesNoOutput;
+use AppBundle\Entity\Delivery\PricingRule;
+use AppBundle\Pricing\PriceCalculationVisitor;
+use AppBundle\ExpressionLanguage\ExpressionLanguage;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+final class EvaluatePricingRuleProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private readonly DeliveryProcessor $decorated,
+        private readonly ItemProvider $provider,
+        private readonly ExpressionLanguage $expressionLanguage,
+        private readonly LoggerInterface $logger = new NullLogger())
+    {}
+
+    public function process($data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        $delivery = $this->decorated->process($data, $operation, $uriVariables, $context);
+        $pricingRule = $this->provider->provide($operation, $uriVariables, $context);
+
+        $pricingRuleSet = $pricingRule->getRuleSet();
+
+        // clone the pricing rule set with only one rule that we want to evaluate
+        $ruleSet = clone $pricingRuleSet;
+        $ruleSet->setRules([$pricingRule]);
+
+        $visitor = new PriceCalculationVisitor($ruleSet, $this->expressionLanguage, $this->logger);
+        $visitor->visit($delivery);
+
+        $output = new YesNoOutput();
+        $output->result = count($visitor->getMatchedRules()) > 0;
+
+        return $output;
+    }
+}
