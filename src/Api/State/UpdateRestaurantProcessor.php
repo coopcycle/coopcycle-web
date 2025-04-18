7edab086e0745c9b0efde6a0cdf01f7288fc2a85
Alexandre Segura <mex.zktk@gmail.com>
2025-04-18 11:03:53 +0200

    Add UpdateRestaurantProcessor.

---
+++ src/Api/State/UpdateRestaurantProcessor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Api\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Doctrine\Orm\State\ItemProvider;
+use ApiPlatform\State\ProcessorInterface;
+use AppBundle\Entity\LocalBusiness;
+
+class UpdateRestaurantProcessor implements ProcessorInterface
+{
+    public function __construct(private readonly ItemProvider $provider)
+    {
+    }
+
+    public function process($data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        $restaurant = $this->provider->provide($operation, $uriVariables, $context);
+
+        if (is_object($data->hasMenu)) {
+            $restaurant->setMenuTaxon($data->hasMenu);
+        }
+
+        if (!empty($data->state)) {
+            $restaurant->setState($data->state);
+        }
+
+        return $restaurant;
+    }
+}
