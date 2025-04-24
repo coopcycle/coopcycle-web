763b75c852cc96baaecfcfc8ad891c29be4f613f
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-04-24 16:31:29 +0200

    wip

--- js/app/foodtech/dashboard/components/ModalContent.js
+++ js/app/foodtech/dashboard/components/ModalContent.js
@@ -26,23 +26,25 @@ import Timeline from './Timeline'
 import Button from './Button'
 import Customer from './Customer'
 
+import DeliveryMap from '../../../widgets/DeliveryMap'
+
 const Reasons = withTranslation()(({ order, onClick, loading, t }) => {
 
   return (
     <div className="d-flex flex-row justify-content-between py-4 border-top">
-      <Button onClick={ () => onClick('CUSTOMER') } loading={ loading } icon="user" danger>
-        { t('cancel.reason.CUSTOMER') }
+      <Button onClick={() => onClick('CUSTOMER')} loading={loading} icon="user" danger>
+        {t('cancel.reason.CUSTOMER')}
       </Button>
-      <Button onClick={ () => onClick('SOLD_OUT') } loading={ loading } icon="times" danger>
-        { t('cancel.reason.SOLD_OUT') }
+      <Button onClick={() => onClick('SOLD_OUT')} loading={loading} icon="times" danger>
+        {t('cancel.reason.SOLD_OUT')}
       </Button>
-      <Button onClick={ () => onClick('RUSH_HOUR') } loading={ loading } icon="fire" danger>
-        { t('cancel.reason.RUSH_HOUR') }
+      <Button onClick={() => onClick('RUSH_HOUR')} loading={loading} icon="fire" danger>
+        {t('cancel.reason.RUSH_HOUR')}
       </Button>
-      { (order.state === 'accepted' && order.takeaway) && (
-      <Button onClick={ () => onClick('NO_SHOW') } loading={ loading } icon="user-times" danger>
-        { t('cancel.reason.NO_SHOW') }
-      </Button>
+      {(order.state === 'accepted' && order.takeaway) && (
+        <Button onClick={() => onClick('NO_SHOW')} loading={loading} icon="user-times" danger>
+          {t('cancel.reason.NO_SHOW')}
+        </Button>
       )}
     </div>
   )
@@ -56,60 +58,60 @@ const LoopeatFormats = withTranslation()(({ t, order, loopeatFormats, updateLoop
 
   return (
     <Formik
-      initialValues={ initialValues }
-      onSubmit={ (values) => {
+      initialValues={initialValues}
+      onSubmit={(values) => {
         updateLoopeatFormats(order, values.loopeatFormats)
       }}
-      validateOnBlur={ false }
-      validateOnChange={ false }>
+      validateOnBlur={false}
+      validateOnChange={false}>
       {({
         values,
         submitForm,
       }) => (
-      <div>
-        { loopeatFormats.map((loopeatFormat, index) => {
+        <div>
+          {loopeatFormats.map((loopeatFormat, index) => {
 
-          return (
-            <div key={ `loopeat-format-${index}` }>
-              <h5>{ loopeatFormat.orderItem.name }</h5>
-              <table className="table table-condensed">
-                <tbody>
-                { loopeatFormat.formats.map((format, formatIndex) => (
-                  <tr key={ `loopeat-format-format-${formatIndex}` }>
-                    <td width="15%">
-                      <Field className="form-control input-sm"
-                        type="number"
-                        value={ values.loopeatFormats[index].formats[formatIndex].quantity }
-                        min="0"
-                        max={ format.quantity }
-                        name={ `loopeatFormats.${index}.formats.${formatIndex}.quantity` } />
-                    </td>
-                    <td><small>{ format.format_name }</small></td>
-                    <td className="text-right">
-                      <small className={
-                        classNames({
-                          'text-success': format.missing_quantity === 0,
-                          'text-warning': format.missing_quantity > 0,
-                        })
-                      }>
-                      { format.missing_quantity === 0 && t('ADMIN_DASHBOARD_ORDERS_LOOPEAT_STOCK_OK') }
-                      { format.missing_quantity > 0 && t('ADMIN_DASHBOARD_ORDERS_LOOPEAT_STOCK_NOK', { quantity: format.missing_quantity }) }
-                      </small>
-                    </td>
-                  </tr>)
-                ) }
-                </tbody>
-              </table>
-            </div>
-          )
-        }) }
-        <div className="text-right">
-          <button type="button" className="btn" onClick={ submitForm }>
-            { t('CART_DELIVERY_TIME_SUBMIT') }
-          </button>
+            return (
+              <div key={`loopeat-format-${index}`}>
+                <h5>{loopeatFormat.orderItem.name}</h5>
+                <table className="table table-condensed">
+                  <tbody>
+                    {loopeatFormat.formats.map((format, formatIndex) => (
+                      <tr key={`loopeat-format-format-${formatIndex}`}>
+                        <td width="15%">
+                          <Field className="form-control input-sm"
+                            type="number"
+                            value={values.loopeatFormats[index].formats[formatIndex].quantity}
+                            min="0"
+                            max={format.quantity}
+                            name={`loopeatFormats.${index}.formats.${formatIndex}.quantity`} />
+                        </td>
+                        <td><small>{format.format_name}</small></td>
+                        <td className="text-right">
+                          <small className={
+                            classNames({
+                              'text-success': format.missing_quantity === 0,
+                              'text-warning': format.missing_quantity > 0,
+                            })
+                          }>
+                            {format.missing_quantity === 0 && t('ADMIN_DASHBOARD_ORDERS_LOOPEAT_STOCK_OK')}
+                            {format.missing_quantity > 0 && t('ADMIN_DASHBOARD_ORDERS_LOOPEAT_STOCK_NOK', { quantity: format.missing_quantity })}
+                          </small>
+                        </td>
+                      </tr>)
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            )
+          })}
+          <div className="text-right">
+            <button type="button" className="btn" onClick={submitForm}>
+              {t('CART_DELIVERY_TIME_SUBMIT')}
+            </button>
+          </div>
         </div>
-      </div>
-    )}
+      )}
     </Formik>
   )
 })
@@ -119,9 +121,25 @@ class ModalContent extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      mode: 'default'
+      mode: 'default',
+      pickup_addr: props.order.restaurant.address,
+      dropoff_addr: props.order.shippingAddress,
+      map: null
     }
     this.cancelOrder = this.cancelOrder.bind(this)
+  }
+
+  componentDidMount() {
+    if (!this.state.map) {
+      try {
+        this.setState({
+          map: new DeliveryMap('map', {
+            pickup: [this.state.pickup_addr.geo.latitude, this.state.pickup_addr.geo.longitude],
+            dropoff: [this.state.dropoff_addr.geo.latitude, this.state.dropoff_addr.geo.longitude]
+          })
+        })
+      } catch (e) { }
+    }
   }
 
   cancelOrder(reason) {
@@ -170,11 +188,11 @@ class ModalContent extends React.Component {
       return (
         <div>
           <Reasons
-            order={ order }
-            loading={ loading }
-            onClick={ reason => this.cancelOrder(reason) } />
+            order={order}
+            loading={loading}
+            onClick={reason => this.cancelOrder(reason)} />
           <div className="text-center text-danger">
-            <span>{ this.props.t('ADMIN_DASHBOARD_ORDERS_CANCEL_REASON') }</span>
+            <span>{this.props.t('ADMIN_DASHBOARD_ORDERS_CANCEL_REASON')}</span>
           </div>
         </div>
       )
@@ -185,11 +203,11 @@ class ModalContent extends React.Component {
       return (
         <div>
           <Reasons
-            order={ order }
-            loading={ loading }
-            onClick={ reason => this.refuseOrder(reason) } />
+            order={order}
+            loading={loading}
+            onClick={reason => this.refuseOrder(reason)} />
           <div className="text-center text-danger">
-            <span>{ this.props.t('ADMIN_DASHBOARD_ORDERS_REFUSE_REASON') }</span>
+            <span>{this.props.t('ADMIN_DASHBOARD_ORDERS_REFUSE_REASON')}</span>
           </div>
         </div>
       )
@@ -199,8 +217,8 @@ class ModalContent extends React.Component {
 
       return (
         <div className="d-flex flex-row justify-content-end py-4 border-top">
-          <Button onClick={ this.restoreOrder.bind(this) } loading={ loading } icon="undo" success>
-            { this.props.t('ADMIN_DASHBOARD_ORDERS_RESTORE') }
+          <Button onClick={this.restoreOrder.bind(this)} loading={loading} icon="undo" success>
+            {this.props.t('ADMIN_DASHBOARD_ORDERS_RESTORE')}
           </Button>
         </div>
       )
@@ -210,11 +228,11 @@ class ModalContent extends React.Component {
 
       return (
         <div className="d-flex flex-row justify-content-between py-4 border-top">
-          <Button onClick={ () => this.setState({ mode: 'refuse' }) } loading={ loading } icon="ban" danger>
-            { this.props.t('ADMIN_DASHBOARD_ORDERS_REFUSE') }
+          <Button onClick={() => this.setState({ mode: 'refuse' })} loading={loading} icon="ban" danger>
+            {this.props.t('ADMIN_DASHBOARD_ORDERS_REFUSE')}
           </Button>
-          <Button onClick={ this.acceptOrder.bind(this) } loading={ loading } icon="check" primary>
-            { this.props.t('ADMIN_DASHBOARD_ORDERS_ACCEPT') }
+          <Button onClick={this.acceptOrder.bind(this)} loading={loading} icon="check" primary>
+            {this.props.t('ADMIN_DASHBOARD_ORDERS_ACCEPT')}
           </Button>
         </div>
       )
@@ -224,18 +242,18 @@ class ModalContent extends React.Component {
 
       return (
         <div className="d-flex flex-row justify-content-between py-4 border-top">
-          <Button onClick={ () => this.setState({ mode: 'cancel' }) } loading={ loading } icon="ban" danger>
-            { this.props.t('ADMIN_DASHBOARD_ORDERS_CANCEL') }
+          <Button onClick={() => this.setState({ mode: 'cancel' })} loading={loading} icon="ban" danger>
+            {this.props.t('ADMIN_DASHBOARD_ORDERS_CANCEL')}
           </Button>
-          { !order.takeaway && (
-          <Button onClick={ this.delayOrder.bind(this) } loading={ loading } icon="clock-o" primary>
-            { this.props.t('ADMIN_DASHBOARD_ORDERS_DELAY') }
-          </Button>
+          {!order.takeaway && (
+            <Button onClick={this.delayOrder.bind(this)} loading={loading} icon="clock-o" primary>
+              {this.props.t('ADMIN_DASHBOARD_ORDERS_DELAY')}
+            </Button>
           )}
-          { order.takeaway && (
-          <Button onClick={ this.fulfillOrder.bind(this) } loading={ loading } icon="check" success>
-            { this.props.t('ADMIN_DASHBOARD_ORDERS_FULFILL') }
-          </Button>
+          {order.takeaway && (
+            <Button onClick={this.fulfillOrder.bind(this)} loading={loading} icon="check" success>
+              {this.props.t('ADMIN_DASHBOARD_ORDERS_FULFILL')}
+            </Button>
           )}
         </div>
       )
@@ -249,10 +267,10 @@ class ModalContent extends React.Component {
     return (
       <div>
         <h5>
-          <i className="fa fa-user"></i>  { this.props.t('ADMIN_DASHBOARD_ORDERS_NOTES') }
+          <i className="fa fa-user"></i>  {this.props.t('ADMIN_DASHBOARD_ORDERS_NOTES')}
         </h5>
         <div className="speech-bubble">
-          <i className="fa fa-quote-left"></i>  { order.notes }
+          <i className="fa fa-quote-left"></i>  {order.notes}
         </div>
       </div>
     )
@@ -267,7 +285,7 @@ class ModalContent extends React.Component {
       <span>
         <span><i className="fa fa-phone"></i></span>
         <span> </span>
-        <span><small>{ phoneNumber ? phoneNumber.formatNational() : phoneNumberAsText }</small></span>
+        <span><small>{phoneNumber ? phoneNumber.formatNational() : phoneNumberAsText}</small></span>
       </span>
     )
   }
@@ -292,8 +310,8 @@ class ModalContent extends React.Component {
       items.push({
         icon: 'envelope-o',
         component: (
-          <a href={ `mailto:${customer.email}` }>
-            <small>{ customer.email }</small>
+          <a href={`mailto:${customer.email}`}>
+            <small>{customer.email}</small>
           </a>
         )
       })
@@ -301,21 +319,21 @@ class ModalContent extends React.Component {
 
     return (
       <ul className="list-unstyled">
-        { items.map((item, key) => {
+        {items.map((item, key) => {
 
           return (
-            <li key={ key }>
-              { item.icon && (
+            <li key={key}>
+              {item.icon && (
                 <span>
-                  <span><i className={ `fa fa-${item.icon}` }></i></span>
+                  <span><i className={`fa fa-${item.icon}`}></i></span>
                   <span> </span>
                 </span>
-              ) }
-              { item.text && ( <span><small>{ item.text }</small></span> ) }
-              { item.component && item.component }
+              )}
+              {item.text && (<span><small>{item.text}</small></span>)}
+              {item.component && item.component}
             </li>
           )
-        }) }
+        })}
       </ul>
     )
   }
@@ -328,16 +346,16 @@ class ModalContent extends React.Component {
       <div>
         <div className="text-right mb-3">
           <button type="button" className="btn btn-md"
-            onClick={ () => this.props.toggleReusablePackagings(this.props.order) }>Modifier les emballages</button>
+            onClick={() => this.props.toggleReusablePackagings(this.props.order)}>Modifier les emballages</button>
         </div>
-        { this.props.isLoopeatSectionOpen && (
-        <div>
-          <LoopeatFormats
-            order={ order }
-            loopeatFormats={ this.props.loopeatFormats }
-            updateLoopeatFormats={ this.props.updateLoopeatFormats } />
-        </div>
-        ) }
+        {this.props.isLoopeatSectionOpen && (
+          <div>
+            <LoopeatFormats
+              order={order}
+              loopeatFormats={this.props.loopeatFormats}
+              updateLoopeatFormats={this.props.updateLoopeatFormats} />
+          </div>
+        )}
       </div>
     )
   }
@@ -349,8 +367,8 @@ class ModalContent extends React.Component {
     return (
       <div className="panel panel-default">
         <div className="panel-heading">
-          <OrderNumber order={ order } />
-          <a className="pull-right" onClick={ () => this.props.setCurrentOrder(null) }>
+          <OrderNumber order={order} />
+          <a className="pull-right" onClick={() => this.props.setCurrentOrder(null)}>
             <i className="fa fa-close"></i>
           </a>
         </div>
@@ -358,34 +376,35 @@ class ModalContent extends React.Component {
           <div className="row">
             <div className="col-xs-6">
               <h5>
-                <Customer customer={ order.customer } />
+                <Customer customer={order.customer} />
               </h5>
             </div>
             <div className="col-xs-6">
               <div className="text-right">
-                { this.renderCustomerDetails(order.customer) }
+                {this.renderCustomerDetails(order.customer)}
               </div>
             </div>
           </div>
           <div>
             <h4 className="text-center">
-              <i className="fa fa-cutlery"></i>  { order.vendor.name }
+              <i className="fa fa-cutlery"></i>  {order.vendor.name}
             </h4>
-            { (order.restaurant && order.restaurant.telephone) && (
+            {(order.restaurant && order.restaurant.telephone) && (
               <div className="text-center text-muted">
-                { this.renderPhoneNumber(order.restaurant.telephone) }
+                {this.renderPhoneNumber(order.restaurant.telephone)}
               </div>
-            ) }
+            )}
           </div>
-          <h5>{ this.props.t('ADMIN_DASHBOARD_ORDERS_DISHES') }</h5>
-          <OrderItems itemsGroups={ itemsGroups } restaurant={ restaurant } />
-          <OrderTotal order={ order } />
-          { order.notes && this.renderNotes() }
-          { (order.restaurant.loopeatEnabled && order.reusablePackagingEnabled) && this.renderLoopeatSection() }
-          <h5>{ this.props.t('ADMIN_DASHBOARD_ORDERS_TIMELINE') }</h5>
-          <Timeline order={ order } />
-          { errorMessage ? <div className="alert alert-danger">{ errorMessage }</div> : null }
-          { this.renderButtons() }
+          <h5>{this.props.t('ADMIN_DASHBOARD_ORDERS_DISHES')}</h5>
+          <OrderItems itemsGroups={itemsGroups} restaurant={restaurant} />
+          <OrderTotal order={order} />
+          {order.notes && this.renderNotes()}
+          {(order.restaurant.loopeatEnabled && order.reusablePackagingEnabled) && this.renderLoopeatSection()}
+          <h5>{this.props.t('ADMIN_DASHBOARD_ORDERS_TIMELINE')}</h5>
+          <Timeline order={order} />
+          <div id="map" style={{ height: '300px', width: '300px' }} />
+          {errorMessage ? <div className="alert alert-danger">{errorMessage}</div> : null}
+          {this.renderButtons()}
         </div>
       </div>
     )
