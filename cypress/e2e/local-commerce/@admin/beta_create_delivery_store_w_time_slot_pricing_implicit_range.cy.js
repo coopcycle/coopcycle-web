c4e97c466af040054112228a51449f0263c226ef
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-04-24 18:23:36 -0700

    WIP(tests): [beta form] create delivery order with manually selected range

---
+++ cypress/e2e/local-commerce/@admin/beta_create_delivery_store_w_time_slot_pricing_implicit_range.cy.js
@@ -0,0 +1,137 @@
+context('Delivery (role: admin); store with time slot pricing', () => {
+  beforeEach(() => {
+    cy.symfonyConsole(
+      'coopcycle:fixtures:load ' +
+        '-s cypress/fixtures/setup.yml ' +
+        '-f cypress/fixtures/admin_user.yml ' +
+        '-f features/fixtures/ORM/store_w_time_slot_pricing.yml',
+    )
+    cy.setMockDateTime('2025-04-23 8:30:00')
+
+    cy.visit('/login')
+    cy.login('admin', '12345678')
+  })
+
+  afterEach(() => {
+    cy.resetMockDateTime()
+  })
+
+  it('WIP: [beta form] create delivery order with manually selected range', function () {
+    cy.visit('/admin/stores')
+
+    cy.get('[data-testid=store_Acme__list_item]')
+      .find('.dropdown-toggle')
+      .click()
+
+    cy.get('[data-testid=store_Acme__list_item]')
+      .contains('Créer une livraison')
+      .click()
+
+    cy.get('body > div.content > div > div > div > a')
+      .contains('click here')
+      .click()
+
+    // Pickup
+
+    cy.betaEnterAddressAtPosition(
+      0,
+      '23 Avenue Claude Vellefaux, 75010 Paris, France',
+      /^23,? Avenue Claude Vellefaux,? 75010,? Paris,? France/i,
+      'Office',
+      '+33112121212',
+      'John Doe',
+      'Pickup comments',
+    )
+
+    // //FIXME: set pickup time range to 12:00 - 14:00 manually
+    // cy.get('[data-testid-form="task-0"]').within(() => {
+    //   cy.get('.timeslot-container > .timeslot-container__icon').click()
+    //   cy.get('.text-secondary').click()
+    //   cy.get('.ant-picker-input')
+    //     .eq(0)
+    //     .within(() => {
+    //       cy.get('input').click()
+    //       cy.get('input').type(
+    //         '{backspace}{backspace}{backspace}{backspace}{backspace}12:00',
+    //       )
+    //     })
+    // })
+    // cy.get('.ant-picker-ok:visible > .ant-btn').click()
+    // cy.get('[data-testid-form="task-0"]').within(() => {
+    //   cy.get('.ant-picker-input')
+    //     .eq(1)
+    //     .within(() => {
+    //       cy.get('input').click()
+    //       cy.get('input').type(
+    //         '{backspace}{backspace}{backspace}{backspace}{backspace}14:00',
+    //       )
+    //     })
+    // })
+    // cy.get('.ant-picker-ok:visible > .ant-btn').click()
+
+    // Dropoff
+
+    cy.betaEnterAddressAtPosition(
+      1,
+      '72 Rue Saint-Maur, 75011 Paris, France',
+      /^72,? Rue Saint-Maur,? 75011,? Paris,? France/i,
+      'Office',
+      '+33112121212',
+      'Jane smith',
+      'Dropoff comments',
+    )
+
+    cy.get(`[name="tasks[${1}].weight"]`).clear()
+    cy.get(`[name="tasks[${1}].weight"]`).type(2.5)
+
+    cy.get('[data-testid="tax-included"]').contains('6,99 €')
+
+    cy.get('button[type="submit"]').click()
+
+    // list of deliveries page
+    // TODO : check for proper redirect when implemented
+    // cy.location('pathname', { timeout: 10000 }).should(
+    //   'match',
+    //   /\/admin\/stores\/[0-9]+\/deliveries$/,
+    // )
+
+    cy.location('pathname', { timeout: 10000 }).should(
+      'match',
+      /\/admin\/deliveries$/,
+    )
+
+    cy.get('[data-testid=delivery__list_item]')
+      .contains(/23,? Avenue Claude Vellefaux,? 75010,? Paris,? France/)
+      .should('exist')
+    cy.get('[data-testid=delivery__list_item]')
+      .contains(/72,? Rue Saint-Maur,? 75011,? Paris,? France/)
+      .should('exist')
+    cy.get('[data-testid=delivery__list_item]')
+      .contains(/€6.99/)
+      .should('exist')
+
+    cy.get('[data-testid="delivery__list_item"]')
+      .find('[data-testid="delivery_id"]')
+      .click()
+
+    // Delivery page
+    //TODO: verify that all input data is saved correctly
+    cy.get('[data-testid="breadcrumb"]')
+      .find('[data-testid="order_id"]')
+      .should('exist')
+
+    cy.get('[data-testid="breadcrumb"]')
+      .find('[data-testid="order_id"]')
+      .click()
+
+    // Order page
+    cy.location('pathname', { timeout: 10000 }).should(
+      'match',
+      /\/admin\/orders\/[0-9]+$/,
+    )
+
+    cy.get('[data-testid="order_item"]')
+      .find('[data-testid="total"]')
+      .contains('€6.99')
+  })
+})
