8fba3d3b3a8a05058f8caf8e652e62167142f5df
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-04-25 12:58:46 -0700

    add retry to deliveries import

--- cypress/e2e/package-delivery/import/deliveries.cy.js
+++ cypress/e2e/package-delivery/import/deliveries.cy.js
@@ -1,6 +1,11 @@
 context('Import deliveries (role: admin)', () => {
   beforeEach(() => {
-    cy.symfonyConsole('coopcycle:fixtures:load -s cypress/fixtures/setup.yml -f cypress/fixtures/admin_user.yml -f features/fixtures/ORM/store_w_time_slot_pricing.yml')
+    cy.symfonyConsole(
+      'coopcycle:fixtures:load ' +
+        '-s cypress/fixtures/setup.yml ' +
+        '-f cypress/fixtures/admin_user.yml ' +
+        '-f features/fixtures/ORM/store_w_time_slot_pricing.yml',
+    )
     cy.setMockDateTime('2019-12-12 8:00:00')
 
     cy.visit('/login')
@@ -14,19 +19,46 @@ context('Import deliveries (role: admin)', () => {
   it('imports deliveries', function () {
     cy.visit('/admin/deliveries')
 
-    cy.get('[data-target="#import-deliveries-modal"]').click();
+    cy.get('[data-target="#import-deliveries-modal"]').click()
     cy.get('input[type=file]').selectFile('cypress/fixtures/csv/deliveries.csv')
-    cy.get('#import-deliveries-modal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
+    cy.get(
+      '#import-deliveries-modal > .modal-dialog > .modal-content > .modal-footer > .btn-primary',
+    ).click()
 
-    cy.location('pathname', { timeout: 10000 }).should('eq', '/admin/deliveries')
+    cy.location('pathname', { timeout: 10000 }).should(
+      'eq',
+      '/admin/deliveries',
+    )
     cy.location('search').should('include', 'section=imports')
 
     // Allow the import to be processed
-    cy.wait(10000)
+    const checkImportStatus = (attempts = 4) => {
+      if (attempts === 0) {
+        throw new Error('Import status check failed after 3 attempts')
+      }
 
-    //TODO: verify imported deliveries
-    cy.get('[data-testid="tab:/admin/deliveries"]').click();
-    cy.location('pathname', { timeout: 10000 }).should('eq', '/admin/deliveries')
+      // The first attempt most likely fails, hence attempts = 4
+
+      cy.get('body').then($body => {
+        if ($body.find('i.fa-check[data-delivery-import-status]').length) {
+          // Element exists, proceed
+        } else {
+          // Element does not exist, refresh and retry
+          cy.wait(10000) // wait for 10 seconds
+          cy.reload()
+          checkImportStatus(attempts - 1)
+        }
+      })
+    }
+
+    checkImportStatus()
+
+    // verify imported deliveries
+    cy.get('[data-testid="tab:/admin/deliveries"]').click()
+    cy.location('pathname', { timeout: 10000 }).should(
+      'eq',
+      '/admin/deliveries',
+    )
 
     // deliveries.csv; line 2; pricing_rule_2
     cy.get('[data-testid=delivery__list_item]')
