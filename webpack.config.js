54589fea896c39f6ceb8c21433bf5d6b7427a0ed
Alexandre Segura <mex.zktk@gmail.com>
2025-05-21 11:51:11 +0200

    Bump deps for Webpack Encore 4.7.

--- webpack.config.js
+++ webpack.config.js
@@ -138,7 +138,7 @@ Encore
   .enableVersioning(Encore.isProduction())
 
 if (!Encore.isProduction()) {
-  Encore.enableEslintPlugin()
+  // Encore.enableEslintPlugin()
 }
 
 // https://github.com/webpack/webpack-dev-server/blob/master/CHANGELOG.md#400-beta0-2020-11-27
