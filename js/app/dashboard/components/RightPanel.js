2d3b3bf171415b400f3eeb1203a9761f9d59c384
Alexandre Segura <mex.zktk@gmail.com>
2025-05-21 20:53:25 +0200

    Bump react-toastify.

--- js/app/dashboard/components/RightPanel.js
+++ js/app/dashboard/components/RightPanel.js
@@ -3,7 +3,6 @@ import { DragDropContext } from '@hello-pangea/dnd'
 import Split from 'react-split'
 
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 
 import {
   setToursEnabled,
