f4061ccdf15c7dd918339f13af68633bebf4808b
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-05-28 15:38:24 -0700

    added missing css

--- js/app/components/delivery-form/RecurrenceRules.js
+++ js/app/components/delivery-form/RecurrenceRules.js
@@ -12,6 +12,8 @@ import {
 import { useTranslation } from 'react-i18next'
 import { useDeliveryFormFormikContext } from './hooks/useDeliveryFormFormikContext'
 
+import './RecurrenceRules.scss'
+
 function Content() {
   const { rruleValue: recurrenceRule } = useDeliveryFormFormikContext()
 
