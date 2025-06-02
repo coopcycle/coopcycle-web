4b583ef70e9e21682a57e90871fc46087d661d36
Alexandre Segura <mex.zktk@gmail.com>
2025-06-02 18:03:57 +0200

    Inject NormalizerInterface.

--- src/Twig/UserRuntime.php
+++ src/Twig/UserRuntime.php
@@ -3,12 +3,12 @@
 namespace AppBundle\Twig;
 
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Twig\Extension\RuntimeExtensionInterface;
 
 class UserRuntime implements RuntimeExtensionInterface
 {
-    public function __construct(private Security $security, private SerializerInterface $serializer)
+    public function __construct(private Security $security, private NormalizerInterface $normalizer)
     {}
 
     public function getUserAddresses()
@@ -22,7 +22,7 @@ class UserRuntime implements RuntimeExtensionInterface
 
         return array_map(function ($address) {
 
-            return $this->serializer->normalize($address, 'jsonld', [
+            return $this->normalizer->normalize($address, 'jsonld', [
                 'groups' => ['address']
             ]);
 
