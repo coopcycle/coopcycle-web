57748d1576a1807230d5cd75ffd1f9f4c8f3fe56
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-06-02 20:15:53 -0700

    updated recurrence rules tests

---
+++ cypress/e2e/package-delivery/@dispatcher/recurrence_rules/list_recurrence_rules.cy.js
@@ -0,0 +1,59 @@
+context('Managing recurrence rules (role: admin)', () => {
+  beforeEach(() => {
+    cy.loadFixtures('../cypress/fixtures/stores.yml')
+
+    // Login
+    cy.login('admin', '12345678')
+
+    // Create a delivery order and a recurrence rule
+    cy.visit('/admin/stores')
+
+    cy.get('[data-testid=store_Acme__list_item]')
+      .find('.dropdown-toggle')
+      .click()
+
+    cy.get('[data-testid=store_Acme__list_item]')
+      .contains('Créer une livraison')
+      .click()
+
+    // Create delivery page
+    cy.urlmatch(/\/admin\/stores\/[0-9]+\/deliveries\/new$/)
+
+    // Pickup
+    cy.betaChooseSavedAddressAtPosition(0, 1)
+
+    // Dropoff
+    cy.betaChooseSavedAddressAtPosition(1, 2)
+
+    cy.get(`[name="tasks[1].weight"]`).clear()
+    cy.get(`[name="tasks[1].weight"]`).type(2.5)
+
+    cy.get('[data-testid="tax-included"]').contains('4,99 €')
+
+    cy.get('[data-testid="recurrence__container"]').find('a').click()
+    cy.chooseDaysOfTheWeek([5, 6])
+    cy.get('[data-testid=save]').click()
+
+    cy.get('button[type="submit"]').click()
+  })
+
+  it('list recurrence rules', function () {
+    // Order page
+    cy.urlmatch(/\/admin\/orders\/[0-9]+$/)
+
+    cy.get('[data-testid="breadcrumb"]').find('[data-testid="store"]').click()
+
+    // Store page
+    cy.urlmatch(/\/admin\/stores\/[0-9]+$/)
+    cy.get('[data-testid="recurrence-rules"]').click()
+
+    // Recurrence rules page
+    cy.urlmatch(/\/admin\/stores\/[0-9]+\/recurrence-rules$/)
+    cy.get('[data-testid=recurrence_rule__list_item]')
+      .contains(/23,? Avenue Claude Vellefaux,? 75010,? Paris,? France/)
+      .should('exist')
+    cy.get('[data-testid=recurrence_rule__list_item]')
+      .contains(/72,? Rue Saint-Maur,? 75011,? Paris,? France/)
+      .should('exist')
+  })
+})
