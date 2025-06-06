a4f17715a1d908cee061dd0ab54f292a7ca84e5b
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-06-06 15:40:48 -0700

    log when an addess is not mocked

--- src/Service/MockGeocoderProvider.php
+++ src/Service/MockGeocoderProvider.php
@@ -13,9 +13,16 @@ use Geocoder\Model\Country;
 use Geocoder\Model\AddressCollection;
 use Geocoder\Query\GeocodeQuery;
 use Geocoder\Query\ReverseQuery;
+use Psr\Log\LoggerInterface;
 
 class MockGeocoderProvider implements GeocoderInterface
 {
+
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    )
+    {
+    }
 
     public function geocode(string $value): Collection
     {
@@ -34,7 +41,7 @@ class MockGeocoderProvider implements GeocoderInterface
                 new Bounds(48.815573, 2.224199, 48.902144, 2.469920),
                 '24',
                 'Rue de la Paix',
-                '75001',
+                '75002',
                 'Paris',
                 null,
                 new Country('France', 'FR'),
@@ -52,7 +59,7 @@ class MockGeocoderProvider implements GeocoderInterface
                 new Bounds(48.815573, 2.224199, 48.902144, 2.469920),
                 '44',
                 'Rue de Rivoli',
-                '75001',
+                '75004',
                 'Paris',
                 null,
                 new Country('France', 'FR'),
@@ -97,6 +104,7 @@ class MockGeocoderProvider implements GeocoderInterface
 
             return new AddressCollection([$address]);
         }  {
+            $this->logger->warning('MockGeocoderProvider:geocode No mock found for the given value: ' . $value);
             return new AddressCollection([]);
         }
     }
