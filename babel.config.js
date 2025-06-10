40a66b71ddb3510725b8bf40fa64d9572ecbdf1c
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-06-09 18:12:39 -0700

    enable mocking es imports in cypress

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,15 @@ module.exports = {
   "plugins": [
     "@babel/plugin-proposal-object-rest-spread",
     "@babel/plugin-proposal-optional-chaining",
+    // https://babeljs.io/docs/en/babel-plugin-transform-modules-commonjs
+    // loose ES6 modules allow us to dynamically mock imports during tests
+    // from https://github.com/cypress-io/cypress/tree/master/npm/react/cypress/component/advanced/mocking-imports
+    [
+      '@babel/plugin-transform-modules-commonjs',
+      {
+        loose: true,
+      },
+    ],
     ["import", { "libraryName": "antd", "style": (path, file) => {
       if (path === 'antd/lib/col' || path === 'antd/lib/row') {
         return 'antd/lib/grid/style/index.css'
