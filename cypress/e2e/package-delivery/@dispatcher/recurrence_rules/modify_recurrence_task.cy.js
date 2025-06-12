ef2fc4450188a865cf4acc22027289da42729e9d
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-06-12 15:59:57 -0700

    move commands into test body

--- cypress/e2e/package-delivery/@dispatcher/recurrence_rules/modify_recurrence_task.cy.js
+++ cypress/e2e/package-delivery/@dispatcher/recurrence_rules/modify_recurrence_task.cy.js
@@ -9,66 +9,71 @@ describe('Delivery with recurrence rule (role: admin)', () => {
     cy.setMockDateTime('2025-04-23 8:30:00')
 
     cy.login('admin', '12345678')
-
-    cy.visit('/admin/stores')
-
-    cy.get('[data-testid=store_Acme__list_item]')
-      .find('.dropdown-toggle')
-      .click()
-
-    cy.get('[data-testid=store_Acme__list_item]')
-      .contains('Créer une livraison')
-      .click()
-
-    // Create delivery page
-    cy.urlmatch(/\/admin\/stores\/[0-9]+\/deliveries\/new$/)
-
-    // Pickup
-
-    cy.betaChooseSavedAddressAtPosition(0, 1)
-
-    cy.betaEnterCommentAtPosition(0, 'Pickup comments')
-
-    cy.get(`[data-testid="form-task-0"]`).within(() => {
-      cy.get(`[data-testid=tags-select]`).click()
-    })
-    cy.get('#react-select-3-option-0').click()
-
-    // Dropoff
-
-    cy.betaEnterAddressAtPosition(
-      1,
-      '72 Rue Saint-Maur, 75011 Paris, France',
-      /^72,? Rue Saint-Maur,? 75011,? Paris,? France/i,
-      'Office',
-      '+33112121414',
-      'Jane smith',
-    )
-
-    cy.get(`[data-testid="form-task-1"]`).within(() => {
-      cy.get(
-        '[data-testid="/api/packages/1"] > .packages-item__quantity > :nth-child(3)',
-      ).click()
-    })
-    cy.betaEnterWeightAtPosition(1, 2.5)
-
-    cy.betaEnterCommentAtPosition(1, 'Dropoff comments')
-
-    cy.get(`[data-testid="form-task-1"]`).within(() => {
-      cy.get(`[data-testid=tags-select]`).click()
-    })
-    cy.get('#react-select-5-option-2').click()
-
-    cy.get('[data-testid="tax-included"]').contains('4,99 €')
-
-    cy.get('[data-testid="recurrence-add"]').click()
-    cy.chooseDaysOfTheWeek([5, 6])
-    cy.get('[data-testid=save]').click()
-
-    cy.get('button[type="submit"]').click()
   })
 
   it('modify recurrence task data', function () {
+    const createOrderWithRecurrence = () => {
+      cy.log('Create order with recurrence')
+
+      cy.visit('/admin/stores')
+
+      cy.get('[data-testid=store_Acme__list_item]')
+        .find('.dropdown-toggle')
+        .click()
+
+      cy.get('[data-testid=store_Acme__list_item]')
+        .contains('Créer une livraison')
+        .click()
+
+      // Create delivery page
+      cy.urlmatch(/\/admin\/stores\/[0-9]+\/deliveries\/new$/)
+
+      // Pickup
+
+      cy.betaChooseSavedAddressAtPosition(0, 1)
+
+      cy.betaEnterCommentAtPosition(0, 'Pickup comments')
+
+      cy.get(`[data-testid="form-task-0"]`).within(() => {
+        cy.get(`[data-testid=tags-select]`).click()
+      })
+      cy.get('#react-select-3-option-0').click()
+
+      // Dropoff
+
+      cy.betaEnterAddressAtPosition(
+        1,
+        '72 Rue Saint-Maur, 75011 Paris, France',
+        /^72,? Rue Saint-Maur,? 75011,? Paris,? France/i,
+        'Office',
+        '+33112121414',
+        'Jane smith',
+      )
+
+      cy.get(`[data-testid="form-task-1"]`).within(() => {
+        cy.get(
+          '[data-testid="/api/packages/1"] > .packages-item__quantity > :nth-child(3)',
+        ).click()
+      })
+      cy.betaEnterWeightAtPosition(1, 2.5)
+
+      cy.betaEnterCommentAtPosition(1, 'Dropoff comments')
+
+      cy.get(`[data-testid="form-task-1"]`).within(() => {
+        cy.get(`[data-testid=tags-select]`).click()
+      })
+      cy.get('#react-select-5-option-2').click()
+
+      cy.get('[data-testid="tax-included"]').contains('4,99 €')
+
+      cy.get('[data-testid="recurrence-add"]').click()
+      cy.chooseDaysOfTheWeek([5, 6])
+      cy.get('[data-testid=save]').click()
+
+      cy.get('button[type="submit"]').click()
+    }
+    createOrderWithRecurrence()
+
     // Order page
     cy.urlmatch(/\/admin\/orders\/[0-9]+$/)
 
@@ -77,64 +82,67 @@ describe('Delivery with recurrence rule (role: admin)', () => {
     // Recurrence rule page
     cy.urlmatch(/\/admin\/stores\/[0-9]+\/recurrence-rules\/[0-9]+$/)
 
-    // Modify task-related data
+    const modifyTaskRelatedData = () => {
+      cy.log('Modify task related data')
 
-    cy.betaEnterAddressAtPosition(
-      0,
-      '24 Rue de la Paix, 75002 Paris, France',
-      /^24,? Rue de la Paix,? 75002,? Paris,? France/i,
-      'Point 1',
-      '+33110101010',
-      'Name 1',
-    )
+      cy.betaEnterAddressAtPosition(
+        0,
+        '24 Rue de la Paix, 75002 Paris, France',
+        /^24,? Rue de la Paix,? 75002,? Paris,? France/i,
+        'Point 1',
+        '+33110101010',
+        'Name 1',
+      )
 
-    cy.get('[data-testid="form-task-0"]').within(() => {
-      cy.antdSelect('.ant-select[data-testid="select-after"]', '10:00')
-      cy.antdSelect('.ant-select[data-testid="select-before"]', '12:00')
-    })
+      cy.get('[data-testid="form-task-0"]').within(() => {
+        cy.antdSelect('.ant-select[data-testid="select-after"]', '10:00')
+        cy.antdSelect('.ant-select[data-testid="select-before"]', '12:00')
+      })
 
-    cy.betaEnterCommentAtPosition(0, 'Comment 1')
+      cy.betaEnterCommentAtPosition(0, 'Comment 1')
 
-    // tags
-    cy.get(`[data-testid="form-task-0"]`).within(() => {
-      cy.get('[aria-label="Remove Important"]').click()
-      cy.get(`[data-testid=tags-select]`).click()
-    })
-    cy.get('#react-select-2-option-1').click()
+      // tags
+      cy.get(`[data-testid="form-task-0"]`).within(() => {
+        cy.get('[aria-label="Remove Important"]').click()
+        cy.get(`[data-testid=tags-select]`).click()
+      })
+      cy.get('#react-select-2-option-1').click()
 
-    cy.betaEnterAddressAtPosition(
-      1,
-      '44 Rue de Rivoli, 75004 Paris, France',
-      /^44,? Rue de Rivoli,? 75004,? Paris,? France/i,
-      'Point 2',
-      '+33120202020',
-      'Name 2',
-    )
+      cy.betaEnterAddressAtPosition(
+        1,
+        '44 Rue de Rivoli, 75004 Paris, France',
+        /^44,? Rue de Rivoli,? 75004,? Paris,? France/i,
+        'Point 2',
+        '+33120202020',
+        'Name 2',
+      )
 
-    cy.get('[data-testid="form-task-1"]').within(() => {
-      cy.antdSelect('.ant-select[data-testid="select-after"]', '12:00')
-      cy.antdSelect('.ant-select[data-testid="select-before"]', '14:00')
-    })
+      cy.get('[data-testid="form-task-1"]').within(() => {
+        cy.antdSelect('.ant-select[data-testid="select-after"]', '12:00')
+        cy.antdSelect('.ant-select[data-testid="select-before"]', '14:00')
+      })
 
-    cy.get(`[data-testid="form-task-1"]`).within(() => {
-      cy.get(
-        '[data-testid="/api/packages/1"] > .packages-item__quantity > :nth-child(1)',
-      ).click()
-      cy.get(
-        '[data-testid="/api/packages/2"] > .packages-item__quantity > :nth-child(3)',
-      ).click()
-    })
+      cy.get(`[data-testid="form-task-1"]`).within(() => {
+        cy.get(
+          '[data-testid="/api/packages/1"] > .packages-item__quantity > :nth-child(1)',
+        ).click()
+        cy.get(
+          '[data-testid="/api/packages/2"] > .packages-item__quantity > :nth-child(3)',
+        ).click()
+      })
 
-    cy.betaEnterWeightAtPosition(1, 1.5)
+      cy.betaEnterWeightAtPosition(1, 1.5)
 
-    cy.betaEnterCommentAtPosition(1, 'Comment 2')
+      cy.betaEnterCommentAtPosition(1, 'Comment 2')
 
-    // tags
-    cy.get(`[data-testid="form-task-1"]`).within(() => {
-      cy.get('[aria-label="Remove Perishable"]').click()
-      cy.get(`[data-testid=tags-select]`).click()
-    })
-    cy.get('#react-select-3-option-0').click()
+      // tags
+      cy.get(`[data-testid="form-task-1"]`).within(() => {
+        cy.get('[aria-label="Remove Perishable"]').click()
+        cy.get(`[data-testid=tags-select]`).click()
+      })
+      cy.get('#react-select-3-option-0').click()
+    }
+    modifyTaskRelatedData()
 
     cy.get('button[type="submit"]').click()
 
