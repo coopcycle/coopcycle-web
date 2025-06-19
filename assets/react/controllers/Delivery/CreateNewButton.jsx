ed8d4dd7c12513ec32d805834e123e41c921cbeb
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-06-19 15:41:10 -0700

    re-use `Create delivery` button on the order page

---
+++ assets/react/controllers/Delivery/CreateNewButton.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { Button, Modal, Select } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import { useTranslation } from 'react-i18next'
+import { RootWithDefaults } from '../../../../js/app/utils/react'
+
+const { Option } = Select
+
+export default function CreateNewButton({ stores, routes }) {
+  const { t } = useTranslation()
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [selectedStore, setSelectedStore] = useState(null)
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+    setSelectedStore(null)
+  }
+
+  const handleStoreChange = value => {
+    setSelectedStore(value)
+    window.location.href = value
+  }
+
+  if (!stores || !routes) {
+    return null
+  }
+
+  return (
+    <RootWithDefaults>
+      <Button
+        className="btn-success-color"
+        type="primary"
+        icon={<PlusOutlined />}
+        onClick={showModal}>
+        {t('CREATE_NEW_ORDER')}
+      </Button>
+
+      <Modal
+        title={t('CREATE_NEW_ORDER')}
+        open={isModalVisible}
+        onCancel={handleCancel}
+        footer={null}>
+        <Select
+          style={{ width: '100%' }}
+          onChange={handleStoreChange}
+          value={selectedStore}>
+          {stores.map(store => (
+            <Option
+              key={store.id}
+              value={window.Routing.generate(routes.store_new, {
+                id: store.id,
+              })}>
+              {store.name}
+            </Option>
+          ))}
+        </Select>
+      </Modal>
+    </RootWithDefaults>
+  )
+}
