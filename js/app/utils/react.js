45ffba00feaa53c5d89366e62a04e7bbeb34b9fc
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-06-18 18:06:05 -0700

    added error boundary to the base component

--- js/app/utils/react.js
+++ js/app/utils/react.js
@@ -2,17 +2,30 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ConfigProvider } from 'antd'
 import { antdLocale } from '../i18n'
+import { ErrorBoundary } from 'react-error-boundary'
+import { useTranslation } from 'react-i18next'
 
-// Drop-in replacement for legacy React 17 render
-export function render(component, el) {
-  const root = createRoot(el)
-  root.render(<StrictMode>{component}</StrictMode>)
+function FallbackComponent() {
+  const { t } = useTranslation()
+  return (
+    <div className="d-flex flex-column align-items-center">
+      {t('ERROR_BOUNDARY_FALLBACK_MESSAGE')}
+    </div>
+  )
 }
 
 export function RootWithDefaults({ children }) {
   return (
     <StrictMode>
-      <ConfigProvider locale={antdLocale}>{children}</ConfigProvider>
+      <ErrorBoundary fallback={<FallbackComponent />}>
+        <ConfigProvider locale={antdLocale}>{children}</ConfigProvider>
+      </ErrorBoundary>
     </StrictMode>
   )
+}
+
+// Drop-in replacement for legacy React 17 render
+export function render(component, el) {
+  const root = createRoot(el)
+  root.render(<RootWithDefaults>{component}</RootWithDefaults>)
 }
