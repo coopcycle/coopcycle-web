907522e07a67999e39e6d6713484a68f6b4b9e60
Alexandre Segura <mex.zktk@gmail.com>
2025-06-20 13:19:40 +0200

    Use EntityPreloader for packages, DRY.

--- src/Api/State/TasksProvider.php
+++ src/Api/State/TasksProvider.php
@@ -7,7 +7,6 @@ use ApiPlatform\State\ProviderInterface;
 use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Extension\QueryResultCollectionExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGenerator;
-use AppBundle\Api\Dto\TaskMapper;
 use AppBundle\Entity\Task;
 use Doctrine\ORM\EntityManagerInterface;
 use ShipMonk\DoctrineEntityPreloader\EntityPreloader;
@@ -17,7 +16,6 @@ final class TasksProvider implements ProviderInterface
 
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-        private readonly TaskMapper $taskMapper,
         private readonly iterable $collectionExtensions,
     )
     {
@@ -57,83 +55,6 @@ final class TasksProvider implements ProviderInterface
     {
         $tasks = iterator_to_array($data);
 
-        // for a pickup in a delivery, the serialized weight is the sum of the dropoff weight and the packages are the "sum" of the dropoffs packages
-        $sql = <<<SQL
-            select
-                t_outer.id,
-                case
-                    WHEN t_outer.delivery_id is not null and t_outer.type = 'PICKUP' THEN
-                        (select json_agg(json_build_object(
-                            'id', packages_rows.id, 'task_package_id', packages_rows.task_package_id, 'name', packages_rows.name, 'type', packages_rows.name, 'quantity', packages_rows.quantity, 'volume_per_package', packages_rows.volume_units, 'short_code', packages_rows.short_code))
-                            FROM
-                                (select p.id AS id, MAX(tp.id) AS task_package_id, p.name AS name, p.average_volume_units AS volume_units, p.short_code as short_code, sum(tp.quantity) AS quantity
-                                    from task t inner join task_package tp on tp.task_id = t.id
-                                    inner join package p on tp.package_id = p.id
-                                    where t.delivery_id = t_outer.delivery_id
-                                    group by p.id, p.name, p.average_volume_units
-                                ) packages_rows)
-                    WHEN t_outer.type = 'DROPOFF' THEN
-                        (select json_agg(json_build_object(
-                            'id', packages_rows.id, 'task_package_id', packages_rows.task_package_id, 'name', packages_rows.name, 'type', packages_rows.name, 'quantity', packages_rows.quantity, 'volume_per_package', packages_rows.volume_units, 'short_code', packages_rows.short_code))
-                            FROM
-                                (select p.id AS id, MAX(tp.id) AS task_package_id, p.name AS name, p.average_volume_units AS volume_units, p.short_code as short_code, sum(tp.quantity) AS quantity
-                                    from task t inner join task_package tp on tp.task_id = t.id
-                                    inner join package p on tp.package_id = p.id
-                                    where t.id = t_outer.id
-                                    group by p.id, p.name, p.average_volume_units
-                                ) packages_rows)
-                    ELSE
-                        NULL
-                    END
-                    as packages,
-                case
-                    WHEN t_outer.delivery_id is not null and t_outer.type = 'PICKUP' THEN
-                        (select sum(weight) from task t where (t.delivery_id = t_outer.delivery_id))
-                    WHEN t_outer.type = 'DROPOFF' THEN
-                        t_outer.weight
-                    ELSE
-                        NULL
-                    END
-                    as weight
-            from task t_outer
-            where t_outer.id IN (:taskIds);
-        SQL;
-
-        $params = ['taskIds' => array_map(function ($task) {
-            return $task->getId();
-        }, $tasks)];
-        $query = $this->entityManager->getConnection()->executeQuery(
-            $sql,
-            $params,
-            ['taskIds' => \Doctrine\DBAL\Connection::PARAM_INT_ARRAY]
-        );
-        $res = $query->fetchAllAssociativeIndexed();
-
-        foreach ($tasks as $task) {
-            $input = $res[$task->getId()];
-
-            $packages = json_decode($input['packages'] ?? '[]', true);
-
-            // Add labels
-            foreach ($packages as $i => $p) {
-
-                $packages[$i]['labels'] = $this->taskMapper->getLabels(
-                    $task->getId(),
-                    //FIXME: should it be package_id instead of task_package_id?
-                    $p['task_package_id'],
-                    $p['quantity']
-                );
-
-                unset($packages[$i]['id']);
-                unset($packages[$i]['task_package_id']);
-            }
-
-            $task->setPrefetchedPackagesAndWeight([
-                    'packages' => $packages,
-                    'weight' => $input['weight']]
-            );
-        }
-
         // Optimization: to avoid extra queries preload one-to-many relations that will be used later
         $this->preloadEntities($tasks);
 
@@ -145,6 +66,9 @@ final class TasksProvider implements ProviderInterface
         $preloader = new EntityPreloader($this->entityManager);
 
         $preloader->preload($tasks, 'incidents');
+
+        $taskPackage = $preloader->preload($tasks, 'packages');
+        $preloader->preload($taskPackage, 'package');
 
         $delivery = $preloader->preload($tasks, 'delivery');
 
