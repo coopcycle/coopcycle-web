952a040a6c89c067cd381e350a0814da3c306533
Alexandre Segura <mex.zktk@gmail.com>
2025-06-23 17:58:36 +0200

    Add test.

--- tests/AppBundle/Api/Dto/TaskMapperTest.php
+++ tests/AppBundle/Api/Dto/TaskMapperTest.php
@@ -3,6 +3,8 @@
 namespace Tests\AppBundle\Api\Dto;
 
 use AppBundle\Api\Dto\TaskMapper;
+use AppBundle\Api\Dto\TaskPackageDto;
+use AppBundle\Entity\Package;
 use AppBundle\Entity\Task;
 use PHPUnit\Framework\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
@@ -39,7 +41,29 @@ class TaskMapperTest extends TestCase
         $dropoff3 = $this->createTask(Task::TYPE_DROPOFF);
         $dropoff3->setWeight(5000);
 
-        $this->assertEquals(15000, $this->taskMapper->getWeight($pickup, [$pickup, $dropoff1, $dropoff1, $dropoff3]));
+        $this->assertEquals(15000, $this->taskMapper->getWeight($pickup, [$pickup, $dropoff1, $dropoff2, $dropoff3]));
+    }
+
+    public function testGetPackagesForMultiDropoff()
+    {
+        $small = new Package();
+        $small->setName('SMALL');
+        $small->setShortCode('AB');
+        $small->setAverageVolumeUnits(1);
+
+        $pickup = $this->createTask(Task::TYPE_PICKUP);
+
+        $dropoff1 = $this->createTask(Task::TYPE_DROPOFF);
+        $dropoff1->addPackageWithQuantity($small, 5);
+        $dropoff2 = $this->createTask(Task::TYPE_DROPOFF);
+        $dropoff2->addPackageWithQuantity($small, 5);
+        $dropoff3 = $this->createTask(Task::TYPE_DROPOFF);
+
+        $packages = $this->taskMapper->getPackages($pickup, [$pickup, $dropoff1, $dropoff2, $dropoff3]);
+
+        $this->assertCount(1, $packages);
+        $this->assertInstanceOf(TaskPackageDto::class, $packages[0]);
+        $this->assertEquals(10, $packages[0]->quantity);
     }
 
     public function testGetWeightForMultiPickup()
@@ -54,5 +78,27 @@ class TaskMapperTest extends TestCase
         $dropoff = $this->createTask(Task::TYPE_DROPOFF);
 
         $this->assertEquals(15000, $this->taskMapper->getWeight($dropoff, [$pickup1, $pickup2, $pickup3, $dropoff]));
+    }
+
+    public function testGetPackagesForMultiPickup()
+    {
+        $small = new Package();
+        $small->setName('SMALL');
+        $small->setShortCode('AB');
+        $small->setAverageVolumeUnits(1);
+
+        $pickup1 = $this->createTask(Task::TYPE_PICKUP);
+        $pickup1->addPackageWithQuantity($small, 5);
+        $pickup2 = $this->createTask(Task::TYPE_PICKUP);
+        $pickup2->addPackageWithQuantity($small, 15);
+        $pickup3 = $this->createTask(Task::TYPE_PICKUP);
+
+        $dropoff = $this->createTask(Task::TYPE_DROPOFF);
+
+        $packages = $this->taskMapper->getPackages($dropoff, [$pickup1, $pickup2, $pickup3, $dropoff]);
+
+        $this->assertCount(1, $packages);
+        $this->assertInstanceOf(TaskPackageDto::class, $packages[0]);
+        $this->assertEquals(20, $packages[0]->quantity);
     }
 }
