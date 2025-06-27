3eab32b15c3137c4fd61165463730674e280a897
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-06-27 10:29:55 -0700

    WIP: put back PricingRuleHumanizer

---
+++ src/Pricing/RuleHumanizer.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AppBundle\Pricing;
+
+use AppBundle\Entity\Delivery\PricingRule;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Symfony\Component\ExpressionLanguage\Node\BinaryNode;
+use Symfony\Component\ExpressionLanguage\Node\Node;
+use Symfony\Component\ExpressionLanguage\Node\FunctionNode;
+
+class RuleHumanizer
+{
+    public function __construct(private ExpressionLanguage $expressionLanguage)
+    {
+    }
+
+    public function humanize(PricingRule $rule)
+    {
+        $parsedExpression = $this->expressionLanguage->parse($rule->getExpression(), [
+            'distance',
+            'weight',
+            'vehicle',
+            'pickup',
+            'dropoff',
+            'packages',
+            'order',
+        ]);
+
+        $accumulator = new \ArrayObject();
+
+        $this->traverseNode($parsedExpression->getNodes(), $accumulator);
+
+        return implode(', ', $accumulator->getArrayCopy());
+    }
+
+    private function traverseNode(Node $node, $accumulator)
+    {
+        if (isset($node->attributes['operator']) && $node->attributes['operator'] === 'and') {
+            $this->traverseNode($node->nodes['left'], $accumulator);
+            $this->traverseNode($node->nodes['right'], $accumulator);
+        } else {
+            if ($node instanceof FunctionNode) {
+                if ($node->attributes['name'] === 'in_zone' || $node->attributes['name'] === 'out_zone') {
+                    $accumulator[] = $this->humanizeZoneFunction($node);
+                }
+            } elseif ($node instanceof BinaryNode) {
+                $accumulator[] = $this->humanizeBinaryNode($node);
+            }
+        }
+    }
+
+    private function humanizeZoneFunction(FunctionNode $node): string
+    {
+        $taskType = $node->nodes['arguments']->nodes[0]->nodes['node']->attributes['name'];
+        $zoneName = $node->nodes['arguments']->nodes[1]->attributes['value'];
+        $direction = $node->attributes['name'] === 'in_zone' ? 'inside' : 'outside';
+
+        return sprintf('%s address %s zone "%s"', $taskType, $direction, $zoneName);
+    }
+
+    private function humanizeBinaryNode(BinaryNode $node): string
+    {
+        $attributeName = $node->nodes['left']->attributes['name'];
+
+        if ($node->attributes['operator'] === 'in') {
+
+            $left = $node->nodes['right']->nodes['left']->attributes['value'];
+            $right = $node->nodes['right']->nodes['right']->attributes['value'];
+
+            return sprintf('between %s and %s', $this->formatValue($left, $attributeName), $this->formatValue($right, $attributeName));
+
+        } else {
+
+            $value = $node->nodes['right']->attributes['value'];
+            if ($node->attributes['operator'] === '<') {
+                return sprintf('less than %s', $this->formatValue($value, $attributeName));
+            }
+            if ($node->attributes['operator'] === '>') {
+                return sprintf('more than %s', $this->formatValue($value, $attributeName));
+            }
+        }
+    }
+
+    private function formatValue($value, $unit)
+    {
+        switch ($unit) {
+            case 'weight':
+                return sprintf('%s kg', number_format($value / 1000, 2));
+            case 'distance':
+                return sprintf('%s km', number_format($value / 1000, 2));
+        }
+
+        return $value;
+    }
+}
---
+++ tests/AppBundle/Pricing/RuleHumanizerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\AppBundle\Pricing;
+
+use AppBundle\Entity\Delivery;
+use AppBundle\Entity\Delivery\PricingRule;
+use AppBundle\Pricing\RuleHumanizer;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class RuleHumanizerTest extends KernelTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        self::bootKernel();
+
+        // @see https://symfony.com/blog/new-in-symfony-4-1-simpler-service-testing
+        $this->expressionLanguage = self::$container->get('coopcycle.expression_language');
+
+        $this->humanizer = new RuleHumanizer($this->expressionLanguage);
+    }
+
+    public function testDistance()
+    {
+        $rule = new PricingRule();
+
+        $rule->setExpression('distance > 5000');
+        $this->assertEquals('more than 5.00 km', $this->humanizer->humanize($rule));
+
+        $rule->setExpression('distance in 3000..5000');
+        $this->assertEquals('between 3.00 km and 5.00 km', $this->humanizer->humanize($rule));
+    }
+
+    public function testWeight()
+    {
+        $rule = new PricingRule();
+
+        $rule->setExpression('weight > 5000');
+        $this->assertEquals('more than 5.00 kg', $this->humanizer->humanize($rule));
+
+        $rule->setExpression('weight in 3000..5000');
+        $this->assertEquals('between 3.00 kg and 5.00 kg', $this->humanizer->humanize($rule));
+    }
+
+    public function testInZone()
+    {
+        $rule = new PricingRule();
+        $rule->setExpression('in_zone(dropoff.address, "south")');
+
+        $this->assertEquals('dropoff address inside zone "south"', $this->humanizer->humanize($rule));
+    }
+
+    public function testInZoneOutZone()
+    {
+        $rule = new PricingRule();
+        $rule->setExpression('in_zone(pickup.address, "south") and out_zone(dropoff.address, "north") and weight > 5000');
+
+        $this->assertEquals('pickup address inside zone "south", dropoff address outside zone "north", more than 5.00 kg', $this->humanizer->humanize($rule));
+    }
+
+    public function testPricePerPackage()
+    {
+        $rule = new PricingRule();
+        $rule->setExpression('packages.containsAtLeastOne("Bouquet L")');
+        $rule->setPrice('price_per_package(packages, "Bouquet L", 100, 0, 100)');
+
+        // $this->assertEquals('contains one package', $this->humanizer->humanize($rule));
+    }
+
+    public function testPriceRange()
+    {
+        $rule = new PricingRule();
+        $rule->setExpression('distance > 0');
+        $rule->setPrice('price_range(distance, 100, 1000, 2000)');
+
+        // $this->assertEquals('contains one package', $this->humanizer->humanize($rule));
+    }
+}
