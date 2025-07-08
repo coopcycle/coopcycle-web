57a9c92e800e0e08970081b744d02f832378070a
Alexandre Segura <mex.zktk@gmail.com>
2025-07-08 10:40:41 +0200

    Rewrite OrderStoreFilter.

--- src/Api/Filter/OrderStoreFilter.php
+++ src/Api/Filter/OrderStoreFilter.php
@@ -2,37 +2,22 @@
 
 namespace AppBundle\Api\Filter;
 
-use ApiPlatform\Api\IriConverterInterface;
 use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
 use AppBundle\Entity\Sylius\Order;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\Persistence\ManagerRegistry;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
+use Symfony\Component\PropertyInfo\Type;
 
 final class OrderStoreFilter extends AbstractFilter
 {
     private string $storeIdProperty = 'delivery.store.id';
     private string $storeIdAlias = 'store';
-
-    public function __construct(
-        ManagerRegistry $managerRegistry,
-        IriConverterInterface $iriConverter,
-        ?PropertyAccessorInterface $propertyAccessor = null,
-        ?LoggerInterface $logger = null,
-        ?NameConverterInterface $nameConverter = null
-    )
-    {
-        $properties = [
-            $this->storeIdProperty => 'exact',
-        ];
-
-        parent::__construct($managerRegistry, $iriConverter, $propertyAccessor, $logger, $properties, $nameConverter);
-    }
 
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?Operation $operation = null, array $context = []): void
     {
@@ -42,24 +27,34 @@ final class OrderStoreFilter extends AbstractFilter
 
         // expose alias in the API instead of a path to a nested property
         if ($this->storeIdAlias === $property) {
-            parent::filterProperty('delivery.store.id', $value, $queryBuilder, $queryNameGenerator, $resourceClass, $operation, $context);
-        }
-    }
 
-    private function renameProperty(array &$description, string $property, string $alias, string $suffix): void
-    {
-        $description[$alias.$suffix] = $description[$property.$suffix];
-        $description[$alias.$suffix]['property'] = $alias;
-        unset($description[$property.$suffix]);
+            $alias = $queryBuilder->getRootAliases()[0];
+
+            [$alias, $field] = $this->addJoinsForNestedProperty($this->storeIdProperty, $alias, $queryBuilder, $queryNameGenerator, $resourceClass, Join::INNER_JOIN);
+
+            $valueParameter = $queryNameGenerator->generateParameterName($field);
+
+            $queryBuilder
+                ->andWhere(\sprintf('%s.%s = :%s', $alias, $field, $valueParameter))
+                ->setParameter($valueParameter, $value, (string) $this->getDoctrineFieldType($property, $resourceClass));
+        }
     }
 
     public function getDescription(string $resourceClass): array
     {
-        $result = parent::getDescription($resourceClass);
-
-        $this->renameProperty($result, $this->storeIdProperty, $this->storeIdAlias, '');
-        $this->renameProperty($result, $this->storeIdProperty, $this->storeIdAlias, '[]');
-
-        return $result;
+        return [
+            'store' => [
+                'property' => $this->storeIdAlias,
+                'type' => Type::BUILTIN_TYPE_INT,
+                'required' => false,
+                'is_collection' => false,
+            ],
+            'store[]'=> [
+                'property' => $this->storeIdAlias,
+                'type' => Type::BUILTIN_TYPE_INT,
+                'required' => false,
+                'is_collection' => true,
+            ]
+        ];
     }
 }
