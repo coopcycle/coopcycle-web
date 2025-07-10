5b2d88e91d5cec52b043e5a464e990f0619c9437
Alexandre Segura <mex.zktk@gmail.com>
2025-07-10 10:55:06 +0200

    Fix 404.

--- src/Api/State/UrbantzWebhookProvider.php
+++ src/Api/State/UrbantzWebhookProvider.php
@@ -6,6 +6,7 @@ use AppBundle\Api\Resource\UrbantzWebhook;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use ApiPlatform\Util\Inflector;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class UrbantzWebhookProvider implements ProviderInterface
 {
@@ -14,8 +15,9 @@ final class UrbantzWebhookProvider implements ProviderInterface
         $id = Inflector::tableize($uriVariables['id']);
 
         if (!UrbantzWebhook::isValidEvent($id)) {
-
-            return null;
+            // We throw an exception instead of returning null,
+            // because otherwise the process would continue
+            throw new NotFoundHttpException();
         }
 
         return new UrbantzWebhook($id);
