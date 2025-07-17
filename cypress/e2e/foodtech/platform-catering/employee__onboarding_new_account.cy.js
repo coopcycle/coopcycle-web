67c94653ec2fff4f7e41e1c1b8dc31d79f2468fc
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-07-17 11:38:56 -0700

    split into 2 tests

--- cypress/e2e/foodtech/platform-catering/employee__onboarding_new_account.cy.js
+++ cypress/e2e/foodtech/platform-catering/employee__onboarding_new_account.cy.js
@@ -1,10 +1,11 @@
 describe('Platform catering; employee; onboarding', () => {
   beforeEach(() => {
+    cy.loadFixtures(
+      '../cypress/fixtures/business_account_employee_invitation_new_user.yml',
+    )
   })
 
   it('should onboard an employee with a new user account', () => {
-    cy.loadFixtures('../cypress/fixtures/business_account_employee_invitation_new_user.yml')
-
     cy.visit('/invitation/define-password/INVITATION_EMPLOYEE')
 
     cy.get('.alert-info').should('contain', 'manager01')
@@ -27,20 +28,5 @@ describe('Platform catering; employee; onboarding', () => {
     // Confirmation page
     cy.urlmatch(/\/register\/confirmed/)
     cy.get('.content').should('contain', 'Félicitations')
-  })
-
-  it('should onboard an employee with an existing user account', () => {
-    cy.loadFixtures('../cypress/fixtures/business_account_employee_invitation_existing_user.yml')
-
-    cy.login('user01', '12345678')
-
-    cy.visit('/invitation/define-password/INVITATION_EMPLOYEE')
-
-    cy.get('.content').should('contain', 'Business Account 1')
-
-    // associate personal account with a business account page
-    cy.get('.btn-primary').click()
-
-    cy.get('.alert-success', { timeout: 10000 }).should('contain', 'Business Account 1')
   })
 })
