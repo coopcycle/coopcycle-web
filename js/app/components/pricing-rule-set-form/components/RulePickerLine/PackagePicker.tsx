b3a47c4251d9639fcec02d7f94537f605f7cd2ef
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-07-16 18:20:21 -0700

    add: search

--- js/app/components/pricing-rule-set-form/components/RulePickerLine/PackagePicker.tsx
+++ js/app/components/pricing-rule-set-form/components/RulePickerLine/PackagePicker.tsx
@@ -18,6 +18,8 @@ export default function PackagePicker({ value, onChange }) {
   return (
     <Select
       data-testid="condition-package-select"
+      showSearch
+      optionFilterProp="label"
       onChange={value =>
         // replicate on change signature of html input until we re-write PricePickerLine component
         onChange({
