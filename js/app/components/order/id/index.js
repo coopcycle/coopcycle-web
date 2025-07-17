e2890ef8cef5d2760ac15f6f0d9d61594bc2d36b
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-07-17 11:34:11 -0700

    move js code outside of twig template

--- js/app/components/order/id/index.js
+++ js/app/components/order/id/index.js
@@ -4,8 +4,17 @@ import ClipboardJS from 'clipboard'
 import { RootWithDefaults } from '../../../utils/react'
 import Map from '../../../components/DeliveryMap'
 import Itinerary from '../../../components/DeliveryItinerary'
+import i18n from '../../../i18n'
 
-new ClipboardJS('#copy');
+new ClipboardJS('#copy')
+
+$('[data-change-state] button[type="submit"]').on('click', function (e) {
+  const message = $(e.target).data('message')
+
+  if (!window.confirm(message || i18n.t('ARE_YOU_SURE'))) {
+    e.preventDefault()
+  }
+})
 
 const el = document.querySelector('#delivery-info')
 
