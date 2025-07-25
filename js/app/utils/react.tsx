f0a63b7a3cb80fbcebb04fd0ed21d54c36af7436
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-07-25 15:12:05 -0700

    WIP: log error to Sentry

--- js/app/utils/react.tsx
+++ js/app/utils/react.tsx
@@ -1,8 +1,16 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { AntdConfigProvider } from './antd'
+import { ErrorInfo, ReactNode, StrictMode } from 'react'
+import { Container, createRoot } from 'react-dom/client'
 import { ErrorBoundary } from 'react-error-boundary'
 import { useTranslation } from 'react-i18next'
+import * as Sentry from '@sentry/browser'
+import { AntdConfigProvider } from './antd'
+
+const logError = (error: Error, info: ErrorInfo) => {
+  Sentry.withScope(scope => {
+    scope.setExtra('errorInfo', info)
+    Sentry.captureException(error)
+  })
+}
 
 function FallbackComponent() {
   const { t } = useTranslation()
@@ -13,10 +21,10 @@ function FallbackComponent() {
   )
 }
 
-export function RootWithDefaults({ children }) {
+export function RootWithDefaults({ children }: { children: ReactNode }) {
   return (
     <StrictMode>
-      <ErrorBoundary fallback={<FallbackComponent />}>
+      <ErrorBoundary fallback={<FallbackComponent />} onError={logError}>
         <AntdConfigProvider>{children}</AntdConfigProvider>
       </ErrorBoundary>
     </StrictMode>
@@ -24,7 +32,7 @@ export function RootWithDefaults({ children }) {
 }
 
 // Drop-in replacement for legacy React 17 render
-export function render(component, el) {
+export function render(component: ReactNode, el: Container) {
   const root = createRoot(el)
   root.render(<RootWithDefaults>{component}</RootWithDefaults>)
 }
