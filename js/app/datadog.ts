e3c14366b36269f739fb80d2cf3ec66c61a63f5a
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-07-25 17:27:44 -0700

    add type-hints

--- js/app/datadog.ts
+++ js/app/datadog.ts
@@ -1,19 +1,21 @@
-import { datadogLogs } from '@datadog/browser-logs'
+import { datadogLogs, Logger } from '@datadog/browser-logs'
 import { datadogRum } from '@datadog/browser-rum'
 
 const el = document.getElementById('datadog')
-const datadogEnabled = Boolean(el)
+const clientToken = el?.dataset.clientToken
+const datadogEnabled = Boolean(clientToken)
 
 // regex patterns to identify known bot instances:
 // see https://docs.datadoghq.com/real_user_monitoring/guide/identify-bots-in-the-ui/#filter-out-bot-sessions-on-intake
-const botPattern = '(googlebot|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)'
+const botPattern =
+  '(googlebot|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)'
 
 const regex = new RegExp(botPattern, 'i')
 const isBot = regex.test(navigator.userAgent)
 
-if (datadogEnabled) {
+if (clientToken) {
   datadogLogs.init({
-    clientToken: el.dataset.clientToken,
+    clientToken: clientToken,
     site: 'datadoghq.com',
     service: el.dataset.service,
     forwardErrorsToLogs: true,
@@ -24,7 +26,7 @@ if (datadogEnabled) {
 
   datadogRum.init({
     applicationId: el.dataset.applicationId,
-    clientToken: el.dataset.clientToken,
+    clientToken: clientToken,
     site: 'datadoghq.com',
     service: el.dataset.service,
     // Specify a version number to identify the deployed version of your application in Datadog
@@ -41,15 +43,13 @@ if (datadogEnabled) {
   })
 }
 
-const DatadogLogger = {
+const DatadogLogger: Logger = {
   /**
    * Send a log with debug level.
-   * @param message: The message to send.
-   * @param context: The additional context to send.
    */
-  debug(message, context) {
+  debug(message: string, context?: object): void {
     if (!datadogEnabled) {
-      console.debug(message, context)
+      console.debug('DatadogLogger (dry run): ' + message, context)
       return
     }
 
@@ -58,12 +58,10 @@ const DatadogLogger = {
 
   /**
    * Send a log with info level.
-   * @param message: The message to send.
-   * @param context: The additional context to send.
    */
-  info(message, context) {
+  info(message: string, context?: object): void {
     if (!datadogEnabled) {
-      console.info(message, context)
+      console.info('DatadogLogger (dry run): ' + message, context)
       return
     }
 
@@ -72,12 +70,10 @@ const DatadogLogger = {
 
   /**
    * Send a log with warn level.
-   * @param message: The message to send.
-   * @param context: The additional context to send.
    */
-  warn(message, context) {
+  warn(message: string, context?: object): void {
     if (!datadogEnabled) {
-      console.warn(message, context)
+      console.warn('DatadogLogger (dry run): ' + message, context)
       return
     }
 
@@ -86,12 +82,10 @@ const DatadogLogger = {
 
   /**
    * Send a log with error level.
-   * @param message: The message to send.
-   * @param context: The additional context to send.
    */
-  error(message, context) {
+  error(message: string, context?: object): void {
     if (!datadogEnabled) {
-      console.error(message, context)
+      console.error('DatadogLogger (dry run): ' + message, context)
       return
     }
 
--- js/types/global.d.ts
+++ js/types/global.d.ts
@@ -1,11 +1,12 @@
+import { Logger } from '@datadog/browser-logs'
 
 // Extend window object for global variables
+
 declare global {
   interface Window {
     Routing: {
-      generate: (route: string, params?: Record<string, any>) => string;
-    };
+      generate: (route: string, params?: Record<string, any>) => string
+    }
+    DatadogLogger: Logger
   }
 }
-
-export {};
