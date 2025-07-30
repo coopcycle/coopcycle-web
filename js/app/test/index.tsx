0b35f1451876a20a625a3c868771194e3b340ad2
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-07-30 13:05:40 -0700

    Revert "add a page to test a crash on production"
    This reverts commit 90af98a52f19d33ac8775b187d0d256121779383.

--- js/app/test/index.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '../utils/react'
-import CrashTestPage from './CrashTestPage'
-
-const container = document.getElementById('crash-test-page')
-if (container) {
-  render(<CrashTestPage />, container)
-}
--- webpack.config.js
+++ webpack.config.js
@@ -87,7 +87,6 @@ Encore
   .addEntry('zone-preview', './js/app/zone/preview.js')
   .addEntry('failure-form', './js/app/failure/form.js')
   .addEntry('incident-form', './js/app/incident/form.js')
-  .addEntry('crash-test-page', './js/app/test/index.tsx')
 
   // @see https://symfony.com/doc/current/frontend/encore/custom-loaders-plugins.html#adding-custom-plugins
   // @see https://github.com/moment/moment/issues/2373
