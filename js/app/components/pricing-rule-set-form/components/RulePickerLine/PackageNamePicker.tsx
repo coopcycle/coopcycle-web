1acb72be0db54de90fb4a96212e096d8683cd38f
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-10-03 12:49:16 -0700

    replace 'no value' with a placeholder

--- js/app/components/pricing-rule-set-form/components/RulePickerLine/PackageNamePicker.tsx
+++ js/app/components/pricing-rule-set-form/components/RulePickerLine/PackageNamePicker.tsx
@@ -9,8 +9,6 @@ type Props = {
   onChange: (event: { target: { value: string } }) => void;
 };
 
-const NO_VALUE_LABEL = '-';
-
 export default function PackageNamePicker({ value, onChange }: Props) {
   const { data: packages, isFetching } = useGetPackagesQuery();
 
@@ -24,11 +22,6 @@ export default function PackageNamePicker({ value, onChange }: Props) {
     packages.forEach(pkg => {
       // ignore packages from deleted package sets
       if (!pkg.packageSet) {
-        return;
-      }
-
-      // exclude to avoid clashes with an unselected state
-      if (pkg.name === NO_VALUE_LABEL) {
         return;
       }
 
@@ -61,14 +54,12 @@ export default function PackageNamePicker({ value, onChange }: Props) {
           },
         })
       }
+      placeholder={'-'}
       value={value}
-      options={[
-        { value: '', label: NO_VALUE_LABEL },
-        ...packageNames.map(item => ({
-          value: item,
-          label: item,
-        })),
-      ]}
+      options={packageNames.map(item => ({
+        value: item,
+        label: item,
+      }))}
     />
   );
 }
