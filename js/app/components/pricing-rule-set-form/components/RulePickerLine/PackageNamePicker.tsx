4ab58b6fdad51cf3554dcd0a969f060e06ddfabf
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-10-03 12:05:08 -0700

    fix: make sure that package names are unique

--- js/app/components/pricing-rule-set-form/components/RulePickerLine/PackageNamePicker.tsx
+++ js/app/components/pricing-rule-set-form/components/RulePickerLine/PackageNamePicker.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Select } from 'antd';
 import { useGetPackagesQuery } from '../../../../api/slice';
 import PickerIsLoading from './PickerIsLoading';
@@ -9,8 +9,36 @@ type Props = {
   onChange: (event: { target: { value: string } }) => void;
 };
 
-export default function PackagePicker({ value, onChange }: Props) {
+const NO_VALUE_LABEL = '-';
+
+export default function PackageNamePicker({ value, onChange }: Props) {
   const { data: packages, isFetching } = useGetPackagesQuery();
+
+  const packageNames = useMemo(() => {
+    if (!packages) {
+      return [];
+    }
+
+    const allPackageNames: string[] = [];
+
+    packages.forEach(pkg => {
+      // ignore packages from deleted package sets
+      if (!pkg.packageSet) {
+        return;
+      }
+
+      // exclude to avoid clashes with an unselected state
+      if (pkg.name === NO_VALUE_LABEL) {
+        return;
+      }
+
+      allPackageNames.push(pkg.name);
+    });
+
+    const uniquePackageNames = Array.from(new Set(allPackageNames)).sort();
+
+    return uniquePackageNames;
+  }, [packages]);
 
   if (isFetching) {
     return <PickerIsLoading />;
@@ -35,10 +63,10 @@ export default function PackagePicker({ value, onChange }: Props) {
       }
       value={value}
       options={[
-        { value: '', label: '-' },
-        ...packages.map(item => ({
-          value: item.name,
-          label: item.name,
+        { value: '', label: NO_VALUE_LABEL },
+        ...packageNames.map(item => ({
+          value: item,
+          label: item,
         })),
       ]}
     />
