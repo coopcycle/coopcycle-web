19e2f0b1f1cf751b385afc5b79af22fdbdb29082
Alexandre Segura <mex.zktk@gmail.com>
2025-10-09 11:00:02 +0200

    Implement refunds.

--- src/Payment/Gateway/Pawapay.php
+++ src/Payment/Gateway/Pawapay.php
@@ -11,65 +11,42 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class Pawapay implements GatewayInterface
 {
-    public function __construct(
-        private HttpClientInterface $pawapayClient,
-        private CurrencyContextInterface $currencyContext,
-        private string $locale,
-        private string $countryCode
-    )
+    public function __construct()
     {}
 
     public function authorize(PaymentInterface $payment, array $context = [])
     {
-        // https://api.sandbox.pawapay.io/v2/predict-provider
-        // $response = $pawapayClient->request('POST', 'v2/predict-provider', [
-        //     'json' => [
-        //         'phoneNumber' => '+233241234567'
-        //     ]
-        // ]);
-
-        $order = $payment->getOrder();
-        $customer = $order->getCustomer();
-
-        $depositId = Uuid::uuid4()->toString();
-
-        $numberFormatter = \NumberFormatter::create($this->locale, \NumberFormatter::DECIMAL);
-        $numberFormatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, 2);
-        $numberFormatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, 2);
-
-        $payload = [
-            'depositId' => $depositId,
-            'returnUrl' => 'https://demo.coopcycle.org/pawaypay/return',
-            'amountDetails' => [
-                "amount" => strval($numberFormatter->format($order->getTotal() / 100)), // Make sure it is a string
-                "currency" => strtoupper($this->currencyContext->getCurrencyCode())
-            ],
-            'country' => $this->countryCode,
-            'phoneNumber' => $customer->getPhoneNumber(),
-            // 'reason' => 'Lorem ipsum',
-        ];
-
-        $response = $this->pawapayClient->request('POST', 'v2/paymentpage', [
-            'json' => $payload
-        ]);
-
-        dd($response->toArray());
+        // Not implemented
     }
 
     public function capture(PaymentInterface $payment)
     {
-        // $captureId = $this->edenred->captureTransaction($payment);
-        // $payment->setEdenredCaptureId($captureId);
+        // Not implemented
     }
 
     public function refund(PaymentInterface $payment, $amount = null): Refund
     {
-        // $refund = $payment->addRefund($amount);
+        $refund = $payment->addRefund($amount);
 
-        // $this->edenred->refund($payment, $amount);
-        // $refund->setData(['edenred_transaction_id' => $payment->getEdenredAuthorizationId()]);
+        $refundId = Uuid::uuid4()->toString();
 
-        // return $refund;
+        $numberFormatter = \NumberFormatter::create($this->locale, \NumberFormatter::DECIMAL);
+        $numberFormatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, 2);
+        $numberFormatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, 2);
+        $numberFormatter->setAttribute(\NumberFormatter::GROUPING_USED, 0);
+
+        $response = $this->pawapayClient->request('POST', 'v2/refunds', [
+            'json' => [
+                'refundId' => $refundId,
+                'depositId' => $payment->getPawapayDepositId(),
+                'amount' => strval($numberFormatter->format($amount / 100)), // Make sure it is a string
+                'currency' => strtoupper($this->currencyContext->getCurrencyCode())
+            ]
+        ]);
+
+        $refund->setData(['pawapay_refund_id' => $refundId]);
+
+        return $refund;
     }
 }
 
