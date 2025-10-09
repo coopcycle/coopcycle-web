1c496895b77b85f80be41c59514a798b291a6191
Alexandre Segura <mex.zktk@gmail.com>
2025-10-09 10:35:23 +0200

    Render a disclaimer box.

--- js/app/components/Payment/pawapay.js
+++ js/app/components/Payment/pawapay.js
@@ -1,8 +1,26 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './pawapay.scss'
+
+import pawapayLogo from './pawapay.svg'
+
+const Pawapay = () => {
+  return (
+    <div className="pawapay p-3 d-flex align-items-center">
+      <img src={pawapayLogo} height="24" className="mr-2" />
+      <span>Pay with our partner <strong>pawaPay</strong>. Click on the button below to proceed.</span>
+    </div>
+  )
+}
+
 export default {
   async init() {
   },
   async mount(el, method, options, formOptions) {
     this.redirectUrl = options.pawapay.pawapay_payment_page_url;
+
+    createRoot(el).render(<Pawapay />)
 
     return new Promise((resolve) => {
       resolve()
