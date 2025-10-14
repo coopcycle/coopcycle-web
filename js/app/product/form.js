f584a3b814e5e5d8550787eb77eec59031772ebe
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-10-14 14:53:34 +0200

    ui(options): add handle to make clearer that list is sortable

--- js/app/product/form.js
+++ js/app/product/form.js
@@ -37,7 +37,7 @@ $(function() {
         }
       },
       images: JSON.parse(formData.dataset.productImages),
-      size: [ 256, 256 ]
+      size: [256, 256]
     })
   }
 })
@@ -82,16 +82,23 @@ if (addReusablePackaging) {
   addReusablePackaging.addEventListener("click", addFormToCollection)
 }
 
-$(document).on('click', '[data-reusable-packaging-delete]', function (e) {
+$(document).on('click', '[data-reusable-packaging-delete]', function(e) {
   $(e.currentTarget).closest('li').remove()
+})
+
+document.querySelectorAll('#product_options > div').forEach(el => {
+  const icon = document.createElement('i')
+  icon.className = 'fa fa-bars handle'
+  el.appendChild(icon)
 })
 
 new Sortable(document.querySelector('#product_options'), {
   group: 'products',
+  handle: '.handle',
   animation: 250,
   onUpdate: function(e) {
     Array.prototype.slice.call(e.to.children).forEach((el, index) => {
-      const enabled = el.querySelector('input[type="checkbox"]')
+      // const enabled = el.querySelector('input[type="checkbox"]')
       const pos = el.querySelector('[data-name="position"]')
       pos.value = index
     })
@@ -118,7 +125,7 @@ const attachPricesAndTaxEventListeners = (el) => {
     const amount = getRateAmount(e.target)
 
     const masterEl = taxIncl ? taxIncludedEl : taxExcludedEl
-    const otherEl  = taxIncl ? taxExcludedEl : taxIncludedEl
+    const otherEl = taxIncl ? taxExcludedEl : taxIncludedEl
 
     const value = numbro.unformat(masterEl.value)
 
