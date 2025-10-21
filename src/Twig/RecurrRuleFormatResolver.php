e197b99b5d4684e673030575bb0d0a5a04b081ed
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-10-21 14:34:26 +0200

    fix(recurrence-rules): Add a check for missconfigured Recurr string

--- src/Twig/RecurrRuleFormatResolver.php
+++ src/Twig/RecurrRuleFormatResolver.php
@@ -11,6 +11,9 @@ class RecurrRuleFormatResolver implements RuntimeExtensionInterface
 {
     public function format($context, Rule $rule)
     {
+        if (null === $rule->getStartDate()) {
+            return 'Invalid recurrence rule: missing start date.';
+        }
         $locale = $context['app']->getRequest()->getLocale();
         $textTransformer = new TextTransformer(new Translator($locale));
 
