45a7ced4efaebafbca28931f1bca55d8114844ca
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-10-21 15:02:21 +0200

    fix(recurrence-rules): Use current date as start date.
    and fix phpstan issue

--- src/Twig/RecurrRuleFormatResolver.php
+++ src/Twig/RecurrRuleFormatResolver.php
@@ -11,8 +11,11 @@ class RecurrRuleFormatResolver implements RuntimeExtensionInterface
 {
     public function format($context, Rule $rule)
     {
+        // @phpstan-ignore-next-line
         if (null === $rule->getStartDate()) {
-            return 'Invalid recurrence rule: missing start date.';
+            // Set start date to current date if not set
+            // this is required for the text transformer
+            $rule->setStartDate(new \DateTime());
         }
         $locale = $context['app']->getRequest()->getLocale();
         $textTransformer = new TextTransformer(new Translator($locale));
