5892467a5025bf595f1e865a4ad227f833b98a79
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-11-12 14:22:02 -0800

    show order history inside a modal

--- js/app/components/order/id/entrypoint.tsx
+++ js/app/components/order/id/entrypoint.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ClipboardJS from 'clipboard';
 import { RootWithDefaults } from '../../../utils/react';
-import Map from '../../../components/DeliveryMap';
-import Itinerary from '../../../components/DeliveryItinerary';
 import i18n from '../../../i18n';
-import { UserContext } from '../../../UserContext';
+import { Content } from './Content';
 
 new ClipboardJS('#copy');
 
@@ -26,22 +24,7 @@ if (el) {
   const root = createRoot(el);
   root.render(
     <RootWithDefaults>
-      <UserContext.Provider value={{ isDispatcher }}>
-        <div>
-          <Map
-            defaultAddress={delivery.tasks[0].address}
-            tasks={delivery.tasks}
-          />
-          <div className="py-3" />
-          <Itinerary
-            tasks={delivery.tasks}
-            withTaskLinks={isDispatcher}
-            withTimeRange
-            withDescription
-            withPackages
-          />
-        </div>
-      </UserContext.Provider>
+      <Content order={order} delivery={delivery} />
     </RootWithDefaults>,
   );
 }
