fcbc28162193527cd6b7ffe181684a37603895af
Vladimir <70273239+vladimir-8@users.noreply.github.com>
2025-11-20 12:16:00 -0800

    don't take weight and packages from cancelled tasks

--- src/Entity/Delivery.php
+++ src/Entity/Delivery.php
@@ -269,7 +269,7 @@ class Delivery extends TaskCollection implements TaskCollectionInterface, Packag
     public function getWeight()
     {
         $totalWeight = null;
-        foreach ($this->getTasks() as $task) {
+        foreach ($this->getTasks('not task.isCancelled()') as $task) {
             if (null !== $task->getWeight()) {
                 $totalWeight += $task->getWeight();
             }
@@ -527,7 +527,7 @@ class Delivery extends TaskCollection implements TaskCollectionInterface, Packag
 
         $hash = new \SplObjectStorage();
 
-        foreach ($this->getTasks() as $task) {
+        foreach ($this->getTasks('not task.isCancelled()') as $task) {
             if ($task->hasPackages()) {
                 foreach ($task->getPackages() as $package) {
                     $object = $package->getPackage();
