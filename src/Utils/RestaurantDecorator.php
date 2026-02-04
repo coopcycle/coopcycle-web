601ac8e2f6cb7057e27742da5dbf142f381cbb08
Alexandre Segura <mex.zktk@gmail.com>
2026-02-04 18:24:53 +0100

    Sort cuisines.

--- src/Utils/RestaurantDecorator.php
+++ src/Utils/RestaurantDecorator.php
@@ -36,6 +36,9 @@ class RestaurantDecorator
                 $tags[] = $this->translator->trans($cuisine->getName(), [], 'cuisines');
             }
 
+            // Make sure the tags are sorted alphabetically
+            asort($tags);
+
             return $tags;
         });
     }
