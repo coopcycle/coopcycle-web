a1bc87aaef3915e014d0fb45796ea15d96898b2b
Alexandre Segura <mex.zktk@gmail.com>
2026-02-11 13:34:58 +0100

    Cleanup.

--- src/Twig/Components/ShopCollection/Type.php
+++ src/Twig/Components/ShopCollection/Type.php
@@ -37,41 +37,6 @@ class Type extends ShopCollection
         $iterator = new SortableRestaurantIterator($items, $this->timingRegistry);
 
         return iterator_to_array($iterator);
-
-        /*
-
-        $typeRepository = $repository->withTypeFilter($type);
-
-        $itemsIds = $cache->get($cacheKey, function (ItemInterface $item) use ($typeRepository, $timingRegistry) {
-
-            $item->expiresAfter(self::EXPIRES_AFTER);
-
-            $items = $typeRepository->findAllForType();
-
-            $iterator = new SortableRestaurantIterator($items, $timingRegistry);
-            $items = iterator_to_array($iterator);
-
-            return array_map(fn(LocalBusiness $lb) => $lb->getId(), $items);
-        });
-
-        foreach ($itemsIds as $id) {
-            // If one of the items can't be found (probably because it's disabled),
-            // we invalidate the cache
-            if (null === $typeRepository->find($id)) {
-                $cache->delete($cacheKey);
-
-                return $this->getItems($repository, $type, $cache, $cacheKey, $timingRegistry);
-            }
-        }
-
-        $count = count($itemsIds);
-        $items = array_map(
-            fn(int $id): LocalBusiness => $typeRepository->find($id),
-            $itemsIds
-        );
-
-        return [ $items, $count ];
-        */
     }
 
     protected function getCacheKeyParts(): array
