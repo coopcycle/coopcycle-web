04fb4545a19534b8a14564cd5558bf35659842f5
Alexandre Segura <mex.zktk@gmail.com>
2026-02-12 11:32:02 +0100

    Introduce shop collection entities.

---
+++ src/Entity/LocalBusiness/CollectionItem.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Entity\LocalBusiness;
+
+use AppBundle\Entity\LocalBusiness;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+class CollectionItem
+{
+    private $id;
+    private $collection;
+    private $position;
+
+    /**
+     * @var LocalBusiness
+     */
+    #[Groups(['shop_collection'])]
+    private $shop;
+
+    public function getShop()
+    {
+        return $this->shop;
+    }
+
+    public function setCollection(Collection $collection)
+    {
+        $this->collection = $collection;
+    }
+
+    public function setShop(LocalBusiness $shop)
+    {
+        $this->shop = $shop;
+    }
+
+    public function setPosition(int $position)
+    {
+        $this->position = $position;
+    }
+}
+
