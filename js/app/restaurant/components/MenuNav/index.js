0b40ee80d2d3310dca4b1a412f356f9530ccba44
Alexandre Segura <mex.zktk@gmail.com>
2026-03-02 23:43:26 +0100

    Style restaurant page.

--- js/app/restaurant/components/MenuNav/index.js
+++ js/app/restaurant/components/MenuNav/index.js
@@ -78,7 +78,7 @@ export default function MenuNav(props) {
       getCurrentAnchor={ getCurrentAnchor }
       onChange={ onChange }
       targetOffset={ height }>
-      <div className="container-fluid pt-3 d-flex"
+      <div className="mx-auto px-4 pt-3 flex"
            ref={ rootRef }>
         { displaySections.map((section) => (
           <div
@@ -87,7 +87,7 @@ export default function MenuNav(props) {
             className={ classNames(
               {
                 'overflow-hidden': section.isVisible,
-                'd-none': !section.isVisible,
+                'hidden': !section.isVisible,
               },
             ) }>
             <Link href={ sectionToLink(section) } title={ section.name } />
