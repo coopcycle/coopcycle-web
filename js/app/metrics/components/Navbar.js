c02819a07fad9185e8b9034d949a613b91f2589e
Alexandre Segura <mex.zktk@gmail.com>
2026-03-03 21:36:46 +0100

    Add date range presets.

--- js/app/metrics/components/Navbar.js
+++ js/app/metrics/components/Navbar.js
@@ -8,11 +8,23 @@ import localeData from 'dayjs/plugin/localeData'
 dayjs.extend(weekday)
 dayjs.extend(localeData)
 
+import { useTranslation } from 'react-i18next'
+
 import { changeDateRange, changeView } from '../redux/actions'
 
 const { RangePicker } = DatePicker
 
 const Navbar = ({ dateRange, changeDateRange, changeView, zeroWaste }) => {
+  const { t } = useTranslation()
+
+  const presets = [
+    { label: t('METRICS.PRESET_THIS_WEEK'),     value: [ dayjs().startOf('week'),                          dayjs().endOf('week') ] },
+    { label: t('METRICS.PRESET_LAST_WEEK'),     value: [ dayjs().subtract(1, 'week').startOf('week'),      dayjs().subtract(1, 'week').endOf('week') ] },
+    { label: t('METRICS.PRESET_THIS_MONTH'),    value: [ dayjs().startOf('month'),                         dayjs().endOf('month') ] },
+    { label: t('METRICS.PRESET_LAST_MONTH'),    value: [ dayjs().subtract(1, 'month').startOf('month'),    dayjs().subtract(1, 'month').endOf('month') ] },
+    { label: t('METRICS.PRESET_LAST_3_MONTHS'), value: [ dayjs().subtract(3, 'month').startOf('month'),    dayjs() ] },
+    { label: t('METRICS.PRESET_LAST_6_MONTHS'), value: [ dayjs().subtract(6, 'month').startOf('month'),    dayjs() ] },
+  ]
 
   return (
     <div className="d-flex align-items-center justify-content-between border-bottom py-4 mb-4">
@@ -43,6 +55,7 @@ const Navbar = ({ dateRange, changeDateRange, changeView, zeroWaste }) => {
       </div>
       <RangePicker
         allowClear={false}
+        presets={presets}
         value={[ dayjs(dateRange[0]), dayjs(dateRange[1]) ]}
         onChange={(_, dateStrings) => changeDateRange(dateStrings)}
       />
