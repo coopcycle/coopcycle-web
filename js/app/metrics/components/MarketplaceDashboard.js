640d59e5903fe003c3c25a087073425a90def9bd
Alexandre Segura <mex.zktk@gmail.com>
2026-03-03 18:56:04 +0100

    Translate metrics.

--- js/app/metrics/components/MarketplaceDashboard.js
+++ js/app/metrics/components/MarketplaceDashboard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { useTranslation } from 'react-i18next'
 
 import BestRestaurants from './BestRestaurants'
 import AverageCart from './AverageCart'
@@ -10,26 +11,27 @@ import OrderCountPerPaymentMethod from './OrderCountPerPaymentMethod'
 import ChartPanel from './ChartPanel'
 
 const Dashboard = ({ dateRange }) => {
+  const { t } = useTranslation()
 
   return (
     <div>
       <div className="metrics-grid">
-        <ChartPanel title="Best restaurants">
+        <ChartPanel title={t('METRICS.BEST_RESTAURANTS')}>
           <BestRestaurants dateRange={ dateRange } />
         </ChartPanel>
-        <ChartPanel title="Average order total">
+        <ChartPanel title={t('METRICS.AVERAGE_ORDER_TOTAL')}>
           <AverageCart dateRange={ dateRange } />
         </ChartPanel>
-        <ChartPanel title="Number of orders per day of week">
+        <ChartPanel title={t('METRICS.ORDERS_PER_DAY_OF_WEEK')}>
           <OrderCountPerDayOfWeek dateRange={ dateRange } />
         </ChartPanel>
-        <ChartPanel title="Number of orders per hour range">
+        <ChartPanel title={t('METRICS.ORDERS_PER_HOUR_RANGE')}>
           <OrderCountPerHourRange dateRange={ dateRange } />
         </ChartPanel>
-        <ChartPanel title="Number of orders per zone">
+        <ChartPanel title={t('METRICS.ORDERS_PER_ZONE')}>
           <OrderCountPerZone dateRange={ dateRange } />
         </ChartPanel>
-        <ChartPanel title="Number of orders per payment method">
+        <ChartPanel title={t('METRICS.ORDERS_PER_PAYMENT_METHOD')}>
           <OrderCountPerPaymentMethod dateRange={ dateRange } />
         </ChartPanel>
       </div>
