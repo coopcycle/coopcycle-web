5d36066aa3f5cb2c9277e41487538e58ec72ffd5
Alexandre Segura <mex.zktk@gmail.com>
2026-03-10 13:25:39 +0100

    Move "global" address autosuggest to dedicated widget.

--- js/app/common.js
+++ js/app/common.js
@@ -8,7 +8,7 @@ require('bootstrap-sass')
 
 import './i18n'
 import { setTimezone, getCurrencySymbol } from './i18n'
-import AddressAutosuggest from './widgets/AddressAutosuggest'
+import AddressAutosuggestFormGroup from './widgets/AddressAutosuggestFormGroup'
 
 global.ClipboardJS = require('clipboard')
 
@@ -57,63 +57,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const inputs = document.querySelectorAll('[data-widget="address-input"]')
   if (inputs.length > 0) {
-
-    const addressElements = {
-      latitude: '$1ddress_latitude',
-      longitude: '$1ddress_longitude',
-      postalCode: '$1ddress_postalCode',
-      addressLocality: '$1ddress_addressLocality',
-    }
-
     inputs.forEach(el => {
-
-      // Try to build an address object
-      let address = {
-        streetAddress: el.value
-      }
-      for (const addressProp in addressElements) {
-        const addressEl = document.getElementById(
-          el.getAttribute('id').replace(/([aA])ddress_streetAddress/, addressElements[addressProp])
-        )
-        if (addressEl) {
-          address = {
-            ...address,
-            [addressProp]: addressEl.value
-          }
-        }
-      }
-
-      address = {
-        ...address,
-        geo: {
-          latitude: address.latitude,
-          longitude: address.longitude,
-        }
-      }
-
-      new AddressAutosuggest(
-        el.closest('.form-group'),
-        {
-          required: el.required,
-          address,
-          inputProps: {
-            id: el.getAttribute('id'),
-            name: el.getAttribute('name'),
-          },
-          onAddressSelected: (text, address) => {
-            for (const addressProp in addressElements) {
-              const addressEl = document.getElementById(
-                el.getAttribute('id').replace(/([aA])ddress_streetAddress/, addressElements[addressProp])
-              )
-              if (addressEl) {
-                addressEl.value = address[addressProp]
-              }
-            }
-          },
-          disabled: el.disabled,
-          placeholder: el.placeholder,
-        }
-      )
+      new AddressAutosuggestFormGroup(el);
     })
   }
 
