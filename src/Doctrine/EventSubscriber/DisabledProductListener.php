08c97a5030bfa95aa93963b254ded6fa36663c81
Antoine Bagnaud <contact@antoinebagnaud.me>
2026-03-18 15:15:26 +0100

    fix: add cache invalidation

--- src/Doctrine/EventSubscriber/DisabledProductListener.php
+++ src/Doctrine/EventSubscriber/DisabledProductListener.php
@@ -6,31 +6,46 @@ use AppBundle\Entity\Sylius\Product;
 use AppBundle\Entity\Sylius\ProductOptionValue;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\ORM\Events;
-use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
 
-#[AsDoctrineListener(event: Events::postUpdate, connection: 'default')]
+#[AsDoctrineListener(event: Events::onFlush, connection: 'default')]
 class DisabledProductListener
 {
-    public function postUpdate(LifecycleEventArgs $args)
+    public function __construct(private EventDispatcherInterface $eventDispatcher)
+    {}
+
+    public function onFlush(OnFlushEventArgs $args): void
     {
-        $entity = $args->getObject();
+        /** @var \Doctrine\ORM\EntityManagerInterface */
+        $objectManager = $args->getObjectManager();
+        $unitOfWork = $objectManager->getUnitOfWork();
 
-        if ($entity instanceof Product) {
+        $scheduledUpdates = $unitOfWork->getScheduledEntityUpdates();
 
-            /** @var \Doctrine\ORM\EntityManagerInterface */
-            $objectManager = $args->getObjectManager();
-            $unitOfWork = $objectManager->getUnitOfWork();
+        foreach ($scheduledUpdates as $entity) {
+            if (!$entity instanceof Product) {
+                continue;
+            }
+
+            $unitOfWork->computeChangeSet($objectManager->getClassMetadata(Product::class), $entity);
 
             $changeset = $unitOfWork->getEntityChangeSet($entity);
 
             if (isset($changeset['enabled'])) {
-                [$oldValue, $newValue] = $changeset['enabled'];
+                [, $newValue] = $changeset['enabled'];
                 $optionValues = $objectManager->getRepository(ProductOptionValue::class)->findBy(['product' => $entity]);
                 foreach ($optionValues as $optionValue) {
                     $optionValue->setEnabled($newValue);
+                    $unitOfWork->scheduleForUpdate($optionValue);
                 }
-                $objectManager->flush();
+
+                $restaurant = $entity->getRestaurant();
+                if (null !== $restaurant) {
+                    $this->eventDispatcher->dispatch(new GenericEvent($restaurant), 'catalog.updated');
+                }
             }
         }
     }
- }
+}
