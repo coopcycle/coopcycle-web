246cfedd435cd73198e4a95fb4284f86fe9d2d36
Alexandre Segura <mex.zktk@gmail.com>
2026-03-19 11:02:03 +0100

    Style plus code suggestions.

--- js/app/components/AddressAutosuggest.js
+++ js/app/components/AddressAutosuggest.js
@@ -329,8 +329,14 @@ const getSuggestionValue = suggestion => suggestion.value
 const renderSuggestion = suggestion => {
   if (suggestion.type === 'plus_code') {
     return (
-      <div>
-        <i className="fa fa-map-marker mr-2" aria-hidden="true"></i>
+      <div className="tw:flex tw:items-center tw:gap-1">
+        {/* https://lucide.dev/icons/map-pin-plus */}
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-map-pin-plus-icon lucide-map-pin-plus tw:w-4">
+          <path d="M19.914 11.105A7.298 7.298 0 0 0 20 10a8 8 0 0 0-16 0c0 4.993 5.539 10.193 7.399 11.799a1 1 0 0 0 1.202 0 32 32 0 0 0 .824-.738" />
+          <circle cx="12" cy="10" r="3" />
+          <path d="M16 18h6" />
+          <path d="M19 15v6" />
+        </svg>
         {suggestion.value}
       </div>
     )
