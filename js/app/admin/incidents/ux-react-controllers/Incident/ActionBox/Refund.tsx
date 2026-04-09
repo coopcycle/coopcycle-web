78f6a50fadb1e50cb42dd887eb2a19deb22b7457
Alexandre Segura <mex.zktk@gmail.com>
2026-04-09 14:38:06 +0200

    Fix floating point conversion.

--- js/app/admin/incidents/ux-react-controllers/Incident/ActionBox/Refund.tsx
+++ js/app/admin/incidents/ux-react-controllers/Incident/ActionBox/Refund.tsx
@@ -61,7 +61,7 @@ const PaymentForm = ({ payment, liablePartyForm }) => {
         <InputNumber
           min={0}
           max={payment.amount - payment.refundedAmount}
-          parser={(value) => value * 100}
+          parser={(value) => parseInt(value * 100, 10)}
           formatter={(value) => value / 100}
           step={50}
           disabled={!payment.supportsPartialRefunds} />
