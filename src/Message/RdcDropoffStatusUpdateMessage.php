a401ce5e4a029614d806f7d7ec65cc40070b3ed6
Antoine Bagnaud <contact@antoinebagnaud.me>
2026-05-29 05:38:18 +0200

    Remove #[AsMessage].

--- src/Message/RdcDropoffStatusUpdateMessage.php
+++ src/Message/RdcDropoffStatusUpdateMessage.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace AppBundle\Message;
 
-use Symfony\Component\Messenger\Attribute\AsMessage;
-
-#[AsMessage]
 final class RdcDropoffStatusUpdateMessage
 {
     public function __construct(
