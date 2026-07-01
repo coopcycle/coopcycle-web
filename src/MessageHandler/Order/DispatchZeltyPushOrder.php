4ad9e434b2aa3a0f7cad4bfafe86b6505bdaa531
Alexandre Segura <mex.zktk@gmail.com>
2026-07-01 11:09:48 +0200

    Fix bus usage.

--- src/MessageHandler/Order/DispatchZeltyPushOrder.php
+++ src/MessageHandler/Order/DispatchZeltyPushOrder.php
@@ -4,14 +4,17 @@ namespace AppBundle\MessageHandler\Order;
 
 use AppBundle\Domain\Order\Event\OrderCreated;
 use AppBundle\Message\Zelty\PushOrder;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsMessageHandler]
 class DispatchZeltyPushOrder
 {
-    public function __construct(private MessageBusInterface $messageBus)
-    {}
+    public function __construct(
+        private MessageBusInterface $commandBus,
+        private LoggerInterface $logger,
+    ) {}
 
     public function __invoke(OrderCreated $event): void
     {
@@ -22,6 +25,13 @@ class DispatchZeltyPushOrder
             return;
         }
 
-        $this->messageBus->dispatch(new PushOrder($order->getId()));
+        try {
+            $this->commandBus->dispatch(new PushOrder($order->getId()));
+        } catch (\Throwable $e) {
+            $this->logger->error('Failed to dispatch Zelty push order', [
+                'order_id' => $order->getId(),
+                'exception' => $e->getMessage(),
+            ]);
+        }
     }
 }
