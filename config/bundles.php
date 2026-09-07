6f5cf359a02e3a634cd9fd883ee8519880684709
Alexandre Segura <mex.zktk@gmail.com>
2026-09-07 10:40:02 +0200

    Fix Symfony cache clear.

--- config/bundles.php
+++ config/bundles.php
@@ -31,7 +31,6 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['all' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['all' => true],
     HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
-    Invenso\Rector\InvensoRectorBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Kreait\Firebase\Symfony\Bundle\FirebaseBundle::class => ['all' => true],
